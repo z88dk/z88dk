baae07040bf444f4a7b00ced6155c2bb1d0fd5c4
suborb <dom@suborbital.org.uk>
2001-01-02 14:06:54 +0000

    *** empty log message ***

---
+++ libsrc/tideps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#  short script to sort out ticalc dependencies
+
+	touch stdio/ticalc/fgetc_cons.asm
+	touch stdio/ticalc/getk.asm
+	touch stdio/ticalc/getk_decode.asm
+	touch stdio/ansi/ticalc/f_ansi_attr.asm
+	touch stdio/ansi/ticalc/f_ansi_bel.asm
+	touch stdio/ansi/ticalc/f_ansi_char.asm
+	touch stdio/ansi/ticalc/f_ansi_cls.asm
+	touch stdio/ansi/ticalc/f_ansi_dline.asm
+	touch stdio/ansi/ticalc/f_ansi_scrollup.asm
+	touch graphics/ticalc/pixladdr.asm
+	touch graphics/ticalc/swapgfxbk.asm
+
