158f78c6fd7a8e089f0b6aaec1faa6dad5526ac3
suborb <dom@suborbital.org.uk>
2001-02-06 17:16:34 +0000

    Added in includes for TI83 and TI83+

---
+++ lib/main.h
@@ -0,0 +1,21 @@
+lstoff
+
+if mirage
+define plus
+endif
+
+DEFC			NULL = $00
+
+IF Plus
+Include		"../lib/ti83p.h"
+ELSE
+include		"../lib/ti83.h"
+ENDIF
+
+If Plus
+ORG	Entry
+DEFB	$BB, $6D
+Else
+ORG	Entry
+EndIf
+
