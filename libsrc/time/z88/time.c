750035e499cc33da5b4b5745c9396fc84bf7bbf7
suborb <dom@suborbital.org.uk>
2001-03-08 10:01:55 +0000

    Improved z88 time() handling

--- libsrc/time/z88/time.c
+++ libsrc/time/z88/time.c
@@ -10,17 +10,35 @@
 
 #include <time.h>
 
+#define JD0101970	2440588
+
+long gtoy();
+
 time_t time(time_t *store)
 {
+	long    days;
 	time_t	tim;
 
-	tim=doepoch(gtoy(),clock());
+	days = (gtoy() - JD0101970)*86400;
+
+	tim=days+(clock()/CLOCKS_PER_SEC);
 
 	if (store) *store=tim;
 
 	return (tim);
 }
 
-
-
+/* Get Julian day */
+long gtoy()
+{
+#asm
+	INCLUDE	"#time.def"
+	ld	de,2
+	call_oz(gn_gmd)	;abc, a=MSB
+	ld	d,0
+	ld	e,a
+	ld	h,b
+	ld	l,c
+#endasm
+}
 
