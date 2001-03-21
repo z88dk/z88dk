99471a9a6eaac8ea1bad957194866aa6ad2f892d
suborb <dom@suborbital.org.uk>
2001-03-21 17:51:04 +0000

    Added targets for tigray libs

---
+++ libsrc/gfxdeps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#  short script to sort out gfx dependencies
+
+	rm -f graphics/*.o
+	rm -f graphics/ticalc/*.o
+	rm -f graphics/ace/*.o
+	rm -f graphics/gray/*.o
+	rm -f graphics/spectrum/*.o
+	rm -f graphics/vz200/*.o
+	rm -f graphics/zx81/*.o
+	rm -f graphics/z88/*.o
+
