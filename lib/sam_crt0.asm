10c0f7b825d3578321fd9944d16acb7ee8a3856b
suborb <dom@suborbital.org.uk>
2001-04-11 11:45:34 +0000

    Stefano's SAM port

---
+++ lib/sam_crt0.asm
@@ -0,0 +1,241 @@
+;       Startup fo SAM Coupe
+;
+;       Stefano 26/3/2001
+;
+;       If an error occurs eg break we just drop back to BASIC
+
+
+
+                MODULE  z88_crt0
+
+;
+; Initially include the zcc_opt.def file to find out lots of lovely
+; information about what we should do..
+;
+
+                INCLUDE "zcc_opt.def"
+
+; No matter what set up we have, main is always, always external to
+; this fileb
+
+                XREF    _main
+
+;
+; Some variables which are needed for both app and basic startup
+;
+
+        XDEF    cleanup
+        XDEF    l_dcal
+
+; Integer rnd seed
+
+        XDEF    int_seed
+
+; vprintf is internal to this file so we only ever include one of the set
+; of routines
+
+	XDEF	_vfprintf
+
+;Exit variables
+
+        XDEF    exitsp
+        XDEF    exitcount
+
+;For stdin, stdout, stder
+
+        XDEF    __sgoioblk
+
+; Graphics stuff
+	XDEF	base_graphics
+	XDEF	coords
+
+; Now, getting to the real stuff now!
+
+
+
+
+        org     32768
+
+
+.start
+        ld      hl,0
+        add     hl,sp
+        ld      (start1+1),hl
+        ld      hl,-64
+        add     hl,sp
+        ld      sp,hl
+        ld      (exitsp),sp
+
+IF !DEFINED_nostreams
+IF DEFINED_ANSIstdio
+; Set up the std* stuff so we can be called again
+	ld	hl,__sgoioblk+2
+	ld	(hl),19	;stdin
+	ld	hl,__sgoioblk+6
+	ld	(hl),21	;stdout
+	ld	hl,__sgoioblk+10
+	ld	(hl),21	;stderr
+ENDIF
+ENDIF
+        call    _main
+.cleanup
+;
+;       Deallocate memory which has been allocated here!
+;
+	push	hl
+IF !DEFINED_nostreams
+IF DEFINED_ANSIstdio
+	LIB	closeall
+	call	closeall
+ENDIF
+ENDIF
+
+	pop	bc
+.start1
+        ld      sp,0
+        ret
+
+.l_dcal
+        jp      (hl)
+
+; Now, define some values for stdin, stdout, stderr
+
+.__sgoioblk
+IF DEFINED_ANSIstdio
+	INCLUDE	"#stdio_fp.asm"
+ELSE
+        defw    -11,-12,-10
+ENDIF
+
+
+; Now, which of the vfprintf routines do we need?
+
+
+._vfprintf
+IF DEFINED_floatstdio
+	LIB	vfprintf_fp
+	jp	vfprintf_fp
+ELSE
+	IF DEFINED_complexstdio
+		LIB	vfprintf_comp
+		jp	vfprintf_comp
+	ELSE
+		IF DEFINED_ministdio
+			LIB	vfprintf_mini
+			jp	vfprintf_mini
+		ENDIF
+	ENDIF
+ENDIF
+
+; Some +3 stuff - this needs to be below 49152
+IF DEFINED_NEEDplus3dodos
+; 	Routine to call +3DOS Routines. Located in startup
+;	code to ensure we don't get paged out
+;	(These routines have to be below 49152)
+;	djm 17/3/2000 (after the manual!)
+	XDEF	dodos
+.dodos
+	call	dodos2		;dummy routine to restore iy afterwards
+	ld	iy,23610
+	ret
+.dodos2
+	push	af
+	push	bc
+	ld	a,7
+	ld	bc,32765
+	di
+	ld	(23388),a
+	out	(c),a
+	ei
+	pop	bc
+	pop	af
+	call	cjumpiy
+	push	af
+	push	bc
+	ld	a,16
+	ld	bc,32765
+	di
+	ld	(23388),a
+	out	(c),a
+	ei
+	pop	bc
+	pop	af
+	ret
+.cjumpiy
+	jp	(iy)
+ENDIF
+
+IF 0
+;	Short routine to set up a +3 DOS header so files
+;	Can be accessed from BASIC, we set to type code
+;	load address 0 and length supplied
+;
+;	Entry:	b = file handle
+;	       hl = file length
+
+.setheader
+	ld	iy,setheader_r
+	call	dodos
+	ret
+.setheader_r
+	push	hl
+	call	271	;DOS_RED_HEAD
+	pop	hl
+	ld	(ix+0),3	;CODE
+	ld	(ix+1),l	;Length
+	ld	(ix+2),h
+	ld	(ix+3),0	;Load address
+	ld	(ix+4),0
+	ret
+ENDIF
+
+
+;Seed for integer rand() routines
+
+.int_seed       defw    0
+
+;Atexit routine
+
+.exitsp
+                defw    0
+.exitcount
+                defb    0
+
+; Heap stuff
+
+.heaplast	defw	0
+.heapblocks	defw	0
+
+; mem stuff
+
+.base_graphics
+		defw	16384
+.coords		defw	0
+
+         defm  "Small C+ SAM Coupe"&0
+
+;All the float stuff is kept in a different file...for ease of altering!
+;It will eventually be integrated into the library
+;
+;Here we have a minor (minor!) problem, we've no idea if we need the
+;float package if this is separated from main (we had this problem before
+;but it wasn't critical..so, now we will have to read in a file from
+;the directory (this will be produced by zcc) which tells us if we need
+;the floatpackage, and if so what it is..kludgey, but it might just work!
+;
+;Brainwave time! The zcc_opt file could actually be written by the
+;compiler as it goes through the modules, appending as necessary - this
+;way we only include the package if we *really* need it!
+
+IF NEED_floatpack
+        INCLUDE         "#float.asm"
+
+;seed for random number generator - not used yet..
+.fp_seed        defb    $80,$80,0,0,0,0
+;Floating point registers...
+.extra          defs    6
+.fa             defs    6
+.fasign         defb    0
+
+ENDIF
+
