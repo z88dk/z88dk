e8a79595ec261182600ca1d74d95d790e5310b2a
suborb <dom@suborbital.org.uk>
2001-06-29 14:36:30 +0000

    Updated Rex example form Damjan Marion

--- examples/rex/hello.c
+++ examples/rex/hello.c
@@ -1,12 +1,34 @@
+/*
+ *
+ * Hello World for REX 6000
+ *
+ * Written by Damjan Marion <dmarion@iskon.hr>
+ *
+ */
 
-
-#include <DataSlim.h>
-
+#include <rex/rex.h>
 
 int main()
 {
-	DsPrintf(0,0,0,"Hello World");
+	REXMSG msg;
+	int flag=1;
 
-	/* Another call to SYSCALLEX */
-	DsSleep(9);
+	RexEventClear();
+	RexClearScreen();
+	RexPrintf(90,20,16,"Hello World");
+	RexDialogTextButton(90, 80, 60, 20, 61, "Exit");
+
+	/*Wait for event*/
+
+	while (flag) {
+	  RexEventMessageGet(&msg);
+	  switch(msg.message) {
+	    case MSG_REX_CLOSE: 
+	    case MSG_REX_COMMAND: 
+	    case MSG_REX_KEY_DOWN:
+		flag=0;
+	        RexBeepOn(2);
+		break;
+	  }
+	}
 }
