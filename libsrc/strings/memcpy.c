922cd78410bf53a65ddb0bdc65735e56c4621bce
suborb <dom@suborbital.org.uk>
2001-09-06 08:34:57 +0000

    better way of getting return value

--- libsrc/strings/memcpy.c
+++ libsrc/strings/memcpy.c
@@ -3,7 +3,7 @@
  *
  *      djm 22/11/99
  *
- *	$Id: memcpy.c,v 1.3 2001-09-06 08:33:56 dom Exp $
+ *	$Id: memcpy.c,v 1.4 2001-09-06 08:34:57 dom Exp $
  */
 
 
@@ -31,8 +31,10 @@ void *memcpy(void *s1, void *s2, int size)
         ld      h,(hl)  
         ld      l,a     ;hl=dest
 #if 1
+	push	hl
 	ex	de,hl
 	ldir
+	pop	hl
 #else
 .memcpy1
         ld      a,(de)
@@ -46,13 +48,13 @@ void *memcpy(void *s1, void *s2, int size)
         or      c
         jp      nz,memcpy1
 .memcpy3
-#endif
         ld      hl,6
         add     hl,sp
         ld      e,(hl)
         inc     hl
         ld      d,(hl)
 	ex	de,hl
+#endif
 #endasm
 }
 
