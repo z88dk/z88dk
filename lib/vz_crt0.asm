8c617a1f8a934da07839cecf5d64924f81de7fe3
suborb <dom@suborbital.org.uk>
2001-09-07 18:17:56 +0000

    Corrected module name

--- lib/vz_crt0.asm
+++ lib/vz_crt0.asm
@@ -4,12 +4,12 @@
 ;
 ;       If an error occurs eg break we just drop back to BASIC
 ;
-;       $Id: vz_crt0.asm,v 1.3 2001-04-12 13:26:13 stefano Exp $
+;       $Id: vz_crt0.asm,v 1.4 2001-09-07 18:17:56 dom Exp $
 ;
 
 
 
-                MODULE  z88_crt0
+                MODULE  vz_crt0
 
 ;
 ; Initially include the zcc_opt.def file to find out lots of lovely
