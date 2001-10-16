12dc06857161b163dc9c3525d20ff417b4ef1d6f
suborb <dom@suborbital.org.uk>
2001-10-16 19:46:07 +0000

    added tape routines

--- include/spectrum.h
+++ include/spectrum.h
@@ -1,11 +1,13 @@
 /*
  * Headerfile for Spectrum specific stuff
  *
- * $Id: spectrum.h,v 1.3 2001-05-01 13:58:43 dom Exp $
+ * $Id: spectrum.h,v 1.4 2001-10-16 19:46:26 dom Exp $
  */
 
-#ifndef _SPECTRUM_H_
-#define _SPECTRUM_H_
+#ifndef __SPECTRUM_H__
+#define __SPECTRUM_H__
+
+#include <sys/types.h>
 
 #ifdef PLUS3
 /* If it's a +3 we want the dodos routine */
@@ -13,6 +15,21 @@
 extern int __LIB__ findhand();
 extern void __LIB__ freehand(int);
 #endif
+
+/* Tape handling routines *
+
+/* Standard tape header */
+struct zxtapehdr {
+        unsigned char type;
+        char    name[10];
+        size_t length;
+        size_t address;
+        size_t offset;
+};
+
+extern int __LIB__ tape_save(char *name, size_t loadstart,void *start, size_t len);
+extern int __LIB__ tape_save_block(void *addr, size_t len, unsigned char type);
+extern int __LIB__ tape_load_block(void *addr, size_t len, unsigned char type);
 
 
 #endif
