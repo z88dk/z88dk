a3f4dd64c0e6207c79143b6a0826c9af2ba88b69
suborb <dom@suborbital.org.uk>
2001-10-16 20:40:08 +0000

    modified to use Ds names for calls

--- examples/rex/hello.c
+++ examples/rex/hello.c
@@ -1,6 +1,6 @@
 /*
  *
- * Hello World for REX 6000
+ * Hello World for DS 6000
  *
  * Written by Damjan Marion <dmarion@iskon.hr>
  *
@@ -10,24 +10,24 @@
 
 int main()
 {
-	REXMSG msg;
+	MSG msg;
 	int flag=1;
 
-	RexEventClear();
-	RexClearScreen();
-	RexPrintf(90,20,16,"Hello World");
-	RexDialogTextButton(90, 80, 60, 20, 61, "Exit");
+	DsEventClear();
+	DsClearScreen();
+	DsPrintf(90,20,16,"Hello World");
+	DsDialogTextButton(90, 80, 60, 20, 61, "Exit");
 
 	/*Wait for event*/
 
 	while (flag) {
-	  RexEventMessageGet(&msg);
+	  DsEventMessageGet(&msg);
 	  switch(msg.message) {
-	    case MSG_REX_CLOSE: 
-	    case MSG_REX_COMMAND: 
-	    case MSG_REX_KEY_DOWN:
+	    case MSG_DS_CLOSE: 
+	    case MSG_DS_COMMAND: 
+	    case MSG_DS_KEY_DOWN:
 		flag=0;
-	        RexBeepOn(2);
+	        DsBeepOn(2);
 		break;
 	  }
 	}
