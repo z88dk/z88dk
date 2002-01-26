3df36bc8f3018bd43596c33e078cc260283ec95f
suborb <dom@suborbital.org.uk>
2002-01-26 23:45:36 +0000

    now returns a long (may need a bit of work re overflow)

--- libsrc/genmath/ifix.asm
+++ libsrc/genmath/ifix.asm
@@ -4,6 +4,7 @@
 
                 XLIB    ifix
                 LIB     floor
+		LIB	l_long_neg
 
                 XREF    fa
 
@@ -15,37 +16,38 @@
 
 .IFIX   CALL    FLOOR          ;take floor first
         LD      HL,0            ;initialize the result
+	ld	d,l
+	ld	e,l
         LD      A,(FA+5)        ;get the exponent
         LD      B,A             ;  and save it
         OR      A
         RET     Z               ;z => number was zero
-        LD      HL,(FA+3)       ;get most significant bits
-        LD      C,H             ;save sign bit (msb)
+        LD      de,(FA+3)       ;get most significant bits
+	ld	hl,(fa+1)
+        LD      C,d             ;save sign bit (msb)
         LD      A,B             ;get exponent again
-        CP      $80+16
+        CP      $80+32
         JP      M,IFIX5         ;m => fabs(fa) < 32768
-        ret     nz
 
-;        JP      NZ,OFLOW        ;nz => fabs(fa) > 32768
-;                               (overflow)
-        LD      A,H
+	ret			;
+IF 0
+        ret     nz		;would be overflow
+        LD      A,d
         CP      $80
-        ret     nz
-;        JP      NZ,OFLOW        ;nz => fa isn't -32768
+        ret     nz		;overflow..
         LD      A,L
         OR      A
-;        JP      NZ,OFLOW        ;nz => overflow
-        RET                     ;return -32768.
+        RET                     ;return -32768. (overflow)
+ENDIF
 ;
-.IFIX5  SET     7,H             ;restore hidden bit
-.IFIX6  SRL     H               ;shift right (0 fill)
+.IFIX5  SET     7,d             ;restore hidden bit
+.IFIX6  SRL     d               ;shift right (0 fill)
+	rr	e
+	rr	h
         RR      L               ;shift right (cy fill)
         INC     A
-        CP      16+$80
+        CP      32+$80
         JR      NZ,IFIX6        ;nz => haven't shifted enough
         RL      C
         RET     NC              ;nc => positive number
-        EX      DE,HL
-        LD      HL,1            ;compensate for cy bit
-        SBC     HL,DE           ;negate result
-        RET
+	jp	l_long_neg	;negate
