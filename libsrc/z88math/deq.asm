64218c494cc18aa86e59c538599d116449678179
suborb <dom@suborbital.org.uk>
2002-12-12 10:11:55 +0000

    missing floating point stuff added (Stefano spot)

--- libsrc/z88math/deq.asm
+++ libsrc/z88math/deq.asm
@@ -2,18 +2,18 @@
 ;       Z88dk Z88 Maths Library
 ;
 ;
-;       $Id: dge.asm,v 1.2 2002-12-12 10:11:55 dom Exp $
+;       $Id: deq.asm,v 1.1 2002-12-12 10:11:55 dom Exp $
 
-		XLIB	dge
+		XLIB	deq
 
 		LIB	fsetup
 		LIB	stkequcmp
 
 		INCLUDE	"#fpp.def"
 
-; TOS >= FA?
-.dge
+; TOS != FA?
+.deq
 	call	fsetup
-	fpp(FP_GEQ)
+	fpp(FP_EQ)
 	jp	stkequcmp
 
