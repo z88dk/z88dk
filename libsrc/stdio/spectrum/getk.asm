76f860a15fde6bfff8549701219f54eb8c8371bb
suborb <dom@suborbital.org.uk>
2003-01-24 23:20:17 +0000

    prevent a possible race(!) condition resulting in keypresses not being registered

--- libsrc/stdio/spectrum/getk.asm
+++ libsrc/stdio/spectrum/getk.asm
@@ -6,16 +6,18 @@
 ;	17/5/99 djm
 ;
 ;
-;	$Id: getk.asm,v 1.2 2001-04-13 14:14:00 stefano Exp $
+;	$Id: getk.asm,v 1.3 2003-01-24 23:20:17 dom Exp $
 ;
 
 
 		XLIB	getk
 
 .getk
+	ld	h,0
 	ld	a,(23560)
 	ld	l,a
+	and	a
+	ret	z
 	xor	a
 	ld	(23560),a
-	ld	h,a
 	ret
