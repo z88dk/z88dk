7ef6b37604b68e035b71adb7f5d92208403c074b
zx70 <stefano_bodrato@hotmail.com>
2003-05-21 13:56:01 +0000

    Sharp OZ700 - forgot to commit a file !  -  FIXED CLS routine for ANSI

--- libsrc/stdio/ansi/oz/f_ansi_cls.asm
+++ libsrc/stdio/ansi/oz/f_ansi_cls.asm
@@ -7,7 +7,7 @@
 ;	Stefano Bodrato - Aug. 2002
 ;
 ;
-;	$Id: f_ansi_cls.asm,v 1.2 2003-05-21 12:53:23 stefano Exp $
+;	$Id: f_ansi_cls.asm,v 1.3 2003-05-21 13:56:01 stefano Exp $
 ;
 
 	XLIB	ansi_cls
@@ -21,11 +21,13 @@
 
 	call	swapgfxbk
 
-        ld      hl,(base_graphics)
-        ld	d,h
-        ld	e,l
+	ld      hl,(base_graphics)
+	ld	d,h
+	ld	e,l
 	inc	de
-        ld      bc,2400-1
-        ldir
+	ld      bc,2400-1
+	xor	a
+	ld	(hl),a
+	ldir
 
 	jp	swapgfxbk1
