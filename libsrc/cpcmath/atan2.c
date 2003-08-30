9d1b2085a1649059cfe6f1eb80858de42cdb8ece
suborb <dom@suborbital.org.uk>
2003-08-30 18:35:24 +0000

    fixes for halfpi and pi stuff

--- libsrc/cpcmath/atan2.c
+++ libsrc/cpcmath/atan2.c
@@ -2,8 +2,7 @@
 #include <math.h>
 #include "float.h"
 
-extern double _pi;
-extern double _halfpi;
+extern double __LIB__ halfpi();
 
 /*	arc tangent of y/x  */
 double atan2(y,x)
@@ -14,14 +13,14 @@ double x, y ;
 	if (fabs(x) >= fabs(y)) {
 		a = atan(y/x) ;
 		if (x < 0.0) {
-			if (y >= 0.0) a += _pi ;
-			else a -= _pi ;
+			if (y >= 0.0) a += pi() ;
+			else a -= pi() ;
 		}
 	}
 	else {
 		a = -atan(x/y) ;
-		if (y < 0.0) a -= _halfpi ;
-		else     a += _halfpi ;
+		if (y < 0.0) a -= halfpi() ;
+		else     a += halfpi() ;
 	}
 	return a ;
 }
