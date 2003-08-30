8c13b13a53c914ea72aa277df7cecb8013bcd82a
suborb <dom@suborbital.org.uk>
2003-08-30 16:41:13 +0000

    add in some extra CPC stuff

--- include/math.h
+++ include/math.h
@@ -1,7 +1,7 @@
 #ifndef __MATH_H__
 #define __MATH_H__
 
-/* $Id: math.h,v 1.6 2003-08-22 21:34:51 dom Exp $ */
+/* $Id: math.h,v 1.7 2003-08-30 16:41:13 dom Exp $ */
 
 
 extern double __LIB__ acos(double);  /* arc cosine */
@@ -15,7 +15,6 @@ extern double __LIB__ exp(double);   /* exponential */
 extern double __LIB__ log(double);   /* natural logarithm */
 extern double __LIB__ log10(double); /* log base 10 */
 extern double __LIB__ pow(double,double);   /* pow(x,y) = x**y */
-extern double __LIB__ pow10(int x);         /* pow(1,x) - CPC only */
 extern double __LIB__ sin(double);   /* sine */
 extern double __LIB__ sinh(double);  /* hyperbolic sine */
 extern double __LIB__ sqrt(double);  /* square root */
@@ -27,5 +26,12 @@ extern double __LIB__ tanh(double);  /* hyperbolic tangent */
 extern double __LIB__ atof(char *);
 #endif
 
+/* Some additional CPC only routines now */
+#ifdef __CPC__
+extern double __LIB__ pow10(int x);         /* pow(1,x) - CPC only */
+extern void __LIB__ deg();
+extern void __LIB__ rad();
+extern double __LIB__ pi();
+#endif
 
 #endif /* _MATH_H */
