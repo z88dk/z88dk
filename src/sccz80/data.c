153c8c7a9f3acee71bdfee6380a0f6b23767cfbe
denniz <denniz>
2003-09-15 22:54:57 +0000

    Libsrc makefiles with better cleaning, cpcfs.lib target, sccz80 version string.

--- src/sccz80/data.c
+++ src/sccz80/data.c
@@ -6,17 +6,17 @@
  *      I'm starting to split these up once more and stick them in
  *      the relevant files..
  *
- *      $Id: data.c,v 1.26 2003-03-17 15:59:36 dom Exp $
+ *      $Id: data.c,v 1.27 2003-09-15 22:54:57 denniz Exp $
  */
 
 #include "ccdefs.h"
 
 /*      Now reserve some storage words          */
 
-char amivers[] = "$VER: sccz80 v17.03.2003.01 (17.3.2003";
+char amivers[] = "$VER: sccz80 v30.08.2003.01 (30.8.2003";
 char titlec[] = "Small-C/Plus - z80 Crosscompiler ";
 char Banner[]  = "* * * * *  Small-C/Plus z88dk * * * * *" ;
-char Version[] = "  Version: v17-03-2003.01";
+char Version[] = "  Version: v30-08-2003.01";
 
 
 
