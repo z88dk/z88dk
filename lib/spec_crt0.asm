1d89e71692971de47844fa60048f48f3ea8529e8
suborb <dom@suborbital.org.uk>
2003-10-10 11:03:41 +0000

    +3 and idedos/residos definitions
    some residos support code in the crt0 file

--- lib/spec_crt0.asm
+++ lib/spec_crt0.asm
@@ -2,7 +2,7 @@
 ;
 ;       djm 18/5/99
 ;
-;       $Id: spec_crt0.asm,v 1.9 2003-03-23 10:28:19 dom Exp $
+;       $Id: spec_crt0.asm,v 1.10 2003-10-10 11:03:42 dom Exp $
 ;
 
 
@@ -89,6 +89,10 @@ IF DEFINED_ANSIstdio
 	ld	(hl),21	;stderr
 ENDIF
 ENDIF
+IF DEFINED_NEEDresidos
+	call	residos_detect
+	jp	c,cleanup_exit
+ENDIF
         call    _main		;Call user program
 .cleanup
 ;
@@ -106,6 +110,7 @@ IF DEFINED_ZXVGS
         RST     8
         DEFB    $FD             ;Program finished
 ELSE
+.cleanup_exit
         ld      hl,10072        ;Restore hl' to what basic wants
         exx
         pop     bc
@@ -147,6 +152,82 @@ ELSE
 		ENDIF
 	ENDIF
 ENDIF
+
+;---------------------------------------------
+; Some +3 stuff - this needs to be below 49152
+;---------------------------------------------
+IF DEFINED_NEEDresidos
+	INCLUDE	"#idedos.def"
+
+        defc    ERR_NR=$5c3a            ; BASIC system variables
+        defc    ERR_SP=$5c3d
+
+	XDEF	dodos
+;
+; This is support for residos, we use the normal
+; +3 -lplus3 library and rewrite the values so
+; that they suit us somewhat
+.dodos
+	exx
+	push	iy
+	pop	hl
+	ld	b,PKG_IDEDOS
+	rst	RST_HOOK
+	defb	HOOK_PACKAGE
+	ld	iy,23610
+	ret
+
+; Detect an installed version of ResiDOS.
+;
+; This should be done before you attempt to call any other ResiDOS/+3DOS/IDEDOS
+; routines, and ensures that the Spectrum is running with ResiDOS installed.
+; Since +3DOS and IDEDOS are present only from v1.40, this version must
+; be checked for before making any further calls.
+;
+; If you need to use calls that were only provided from a certain version of
+; ResiDOS, you can check that here as well.
+;
+; The ResiDOS version call is made with a special hook-code after a RST 8,
+; which will cause an error on Speccies without ResiDOS v1.20+ installed,
+; or error 0 (OK) if ResiDOS v1.20+ is present. Therefore, we need
+; to trap any errors here.
+.residos_detect
+        ld      hl,(ERR_SP)
+        push    hl                      ; save the existing ERR_SP
+        ld      hl,detect_error
+        push    hl                      ; stack error-handler return address
+        ld      hl,0
+        add     hl,sp
+        ld      (ERR_SP),hl             ; set the error-handler SP
+        rst     RST_HOOK                ; invoke the version info hook code
+        defb    HOOK_VERSION
+        pop     hl                      ; ResiDOS doesn't return, so if we get
+        jr      noresidos               ; here, some other hardware is present
+.detect_error
+        pop     hl
+        ld      (ERR_SP),hl             ; restore the old ERR_SP
+        ld      a,(ERR_NR)
+        inc     a                       ; is the error code now "OK"?
+        jr      nz,noresidos            ; if not, ResiDOS was not detected
+        ex      de,hl                   ; get HL=ResiDOS version
+        push    hl                      ; save the version
+        ld      de,$0140                ; DE=minimum version to run with
+        and     a
+        sbc     hl,de
+        pop     bc                      ; restore the version to BC
+       ret     nc                      ; and return with it if at least v1.40
+.noresidos
+        ld      bc,0                    ; no ResiDOS
+        ld      a,$ff
+        ld      (ERR_NR),a              ; clear error
+        ret
+
+
+ENDIF
+
+
+
+
 
 ;---------------------------------------------
 ; Some +3 stuff - this needs to be below 49152
