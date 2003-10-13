28fb332521e6b8bfd7099c15190d961bfcff96d1
suborb <dom@suborbital.org.uk>
2003-10-13 22:56:15 +0000

    clear up some long confusion

--- libsrc/stdio/fgetpos.c
+++ libsrc/stdio/fgetpos.c
@@ -8,7 +8,7 @@
  *	djm 1/4/2000
  *
  * --------
- * $Id: fgetpos.c,v 1.2 2001-04-13 14:13:58 stefano Exp $
+ * $Id: fgetpos.c,v 1.3 2003-10-13 22:56:15 dom Exp $
  */
 
 #define ANSI_STDIO
@@ -45,15 +45,13 @@ int fgetpos(FILE *fp, fpos_t *posn)
 	pop	bc
 	ret
 .fgetpos_abort
-	ld	de,65535	;-1
-	ld	l,e
-	ld	h,d
+	ld	hl,-1
 #endasm
 #else
 	if ( fp->flags&_IOUSE && fchkstd(fp)== 0 ) {
 		return (fdgetpos(fp->fd,posn));
 	}
-	return -1L;
+	return -1;
 #endif
 }
 
