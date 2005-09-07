ab78289ad6140a4a779c9196f880120f719e5581
aralbrec <alvin_albrecht@hotmail.com>
2005-09-07 01:12:59 +0000

    Added inp(), outp(), l_qsort, l_bsearch to stdlib and makefile lists for all targets

--- libsrc/gamesdeps.sh
+++ libsrc/gamesdeps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#  $Id: gamesdeps.sh,v 1.1 2001-10-22 09:33:55 stefano Exp $
+#  $Id: gamesdeps.sh,v 1.2 2005-09-07 01:12:59 aralbrec Exp $
 #  short script to sort out games dependencies
 
-	rm -f games/*.o
+	rm -f games/*.o 
