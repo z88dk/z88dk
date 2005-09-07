e1e66f1684107a4b3d28066782d2624cfe725f62
aralbrec <alvin_albrecht@hotmail.com>
2005-09-07 01:06:58 +0000

    Added hash table data type

--- include/adt.h
+++ include/adt.h
@@ -4,14 +4,15 @@
 /*
  * Abstract Data Types Library
  *
- * - Doubly Linked List      ; added 08.2005 aralbrec
+ * - Doubly Linked List +    ; added 08.2005 aralbrec
+ * - Hash Table         +    ; added 08.2005 aralbrec
  * - Heap                    ; added 08.2005 aralbrec
  *
- * These functions require a memory allocation policy that allows
- * memory to be allocated and freed implicitly.  You can determine
- * how the memory is allocated by declaring the u_malloc and u_free
- * functions globally in your main.c file (here the examples use
- * the standard C malloc library functions but you could use the
+ * The data types marked with "+" require a memory allocation policy
+ * that allows memory to be allocated and freed implicitly.  You can
+ * determine how that memory is allocated by declaring the u_malloc
+ * and u_free functions globally in your main.c file (here the examples
+ * use the standard C malloc library functions but you could use the
  * block allocator, a mixture of the two, or something of your own):
  *
  * ( u_malloc must return carry flag set if allocation successful )
@@ -39,6 +40,9 @@ typedef unsigned int uint;
 
 Items are stored in an ordered list.  Each list has a current pointer
 that can point at: a specific item, before the list or after the list.
+The list iterator is ListSearch, which searches from the item pointed
+at by the current pointer and stops when the matching function
+reports a match or when the current ptr points past the end of the list.
 
 In the following:
 
@@ -79,6 +83,61 @@ extern void __LIB__ __FASTCALL__ *adt_ListTrim(struct adt_List *list);
 extern void __LIB__ *adt_ListSearch(struct adt_List *list, void *match, void *item1);
 
 
+/*** HASH TABLE
+
+A hash table stores (key,value) pairs and is intended as a fast table lookup.
+You provide the key, the hash table looks up the value.
+
+This implementation of hash table uses a linked list to store (key,value)
+pairs that map to the same table location.  Keys must be unique, with the
+lib functions taking sensible actions if the user violates this requirement.
+Unlike most hash table implementations that only require an equals operator
+on keys, this implementation requires a less/equal operator in order
+to keep items in the lists sorted for marginally faster lookup when the load
+factor of the hash table becomes high.
+
+On creation, you must determine the hash table's hashing size and provide
+several functions including key compare and hash function which will be
+stored in the hashtable data structure and will be used implicity in hash
+table function calls.  As always performance depends on a good hash function
+and appropriate hash table size.  The goal is to evenly spread keys among all
+table indices -- this can be helped by choosing a table size that is a prime
+number.
+
+In the following:
+
+   void *delete    <->  void __FASTCALL__ (*delete)(struct adt_HashCell *hc)
+                        an opportunity for user cleanup; delete hc->key, hc->value only
+   void *hashfunc  <->  uint (*hashfunc)(void *key, uint size)
+                        return a uint from 0..size-1
+   void *compare   <->  char (*compare)(void *key1, void *key2)
+                        if *key1 < *key2 return -ve, if = return 0
+
+delete = 0 to do no user clean-up on deleted adt_HashCells.
+
+*/
+
+struct adt_HashCell {               /* Invisible to user, one for each (key,value) pair in table */
+   void *key;
+   struct adt_HashCell *next;       /* big endian */
+   void *value;
+};
+
+struct adt_HashTable {              /* One as handle for the hash table */
+   uint                size;        /* size of table */
+   struct adt_HashCell **table;     /* table of buckets */
+   void                *hashfunc;   /* user supplied hash function */
+   void                *compare;    /* user supplied comparison of keys function */
+   void                *delete;     /* user supplied function for deleting (key,value) pairs */
+};
+
+extern struct adt_HashTable __LIB__ *adt_HashCreate(uint size, void *delete, void *compare, void *hashfunc);
+extern void __LIB__ *adt_HashRemove(struct adt_HashTable *ht, void *key);
+extern void __LIB__ *adt_HashLookup(struct adt_HashTable *ht, void *key);
+extern void __LIB__ *adt_HashAdd(struct adt_HashTable *ht, void *key, void *value);
+extern void __LIB__ __FASTCALL__ adt_HashDelete(struct adt_HashTable *ht);
+
+
 /*** HEAP (PRIORITY QUEUE)
 
 The heap is an array of void* with indices 1..N used to store items.  Items in a
@@ -90,11 +149,19 @@ Heapify can be used to convert an unsorted random array into a heap with the hea
 property.  HeapAdd is used to add items to a valid heap incrementally.  HeapExtract
 pulls items out of the heap one at a time in order.
 
+Besides returning the smallest (min heap) / largest (max heap) item in the heap,
+HeapExtract will also write the extracted item to the end of the array.  If all
+items in an array are extracted one after the other, the array, after all
+extractions, will be sorted in ascending order (max heap) or descending order
+(min heap).  This is an implementation of heapsort.
+
 In the following:
 
-     void *compare  <->  char (*compare)(void *item1, void *item2)
+     void *compare  <->  char (*compare)(void **item1, void **item2)
        return negative if item1>item2 for max heap (extract largest item first)
        return negative if item1<item2 for min heap (extract smallest item first)
+
+     NOTE ON VOID** Addresses within the array are being passed not array contents
 
 Maximum array size (N) is 32767.  You provide the array and N, the number of items
 in the heap.  N will automatically be modified as items are added and removed from
