e5cd9c3ac5ddf1d7eaff5a9bd18442ee03caac27
aralbrec <alvin_albrecht@hotmail.com>
2006-11-08 22:35:30 +0000

    fixed function name

--- libsrc/adt/stack/adt_StackCreate.asm
+++ libsrc/adt/stack/adt_StackCreate.asm
@@ -1,11 +1,11 @@
 ; struct adt_Stack *adt_StackCreate(void)
 ; 09.2005 aralbrec
 
-XLIB adt_Stack
+XLIB adt_StackCreate
 
 ; yeah, pretty much does nothing but there
 ; to make code more readable.
 
-.adt_Stack
+.adt_StackCreate
    ld hl,0
    ret
