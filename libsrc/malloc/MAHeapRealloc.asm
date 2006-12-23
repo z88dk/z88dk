336c1bd33ec87dc77c2c535b6ca9d9ea71675963
aralbrec <alvin_albrecht@hotmail.com>
2006-12-23 08:09:19 +0000

    made ansi compliant; no longer attempts to merge current block with free adjacent blocks before allocation -- this should be done eventually

--- libsrc/malloc/MAHeapRealloc.asm
+++ libsrc/malloc/MAHeapRealloc.asm
@@ -4,23 +4,25 @@
 XLIB MAHeapRealloc
 LIB MAHeapAlloc, MAHeapFree
 
-; Grab a new memory block from the heap specified,
-; replacing the one passed in and possibly of different
-; size.  Copy as much of the old block possible to
+; Grab a new memory block from the heap specified.
+; Copy as much of the old block possible to
 ; the new memory block.  If reallocation is not
-; possible a block the same size as the old block is
-; returned.
+; possible return 0 to indicate failure.
 ;
 ; enter : hl = old block address (+2)
 ;         de = & heap pointer
 ;         bc = new block size request
 ; exit  : hl = address of memory block and carry set if successful
-;              else address of block of same size as before and no carry
+;              else 0 and no carry (original block left as is)
 ; used  : af, bc, de, hl
-
-; strategy is to first free the old block then request
-; a new one of given size, followed by a copy from old
-; to new.  clearly this is not multithread safe.
+;
+; Not allowed to use IX,IY,EXX which really constrains things.
+;
+; ** For now just attempts to allocate a new block of given size.
+; Must revisit to investigate if the old block can be merged with
+; any free blocks in the free list to form a bigger block.  The
+; ansi requirements for this function make it harder than it should
+; be.
 
 .MAHeapRealloc
 
@@ -28,72 +30,75 @@ LIB MAHeapAlloc, MAHeapFree
    or l
    ret z
 
-   push hl                   ; save old block address + 2 (data portion)
-   push bc                   ; save new size request
+   ld a,b
+   or a
+   jp nz, sizeok
+   ld a,c
+   cp 2
+   jp nc, sizeok
+   ld c,2
    
-   dec hl
-   ld b,(hl)
-   dec hl
-   ld c,(hl)
-   push bc                   ; save old block's size just in case
-   inc hl
-   inc hl
-   
-   push de                   ; save & heap pointer
-   call MAHeapFree
-   pop hl                    ; hl = & heap pointer
-   pop de
-   pop bc                    ; bc = new size request
+.sizeok
+
    push de
+   push hl
    push bc
-   push hl                   ; stack = old block addr, old block size, new size req, & heap pointer
-
-   call MAHeapAlloc
-   jr nc, blockfail          ; failed to get new block
-   
-.gotblock
-
-   ; hl = new block address
-   ; stack = old block addr, old block size, new size req, & heap pointer
-   
-   pop de                    ; junk heap pointer
-   pop bc                    ; bc = new size
-   pop de                    ; de = old size
    ex de,hl
+   call MAHeapAlloc
+   jr c, success
+   
+.fail
+
+   pop bc
+   pop bc
+   pop de
+   ret
+
+.success
+
+   ; hl = & new block (+2)
+   ; stack = & heap, & old block (+2), new block size
+   
+   pop bc                    ; bc = new block size
+   pop de
+   ex de,hl                  ; de = & new block (+2), hl = & old block (+2)
+   push hl
+   dec hl
+   ld a,(hl)
+   dec hl
+   ld l,(hl)
+   ld h,a                    ; hl = size of old block
+   
    or a
    sbc hl,bc                 ; old size - new size
    jr nc, usenewsize
    add hl,bc
    ld c,l
-   ld b,h                    ; use old size
+   ld b,h                    ; bc = old size
    
 .usenewsize
 
    ; bc = number of bytes to copy
-   ; de = new block address
-   ; stack = old block address
+   ; de = & new block (+2)
+   ; stack = & heap, & old block (+2)
    
    pop hl
-   push de                   ; save new block address
-   ldir                      ; copy old block to new block
-   pop hl                    ; hl = new block address
-   scf
-   ret
-
-.blockfail
-
-   ; stack = old block addr, old block size, new size req, & heap pointer
-
-   pop hl                    ; hl = heap pointer
-   pop bc
-   pop bc                    ; bc = old block size
-   push bc
-   call MAHeapAlloc          ; must succeed since we just put in a memory block this size
-   pop bc
-   pop de
-   ex de,hl                  ; hl = old block address + 2 (data portion), de = new block, bc = old block size
+   push hl
    push de
-   ldir                      ; copy old block to new block
+   ldir                      ; copy old data block to new data block
+   
+   ; stack = & heap, & old block (+2), & new block (+2)
+   
    pop hl
-   or a
+   pop de
+   ex (sp),hl
+   ex de,hl
+   
+   ; de = & heap, hl = & old block (+2)
+   ; stack = & new block
+   
+   call MAHeapFree           ; return old block to free list
+   
+   pop hl
+   scf
    ret
