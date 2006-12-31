ae5a789781181a5b5716243a0a93ec81ece01973
aralbrec <alvin_albrecht@hotmail.com>
2006-12-31 22:17:17 +0000

    Much cleaned up, functions made FASTCALL where possible

--- include/stdlib.h
+++ include/stdlib.h
@@ -5,113 +5,155 @@
  * Lots of nice support functions here and a few defines
  * to support some functions
  *
- * $Id: stdlib.h,v 1.21 2006-11-28 22:48:29 aralbrec Exp $
+ * $Id: stdlib.h,v 1.22 2006-12-31 22:17:17 aralbrec Exp $
  */
 
 #include <sys/types.h>
 
-extern __LIB__ abs(int);
-extern long __LIB__ labs(long);
-extern __LIB__ atexit();
-extern __LIB__ __FASTCALL__ exit(int);  /* Fast call stops pushing on stack*/
 
-/* Exit status can be defined for all machines */
-#define EXIT_FAILURE     1     /* Failed */
-#define EXIT_SUCCESS     0     /* Succeeded */
+/**********************************/
+/* STANDARD K&R LIBRARY FUNCTIONS */
+/**********************************/
 
 
-extern long __LIB__ strtol(char *,char **,int);
-extern unsigned long __LIB__ strtoul(char *,char **,int);
+//////////////////////////////////
+//// String <-> Number Conversions
+//////////////////////////////////
 
-#ifndef USEATOISTRTOL
-extern int __LIB__ atoi(char *);
-#else
-#define atoi(s) (int)strtol((s),(char **)NULL,10)
-#endif
+// double atof(char *s);                    /* check math library for availability */
 
-#ifdef __Z88__
-extern long __LIB__ atol(char *);
-#else
-#define atol(s) strtol((s),(char **)NULL,10)
-#endif
+extern int  __LIB__ __FASTCALL__  atoi(char *s);
+extern char __LIB__              *itoa(char *s, int n);
+extern long __LIB__ __FASTCALL__  atol(char *s);
 
-#if 0
-#ifndef _HAVE_ATOF_
-#define _HAVE_ATOF_
-extern double __LIB__ atof(char *);
-#endif
-#endif
+// double strtod(char *s, char **endp);     /* check math library for availability */
 
+extern long          __LIB__ strtol(char *s, char **endp, int base);
+extern unsigned long __LIB__ strtoul(char *s, char **endp, int base);
 
-/* random number things */
+///////////////////
+//// Random Numbers
+///////////////////
 
-#define RAND_MAX 32767
+// To use the random number generator you must declare the integer seed variable
+// "std_seed" globally in your main.c file:  int std_seed;
 
-extern int __LIB__ rand(void);
-extern void __LIB__ srand(int);
+#define RAND_MAX    32767
+#define M_SRAND(a)  asm("ld\thl,"#a"\nld\t(_std_seed),hl\n");
 
-extern __LIB__ sleep(int);
+extern int  __LIB__              rand(void);
+extern void __LIB__ __FASTCALL__ srand(unsigned int seed);
 
+// Not sure why Rex has it's own rand() routine using different seed?
 
-/* Non standard stdlib.h defs (mode is ignored)*/
-#ifndef __FCNTL_H__
-extern __LIB__ mkdir(char *, int mode);
-extern char __LIB__ *getcwd(char *buf, size_t maxlen); 
-#endif
+#define randRex() rand()
 
-#ifdef __Z88__
-extern __LIB__ csleep(int);  /* Very non standard! sleep for centisecs! (z88)*/
-#endif
+//////////////////////
+//// Memory Allocation
+//////////////////////
 
-/* abort is a macro to exit with RC_Err - only for apps */
+// Before using the malloc library you must initialize the heap -- see malloc.h for details
+// calloc(), malloc(), realloc(), free(), mallinit(), mallinfo(), sbrk()
+
+#include <malloc.h>
+
+///////////////////////
+//// System Environment
+///////////////////////
+
+// Z88: abort is a macro to exit with RC_Err - only for apps
 
 #define abort() exit(15)
 
-/* itoa is used by the Rex6000 port, but anyone could use it */
-extern char __LIB__ *itoa(char *buf, int num);
-/* Not sure why Rex has it's own rand() routine using different seed? */
-#define randRex() rand()
+#define EXIT_FAILURE   1
+#define EXIT_SUCCESS   0
 
+extern void __LIB__ __FASTCALL__ exit(int status);
+extern int  __LIB__ __FASTCALL__ atexit(void *fcn);
 
-#ifdef __Z88__
-/* Define a system() function - only valid for z88 shell */
-extern int system(char *text);
-#endif
+// int system(char *s);                     /* might be implemented in target's library but doubtful */
+// char *getenv(char *name);                /* might be implemented in target's library but doubtful */
 
-
-extern int __LIB__  getopt (int, char **, char *);
-extern   char *optarg;                  /* getopt(3) external variables */
+extern void __LIB__  sleep(int);
+extern int  __LIB__  getopt (int, char **, char *);
+extern   char *optarg;                      /* getopt(3) external variables */
 extern   int opterr;
 extern   int optind;
 extern   int optopt;
 extern   int optreset;
 
+#ifdef __Z88__
+extern int system(char *text);              /* should this be in the z88 library? */
+#endif
 
-/* Little Versions of Quicksort and Binary Search Added */
+//////////////////
+//// Search & Sort
+//////////////////
+
+// These are not quite ansi (array items are assumed to be two bytes in length).  Also look
+// into the heapsort implementation in the abstract data types library (adt.h) as a stack-
+// usage-free alternative to quicksort.
+//
+// void *cmp == char (*cmp)(void *key, void *datum);
 
 extern void __LIB__ *l_bsearch(void *key, void *base, unsigned int n, void *cmp);
-extern void __LIB__ l_qsort(void *base, unsigned int size, void *cmp);
-/* void *cmp  <=>  char (*cmp)(const void *key, const void *datum) */
+extern void __LIB__  l_qsort(void *base, unsigned int size, void *cmp);
+
+//////////////////////////
+//// Misc Number Functions
+//////////////////////////
+
+extern int  __LIB__ __FASTCALL__ abs(int n);
+extern long __LIB__              labs(long n);
+
+struct div_t {
+   int quot;
+   int rem;
+};
+
+struct ldiv_t {
+   long quot;
+   long rem;
+};
+
+typedef struct div_t div_t;
+typedef struct ldiv_t ldiv_t;
+
+// div_t div(int num, int denom);           /* not implemented because can't return structs! */
+// ldiv_t ldiv(long num, long denom);       /* not implemented because can't return structs! */
+
+extern void __LIB__ div(div_t *d, int num, int denom);
+extern void __LIB__ ldiv(ldiv_t *d, long num, long denom);
 
 
-/* Some Functions to Access 16-bit I/O Ports from C
-   Non-Standard But Required for Any Z80 Machine     */
+/******************************************************/
+/* NON-STANDARD BUT GENERALLY USEFUL FOR Z80 MACHINES */
+/******************************************************/
+
+
+//////////////
+//// I/O PORTS
+//////////////
+
+// For accessing 16-bit i/o ports from C.  The macros can be
+// used to inline code if the parameters resolve to constants.
 
 extern unsigned char __LIB__ __FASTCALL__ inp(unsigned int port);
-extern void __LIB__ outp(unsigned int port, unsigned char byte);
+extern void          __LIB__              outp(unsigned int port, unsigned char byte);
 
 #define M_INP(port) asm("ld\tbc,"#port"\nin\tl,(c)\nld\th,0\n");
 #define M_OUTP(port,byte) asm("ld\tbc,"#port"\nld\ta,"#byte"\nout\t(c),a\n");
 
-/* An endian swap function */
+///////////////////////////////
+//// Direct Memory Manipulation
+///////////////////////////////
 
 extern void __LIB__ __FASTCALL__ *swapendian(void *addr);
 
-/* Non-Standard Functions to Peek and Poke Bytes and Words to Memory */
-/* the need turns up often and is cumbersome to do without these     */
+// The macros can be used to inline code if the parameters resolve to constants
 
-extern void  __LIB__ bpoke(void *addr, unsigned char byte);
-extern void  __LIB__ wpoke(void *addr, unsigned int word);
+extern void          __LIB__              bpoke(void *addr, unsigned char byte);
+extern void          __LIB__              wpoke(void *addr, unsigned int word);
 extern unsigned char __LIB__ __FASTCALL__ bpeek(void *addr);
 extern unsigned int  __LIB__ __FASTCALL__ wpeek(void *addr);
 
@@ -120,9 +162,21 @@ extern unsigned int  __LIB__ __FASTCALL__ wpeek(void *addr);
 #define M_BPEEK(addr) asm("ld\thl,("#addr")\nld\th,0\n");
 #define M_WPEEK(addr) asm("ld\thl,("#addr")\n");
 
-/*
- *	Okay..include in malloc.h to be standard..
- */
-#include <malloc.h>
+
+/*********/
+/* OTHER */
+/*********/
+
+// Non standard stdlib.h defs (mode is ignored)
+
+#ifndef __FCNTL_H__
+extern __LIB__ mkdir(char *, int mode);
+extern char __LIB__ *getcwd(char *buf, size_t maxlen); 
+#endif
+
+#ifdef __Z88__
+extern __LIB__ csleep(int);                 /* Very non standard! sleep for centisecs! (z88)*/
+#endif
+
 
 #endif
