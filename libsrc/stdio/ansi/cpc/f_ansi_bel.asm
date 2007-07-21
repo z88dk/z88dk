9ae9590a7102d563b4215da657fb4ba9e7ee8a4c
suborb <dom@suborbital.org.uk>
2007-07-21 21:28:22 +0000

    Use the firmware interposer for all routines.
    Reformatting them as we go along

--- libsrc/stdio/ansi/cpc/f_ansi_bel.asm
+++ libsrc/stdio/ansi/cpc/f_ansi_bel.asm
@@ -7,13 +7,16 @@
 ;	Stefano Bodrato - Jul. 2004
 ;
 ;
-;	$Id: f_ansi_bel.asm,v 1.1 2004-07-15 10:03:49 stefano Exp $
+;	$Id: f_ansi_bel.asm,v 1.2 2007-07-21 21:28:22 dom Exp $
 ;
 
-	XLIB	ansi_BEL
+        XLIB	ansi_BEL
 
+        XREF    firmware
 ; A fine double frequency beep for BEL
 
 .ansi_BEL
-	ld	a,7
-	jp	$BB5A
+        ld      a,7
+        call    firmware
+        defw    txt_output
+        ret
