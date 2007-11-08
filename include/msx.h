ff37045530a9e6fdb8e24876309a12d5b163ba76
zx70 <stefano_bodrato@hotmail.com>
2007-11-08 14:11:49 +0000

    Various improvements/fixes - ABC800 added

---
+++ include/msx.h
@@ -0,0 +1,18 @@
+/*
+ * Headerfile for MSX specific stuff
+ *
+ * $Id: msx.h,v 1.1 2007-11-08 14:11:49 stefano Exp $
+ */
+
+#ifndef __MSX_H__
+#define __MSX_H__
+
+#include <sys/types.h>
+
+
+// Check if MSX 1 or MSX 2
+
+extern int  __LIB__ int msx_type();
+
+
+#endif
