de5f24e4383f2e9231c07749262e8e930b6e09dc
zx70 <stefano_bodrato@hotmail.com>
2008-01-30 14:05:40 +0000

    Much better (and stable) diagnostic functions for Interface 1

--- libsrc/spectrum/if1_installed.asm
+++ libsrc/spectrum/if1_installed.asm
@@ -7,16 +7,18 @@
 ;	- 0 (false) if the ZX Interface1 is missing or not paged in
 ;	- 1 (true) if the ZX Interface1 is connected and activated.
 ;
-;	$Id: if1_installed.asm,v 1.1 2006-06-28 22:21:26 stefano Exp $
+;	$Id: if1_installed.asm,v 1.2 2008-01-30 14:05:41 stefano Exp $
 ;
 
 	XLIB	if1_installed
 	
 if1_installed:
 	ld	hl,(23635)
-	ld	de,23755
+	ld	de,23813
 	sbc	hl,de
+	ld	a,h
+	or	l
 	ld	hl,0
-	ret	z
+	ret	nz
 	inc	hl
 	ret
