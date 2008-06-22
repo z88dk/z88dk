cff3977ef9a6baa37d35c041a1a018264fa33517
aralbrec <alvin_albrecht@hotmail.com>
2008-06-22 19:40:48 +0000

    whence constants changed

--- libsrc/stdio_new/rewind.asm
+++ libsrc/stdio_new/rewind.asm
@@ -12,7 +12,7 @@ INCLUDE "stdio.def"
    
    res 0,(ix+3)                ; no unget char
    
-   ld b,1                      ; set file position
+   ld b,3                      ; set file position
    ld c,STDIO_MSG_SEEK
    ld hl,0
    ld e,l
