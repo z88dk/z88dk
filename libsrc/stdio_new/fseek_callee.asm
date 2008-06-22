10f0db3d2cbe96240107cec913df3a9d9f2c3818
aralbrec <alvin_albrecht@hotmail.com>
2008-06-22 19:16:12 +0000

    v1.0

---
+++ libsrc/stdio_new/fseek_callee.asm
@@ -0,0 +1,41 @@
+; int __CALLEE__ fseek_callee(FILE *stream, long offset, int whence)
+; 06.2008 aralbrec
+
+XLIB fseek_callee
+XDEF ASMDISP_FSEEK_CALLEE
+
+LIB stdio_success_znc, stdio_error_mc, l_jpix, l_declong
+
+.fseek_callee
+
+   pop af
+   pop bc
+   ld b,c
+   pop hl
+   pop de
+   pop ix
+   push af
+
+.asmentry
+
+   ; enter : dehl = offset
+   ;            b = whence (1=SET, 2=CUR, 3=END)
+   ;           ix = FILE *stream
+   
+   ld a,b                      ; if this is a relative seek from current pos
+   cp 2
+   jr nz, seek
+   
+   bit 0,(ix+3)                ; and an unget char is available
+   call nz, l_declong          ; then we need to adjust the seek value by 1
+   
+.seek
+   
+   ld c,STDIO_MSG_SEEK
+   call l_jpix
+   jp c, stdio_error_mc
+   
+   res 0,(ix+3)                ; clear unget char
+   jp stdio_success_znc
+
+defc ASMDISP_FSEEK_CALLEE = asmentry - fseek_callee
---
+++ libsrc/stdio_new/ftell.asm
@@ -0,0 +1,34 @@
+; long __FASTCALL__ ftell(FILE *stream)
+; 06.2008 aralbrec
+
+XLIB ftell
+LIB stdio_error_mc, l_declong, l_jpix
+
+INCLUDE "stdio.def"
+
+.ftell
+
+   push hl
+   pop ix                      ; ix = FILE *
+   
+   ld hl,0
+   ld e,l
+   ld d,h                      ; relative seek by 0 bytes
+   
+   ld b,2                      ; seek from current position
+   ld c,STDIO_MSG_SEEK
+   call l_jpix
+   jr c, error
+
+   bit 0,(ix+3)                ; if an unget char is pending
+   call nz, l_declong          ;  decrement file position
+   
+   ret
+
+.error
+
+   call stdio_error_mc
+   ld e,l
+   ld d,h
+   
+   ret
