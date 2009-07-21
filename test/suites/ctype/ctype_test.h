70d882910f9648f1af8699f05babc05b4c4ae17d
suborb <dom@suborbital.org.uk>
2009-07-21 20:30:52 +0000

    missing header file

---
+++ test/suites/ctype/ctype_test.h
@@ -0,0 +1,15 @@
+
+
+#ifndef CTYPE_TESTS_H
+#define CTYPE_TESTS_H
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "test.h"
+
+extern int test_isalnum();
+extern int test_isupper();
+
+
+#endif
