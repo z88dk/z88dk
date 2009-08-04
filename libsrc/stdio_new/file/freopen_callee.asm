b9d6df889a3a01c05e7c2808da916e5dbc1d80ee
aralbrec <alvin_albrecht@hotmail.com>
2009-08-04 04:40:21 +0000

    missing function from LIB declaration

--- libsrc/stdio_new/file/freopen_callee.asm
+++ libsrc/stdio_new/file/freopen_callee.asm
@@ -4,7 +4,7 @@
 XLIB freopen_callee
 XDEF ASMDISP_FREOPEN_CALLEE
 
-LIB fclose, close, stdio_parseperm, fopen_callee, stdio_findfilestruct, stdio_free, fflush
+LIB fclose, close, stdio_parseperm, fopen_callee, stdio_findfilestruct, stdio_findfdstruct, stdio_free, fflush
 LIB stdio_error_einval_zc, stdio_error_zc, stdio_error_ebadf_zc, l_jpix, stdio_descendchain
 XREF LIBDISP_FCLOSE, LIBDISP_CLOSE, LIBDISP2_CLOSE, LIBDISP_FOPEN_CALLEE, ASMDISP_FFLUSH
 
