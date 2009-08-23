acbdafcd5bd6f1e4d8ff275459bcb568169a22d2
aralbrec <alvin_albrecht@hotmail.com>
2009-08-23 19:21:47 +0000

    added skeleton for saving state in comments as a reminder

--- libsrc/stdio_new/general/stdio_idle.asm
+++ libsrc/stdio_new/general/stdio_idle.asm
@@ -2,6 +2,7 @@
 ; 08.2009 aralbrec
 
 XLIB stdio_idle
+LIB stdio_statesave, stdio_staterestore
 
 ; The library calls this function when it is waiting for data
 ; to be written or read from a device that is not ready.  This
@@ -9,5 +10,11 @@ XLIB stdio_idle
 ; environment.
 
 .stdio_idle
+
+   ; call stdio_statesave        ; push all registers to stack
+   
+   ; insert code here
+   
+   ; call stdio_staterestore     ; pop all registers from stack
 
    ret
