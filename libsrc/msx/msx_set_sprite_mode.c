bab8c33b6468d3df33992928ad33fbc8187fa520
zx70 <stefano_bodrato@hotmail.com>
2009-09-04 05:56:39 +0000

    *** empty log message ***

--- libsrc/msx/msx_set_sprite_mode.c
+++ libsrc/msx/msx_set_sprite_mode.c
@@ -9,14 +9,14 @@
 	
 	Set the sprite mode
 	
-	$Id: msx_set_sprite_mode.c,v 1.1 2009-01-07 09:50:15 stefano Exp $
+	$Id: msx_set_sprite_mode.c,v 1.2 2009-09-04 05:56:40 stefano Exp $
 */
 
 #include <msx.h>
 
 void msx_set_sprite_mode(unsigned char mode) {
-	unsigned char m = msx_get_vdp(1);
-	msx_set_vdp(1, (m & 0xFC) | mode);
+	unsigned char m = get_vdp_reg(1);
+	set_vdp_reg(1, (m & 0xFC) | mode);
 
 	//_init_sprites();
 }
