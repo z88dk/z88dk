56eaa1f97b78e24078ab4f7631ddaa762be3581f
zx70 <stefano_bodrato@hotmail.com>
2009-09-29 14:59:16 +0000

    Fixed a bug: a ret was forcing to skip swapgfxbk1.

--- libsrc/graphics/stencil_add_circle.asm
+++ libsrc/graphics/stencil_add_circle.asm
@@ -9,7 +9,7 @@
 ;       Stefano Bodrato - 13/3/2009
 ;
 ;
-;	$Id: stencil_add_circle.asm,v 1.2 2009-09-18 20:37:43 stefano Exp $
+;	$Id: stencil_add_circle.asm,v 1.3 2009-09-29 14:59:16 stefano Exp $
 ;
 
 ;; void stencil_add_circle(int x1, int y1, int x2, int y2, unsigned char *stencil)
@@ -47,7 +47,6 @@
 		
                 ld      ix,stencil_add_pixel
                 call    draw_circle
-                ret
                 
                 jp      swapgfxbk1
 
