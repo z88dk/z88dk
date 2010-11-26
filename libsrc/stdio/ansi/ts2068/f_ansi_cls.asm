decc299a120c606a9fd81082865971d6db0d0122
zx70 <stefano_bodrato@hotmail.com>
2010-11-26 13:24:28 +0000

    Forced the HRG mode when a CLS is invoked by console

--- libsrc/stdio/ansi/ts2068/f_ansi_cls.asm
+++ libsrc/stdio/ansi/ts2068/f_ansi_cls.asm
@@ -9,17 +9,23 @@
 ;	Stefano Bodrato - Apr. 2000
 ;
 ;
-;	$Id: f_ansi_cls.asm,v 1.1 2010-11-26 11:37:32 stefano Exp $
+;	$Id: f_ansi_cls.asm,v 1.2 2010-11-26 13:24:28 stefano Exp $
 ;
 
 	XLIB	ansi_cls
 
 .ansi_cls
-        ld      hl,16384
-        ld      de,16385
-        ld      (hl),0
-        ld      bc,6143
-        ldir
+		
+	in	a,(255)
+	and	@11111000
+	or	@00000110
+	out     (255),a
+
+	ld      hl,16384
+	ld      de,16385
+	ld      (hl),0
+	ld      bc,6143
+	ldir
 	ld	hl,24576
 	ld	de,24577
 	ld	(hl),0
