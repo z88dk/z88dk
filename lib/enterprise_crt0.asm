d9ebacbd09f20fb87636ff0b4394ca500afc6cbe
zx70 <stefano_bodrato@hotmail.com>
2011-02-27 12:01:43 +0000

    Enterprise 64/128 support, alpha release.

--- lib/enterprise_crt0.asm
+++ lib/enterprise_crt0.asm
@@ -2,7 +2,7 @@
 ;
 ;       Stefano Bodrato - 2011
 ;
-;	$Id: enterprise_crt0.asm,v 1.1 2011-02-21 21:01:57 stefano Exp $
+;	$Id: enterprise_crt0.asm,v 1.2 2011-02-27 12:01:43 stefano Exp $
 ;
 
 ; 	There are a couple of #pragma commands which affect
@@ -27,37 +27,39 @@
 ; Some scope definitions
 ;--------
 
-	XREF    _main
+        XREF    _main
 
         XDEF    cleanup
         XDEF    l_dcal
 
         XDEF    _std_seed
 
-	XDEF	snd_tick	; Sound variable
+        XDEF	snd_tick	; Sound variable
 
-	XDEF	_vfprintf
+        XDEF	_vfprintf
 
         XDEF    exitsp
         XDEF    exitcount
 
        	XDEF	heaplast	; Near malloc heap variables
-	XDEF	heapblocks
+        XDEF	heapblocks
 
         XDEF    __sgoioblk
 
 ; Graphics stuff
-	XDEF	pixelbyte	; Temp store for non-buffered mode
+        XDEF	pixelbyte	; Temp store for non-buffered mode
         XDEF    base_graphics   ; Graphical variables
         XDEF    coords          ; Current xy position
 
-
-; Now, getting to the real stuff now!
-
 		XDEF	__fcb		; file control block
 
+; Enterprise 64/128 specific stuff
+		XDEF	warmreset
+		XDEF	setEXOSVariables
+		
+
 IF      !myzorg
-		defc    myzorg  = $100
+		defc    myzorg  = 100h
 ENDIF
 		org     myzorg
 
@@ -66,7 +68,6 @@ ENDIF
 ; Execution starts here
 ;----------------------
 start:
-
 IF (!DEFINED_startup | (startup=1))
 IF !DEFINED_noprotectmsdos
 	; This protection takes little less than 50 bytes
@@ -87,12 +88,59 @@ begin:
 ENDIF
 ENDIF
 
-        ld      hl,0
-        add     hl,sp
-        ld      (start1+1),hl
-        ld      hl,-64
-        add     hl,sp
-        ld      sp,hl
+; Inspired by the DizzyLord loader by ORKSOFT
+        di
+        ld    a, 004h
+        out   (0bfh), a
+        ld    sp, 03F00h
+        ld    a, 0ffh
+        out   (0b2h), a
+
+        ld    c, 060h
+        rst   30h
+        defb  0
+
+        ld    hl, __VideoVariables
+        call  setEXOSVariables
+        halt
+        halt
+
+        ld    a, 66h
+        ld    de, __videoDeviceName
+        rst   30h
+        defb  1
+
+        ld    a, 69h
+        ld    de, __keyboardDeviceName
+        rst   30h
+        defb  1
+
+;        ld    a, 66h
+;        ld    b, 4                      ; @@FONT
+;        rst   30h
+;        defb  11
+
+        ld    a, 66h
+        ld    bc, 00101h                ; @@DISP, from first line
+        ld    de, 01901h                ; to line 25, at screen line 1
+        rst   30h
+        defb  11						; set 40x35 characters window
+
+        ld    de, __scrinitBegin
+        ld    bc, __scrinitEnd - __scrinitBegin
+
+		ld      a,66h	; output channel (video)
+		rst     30h		; EXOS
+		defb    8		; write block
+
+
+;        ld      hl,0
+;        add     hl,sp
+;        ld      (start1+1),hl
+        ld      (start1+1),sp
+;        ld      hl,-64
+;        add     hl,sp
+;        ld      sp,hl
         ld      (exitsp),sp
 
 IF !DEFINED_nostreams
@@ -110,6 +158,7 @@ ENDIF
         call    _main
 	
 cleanup:
+;jr cleanup
 ;
 ;       Deallocate memory which has been allocated here!
 ;
@@ -122,8 +171,8 @@ ENDIF
 ENDIF
 
 start1:
-        ld      sp,0
-; _warmreset:
+;        ld      sp,0
+warmreset:
         ld      sp, 0100h
         ld      a, 0ffh
         out     (0b2h), a
@@ -242,3 +291,54 @@ fasign:         defb    0
 
 ENDIF
 
+
+setEXOSVariables:
+_l1:    ld    b, 1
+        ld    c, (hl)
+        inc   c
+        dec   c
+        ret   z
+        inc   hl
+        ld    d, (hl)
+        inc   hl
+        rst   30h
+        defb  16
+        jr    _l1
+daveReset:
+        push  bc
+        xor   a
+        ld    bc, 010afh
+_l2:    out   (c), a
+        dec   c
+        djnz  _l2
+        pop   bc
+        ret 
+
+__videoDeviceName:
+        defb  6
+        defm  "VIDEO:"
+
+__keyboardDeviceName:
+        defb  9
+        defm  "KEYBOARD:"
+
+__videoVariables:
+       defb  22, 0                      ; MODE_VID
+        defb  23, 2                     ; COLR_VID
+        defb  24, 40                    ; X_SIZ_VID
+        defb  25, 25                    ; Y_SIZ_VID
+        ;defb  27, 0                    ; BORD_VID
+        defb  0
+
+__scrinitBegin:
+        ; SET COLOR 7, 2
+;        defb  01bh, 'c', 007h, 002h
+        ; set character
+;        defb  01bh, 'K', 080h
+;        defb  001h, 003h, 006h, 00ch, 018h, 030h, 060h, 0c0h, 080h
+        ; SET INK 5
+;        defb  01bh, 'I', 005h
+        ; set cursor position to 5,6
+;        defb  01bh, '=', 025h, 026h
+        defm  "Dizzy Lord\r\n"
+__scrinitEnd:
