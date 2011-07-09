690f0978895652dea14f0db845d46298766564ae
pauloscustodio <pauloscustodio@gmail.com>
2011-07-09 01:31:24 +0000

    Moved snprintf macro to z80asm.h

--- src/z80asm/z80asm.h
+++ src/z80asm/z80asm.h
@@ -1,7 +1,28 @@
+/*
+     ZZZZZZZZZZZZZZZZZZZZ    8888888888888       00000000000
+   ZZZZZZZZZZZZZZZZZZZZ    88888888888888888    0000000000000
+                ZZZZZ      888           888  0000         0000
+              ZZZZZ        88888888888888888  0000         0000
+            ZZZZZ            8888888888888    0000         0000       AAAAAA         SSSSSSSSSSS   MMMM       MMMM
+          ZZZZZ            88888888888888888  0000         0000      AAAAAAAA      SSSS            MMMMMM   MMMMMM
+        ZZZZZ              8888         8888  0000         0000     AAAA  AAAA     SSSSSSSSSSS     MMMMMMMMMMMMMMM
+      ZZZZZ                8888         8888  0000         0000    AAAAAAAAAAAA      SSSSSSSSSSS   MMMM MMMMM MMMM
+    ZZZZZZZZZZZZZZZZZZZZZ  88888888888888888    0000000000000     AAAA      AAAA           SSSSS   MMMM       MMMM
+  ZZZZZZZZZZZZZZZZZZZZZ      8888888888888       00000000000     AAAA        AAAA  SSSSSSSSSSS     MMMM       MMMM
 
+Copyright (C) Gunther Strube, InterLogic 1993-99
+*/
+
+/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/z80asm.h,v 1.3 2011-07-09 01:31:24 pauloscustodio Exp $ */
+/* $History: Z80ASM.C $ */
 
 #ifndef Z80ASM_H
 #define Z80ASM_H
+
+/* snprintf is _snprintf in WIN32 */
+#ifdef WIN32
+#define snprintf _snprintf
+#endif
 
 
 #define REG16_BC   0
