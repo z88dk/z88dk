b84e85b2daab297587fe4487053b58e57c019ea8
pauloscustodio <pauloscustodio@gmail.com>
2011-10-14 13:48:05 +0000

    String utilities

---
+++ src/z80asm/strutil.c
@@ -0,0 +1,40 @@
+/*
+     ZZZZZZZZZZZZZZZZZZZZ    8888888888888       00000000000
+   ZZZZZZZZZZZZZZZZZZZZ    88888888888888888    0000000000000
+                ZZZZZ      888           888  0000         0000
+              ZZZZZ        88888888888888888  0000         0000
+            ZZZZZ            8888888888888    0000         0000       AAAAAA         SSSSSSSSSSS   MMMM       MMMM
+          ZZZZZ            88888888888888888  0000         0000      AAAAAAAA      SSSS            MMMMMM   MMMMMM
+        ZZZZZ              8888         8888  0000         0000     AAAA  AAAA     SSSSSSSSSSS     MMMMMMMMMMMMMMM
+      ZZZZZ                8888         8888  0000         0000    AAAAAAAAAAAA      SSSSSSSSSSS   MMMM MMMMM MMMM
+    ZZZZZZZZZZZZZZZZZZZZZ  88888888888888888    0000000000000     AAAA      AAAA           SSSSS   MMMM       MMMM
+  ZZZZZZZZZZZZZZZZZZZZZ      8888888888888       00000000000     AAAA        AAAA  SSSSSSSSSSS     MMMM       MMMM
+
+Copyright (C) Gunther Strube, InterLogic 1993-99
+Copyright (C) Paulo Custodio, 2011
+
+Utilities for string handling
+*/
+
+/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/strutil.c,v 1.1 2011-10-14 13:48:05 pauloscustodio Exp $ */
+/* $Log: strutil.c,v $
+/* Revision 1.1  2011-10-14 13:48:05  pauloscustodio
+/* String utilities
+/*
+/*
+/* */
+
+#include <string.h>
+#include <ctype.h>
+#include "strutil.h"
+
+/*-----------------------------------------------------------------------------
+*   convert string to upper case
+*----------------------------------------------------------------------------*/
+char * strtoupper ( char *string )
+{
+    char *p;
+    for (p = string; *p; p++)
+	*p = toupper(*p);
+    return string;
+}
---
+++ src/z80asm/strutil.h
@@ -0,0 +1,35 @@
+/*
+     ZZZZZZZZZZZZZZZZZZZZ    8888888888888       00000000000
+   ZZZZZZZZZZZZZZZZZZZZ    88888888888888888    0000000000000
+                ZZZZZ      888           888  0000         0000
+              ZZZZZ        88888888888888888  0000         0000
+            ZZZZZ            8888888888888    0000         0000       AAAAAA         SSSSSSSSSSS   MMMM       MMMM
+          ZZZZZ            88888888888888888  0000         0000      AAAAAAAA      SSSS            MMMMMM   MMMMMM
+        ZZZZZ              8888         8888  0000         0000     AAAA  AAAA     SSSSSSSSSSS     MMMMMMMMMMMMMMM
+      ZZZZZ                8888         8888  0000         0000    AAAAAAAAAAAA      SSSSSSSSSSS   MMMM MMMMM MMMM
+    ZZZZZZZZZZZZZZZZZZZZZ  88888888888888888    0000000000000     AAAA      AAAA           SSSSS   MMMM       MMMM
+  ZZZZZZZZZZZZZZZZZZZZZ      8888888888888       00000000000     AAAA        AAAA  SSSSSSSSSSS     MMMM       MMMM
+
+Copyright (C) Gunther Strube, InterLogic 1993-99
+Copyright (C) Paulo Custodio, 2011
+
+Utilities for string handling
+*/
+
+/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/strutil.h,v 1.1 2011-10-14 13:48:05 pauloscustodio Exp $ */
+/* $Log: strutil.h,v $
+/* Revision 1.1  2011-10-14 13:48:05  pauloscustodio
+/* String utilities
+/*
+/*
+/* */
+
+#ifndef STRUTIL_H
+#define STRUTIL_H
+
+#include "memalloc.h"			/* before any other include to enable memory leak detection */
+
+/* convert string to upper case - modify in place, return address of string */
+extern char * strtoupper ( char *string );
+
+#endif /* ndef STRUTIL_H */
