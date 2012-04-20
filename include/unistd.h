9042dd3d0185520bc66e29724ffdc7ff30eafb9f
zx70 <stefano_bodrato@hotmail.com>
2012-04-20 15:46:39 +0000

    Few more include placeholders, added to help porting programs.

---
+++ include/unistd.h
@@ -0,0 +1,15 @@
+/*
+ *  Just a placeholder
+ *
+ *	$Id: unistd.h,v 1.1 2012-04-20 15:46:39 stefano Exp $
+ */
+
+#ifndef __UNISTD_H__
+#define __UNISTD_H__
+
+#include <sys/compiler.h>
+#include <sys/types.h>
+
+extern char *environ[];
+
+#endif
