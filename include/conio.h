c0b701465549168ab0dbed3d8784d5f90a563d87
zx70 <stefano_bodrato@hotmail.com>
2012-05-14 16:45:21 +0000

    Improved old-style MS-DOS console lib emultaion

--- include/conio.h
+++ include/conio.h
@@ -1,29 +1,74 @@
 /*
  *      Small C+ Library
  *
- *      conio.h - old apps compatibility
- *	this helps expecially with the kbhit() instruction
+ *  conio.h - old apps compatibility
+ * 
+ *	This helps expecially with the kbhit() instruction
  *	it exists on many old compilers; the mingw port has it
  *	on "conio.h", so here it is !
  *
  *      stefano - 18/3/2004
  *
- *	$Id: conio.h,v 1.2 2010-09-19 00:24:08 dom Exp $
+ *	$Id: conio.h,v 1.3 2012-05-14 16:45:21 stefano Exp $
  */
 
 #ifndef __CONIO_H__
 #define __CONIO_H__
 
 // this is used by getch, putch and ungetch.
-#include <sys/compiler.h>
+//#include <sys/compiler.h>
 #include <stdio.h>
 
-#define getch()  fgetc(stdin)
-#define getche() getch()                // not sure about this one...
-#define putch(bp,fp) fputc(bp,fp)
+#define    BLACK         0
+#define    BLUE          1
+#define    GREEN         2
+#define    CYAN          3
+#define    RED           4
+#define    MAGENTA       5
+#define    BROWN         6
+#define    LIGHTGRAY     7
+#define    DARKGRAY      8
+#define    LIGHTBLUE     9
+#define    LIGHTGREEN   10
+#define    LIGHTCYAN    11
+#define    LIGHTRED     12
+#define    LIGHTMAGENTA 13
+#define    YELLOW       14
+#define    WHITE        15
+#define    BLINK        13
+
+
+// Color translation table
+int PCDOS_COLORS[]={0,4,2,6,1,5,1,7,4,6,2,6,1,5,3,7};
+// QUICK C syntax
+#define settextcolor(a)	printf("%c[%um",27,PCDOS_COLORS[a]+30)
+// TURBO C syntax
+#define textcolor(a)	printf("%c[%um",27,PCDOS_COLORS[a]+30)
+#define textbackground(a)	printf("%c[%um",27,PCDOS_COLORS[a]+40)
+#define textattr(a)	printf("%c[%um%c[%um",27,PCDOS_COLORS[a&0xF]+30,27,PCDOS_COLORS[a>>4]+40)
+
+#define cprintf printf
+#define cputs puts_cons
+#define cgets gets
+#define cscanf scanf
+
+#define gotoxy(a,b)     printf("%c[%u;%uH",27,b-1,a-1)
+
+#define clrscr() fputc_cons(12)
+#define clreol() printf("%c[K",27)
+
+/* Reads a character directly from the console, without echo. */
+#define getch()  fgetc_cons()
+/* Reads a character directly from the console, without echo. */
+#define getche() fputc_cons(fgetc_cons())                // not sure about this one...
+// Direct output to console
+#define putch(a) fputc_cons(a)
+//#define putch(bp,fp) fputc(bp,fp)
 // #define ungetch(bp)  ungetc(bp,stdout)  // this one doesn't work
 
 #define kbhit() (getk() ? 1 : 0)
+
+#define random(a) rand()%a
 
 
 #endif /* _CONIO_H */
