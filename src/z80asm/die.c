87666d3af722c891dfc3c0611a54d9fc4517a4a0
pauloscustodio <pauloscustodio@gmail.com>
2012-05-20 06:04:18 +0000

    die() with exception instead of exit(1)

--- src/z80asm/die.c
+++ src/z80asm/die.c
@@ -16,9 +16,12 @@ Copyright (C) Paulo Custodio, 2011
 Exit with a fatal error, warn on stderr
 */
 
-/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/die.c,v 1.1 2012-05-17 14:40:39 pauloscustodio Exp $ */
+/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/die.c,v 1.2 2012-05-20 06:04:18 pauloscustodio Exp $ */
 /* $Log: die.c,v $
-/* Revision 1.1  2012-05-17 14:40:39  pauloscustodio
+/* Revision 1.2  2012-05-20 06:04:18  pauloscustodio
+/* die() with exception instead of exit(1)
+/*
+/* Revision 1.1  2012/05/17 14:40:39  pauloscustodio
 /* Exit with a fatal error, warn on stderr
 /*
 /*
@@ -26,26 +29,25 @@ Exit with a fatal error, warn on stderr
 
 #include "memalloc.h"  /* before any other include */
 #include "die.h"
+#include "types.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
 /*-----------------------------------------------------------------------------
-*   Constants
-*----------------------------------------------------------------------------*/
-#define EXIT_VALUE 1    /* value returned to OS */
-
-/*-----------------------------------------------------------------------------
 *   die()
 *----------------------------------------------------------------------------*/
-void die( char *msg, ... )
+void die( e4c_exception_type exception, char *msg, ... )
 {
     va_list argptr;
+	char errstr[MAXLINE];
+
     va_start( argptr, msg ); /* init variable args */
 
-    vfprintf( stderr, msg, argptr );
-    exit( EXIT_VALUE );
+	vsnprintf( errstr, sizeof(errstr)-1, msg, argptr );
+    fprintf( stderr, errstr );
+    throw( exception, errstr );
 }
 
 /*-----------------------------------------------------------------------------
--- src/z80asm/die.h
+++ src/z80asm/die.h
@@ -16,9 +16,12 @@ Copyright (C) Paulo Custodio, 2011
 Exit with a fatal error, warn on stderr
 */
 
-/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/die.h,v 1.2 2012-05-17 15:10:47 pauloscustodio Exp $ */
+/* $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/die.h,v 1.3 2012-05-20 06:04:18 pauloscustodio Exp $ */
 /* $Log: die.h,v $
-/* Revision 1.2  2012-05-17 15:10:47  pauloscustodio
+/* Revision 1.3  2012-05-20 06:04:18  pauloscustodio
+/* die() with exception instead of exit(1)
+/*
+/* Revision 1.2  2012/05/17 15:10:47  pauloscustodio
 /* normalize constant for include-once #ifndef
 /*
 /* Revision 1.1  2012/05/17 14:40:39  pauloscustodio
@@ -30,8 +33,10 @@ Exit with a fatal error, warn on stderr
 #ifndef DIE_H
 #define DIE_H
 
+#include "except.h"             /* CH_0004 : Exception mechanism to handle fatal errors */
+
 /* fatal error on stderr with printf-like argument */
-extern void die( char *msg, ... );
+extern void die( e4c_exception_type exception, char *msg, ... );
 
 /* warning on stderr with printf-like argument */
 extern void warn( char *msg, ... );
