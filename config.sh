04d3ccbb78213f807299af31d6736e77fcf3c3d3
pauloscustodio <pauloscustodio@gmail.com>
2013-01-08 23:14:20 +0000

    Convert absolute paths to Windows notation in MinGW pwd returns /c/xxx, but MinGW needs c:/xxx in the config files

--- config.sh
+++ config.sh
@@ -2,10 +2,14 @@
 
 # Some script hackery to allow global installs
 
-
 destdir=$1
 default=$2
 
+# Convert absolute paths to Windows notation in MinGW 
+# pwd returns /c/xxx, but MinGW needs c:/xxx in the config files
+if uname | grep -q ^MINGW32 ; then
+   destdir=`echo $1 | sed 's?^/\([a-z]\)/?\1:/?'`
+fi
 
 rm -f lib/config/*.cfg
 for file in `ls lib/config/*.lnx`; do
