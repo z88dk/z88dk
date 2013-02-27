5f2828e3c3961bdedb0a7420dec16ecfdd3d0aba
zx70 <stefano_bodrato@hotmail.com>
2013-02-27 12:53:28 +0000

    CLG fix for ts2068 - now hrg display is switched on

--- libsrc/graphics/ts2068hr/clg.asm
+++ libsrc/graphics/ts2068hr/clg.asm
@@ -3,12 +3,17 @@
 ;       Stefano - 10/1/2007
 ;
 ;
-;	$Id: clg.asm,v 1.1 2010-11-16 19:41:11 stefano Exp $
+;	$Id: clg.asm,v 1.2 2013-02-27 12:53:28 stefano Exp $
 ;
 
                 XLIB    clg
 
 .clg
+		in	a,(255)
+		and	@11111000
+		or	@00000110
+		out     (255),a
+
 		ld	hl,0
 		ld	d,h
 		ld	e,h
