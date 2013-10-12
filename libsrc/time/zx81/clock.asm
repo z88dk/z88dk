86b93a1a5d58bffc761d2bf12f3fd48f352e196e
zx70 <stefano_bodrato@hotmail.com>
2013-10-12 15:11:56 +0000

    ZX80<>ZX81 lib build conflict with clock() solved

--- libsrc/time/zx81/clock.asm
+++ libsrc/time/zx81/clock.asm
@@ -4,20 +4,23 @@
 ;	Back to default FRAMES counter + 1 extra byte ;)
 ;
 ; --------
-; $Id: clock.asm,v 1.7 2012-12-31 10:38:23 stefano Exp $
+; $Id: clock.asm,v 1.8 2013-10-12 15:11:56 stefano Exp $
 
 
 	XLIB	clock
 ;	XREF	frames3
+	XREF	_FRAMES
 
 .clock
 IF FORzx81
 	ld	hl,-1
-	ld	de,($4034)
+	;ld	de,($4034)
+	ld	de,(_FRAMES)
 	and a
 	sbc hl,de
 ELSE
-	ld	hl,($401E)
+	;ld	hl,($401E)
+	ld	hl,(_FRAMES)
 ENDIF
 	ld	de,0
 	ret
