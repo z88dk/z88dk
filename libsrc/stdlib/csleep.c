200335eba1d590099a240efba22ed12eae4a7d39
zx70 <stefano_bodrato@hotmail.com>
2013-11-04 08:57:56 +0000

    Working version of the PAUSE/DELAY commands for the ZX80 As for the PAUSE in ZX81 in FAST mode the video is visible for the given period.

--- libsrc/stdlib/csleep.c
+++ libsrc/stdlib/csleep.c
@@ -4,23 +4,34 @@
  *
  *	djm 15/10/2001
  *
- *	$Id: csleep.c,v 1.1 2012-05-16 16:59:56 stefano Exp $
+ *	$Id: csleep.c,v 1.2 2013-11-04 08:57:56 stefano Exp $
  */
 
 #include <stdlib.h>
 #include <time.h>
+#ifdef __ZX80__
+#include <zx81.h>
+#endif
 
 
 void  csleep(unsigned int centiseconds)
 {
 #ifndef __Z88__
-
-
     long start = clock();  
 	long per   = (long) centiseconds * CLOCKS_PER_SEC / 100;
+  #ifdef __ZX80__
+	gen_tv_field_init(0);
+  #endif
         
         while ( (clock() - start) < per )
-                ;
+  #ifdef __ZX80__
+			{
+			  gen_tv_field();
+              FRAMES++;
+			}
+  #else
+	;
+  #endif
 
 #else
 
--- libsrc/stdlib/delay.c
+++ libsrc/stdlib/delay.c
@@ -4,19 +4,29 @@
  *
  *	djm 15/10/2001
  *
- *	$Id: delay.c,v 1.1 2012-05-16 16:59:56 stefano Exp $
+ *	$Id: delay.c,v 1.2 2013-11-04 08:57:56 stefano Exp $
  */
 
 #include <stdlib.h>
 #include <time.h>
+#ifdef __ZX80__
+#include <zx81.h>
+#endif
 
 
 void   delay (long msecs)
 {
     long start = clock();  
 	long per   = msecs * CLOCKS_PER_SEC / 1000;
-        
+
         while ( (clock() - start) < per )
-                ;
+  #ifdef __ZX80__
+			{
+			  gen_tv_field();
+              FRAMES++;
+			}
+  #else
+	;
+  #endif
 }
 
