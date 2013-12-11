9d8d8e7d8ab41f4039a9e16cbe1f4513f1485132
aralbrec <alvin_albrecht@hotmail.com>
2013-12-11 07:03:08 +0000

    aids computation of aligned memory addresses

---
+++ libsrc/_DEVELOPMENT/l/z80/integer/l_andc_hlbc.asm
@@ -0,0 +1,22 @@
+
+XLIB l_andc_hlbc
+
+l_andc_hlbc:
+
+   ; enter : hl, bc
+   ;
+   ; exit  : hl = hl & (~bc)
+   ;
+   ; uses  : af, hl
+
+   ld a,c
+   cpl
+   and l
+   ld l,a
+   
+   ld a,b
+   cpl
+   and h
+   ld h,a
+   
+   ret
