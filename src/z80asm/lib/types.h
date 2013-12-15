f2567808e4c1e6118a39e14b06bf2600bc2a7bea
pauloscustodio <pauloscustodio@gmail.com>
2013-12-15 19:04:31 +0000

    Move types.h to the z80asm/lib directory

--- src/z80asm/lib/types.h
+++ src/z80asm/lib/types.h
@@ -1,20 +1,9 @@
 /*
-     ZZZZZZZZZZZZZZZZZZZZ    8888888888888       00000000000
-   ZZZZZZZZZZZZZZZZZZZZ    88888888888888888    0000000000000
-                ZZZZZ      888           888  0000         0000
-              ZZZZZ        88888888888888888  0000         0000
-            ZZZZZ            8888888888888    0000         0000       AAAAAA         SSSSSSSSSSS   MMMM       MMMM
-          ZZZZZ            88888888888888888  0000         0000      AAAAAAAA      SSSS            MMMMMM   MMMMMM
-        ZZZZZ              8888         8888  0000         0000     AAAA  AAAA     SSSSSSSSSSS     MMMMMMMMMMMMMMM
-      ZZZZZ                8888         8888  0000         0000    AAAAAAAAAAAA      SSSSSSSSSSS   MMMM MMMMM MMMM
-    ZZZZZZZZZZZZZZZZZZZZZ  88888888888888888    0000000000000     AAAA      AAAA           SSSSS   MMMM       MMMM
-  ZZZZZZZZZZZZZZZZZZZZZ      8888888888888       00000000000     AAAA        AAAA  SSSSSSSSSSS     MMMM       MMMM
+Common types and macros
 
 Copyright (C) Paulo Custodio, 2011-2013
 
-Common types
-
-$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/Attic/types.h,v 1.14 2013-12-15 19:01:07 pauloscustodio Exp $
+$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/lib/types.h,v 1.1 2013-12-15 19:04:32 pauloscustodio Exp $
 */
 
 #pragma once
@@ -60,7 +49,10 @@ typedef unsigned char byte_t;		/*  8-bit */
 
 /*
 * $Log: types.h,v $
-* Revision 1.14  2013-12-15 19:01:07  pauloscustodio
+* Revision 1.1  2013-12-15 19:04:32  pauloscustodio
+* Move types.h to the z80asm/lib directory
+*
+* Revision 1.14  2013/12/15 19:01:07  pauloscustodio
 * Move platform specific defines from types.h to config.h.
 * Remove dependency of types.h from glib.h.
 * Use NUM_ELEMS() instead of glib G_N_ELEMENTS().
