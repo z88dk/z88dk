0eea97e7d2a2513a574ade24e47d1e5cffbfe0ea
aralbrec <alvin_albrecht@hotmail.com>
2013-12-16 16:51:13 +0000

    prefer external functions in a jump table to storing a pointer to them.

--- libsrc/_DEVELOPMENT/stdlib/z80/system.asm
+++ libsrc/_DEVELOPMENT/stdlib/z80/system.asm
@@ -16,49 +16,26 @@ INCLUDE "../../crt_vars.inc"
 XLIB system
 XDEF asm_system
 
-LIB error_zc, l_inc_sp
-
 system:
 asm_system:
 
    ; enter : hl = char *string
    ;
-   ; exit  : 
+   ; exit  : string == 0 tests if system() is available
    ;
-   ;         string == 0:
+   ;           hl = 0 and carry set if unavailable
+   ;           hl = non-zero and carry reset if available
    ;
-   ;            hl = non-zero and carry reset if system() available
-   ;            hl = 0 and carry set if system() not available
+   ;         string != 0
    ;
-   ;         string != 0 and system() available:
+   ;           hl = result if system available
+   ;           hl = 0 if system unavailable
    ;
-   ;            hl = result
-   ;
-   ;         string != 0 and system() unavailable:
-   ;
-   ;            hl = 0 and carry set
-   ;
-   ; uses  :  af, de, hl, plus all if system() invoked
+   ; uses  :  all
    
-   ex de,hl                    ; de = char *
-   ld hl,(__system)            ; hl = address of system function
-   
-   ld a,h
-   or l
-   jp z, error_zc              ; system unavailable
-   
-   ld a,d
-   or e
-   ret z                       ; if string == NULL, return with carry reset
-   
-   ; de = char *
-   ; hl = & system_function
-   
-   push de                     ; parameter on stack
-   
-   ld bc,l_inc_sp - 2
-   push bc                     ; pop parameter on return
-   
-   ex de,hl                    ; hl = parameter
-   push de
-   ret                         ; jump to system_function
+   push hl
+
+   call __ch_system
+
+   pop hl
+   ret
