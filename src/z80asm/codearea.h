167648ed15435fc648a33a62aa90577bcb92edff
pauloscustodio <pauloscustodio@gmail.com>
2014-01-02 19:44:39 +0000

    warning: "/","*" within comment [-Wcomment]

--- src/z80asm/codearea.h
+++ src/z80asm/codearea.h
@@ -15,7 +15,7 @@ Copyright (C) Paulo Custodio, 2011-2013
 
 Manage the code area in memory
 
-$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/codearea.h,v 1.11 2013-12-15 13:18:33 pauloscustodio Exp $
+$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/codearea.h,v 1.12 2014-01-02 19:44:39 pauloscustodio Exp $
 */
 
 #pragma once
@@ -76,62 +76,65 @@ extern void append_long( long dword );
 extern byte_t get_byte( size_t *paddr );
 
 
-/* */
-/* $Log: codearea.h,v $
-/* Revision 1.11  2013-12-15 13:18:33  pauloscustodio
-/* Move memory allocation routines to lib/xmalloc, instead of glib,
-/* introduce memory leak report on exit and memory fence check.
 /*
-/* Revision 1.10  2013/09/09 00:15:11  pauloscustodio
-/* Integrate codearea in init() mechanism.
-/*
-/* Revision 1.9  2013/05/16 22:39:39  pauloscustodio
-/* Bad include-once define
-/*
-/* Revision 1.8  2013/02/22 17:19:19  pauloscustodio
-/* Add listfile interface to append bytes to the listing
-/* Remove oldPC - no longer needed with new listfile
-/* Solve memory leak
-/*
-/* Revision 1.7  2013/01/24 23:03:03  pauloscustodio
-/* Replaced (unsigned char) by (byte_t)
-/* Replaced (unisigned int) by (size_t)
-/* Replaced (short) by (int)
-/*
-/* Revision 1.6  2013/01/20 21:24:28  pauloscustodio
-/* Updated copyright year to 2013
-/*
-/* Revision 1.5  2012/11/03 17:39:35  pauloscustodio
-/* astyle, comments
-/*
-/* Revision 1.4  2012/05/24 17:09:27  pauloscustodio
-/* Unify copyright header
-/*
-/* Revision 1.3  2012/05/11 19:29:49  pauloscustodio
-/* Format code with AStyle (http://astyle.sourceforge.net/) to unify brackets, spaces instead of tabs, indenting style, space padding in parentheses and operators. Options written in the makefile, target astyle.
-/*         --mode=c
-/*         --lineend=linux
-/*         --indent=spaces=4
-/*         --style=ansi --add-brackets
-/*         --indent-switches --indent-classes
-/*         --indent-preprocessor --convert-tabs
-/*         --break-blocks
-/*         --pad-oper --pad-paren-in --pad-header --unpad-paren
-/*         --align-pointer=name
-/*
-/* Revision 1.2  2011/10/07 17:53:04  pauloscustodio
-/* BUG_0015 : Relocation issue - dubious addresses come out of linking
-/* (reported on Tue, Sep 27, 2011 at 8:09 PM by dom)
-/* - Introduced in version 1.1.8, when the CODESIZE and the codeptr were merged into the same entity.
-/* - This caused the problem because CODESIZE keeps track of the start offset of each module in the sequence they will appear in the object file, and codeptr is reset to the start of the codearea for each module.
-/* The effect was that all address calculations at link phase were considering
-/*  a start offset of zero for all modules.
-/* - Moreover, when linking modules from a libary, the modules are pulled in to the code area as they are needed, and not in the sequence they will be in the object file. The start offset was being ignored and the modules were being loaded in the incorrect order
-/* - Consequence of these two issues were all linked addresses wrong.
-/*
-/* Revision 1.1  2011/08/19 15:53:58  pauloscustodio
-/* BUG_0010 : heap corruption when reaching MAXCODESIZE
-/* - test for overflow of MAXCODESIZE is done before each instruction at parseline(); if only one byte is available in codearea, and a 2 byte instruction is assembled, the heap is corrupted before the exception is raised.
-/* - Factored all the codearea-accessing code into a new module, checking for MAXCODESIZE on every write.
-/*
-/* */
+* $Log: codearea.h,v $
+* Revision 1.12  2014-01-02 19:44:39  pauloscustodio
+* warning: "/","*" within comment [-Wcomment]
+*
+* Revision 1.11  2013/12/15 13:18:33  pauloscustodio
+* Move memory allocation routines to lib/xmalloc, instead of glib,
+* introduce memory leak report on exit and memory fence check.
+*
+* Revision 1.10  2013/09/09 00:15:11  pauloscustodio
+* Integrate codearea in init() mechanism.
+*
+* Revision 1.9  2013/05/16 22:39:39  pauloscustodio
+* Bad include-once define
+*
+* Revision 1.8  2013/02/22 17:19:19  pauloscustodio
+* Add listfile interface to append bytes to the listing
+* Remove oldPC - no longer needed with new listfile
+* Solve memory leak
+*
+* Revision 1.7  2013/01/24 23:03:03  pauloscustodio
+* Replaced (unsigned char) by (byte_t)
+* Replaced (unisigned int) by (size_t)
+* Replaced (short) by (int)
+*
+* Revision 1.6  2013/01/20 21:24:28  pauloscustodio
+* Updated copyright year to 2013
+*
+* Revision 1.5  2012/11/03 17:39:35  pauloscustodio
+* astyle, comments
+*
+* Revision 1.4  2012/05/24 17:09:27  pauloscustodio
+* Unify copyright header
+*
+* Revision 1.3  2012/05/11 19:29:49  pauloscustodio
+* Format code with AStyle (http://astyle.sourceforge.net/) to unify brackets, spaces instead of tabs, indenting style, space padding in parentheses and operators. Options written in the makefile, target astyle.
+*         --mode=c
+*         --lineend=linux
+*         --indent=spaces=4
+*         --style=ansi --add-brackets
+*         --indent-switches --indent-classes
+*         --indent-preprocessor --convert-tabs
+*         --break-blocks
+*         --pad-oper --pad-paren-in --pad-header --unpad-paren
+*         --align-pointer=name
+*
+* Revision 1.2  2011/10/07 17:53:04  pauloscustodio
+* BUG_0015 : Relocation issue - dubious addresses come out of linking
+* (reported on Tue, Sep 27, 2011 at 8:09 PM by dom)
+* - Introduced in version 1.1.8, when the CODESIZE and the codeptr were merged into the same entity.
+* - This caused the problem because CODESIZE keeps track of the start offset of each module in the sequence they will appear in the object file, and codeptr is reset to the start of the codearea for each module.
+* The effect was that all address calculations at link phase were considering
+*  a start offset of zero for all modules.
+* - Moreover, when linking modules from a libary, the modules are pulled in to the code area as they are needed, and not in the sequence they will be in the object file. The start offset was being ignored and the modules were being loaded in the incorrect order
+* - Consequence of these two issues were all linked addresses wrong.
+*
+* Revision 1.1  2011/08/19 15:53:58  pauloscustodio
+* BUG_0010 : heap corruption when reaching MAXCODESIZE
+* - test for overflow of MAXCODESIZE is done before each instruction at parseline(); if only one byte is available in codearea, and a 2 byte instruction is assembled, the heap is corrupted before the exception is raised.
+* - Factored all the codearea-accessing code into a new module, checking for MAXCODESIZE on every write.
+*
+*/
