595c72bc093118876f5f6e81ac9cc9ac7e429f80
aralbrec <alvin_albrecht@hotmail.com>
2014-01-06 08:32:10 +0000

    unsigned int to signed int conversion with saturation

---
+++ libsrc/_DEVELOPMENT/l/z80/integer/l_utod_hl.asm
@@ -0,0 +1,19 @@
+
+XLIB l_utod_hl
+
+l_utod_hl:
+
+   ; convert unsigned int to signed int, saturate if necessary
+   ;
+   ; enter : hl = unsigned int
+   ;
+   ; exit  : hl = int, maximum $7fff
+   ;         carry unaffected
+   ;
+   ; uses  : f, hl
+   
+   bit 7,h
+   ret z
+   
+   ld hl,$7fff
+   ret
