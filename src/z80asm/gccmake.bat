5cbc608d5d032ba5630e28f443dad74a488e4445
pauloscustodio <pauloscustodio@gmail.com>
2014-01-11 00:16:01 +0000

    Reported by Stefan Drissen (stefan.drissen@gmail.com) 2013-01-11: z80asm however does not compile - xmalloc.h cannot be found. The header is in the lib directory, but there is nothing telling gcc to look in the lib directory. I see that the Linux makefile is adding -I lib to also look in the lib directory.
    If I add -I lib to all the gcc -c lines in gccmake.bat then z80asm also compiles fine.
    
    So gccmake.bat needs an update?

--- src/z80asm/gccmake.bat
+++ src/z80asm/gccmake.bat
@@ -1,45 +1,45 @@
 @echo off
-rem $Id: gccmake.bat,v 1.31 2014-01-02 17:18:16 pauloscustodio Exp $
+rem $Id: gccmake.bat,v 1.32 2014-01-11 00:16:01 pauloscustodio Exp $
 
 echo *******************
 echo * Building z80asm *
 echo *******************
 del *.o lib\*.o
-gcc -c -o asmdrctv.o             asmdrctv.c
-gcc -c -o codearea.o             codearea.c
-gcc -c -o deffile.o              deffile.c
-gcc -c -o errors.o               errors.c
-gcc -c -o exprprsr.o             exprprsr.c
-gcc -c -o file.o                 file.c
-gcc -c -o hist.o                 hist.c
-gcc -c -o ldinstr.o              ldinstr.c
-gcc -c -o lib/class.o            lib/class.c
-gcc -c -o lib/die.o              lib/die.c
-gcc -c -o lib/except.o           lib/except.c
-gcc -c -o lib/fileutil.o         lib/fileutil.c
-gcc -c -o lib/list.o             lib/list.c
-gcc -c -o lib/strhash.o          lib/strhash.c
-gcc -c -o lib/strpool.o          lib/strpool.c
-gcc -c -o lib/strutil.o          lib/strutil.c
-gcc -c -o lib/xmalloc.o          lib/xmalloc.c
-gcc -c -o listfile.o             listfile.c
-gcc -c -o mapfile.o              mapfile.c
-gcc -c -o model.o                model.c
-gcc -c -o model_symref.o         model_symref.c
-gcc -c -o modlink.o              modlink.c
-gcc -c -o module.o               module.c
-gcc -c -o objfile.o              objfile.c
-gcc -c -o options.o              options.c
-gcc -c -o prsident.o             prsident.c
-gcc -c -o prsline.o              prsline.c
-gcc -c -o scan.o                 scan.c
-gcc -c -o srcfile.o              srcfile.c
-gcc -c -o sym.o                  sym.c
-gcc -c -o symref.o               symref.c
-gcc -c -o symtab.o               symtab.c
-gcc -c -o z80asm.o               z80asm.c
-gcc -c -o z80instr.o             z80instr.c
-gcc -c -o z80pass.o              z80pass.c
+gcc -Ilib -Wall -c -o asmdrctv.o             asmdrctv.c
+gcc -Ilib -Wall -c -o codearea.o             codearea.c
+gcc -Ilib -Wall -c -o deffile.o              deffile.c
+gcc -Ilib -Wall -c -o errors.o               errors.c
+gcc -Ilib -Wall -c -o exprprsr.o             exprprsr.c
+gcc -Ilib -Wall -c -o file.o                 file.c
+gcc -Ilib -Wall -c -o hist.o                 hist.c
+gcc -Ilib -Wall -c -o ldinstr.o              ldinstr.c
+gcc -Ilib -Wall -c -o lib/class.o            lib/class.c
+gcc -Ilib -Wall -c -o lib/die.o              lib/die.c
+gcc -Ilib -Wall -c -o lib/except.o           lib/except.c
+gcc -Ilib -Wall -c -o lib/fileutil.o         lib/fileutil.c
+gcc -Ilib -Wall -c -o lib/list.o             lib/list.c
+gcc -Ilib -Wall -c -o lib/strhash.o          lib/strhash.c
+gcc -Ilib -Wall -c -o lib/strpool.o          lib/strpool.c
+gcc -Ilib -Wall -c -o lib/strutil.o          lib/strutil.c
+gcc -Ilib -Wall -c -o lib/xmalloc.o          lib/xmalloc.c
+gcc -Ilib -Wall -c -o listfile.o             listfile.c
+gcc -Ilib -Wall -c -o mapfile.o              mapfile.c
+gcc -Ilib -Wall -c -o model.o                model.c
+gcc -Ilib -Wall -c -o model_symref.o         model_symref.c
+gcc -Ilib -Wall -c -o modlink.o              modlink.c
+gcc -Ilib -Wall -c -o module.o               module.c
+gcc -Ilib -Wall -c -o objfile.o              objfile.c
+gcc -Ilib -Wall -c -o options.o              options.c
+gcc -Ilib -Wall -c -o prsident.o             prsident.c
+gcc -Ilib -Wall -c -o prsline.o              prsline.c
+gcc -Ilib -Wall -c -o scan.o                 scan.c
+gcc -Ilib -Wall -c -o srcfile.o              srcfile.c
+gcc -Ilib -Wall -c -o sym.o                  sym.c
+gcc -Ilib -Wall -c -o symref.o               symref.c
+gcc -Ilib -Wall -c -o symtab.o               symtab.c
+gcc -Ilib -Wall -c -o z80asm.o               z80asm.c
+gcc -Ilib -Wall -c -o z80instr.o             z80instr.c
+gcc -Ilib -Wall -c -o z80pass.o              z80pass.c
 gcc -o z80asm.exe *.o lib\*.o
 copy z80asm.exe ..\..\bin
 del z80asm.exe
