4f92b1533b3627eee134f5e51a2753e69fee543f
pauloscustodio <pauloscustodio@gmail.com>
2014-01-11 00:28:30 +0000

    Link sequence: lib at the end

--- src/z80asm/gccmake.bat
+++ src/z80asm/gccmake.bat
@@ -1,5 +1,5 @@
 @echo off
-rem $Id: gccmake.bat,v 1.32 2014-01-11 00:16:01 pauloscustodio Exp $
+rem $Id: gccmake.bat,v 1.33 2014-01-11 00:28:30 pauloscustodio Exp $
 
 echo *******************
 echo * Building z80asm *
@@ -13,15 +13,6 @@ gcc -Ilib -Wall -c -o exprprsr.o             exprprsr.c
 gcc -Ilib -Wall -c -o file.o                 file.c
 gcc -Ilib -Wall -c -o hist.o                 hist.c
 gcc -Ilib -Wall -c -o ldinstr.o              ldinstr.c
-gcc -Ilib -Wall -c -o lib/class.o            lib/class.c
-gcc -Ilib -Wall -c -o lib/die.o              lib/die.c
-gcc -Ilib -Wall -c -o lib/except.o           lib/except.c
-gcc -Ilib -Wall -c -o lib/fileutil.o         lib/fileutil.c
-gcc -Ilib -Wall -c -o lib/list.o             lib/list.c
-gcc -Ilib -Wall -c -o lib/strhash.o          lib/strhash.c
-gcc -Ilib -Wall -c -o lib/strpool.o          lib/strpool.c
-gcc -Ilib -Wall -c -o lib/strutil.o          lib/strutil.c
-gcc -Ilib -Wall -c -o lib/xmalloc.o          lib/xmalloc.c
 gcc -Ilib -Wall -c -o listfile.o             listfile.c
 gcc -Ilib -Wall -c -o mapfile.o              mapfile.c
 gcc -Ilib -Wall -c -o model.o                model.c
@@ -40,6 +31,15 @@ gcc -Ilib -Wall -c -o symtab.o               symtab.c
 gcc -Ilib -Wall -c -o z80asm.o               z80asm.c
 gcc -Ilib -Wall -c -o z80instr.o             z80instr.c
 gcc -Ilib -Wall -c -o z80pass.o              z80pass.c
+gcc -Ilib -Wall -c -o lib/class.o            lib/class.c
+gcc -Ilib -Wall -c -o lib/die.o              lib/die.c
+gcc -Ilib -Wall -c -o lib/except.o           lib/except.c
+gcc -Ilib -Wall -c -o lib/fileutil.o         lib/fileutil.c
+gcc -Ilib -Wall -c -o lib/list.o             lib/list.c
+gcc -Ilib -Wall -c -o lib/strhash.o          lib/strhash.c
+gcc -Ilib -Wall -c -o lib/strpool.o          lib/strpool.c
+gcc -Ilib -Wall -c -o lib/strutil.o          lib/strutil.c
+gcc -Ilib -Wall -c -o lib/xmalloc.o          lib/xmalloc.c
 gcc -o z80asm.exe *.o lib\*.o
 copy z80asm.exe ..\..\bin
 del z80asm.exe
