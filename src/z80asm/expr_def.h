5527084677d1d2808e05e777e25c719f5834f109
pauloscustodio <pauloscustodio@gmail.com>
2014-02-17 23:41:35 +0000

    Embryo of new expression parser

---
+++ src/z80asm/expr_def.h
@@ -0,0 +1,36 @@
+/*
+     ZZZZZZZZZZZZZZZZZZZZ    8888888888888       00000000000
+   ZZZZZZZZZZZZZZZZZZZZ    88888888888888888    0000000000000
+                ZZZZZ      888           888  0000         0000
+              ZZZZZ        88888888888888888  0000         0000
+            ZZZZZ            8888888888888    0000         0000       AAAAAA         SSSSSSSSSSS   MMMM       MMMM
+          ZZZZZ            88888888888888888  0000         0000      AAAAAAAA      SSSS            MMMMMM   MMMMMM
+        ZZZZZ              8888         8888  0000         0000     AAAA  AAAA     SSSSSSSSSSS     MMMMMMMMMMMMMMM
+      ZZZZZ                8888         8888  0000         0000    AAAAAAAAAAAA      SSSSSSSSSSS   MMMM MMMMM MMMM
+    ZZZZZZZZZZZZZZZZZZZZZ  88888888888888888    0000000000000     AAAA      AAAA           SSSSS   MMMM       MMMM
+  ZZZZZZZZZZZZZZZZZZZZZ      8888888888888       00000000000     AAAA        AAAA  SSSSSSSSSSS     MMMM       MMMM
+
+Copyright (C) Gunther Strube, InterLogic 1993-99
+Copyright (C) Paulo Custodio, 2011-2014
+
+Define expression operators
+
+$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/expr_def.h,v 1.1 2014-02-17 23:41:35 pauloscustodio Exp $
+*/
+
+#ifndef OPERATOR
+#define OPERATOR(_symbol, _type, _prec, _assoc, _eval)
+#endif
+
+/* define list of operators */
+
+
+#undef OPERATOR
+
+/*
+* $Log: expr_def.h,v $
+* Revision 1.1  2014-02-17 23:41:35  pauloscustodio
+* Embryo of new expression parser
+*
+* 
+*/
