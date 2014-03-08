0c71436bda3114dec536a70c0593ecbc9bb740a5
aralbrec <alvin_albrecht@hotmail.com>
2014-03-08 06:37:34 +0000

    exact t-state and ms delay loops

---
+++ libsrc/_DEVELOPMENT/z80/z80/asm_z80_delay_ms.asm
@@ -0,0 +1,49 @@
+
+; ===============================================================
+; Mar 2014
+; ===============================================================
+;
+; void z80_delay_ms(uint ms)
+;
+; Busy wait exactly the number of milliseconds, which includes the
+; time needed for an unconditional call and the ret.
+;
+; ===============================================================
+
+XLIB asm_z80_delay_ms
+
+LIB asm_z80_delay_tstate
+
+asm_z80_delay_ms:
+
+   ; enter : hl = milliseconds (0 = 65536)
+   ;
+   ; uses  : af, bc, de, hl
+
+   ex de,hl
+   
+   ld hl,(__clock_freq / 1000) - 49
+   call asm_z80_delay_tstate
+   
+   ex de,hl
+
+ms_loop:
+
+   dec hl
+
+   ld a,h
+   or l
+
+   jr nz, not_done             ; must slow down the return path
+   ret
+
+not_done:
+
+   ret z                       ; never taken
+   ex de,hl
+   
+   ld hl,(__clock_freq / 1000) - 61
+   call asm_z80_delay_tstate
+   
+   ex de,hl
+   jr ms_loop
