00a86378608a4f5084cecc18a0783b9a76c610d1
suborb <dom@suborbital.org.uk>
2014-03-20 20:58:29 +0000

    Don't update destdir...use built in translations..

--- config.sh
+++ config.sh
@@ -2,19 +2,16 @@
 
 # Some script hackery to allow global installs
 
+# Ignored now
 destdir=$1
+
 default=$2
 
-# Convert absolute paths to Windows notation in MinGW 
-# pwd returns /c/xxx, but MinGW needs c:/xxx in the config files
-if uname | grep -q ^MINGW32 ; then
-   destdir=`echo $1 | sed 's?^/\([a-z]\)/?\1:/?'`
-fi
 
 rm -f lib/config/*.cfg
 for file in `ls lib/config/*.lnx`; do
    dest=`echo $file | sed 's/\.lnx/\.cfg/g'`
-   sed "s?DESTDIR\/?$destdir?g" < $file > $dest
+   cp $file $dest
 done
 
 if [ x$default != x ]; then
