333bfeb28a789d75b5a5654e3863d3a423057c1e
aralbrec <alvin_albrecht@hotmail.com>
2014-03-26 05:25:58 +0000

    sets fzx mode, determining how foreground text is mixed with background pixels

---
+++ libsrc/_DEVELOPMENT/font/fzx/z80/asm_fzx_mode.asm
@@ -0,0 +1,32 @@
+
+; ===============================================================
+; aralbrec - Mar 2014
+; ===============================================================
+;
+; int fzx_mode(int mode)
+;
+; Set the fzx printing mode to OR, XOR or RESET
+;
+; ===============================================================
+
+XLIB asm_fzx_mode
+
+XDEF fzx_mode_or, fzx_mode_xor, fzx_mode_reset
+XREF __fzx_inst_0, __fzx_inst_1, fzx_inst_2
+
+asm_fzx_mode:
+
+   ; enter : hl = {fzx_mode_or, fzx_mode_xor, fzx_mode_reset}
+   ;
+   ; exit  : hl = old fzx mode
+   ;
+   ; uses  : de, hl
+   
+   ld de,(__fzx_inst_0)        ; get current mode
+   
+   ld (__fzx_inst_0),hl        ; store new mode
+   ld (__fzx_inst_1),hl
+   ld (__fzx_inst_2),hl
+   
+   ex de,hl                    ; return old mode
+   ret
