1543f2fec54d23c6eb8c92fd83f996a38327fe7f
aralbrec <alvin_albrecht@hotmail.com>
2014-03-31 22:14:39 +0000

    allow line editing from stdin

--- libsrc/stdio/fgets.c
+++ libsrc/stdio/fgets.c
@@ -4,7 +4,7 @@
  *      djm 4/5/99
  *
  * --------
- * $Id: fgets.c,v 1.3 2013-06-07 14:12:57 stefano Exp $
+ * $Id: fgets.c,v 1.4 2014-03-31 22:14:39 aralbrec Exp $
  */
 
 #define ANSI_STDIO
@@ -16,9 +16,11 @@ char *fgets(unsigned char *s,int n,FILE *f)
     unsigned char *p=s;
     int	k=0;
     if(n<=0) return s;
-//    if (fchkstd(f) == -1 ) {	/* i.e. stdin */
-//	    return(fgets_cons(s,n));
-//    }
+    
+if (fchkstd(f) == -1 ) {	/* i.e. stdin */
+   return(fgets_cons(s,n));
+}
+
     while (--n) {
 /* Handle those annoying CR/LF conventions */
 	if (k=='\n' || k=='\r') break;
