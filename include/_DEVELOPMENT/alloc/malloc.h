00b7c0a1f9fbe276b74b2584509b2085ccec9203
aralbrec <alvin_albrecht@hotmail.com>
2014-04-12 05:41:55 +0000

    heap_info added

--- include/_DEVELOPMENT/alloc/malloc.h
+++ include/_DEVELOPMENT/alloc/malloc.h
@@ -4,6 +4,18 @@
 
 #include <stddef.h>
 
+// DATA STRUCTURES
+
+typedef struct heap_info_s
+{
+
+   void   *address;
+   size_t  size;
+   int     type;     // 0 = HEADER, 1 = ALLOCATED, 2 = FREE
+
+} heap_info_t;
+
+
 #if __SDCC | __SDCC_IX | __SDCC_IY
 
 // SDCC
@@ -15,6 +27,7 @@ extern void   *heap_alloc_fixed(void *heap, void *p, size_t size);
 extern void   *heap_calloc(void *heap, size_t nmemb, size_t size);
 extern void   *heap_destroy(void *heap);
 extern void    heap_free(void *heap, void *p);
+extern void    heap_info(void *heap, void *callback);
 extern void   *heap_init(void *heap, size_t size);
 extern void   *heap_realloc(void *heap, void *p, size_t size);
 extern void   *memalign(size_t alignment, size_t size);
@@ -26,6 +39,7 @@ extern void   *heap_alloc_aligned_unlocked(void *heap, size_t alignment, size_t 
 extern void   *heap_alloc_fixed_unlocked(void *heap, void *p, size_t size);
 extern void   *heap_calloc_unlocked(void *heap, size_t nmemb, size_t size);
 extern void    heap_free_unlocked(void *heap, void *p);
+extern void    heap_info_unlocked(void *heap, void *callback);
 extern void   *heap_realloc_unlocked(void *heap, void *p, size_t size);
 extern void   *memalign_unlocked(size_t alignment, size_t size);
 extern int     posix_memalign_unlocked(void **memptr, size_t alignment, size_t size);
@@ -57,6 +71,7 @@ extern void   __LIB__              *heap_alloc_fixed(void *heap, void *p, size_t
 extern void   __LIB__              *heap_calloc(void *heap, size_t nmemb, size_t size);
 extern void   __LIB__ __FASTCALL__ *heap_destroy(void *heap);
 extern void   __LIB__               heap_free(void *heap, void *p);
+extern void   __LIB__               heap_info(void *heap, void *callback);
 extern void   __LIB__              *heap_init(void *heap, size_t size);
 extern void   __LIB__              *heap_realloc(void *heap, void *p, size_t size);
 extern void   __LIB__              *memalign(size_t alignment, size_t size);
@@ -68,6 +83,7 @@ extern void   __LIB__              *heap_alloc_aligned_unlocked(void *heap, size
 extern void   __LIB__              *heap_alloc_fixed_unlocked(void *heap, void *p, size_t size);
 extern void   __LIB__              *heap_calloc_unlocked(void *heap, size_t nmemb, size_t size);
 extern void   __LIB__               heap_free_unlocked(void *heap, void *p);
+extern void   __LIB__               heap_info_unlocked(void *heap, void *callback);
 extern void   __LIB__              *heap_realloc_unlocked(void *heap, void *p, size_t size);
 extern void   __LIB__              *memalign_unlocked(size_t alignment, size_t size);
 extern int    __LIB__               posix_memalign_unlocked(void **memptr, size_t alignment, size_t size);
@@ -96,6 +112,7 @@ extern void   __LIB__ __CALLEE__   *heap_alloc_aligned_callee(void *heap, size_t
 extern void   __LIB__ __CALLEE__   *heap_alloc_fixed_callee(void *heap, void *p, size_t size);
 extern void   __LIB__ __CALLEE__   *heap_calloc_callee(void *heap, size_t nmemb, size_t size);
 extern void   __LIB__ __CALLEE__    heap_free_callee(void *heap, void *p);
+extern void   __LIB__ __CALLEE__    heap_info_callee(void *heap, void *callback);
 extern void   __LIB__ __CALLEE__   *heap_init_callee(void *heap, size_t size);
 extern void   __LIB__ __CALLEE__   *heap_realloc_callee(void *heap, void *p, size_t size);
 extern void   __LIB__ __CALLEE__   *memalign_callee(size_t alignment, size_t size);
@@ -107,6 +124,7 @@ extern void   __LIB__ __CALLEE__   *heap_alloc_aligned_unlocked_callee(void *hea
 extern void   __LIB__ __CALLEE__   *heap_alloc_fixed_unlocked_callee(void *heap, void *p, size_t size);
 extern void   __LIB__ __CALLEE__   *heap_calloc_unlocked_callee(void *heap, size_t nmemb, size_t size);
 extern void   __LIB__ __CALLEE__    heap_free_unlocked_callee(void *heap, void *p);
+extern void   __LIB__ __CALLEE__    heap_info_unlocked_callee(void *heap, void *callback);
 extern void   __LIB__ __CALLEE__   *heap_realloc_unlocked_callee(void *heap, void *p, size_t size);
 extern void   __LIB__ __CALLEE__   *memalign_unlocked_callee(size_t alignment, size_t size);
 extern int    __LIB__ __CALLEE__    posix_memalign_unlocked_callee(void **memptr, size_t alignment, size_t size);
@@ -131,6 +149,7 @@ extern void   __LIB__ __CALLEE__   *realloc_callee(void *p, size_t size);
 #define heap_alloc_fixed(a,b,c)     heap_alloc_fixed_callee(a,b,c)
 #define heap_calloc(a,b,c)          heap_calloc_callee(a,b,c)
 #define heap_free(a,b)              heap_free_callee(a,b)
+#define heap_info(a,b)              heap_info_callee(a,b)
 #define heap_init(a,b)              heap_init_callee(a,b)
 #define heap_realloc(a,b,c)         heap_realloc_callee(a,b,c)
 #define memalign(a,b)               memalign_callee(a,b)
@@ -142,6 +161,7 @@ extern void   __LIB__ __CALLEE__   *realloc_callee(void *p, size_t size);
 #define heap_alloc_fixed_unlocked(a,b,c)     heap_alloc_fixed_unlocked_callee(a,b,c)
 #define heap_calloc_unlocked(a,b,c)          heap_calloc_unlocked_callee(a,b,c)
 #define heap_free_unlocked(a,b)              heap_free_unlocked_callee(a,b)
+#define heap_info_unlocked(a,b)              heap_info_unlocked_callee(a,b)
 #define heap_realloc_unlocked(a,b,c)         heap_realloc_unlocked_callee(a,b,c)
 #define memalign_unlocked(a,b)               memalign_unlocked_callee(a,b)
 #define posix_memalign_unlocked(a,b,c)       posix_memalign_unlocked_callee(a,b,c)
