a68b5e4fe5509e96c107dcc8eb9356bf16a7b46e
zx70 <stefano_bodrato@hotmail.com>
2014-04-16 06:16:15 +0000

    making the x11 libraries case sensitive

---
+++ libsrc/graphics/x11/XestroyWindow.c
@@ -0,0 +1,12 @@
+/*
+	Minimal Xlib port
+	Stefano Bodrato, 14/3/2007
+	
+	$Id: XestroyWindow.c,v 1.1 2014-04-16 06:16:41 stefano Exp $
+*/
+
+#define _BUILDING_X
+#include <X11/Xlib.h>
+
+int XDestroyWindow(Display *display, Window win) {
+}
