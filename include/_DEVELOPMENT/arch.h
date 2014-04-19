37cc59187b23540bc2121f708c11b1726f654b44
aralbrec <alvin_albrecht@hotmail.com>
2014-04-19 15:59:28 +0000

    arch header for target details

---
+++ include/_DEVELOPMENT/arch.h
@@ -0,0 +1,23 @@
+
+#ifndef _ARCH_H
+#define _ARCH_H
+
+// machine constants
+
+#ifdef __SPECTRUM
+
+   #ifndef __MODEL
+      #define __MODEL 48
+   #endif
+   
+   #if __MODEL = 48
+   
+      #ifndef __CPU_FREQ
+         #define __CPU_FREQ 3500000
+      #endif
+   
+   #endif
+
+#endif
+
+#endif
