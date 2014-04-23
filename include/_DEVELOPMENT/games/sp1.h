7271ba5cebd667a8ccca57174e816d6725c0582b
aralbrec <alvin_albrecht@hotmail.com>
2014-04-23 04:23:12 +0000

    sp1 modified to work with the new clib... placed in a temp directory because it will see a rewrite before it is added to a permanent location

---
+++ include/_DEVELOPMENT/games/sp1.h
@@ -0,0 +1,27 @@
+
+#ifndef _SP1_H
+#define _SP1_H
+
+///////////////////////////////////////////////////////////
+//                  SPRITE PACK v3.0                     //
+//            aralbrec - April / May 2006                //
+///////////////////////////////////////////////////////////
+//                                                       //
+//    Software sprite engine for bitmapped displays      //
+//                                                       //
+//    * Differential screen update                       //
+//    * Any number and any size of sprites               //
+//    * Clipping to character cell boundaries            //
+//    * Background tile animation                        //
+//                                                       //
+//    Currently supported platforms:                     //
+//                                                       //
+//    spectrum, ts2068, zx81 hi-res                      //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+#ifdef __SPECTRUM
+#include <games/sp1/sp1_spectrum.h>
+#endif
+
+#endif
