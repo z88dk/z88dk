2fe7e6929a7be499f0033a742621114caba4e46e
suborb <dom@suborbital.org.uk>
2014-05-04 21:56:20 +0000

    Command line parsing adde.

--- libsrc/_DEVELOPMENT/target/cpm/startup/cpm_crt0.asm
+++ libsrc/_DEVELOPMENT/target/cpm/startup/cpm_crt0.asm
@@ -1,5 +1,7 @@
+; CPM crt0 for new library
+;
+;	#pragma output noprotectmsdos - strip the MS-DOS protection header
 
-; zx spectrum crt0 for testing purposes
 
 ;; origin ;;;;;;;;;;;;;;;;;;;;;
 
@@ -33,16 +35,92 @@ defc __HAVE_FILE_STDERR   = 1  ; set to 0 if stderr not available
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 start:
-   ld (__sp),sp
+IF !DEFINED_noprotectmsdos
+	defb	$eb,$04		;DOS protection... JMPS LABE
+	ex	    de,hl
+	jp	    begin
+	defb	$b4,$09		;DOS protection... MOV AH,9
+	defb	$ba
+	defw	dosmessage	;DOS protection... MOV DX,OFFSET dosmessage
+	defb	$cd,$21		;DOS protection... INT 21h.
+	defb	$cd,$20		;DOS protection... INT 20h.
+
+dosmessage:
+	defm	"This program is for a CP/M system."
+	defb	13,10,'$'
+
+begin:
+ENDIF
+   ld       (__sp),sp
 
 
    INCLUDE "../crt_init.asm"   ; intialize crt state
 
-   ; TODO: command line arguments/redirection etc
+    ; Command line arguments - push points to argv[n] onto the stack
+	ld	    hl,0	;NULL pointer at end, just in case
+	push	hl
+	ld	    hl,$80
+	ld	    a,(hl)
+	ld	    b,0
+	and	    a
+	jr	    z,argv_done
+	ld	    c,a
+	add	    hl,bc	;now points to the end
+    ; Try to find the end of the arguments
+argv_loop_1:
+	ld	    a,(hl)
+	cp	    ' '
+	jr	    nz,argv_loop_2
+	ld	    (hl),0
+	dec	    hl
+	dec	    c
+	jr	    nz,argv_loop_1
+; We've located the end of the last argument, try to find the start
+argv_loop_2:
+	ld	    a,(hl)
+	cp	    ' '
+	jr	    nz,argv_loop_3
+	inc	    hl
+
+; TODO: Redirection (when we have file i/o)
+
+	push	hl
+	inc	    b
+	dec	    hl
+
+; skip extra blanks
+argv_zloop:
+	ld	    (hl),0
+	dec	    c
+	jr	    z,argv_done
+	dec	    hl
+	ld	    a,(hl)
+	cp	    ' '
+	jr	    z,argv_zloop
+	inc     c
+	inc     hl
+
+argv_loop_3:
+	dec	    hl
+	dec	    c
+	jr	    nz,argv_loop_2
+
+argv_done:
+	ld	    hl,__null_string	;name of program (NULL)
+	push	hl
+	inc	    b
+	ld	    hl,0
+	add	    hl,sp	;address of argv
+	ld	    c,b
+	ld	    b,0
+
 
    ; call user program
-
-   call _main                  ; hl holds return status
+    push    bc  ; argc
+    push    hl  ; argv
+    call    _main                  ; hl holds return status
+    pop     bc
+    pop     bc
 
    INCLUDE "../crt_exit.asm"   ; run exit stack
    
@@ -50,11 +128,11 @@ __Exit:
 
    INCLUDE "../crt_cleanup.asm"  ; close files 
 
-   ld c,l
-   ld b,h                      ; return status to basic
+    ld      c,l
+    ld      b,h                      ; return status to basic
 
-   ld sp,(__sp)
-   jp 0
+    ld      sp,(__sp)
+    jp      0
    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -63,6 +141,7 @@ INCLUDE "../crt_stubs.asm"     ; crt stubs for unimplemented lib functions
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+__null_string:
    defw 0
    
 __FILE_STDIN:
