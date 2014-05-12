5a7bb040a1370842ecdd938f6460473f62dc6498
aralbrec <alvin_albrecht@hotmail.com>
2014-05-12 02:15:23 +0000

    name change lr_sccz80 to sccz80

--- libsrc/_DEVELOPMENT/makelib.bat
+++ libsrc/_DEVELOPMENT/makelib.bat
@@ -1,18 +1,18 @@
-copy /Y target\spectrum\clib_cfg.asm .
-copy /Y target\spectrum\clib_target_cfg.asm .
-z80asm -d -ns -nm -Mo -xzx_asm @target/spectrum/library/zx_asm.lst
+copy /Y target\zx\clib_cfg.asm .
+copy /Y target\zx\clib_target_cfg.asm .
+z80asm -d -ns -nm -Mo -xzx_asm @target/zx/library/zx_asm.lst
 move /Y zx_asm.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -xzx_sccz80 @target/spectrum/library/zx_lr_sccz80.lst
+z80asm -d -ns -nm -Mo -xzx_sccz80 @target/zx/library/zx_sccz80.lst
 move /Y zx_sccz80.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -xzx_sdcc_ix -D__SDCC_IX @target/spectrum/library/zx_sdcc_ix.lst
+z80asm -d -ns -nm -Mo -xzx_sdcc_ix -D__SDCC_IX @target/zx/library/zx_sdcc_ix.lst
 move /Y zx_sdcc_ix.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -xzx_sdcc_iy -D__SDCC_IY -IXIY @target/spectrum/library/zx_sdcc_iy.lst
+z80asm -d -ns -nm -Mo -xzx_sdcc_iy -D__SDCC_IY -IXIY @target/zx/library/zx_sdcc_iy.lst
 move /Y zx_sdcc_iy.lib lib
 del /S *.o
 del /S *.err
