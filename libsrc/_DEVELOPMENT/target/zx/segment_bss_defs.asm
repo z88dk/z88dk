9d72859ce713e485adc5cb7b793e09e0fe0e3248
aralbrec <alvin_albrecht@hotmail.com>
2014-05-19 17:13:14 +0000

    add kempston and amx mouse variables to target segments

---
+++ libsrc/_DEVELOPMENT/target/zx/segment_bss_defs.asm
@@ -0,0 +1,24 @@
+
+; target bss segment storage allocation
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; input
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; amx mouse coordinate will be initialized to (0,0)
+
+PUBLIC __input_amx_mouse_x, __input_amx_mouse_y
+
+__input_amx_mouse_x:           defs 2
+__input_amx_mouse_y:           defs 2
+
+; kempston mouse coordinate will be initialized to (0,0)
+
+PUBLIC __input_kempston_mouse_x, __input_kempston_mouse_y
+PUBLIC __input_kempston_mouse_rawx, __input_kempston_mouse_rawy
+
+__input_kempston_mouse_x:      defs 2
+__input_kempston_mouse_y:      defs 2
+__input_kempston_mouse_rawx:   defs 1
+__input_kempston_mouse_rawy:   defs 1
+
