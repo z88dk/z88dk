2db749de3b553d205253cb77ba5b073b32d362d5
aralbrec <alvin_albrecht@hotmail.com>
2014-05-20 05:20:54 +0000

    amx mouse deltas added to static segment

--- libsrc/_DEVELOPMENT/target/zx/segment_data_defb.asm
+++ libsrc/_DEVELOPMENT/target/zx/segment_data_defb.asm
@@ -16,3 +16,15 @@ __sound_bit_state:             defb 7
 PUBLIC _crt_attr
 
 _crt_attr:                     defb 56
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; input
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+PUBLIC __input_amx_mouse_dx, __input_amx_mouse_dy
+
+; amx mouse moves one pixel per interrupt
+
+__input_amx_mouse_dx:          defw $0010
+__input_amx_mouse_dy:          defw $0010
+
