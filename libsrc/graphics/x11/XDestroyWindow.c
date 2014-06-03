a95bc83bf5e7e580237e72229da6f05e535ab5de
zx70 <stefano_bodrato@hotmail.com>
2014-06-03 12:35:52 +0000

    *** empty log message ***

--- libsrc/graphics/x11/XDestroyWindow.c
+++ libsrc/graphics/x11/XDestroyWindow.c
@@ -2,7 +2,7 @@
 	Minimal Xlib port
 	Stefano Bodrato, 14/3/2007
 	
-	$Id: XestroyWindow.c,v 1.1 2014-04-16 06:16:41 stefano Exp $
+	$Id: XDestroyWindow.c,v 1.1 2014-06-03 12:37:19 stefano Exp $
 */
 
 #define _BUILDING_X
