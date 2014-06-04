41109b95825b6db97b487458f1b10e9e6946d33a
aralbrec <alvin_albrecht@hotmail.com>
2014-06-04 15:42:29 +0000

    add l_ltu_bc_de, l_andc_hl_de

---
+++ libsrc/_DEVELOPMENT/l/z80/l_andc_hl_de.asm
@@ -0,0 +1,23 @@
+
+PUBLIC l_andc_hl_de
+
+l_andc_hl_de:
+
+   ; enter : hl, de
+   ;
+   ; exit  : hl = hl & (~de)
+   ;         carry reset
+   ;
+   ; uses  : af, hl
+   
+   ld a,e
+   cpl
+   and l
+   ld l,a
+   
+   ld a,d
+   cpl
+   and h
+   ld h,a
+   
+   ret
