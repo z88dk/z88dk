60d60442b7d4540362b92e5f6cf9b944a45e9a3c
pauloscustodio <pauloscustodio@gmail.com>
2014-06-29 22:26:16 +0000

    remove log comment

--- src/z80asm/deffile.h
+++ src/z80asm/deffile.h
@@ -14,7 +14,7 @@ Copyright (C) Paulo Custodio, 2011-2014
 
 Define file writing - list of all global address symbols after link phase in DEFC format
 
-$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/deffile.h,v 1.3 2014-01-11 01:29:40 pauloscustodio Exp $
+$Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/deffile.h,v 1.4 2014-06-29 22:26:16 pauloscustodio Exp $
 
 */
 
@@ -23,18 +23,3 @@ $Header: /home/dom/z88dk-git/cvs/z88dk/src/z80asm/deffile.h,v 1.3 2014-01-11 01:
 #include "xmalloc.h"   /* before any other include */
 
 extern void write_def_file( void );
-
-/*
-* $Log: deffile.h,v $
-* Revision 1.3  2014-01-11 01:29:40  pauloscustodio
-* Extend copyright to 2014.
-* Move CVS log to bottom of file.
-*
-* Revision 1.2  2013/12/15 13:18:33  pauloscustodio
-* Move memory allocation routines to lib/xmalloc, instead of glib,
-* introduce memory leak report on exit and memory fence check.
-* 
-* Revision 1.1  2013/06/16 20:14:39  pauloscustodio
-* Move deffile writing to deffile.c, remove global variable deffile
-* 
-*/
