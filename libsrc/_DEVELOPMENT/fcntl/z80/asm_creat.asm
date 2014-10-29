cdb98ec7b76cfb6a9f1d246073cd2eefb707952f
aralbrec <alvin_albrecht@hotmail.com>
2014-10-29 05:03:16 +0000

    creat() implemented

---
+++ libsrc/_DEVELOPMENT/fcntl/z80/asm_creat.asm
@@ -0,0 +1,44 @@
+
+; ===============================================================
+; October 2014
+; ===============================================================
+; 
+; int creat(const char *path, mode_t mode)
+;
+; Equivalent to open(path, O_WRONLY|O_CREAT|O_TRUNC, mode)
+;
+; ===============================================================
+
+SECTION seg_code_fcntl
+
+PUBLIC asm_creat
+
+EXTERN asm0_vopen, l_inc_sp
+
+asm_creat:
+
+   ; enter : de = char *path
+   ;         bc = int mode
+   ;
+   ; exit  : success
+   ;
+   ;            hl = int fd
+   ;            de = FDSTRUCT *
+   ;            carry reset
+   ;
+   ;         fail
+   ;
+   ;            hl = -1
+   ;            carry set, errno set
+   ;
+   ; uses  : all except iy
+
+   push bc                     ; save mode
+   
+   ld hl,0
+   add hl,sp                   ; hl = void *arg
+   
+   ld bc,$52                   ; O_WRONLY|O_CREAT|O_TRUNC
+   call asm0_vopen
+   
+   jp l_inc_sp - 2             ; pop item from stack
