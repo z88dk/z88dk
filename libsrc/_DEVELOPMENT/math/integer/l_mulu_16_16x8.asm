f082de551a6f57c5a8ab74125408f5671c499239
aralbrec <alvin_albrecht@hotmail.com>
2014-11-17 00:51:29 +0000

    math related primitives moved to new directory

---
+++ libsrc/_DEVELOPMENT/math/integer/l_mulu_16_16x8.asm
@@ -0,0 +1,23 @@
+
+INCLUDE "clib_cfg.asm"
+
+SECTION seg_code_math
+
+PUBLIC l_mulu_16_16x8
+
+   ; compute:  hl = hl * e
+   ; alters :  af, bc, de, hl
+
+IF __CLIB_OPT_IMATH <= 50
+
+   EXTERN l_small_mul_16_16x8
+   defc l_mulu_16_16x8 = l_small_mul_16_16x8 - 1
+
+ENDIF
+
+IF __CLIB_OPT_IMATH > 50
+
+   EXTERN l_fast_mulu_24_16x8
+   defc l_mulu_16_16x8 = l_fast_mulu_24_16x8
+
+ENDIF
