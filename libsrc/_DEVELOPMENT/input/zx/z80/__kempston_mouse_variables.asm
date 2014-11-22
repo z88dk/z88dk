db05db310a86d4ce18bea8b8789992b4690d6d87
aralbrec <alvin_albrecht@hotmail.com>
2014-11-22 17:46:32 +0000

    separate variables into independent file

---
+++ libsrc/_DEVELOPMENT/input/zx/z80/__kempston_mouse_variables.asm
@@ -0,0 +1,13 @@
+
+SECTION seg_bss_input
+
+PUBLIC __input_kempston_mouse_x
+PUBLIC __input_kempston_mouse_y
+PUBLIC __input_kempston_mouse_rawx
+PUBLIC __input_kempston_mouse_rawy
+
+__input_kempston_mouse_x:      defw 0
+__input_kempston_mouse_y:      defw 0
+__input_kempston_mouse_rawx:   defb 0
+__input_kempston_mouse_rawy:   defb 0
+
--- libsrc/_DEVELOPMENT/input/zx/z80/asm_in_mouse_amx.asm
+++ libsrc/_DEVELOPMENT/input/zx/z80/asm_in_mouse_amx.asm
@@ -64,19 +64,3 @@ button_loop:
 button_table:
 
    defb $07, $03, $05, $01, $06, $02, $04, $00
-
-; =============================================================
-
-SECTION seg_bss_input
-
-PUBLIC __input_amx_mouse_x
-PUBLIC __input_amx_mouse_y
-PUBLIC __input_amx_mouse_dx
-PUBLIC __input_amx_mouse_dy
-
-__input_amx_mouse_x:           defw 0
-__input_amx_mouse_y:           defw 0
-__input_amx_mouse_dx:          defw 0
-__input_amx_mouse_dy:          defw 0
-
-; =============================================================
