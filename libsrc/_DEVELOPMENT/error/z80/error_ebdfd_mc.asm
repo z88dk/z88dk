1d1b9971421ac26ecc4e05d9102d449842de01dc
aralbrec <alvin_albrecht@hotmail.com>
2014-11-22 05:04:26 +0000

    error EBDFD exit points

---
+++ libsrc/_DEVELOPMENT/error/z80/error_ebdfd_mc.asm
@@ -0,0 +1,48 @@
+
+INCLUDE "clib_cfg.asm"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+IF __CLIB_OPT_ERROR
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   ; verbose mode
+
+   SECTION seg_code_error
+   
+   PUBLIC error_ebdfd_mc
+   
+   EXTERN __EBDFD, errno_mc
+   
+      pop hl
+   
+   error_ebdfd_mc:
+   
+      ; set hl = -1
+      ; set carry flag
+      ; set errno = EBDFD
+      
+      ld l,__EBDFD
+      jp errno_mc
+   
+   
+   SECTION seg_rodata_error_strings
+   
+   defb __EBDFD
+   defm "EBDFD - Bad file descriptor"
+   defb 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ELSE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   SECTION seg_code_error
+   
+   PUBLIC error_ebdfd_mc
+   
+   EXTERN errno_mc
+   
+   defc error_ebdfd_mc = errno_mc - 2
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ENDIF
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ libsrc/_DEVELOPMENT/error/z80/error_ebdfd_zc.asm
@@ -0,0 +1,43 @@
+
+INCLUDE "clib_cfg.asm"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+IF __CLIB_OPT_ERROR
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   ; verbose mode
+
+   SECTION seg_code_error
+   
+   PUBLIC error_ebdfd_zc
+   
+   EXTERN error_ebdfd_mc
+   
+      pop hl
+   
+   error_ebdfd_zc:
+   
+      ; set hl = 0
+      ; set carry flag
+      ; set errno = EBDFD
+      
+      call error_ebdfd_mc
+      
+      inc hl
+      ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ELSE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   SECTION seg_code_error
+   
+   PUBLIC error_ebdfd_zc
+   
+   EXTERN errno_zc
+   
+   defc error_ebdfd_zc = errno_zc - 2
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ENDIF
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
