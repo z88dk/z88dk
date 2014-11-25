d5782757270ad5cbc24c3930af10514758595d36
aralbrec <alvin_albrecht@hotmail.com>
2014-11-25 08:39:49 +0000

    random number generator code moved to a separate function to allow library code to generate random numbers without altering the program seed

--- libsrc/_DEVELOPMENT/stdlib/z80/asm_rand.asm
+++ libsrc/_DEVELOPMENT/stdlib/z80/asm_rand.asm
@@ -6,14 +6,6 @@
 ; int rand(void)
 ;
 ; Generates a random number in the range 0 .. RAND_MAX = 32767
-; 
-; This random number generator is a 32-bit XorShift RNG as
-; described by Marsaglia:
-;
-; http://www.jstatsoft.org/v08/i14/paper
-;
-; The triple used is (L,R,L) = (8,9,23) which is very quickly
-; implemented on the z80.
 ;
 ; ===============================================================
 
@@ -21,69 +13,29 @@ SECTION seg_code_stdlib
 
 PUBLIC asm_rand
 
-EXTERN __stdlib_seed
+EXTERN __stdlib_seed, asm_random_uniform_xor_32
 
 asm_rand:
 
-   ; XorShift RNG using triple (8,9,23)
+   ; Generate random number in [0, 32767]
+   ;
+   ; Note:  The random sequence generator is kept separate
+   ; from the standard C subroutine so that other areas of
+   ; the clib can generate random numbers without affecting
+   ; the user program's seed.
    ;
    ; exit : hl = random number in [0, 32767]
    ;
-   ; uses : af, de, hl
+   ; uses : af, bc, de, hl
 
+   ; get current seed
+   
    ld hl,(__stdlib_seed)
    ld de,(__stdlib_seed + 2)
 
-   ; dehl ^= dehl << 8
-
-   ld a,d
-   xor e
-   ld d,a
-
-   ld a,e
-   xor h
-   ld e,a
-
-   ld a,h
-   xor l
-   ld h,a
-
-   ; dehl ^= dehl >> 9
-
-   ld a,e
-   rra
-   ld a,h
-   rra
-   xor l
-   ld l,a
-
-   ld a,d
-   rra
-   ld a,e
-   rra
-   xor h
-   ld h,a
-
-   ld a,d
-   srl a
-   xor e
-   ld e,a
-
-   ; dehl ^= dehl << 23
-
-   ld a,h
-   rra
-   ld a,l
-   rra
-   xor d
-   ld d,a
-
-   ld a,l
-   rra
-   ld a,0
-   rra
-   xor e
-   ld e,a
+   ; generate next prng in sequence
+   
+   call asm_random_uniform_xor_32  ; marsaglia xor generator
 
    ; store new seed
 
