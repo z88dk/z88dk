283d64d55e03bd4aeea69e065567059f61c26511
aralbrec <alvin_albrecht@hotmail.com>
2014-12-12 00:13:10 +0000

    move fonts to font subdir

--- libsrc/_DEVELOPMENT/target/zx/driver/terminal/font_64_4x8/_font_64_default.asm
+++
@@ -1,8 +0,0 @@
-
-SECTION rodata_fcntl
-
-PUBLIC _font_64_default
-
-_font_64_default:
-
-BINARY "target/zx/driver/terminal/font_64_4x8/font_64_default.bin"
--- libsrc/_DEVELOPMENT/target/zx/driver/terminal/font_64_4x8/readme.txt
+++
@@ -1,6 +0,0 @@
-
-Collection of 4x8 fonts for the 64 column terminal.
-
-* font_64_def.bin
-
-The default 64 column font by Dominic Morris.
