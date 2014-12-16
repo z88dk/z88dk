c9e9d4c0b9b97e260119749372fb669c1dc33ee4
zx70 <stefano_bodrato@hotmail.com>
2014-12-16 20:13:56 +0000

    Alvin's rewrite of l_long_case

--- libsrc/z80_crt0s/crt0_long/l_long_case.asm
+++ libsrc/z80_crt0s/crt0_long/l_long_case.asm
@@ -1,48 +1,79 @@
 ;       Small C+ Z80 Run time library
-;       The new case statement..maybe things will work now!
-;       13/10/98
+;       13/10/98 The new case statement..maybe things will work now!
+;       11/2014  Removed use of ix
 
-        XLIB    l_long_case
+SECTION code_l_sccz80
 
+PUBLIC l_long_case
 
-;Case statement for longs
-;Enter with dehl=number to check against..
+l_long_case:
 
+   ; Case statement for longs
+   ; Enter with dehl = number to check against..
 
-.l_long_case
-        pop     ix              ;switch table
-.swloop
-	ld	a,(ix+0)
-	or	(ix+1)
-        inc     ix
-        inc     ix
-        jr      z,swend         ;enod of table
-;Now, have to check our values
-        ld      bc,4
-        add     ix,bc           ;points to next entry,,
-        ld      a,(ix-4)
-        cp      l
-        jr      nz,swloop
-        ld      a,(ix-3)
-        cp      h
-        jr      nz,swloop
-        ld      a,(ix-2)
-        cp      e
-        jr      nz,swloop
-	ld	a,(ix-1)
-        cp      d
-        jr      nz,swloop
-;Have had a match here...so, load up address, and get there
-IF Z80S183
-	ld	e,(ix-6)	;low byte
-	ld	d,(ix-5)	;high
-	push	de
-	pop	ix
-ELSE
-        ld      a,(ix-6)        ;low byte
-        ld      ixh,(ix-5)
-        ld      ixl,a
-ENDIF
-.swend
-        jp      (ix)
-        
+   ld c,l
+   ld b,h
+   
+   ; debc = number to check against
+
+   pop hl                      ; hl = & switch table
+
+loop:
+
+   ld a,(hl)
+   inc hl
+   or (hl)
+   inc hl
+   
+   jr z, end                   ; if end of table
+   
+   ld a,(hl)
+   inc hl
+   
+   cp c
+   jr nz, loop_3
+   
+   ld a,(hl)
+   inc hl
+   
+   cp b
+   jr nz, loop_2
+   
+   ld a,(hl)
+   inc hl
+   
+   cp e
+   jr nz, loop_1
+   
+   ld a,(hl)
+   inc hl
+   
+   cp d
+   jr nz, loop
+
+   ; match
+   
+   ld bc,-5
+   add hl,bc
+   
+   ld a,(hl)
+   dec hl
+   ld l,(hl)
+   ld h,a
+   
+end:
+
+   jp (hl)
+
+loop_3:
+
+   inc hl
+
+loop_2:
+
+   inc hl
+
+loop_1:
+
+   inc hl
+   jr loop
