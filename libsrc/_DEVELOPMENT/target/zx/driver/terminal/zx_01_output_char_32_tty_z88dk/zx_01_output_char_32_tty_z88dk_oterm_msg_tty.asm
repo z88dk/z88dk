42a6310e808aa564418c9fa25cbd9e642342dcab
aralbrec <alvin_albrecht@hotmail.com>
2014-12-16 05:32:11 +0000

    all tty interpretters now use a common state machine

--- libsrc/_DEVELOPMENT/target/zx/driver/terminal/zx_01_output_char_32_tty_z88dk/zx_01_output_char_32_tty_z88dk_oterm_msg_tty.asm
+++ libsrc/_DEVELOPMENT/target/zx/driver/terminal/zx_01_output_char_32_tty_z88dk/zx_01_output_char_32_tty_z88dk_oterm_msg_tty.asm
@@ -3,8 +3,8 @@ SECTION code_fcntl
 
 PUBLIC zx_01_output_char_32_tty_z88dk_oterm_msg_tty
 
-EXTERN l_offset_ix_de, asm_tty_z88dk
-EXTERN tty_execute_action
+EXTERN l_offset_ix_de, zx_tty_z88dk_state_table
+EXTERN asm_tty_execute_action, l_jphl
 
 zx_01_output_char_32_tty_z88dk_oterm_msg_tty:
 
@@ -18,59 +18,75 @@ zx_01_output_char_32_tty_z88dk_oterm_msg_tty:
    ld hl,26
    call l_offset_ix_de         ; hl = & tty_state
    
-   ; hl = & tty_state
+   ; hl = & tty
    ;  c = ascii char
    
-   call asm_tty_z88dk
+   ld de,zx_tty_z88dk_state_table
+   
+   call l_jphl                 ; execute tty
+   ret c                       ; if producing a char for the terminal
+   
+   ;  a = action code
+   ; de = & parameters
 
    ld hl,action_table
-   jp tty_execute_action
+   jp asm_tty_execute_action
 
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
    ;; ACTION TABLE FOR TTY_Z88DK
-   ;;
-   ;; enter : bc = parameter
-   ;;         de = parameter
-   ;;
-   ;; exit  : if delivering char to terminal
-   ;;
-   ;;            c = ascii code
-   ;;            carry set
-   ;;
-   ;;         if not delivering char to terminal
-   ;;
-   ;;            carry reset
-   ;;
-   ;; uses  : af, bc, de, hl
    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   
+   EXTERN error_einval_znc
+   EXTERN zx_01_output_char_32_tty_02_font_address
+   EXTERN zx_01_output_char_32_tty_08_backspace
+   EXTERN zx_01_output_char_32_tty_09_tab
+   EXTERN zx_01_output_char_32_tty_11_scroll
+   EXTERN zx_01_output_char_32_tty_12_cls
+   EXTERN zx_01_output_char_32_tty_14_foreground_mask
+   EXTERN zx_01_output_char_32_tty_15_background_attr
+   EXTERN zx_01_output_char_32_tty_16_ink
+   EXTERN zx_01_output_char_32_tty_17_paper
+   EXTERN zx_01_output_char_32_tty_18_flash
+   EXTERN zx_01_output_char_32_tty_19_bright
+   EXTERN zx_01_output_char_32_tty_20_inverse
+   EXTERN zx_01_output_char_32_tty_22_at
+   EXTERN zx_01_output_char_32_tty_23_atr
+   EXTERN zx_01_output_char_32_tty_27_escape
+   EXTERN zx_01_output_char_32_tty_28_left
+   EXTERN zx_01_output_char_32_tty_29_right
+   EXTERN zx_01_output_char_32_tty_30_up
+   EXTERN zx_01_output_char_32_tty_31_down
    
 action_table:
 
-   defw ; action  1 = cls
-   defw ; action  2 = home
-   defw ; action  3 = cursor left
-   defw ; action  4 = cursor right
-   defw ; action  5 = cursor up
-   defw ; action  6 = cursor down
-   defw ; action  7 = tab x (e = x)
-   defw ; action  8 = at x,y (e = x, d = y)
-   defw ; action  9 = atr dx,dy (e = dx, d = dy)
-   defw ; action 10 = scroll
-   defw ; action 11 = scroll x (e = x)
-   defw ; action 12 = escape
-   defw ; action 13 = special_0_0
-   defw ; action 14 = special_0_1
-   defw ; action 15 = special_0_2
-   defw ; action 16 = special_0_3
-   defw ; action 17 = special_1_0 x (e = x)
-   defw ; action 18 = special_1_1 x (e = x)
-   defw ; action 19 = special_1_2 x (e = x)
-   defw ; action 20 = special_1_3 x (e = x)
-   defw ; action 21 = special_1_4 x (e = x)
-   defw ; action 22 = special_1_5 x (e = x)
-   defw ; action 23 = special_1_6 x (e = x)
-   defw ; action 24 = special_1_7 x (e = x)
-   defw ; action 25 = special_2_0 x,y (e = x, d = y)
-   defw ; action 26 = special_2_1 x,y (e = x, d = y)
-   defw ; action 27 = special_2_2 x,y (e = x, d = y)
-   defw ; action 28 = special_2_3 x,y (e = x, d = y)
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_02_font_address
+   defw error_einval_znc
+   defw error_einval_znc
+   defw error_einval_znc
+   defw error_einval_znc
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_08_backspace
+   defw zx_01_output_char_32_tty_09_tab
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_11_scroll
+   defw zx_01_output_char_32_tty_12_cls
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_14_foreground_mask   ; [ 14 = foreground mask ]
+   defw zx_01_output_char_32_tty_15_background_attr   ; [ 15 = background attr ]
+   defw zx_01_output_char_32_tty_16_ink               ; [ 16 = ink '0'..'7' ]
+   defw zx_01_output_char_32_tty_17_paper             ; [ 17 = paper '0'..'7' ]
+   defw zx_01_output_char_32_tty_18_flash             ; [ 18 = flash '0'..'1' ]
+   defw zx_01_output_char_32_tty_19_bright            ; [ 19 = bright '0'..'1' ]
+   defw zx_01_output_char_32_tty_20_inverse           ; [ 20 = inverse '0'..'1' ]
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_22_at
+   defw zx_01_output_char_32_tty_23_atr
+   defw error_einval_znc
+   defw error_einval_znc
+   defw error_einval_znc
+   defw zx_01_output_char_32_tty_27_escape
+   defw zx_01_output_char_32_tty_28_left
+   defw zx_01_output_char_32_tty_29_right
+   defw zx_01_output_char_32_tty_30_up
+   defw zx_01_output_char_32_tty_31_down
