1a4af5fb408bcef7ba1961762c7cae4f53633acc
aralbrec <alvin_albrecht@hotmail.com>
2014-12-25 06:35:29 +0000

    resetting inkey state on flush caused immediate key response in two successive scanfs separated by an fflush; this felt wrong to the user

--- libsrc/_DEVELOPMENT/target/zx/driver/terminal/zx_01_input_kbd_inkey/zx_01_input_inkey_stdio_msg_flsh.asm
+++ libsrc/_DEVELOPMENT/target/zx/driver/terminal/zx_01_input_kbd_inkey/zx_01_input_inkey_stdio_msg_flsh.asm
@@ -3,17 +3,20 @@ SECTION code_fcntl
 
 PUBLIC zx_01_input_inkey_stdio_msg_flsh
 
-EXTERN zx_01_input_inkey_proc_getk_address
+;EXTERN zx_01_input_inkey_proc_getk_address
 EXTERN console_01_input_stdio_msg_flsh
 
-zx_01_input_inkey_stdio_msg_flsh:
+defc zx_01_input_inkey_stdio_msg_flsh = console_01_input_stdio_msg_flsh
 
-   call zx_01_input_inkey_proc_getk_address  ; hl = & getk_state
-   
-   xor a
-   
-   ld (hl),a                   ; getk_state = 0
-   inc hl
-   ld (hl),a                   ; getk_lastk = 0
 
-   jp console_01_input_stdio_msg_flsh   ; forward to library
+; zx_01_input_inkey_stdio_msg_flsh:
+;
+;   call zx_01_input_inkey_proc_getk_address  ; hl = & getk_state
+;   
+;   xor a
+;   
+;   ld (hl),a                   ; getk_state = 0
+;   inc hl
+;   ld (hl),a                   ; getk_lastk = 0
+;
+;   jp console_01_input_stdio_msg_flsh   ; forward to library
