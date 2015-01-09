26efed9e7dfea21954571076de46e4ebca00b56b
aralbrec <alvin_albrecht@hotmail.com>
2015-01-09 08:32:21 +0000

    completed stddef

--- include/_DEVELOPMENT/stddef.h
+++ include/_DEVELOPMENT/stddef.h
@@ -2,24 +2,20 @@
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
-#ifndef _STDIO_H
-
-   typedef unsigned int size_t;
-   #define NULL ((void*)(0))
-
-#endif
-
 typedef int ptrdiff_t;
+
+typedef unsigned int size_t;
+
 typedef unsigned char max_align_t;
-typedef char wchar_t;
+
+typedef unsigned char wchar_t;
+
+#undef NULL
+#define NULL ((void *)(0))
 
 #ifdef __SDCC
 
    #define offsetof(t,m) __builtin_offsetof(t,m)
-
-#else
-
-   #define offsetof(t,m) (&t.m - &t)
 
 #endif
 
