77ab52cef98d18741e068924dbaeba1fa3695f96
aralbrec <alvin_albrecht@hotmail.com>
2015-01-09 08:33:38 +0000

    assert completed

---
+++ include/_DEVELOPMENT/assert.h
@@ -0,0 +1,17 @@
+
+#ifndef _ASSERT_H
+#define _ASSERT_H
+
+#include <stdio.h>
+
+#ifdef NDEBUG
+
+   #define assert(x)           ((void)0)
+
+#else
+
+   #define assert(x)           if (x == 0) fprintf(stderr, __FILE__ " line " #__LINE__ ": assert(" #exp ") failed\n");
+
+#endif
+
+#endif
