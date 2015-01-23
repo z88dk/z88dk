59704039bd0d390c4faf1113da5e6186fd19adca
aralbrec <alvin_albrecht@hotmail.com>
2015-01-23 04:00:10 +0000

    factor out line spacing calculation

--- libsrc/_DEVELOPMENT/font/fzx/z80/__fzx_puts_newline.asm
+++ libsrc/_DEVELOPMENT/font/fzx/z80/__fzx_puts_newline.asm
@@ -4,25 +4,25 @@ SECTION code_font_fzx
 PUBLIC __fzx_puts_newline
 PUBLIC __fzx_puts_newline_set_y
 
+EXTERN __fzx_puts_single_spacing
+
 __fzx_puts_newline:
 
+   ; execute newline by updating coordinates
+   ;
+   ; enter : ix = struct fzx_state *
+   ;
+   ; exit  : de = font height
+   ;         hl = new_y = y + font_height
+   ;
    ; uses : af, de, hl
 
    ld a,(ix+17)
    ld (ix+5),a
    ld a,(ix+18)
    ld (ix+6),a                 ; x = fzx_state.left_margin
-   
-   ld l,(ix+3)
-   ld h,(ix+4)                 ; hl = struct fzx_font *
-   
-   ld e,(hl)
-   ld d,0                      ; de = font height
-   
-   ld l,(ix+7)
-   ld h,(ix+8)                 ; hl = fzx_state.y
-   
-   add hl,de
+
+   call __fzx_puts_single_spacing
 
 __fzx_puts_newline_set_y:
 
