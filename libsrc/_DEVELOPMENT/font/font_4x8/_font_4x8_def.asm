1ea53c6f372578cbf01c09d8694e2f64e98f99a6
aralbrec <alvin_albrecht@hotmail.com>
2015-01-23 03:56:24 +0000

    font name changed

--- libsrc/_DEVELOPMENT/font/font_4x8/_font_4x8_def.asm
+++
@@ -1,8 +0,0 @@
-
-SECTION rodata_font_4x8
-
-PUBLIC _font_4x8_def
-
-_font_4x8_def:
-
-BINARY "font/font_4x8/font_4x8_def.bin"
---
+++ libsrc/_DEVELOPMENT/font/font_4x8/_font_4x8_default.asm
@@ -0,0 +1,8 @@
+
+SECTION rodata_font_4x8
+
+PUBLIC _font_4x8_default
+
+_font_4x8_default:
+
+BINARY "font/font_4x8/font_4x8_default.bin"
