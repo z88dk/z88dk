255576eea8f294565781b973b309122121829d4a
aralbrec <alvin_albrecht@hotmail.com>
2015-01-30 04:19:12 +0000

    make sdcc libraries

--- libsrc/_DEVELOPMENT/makelib.bat
+++ libsrc/_DEVELOPMENT/makelib.bat
@@ -8,6 +8,15 @@ z80asm -d -ns -nm -Mo -xzx_sccz80 @target/zx/library/zx_sccz80.lst
 move /Y zx_sccz80.lib lib
 del /S *.o
 del /S *.err
+z80asm -d -ns -nm -Mo -xzx_sdcc_ix @target/zx/library/zx_sdcc_ix.lst
+move /Y zx_sdcc_ix.lib lib
+del /S *.o
+del /S *.err
+z80asm -d -ns -nm -Mo -IXIY -xzx_sdcc_iy @target/zx/library/zx_sdcc_iy.lst
+move /Y zx_sdcc_iy.lib lib
+del /S *.o
+del /S *.err
+
 
 copy /Y target\embedded\clib_cfg.asm .
 copy /Y target\embedded\clib_target_cfg.asm .
@@ -17,5 +26,13 @@ del /S *.o
 del /S *.err
 z80asm -d -ns -nm -Mo -xembedded_sccz80 @target/embedded/library/embedded_sccz80.lst
 move /Y embedded_sccz80.lib lib
+del /S *.o
+del /S *.err
+z80asm -d -ns -nm -Mo -xembedded_sdcc_ix @target/embedded/library/embedded_sdcc_ix.lst
+move /Y embedded_sdcc_ix.lib lib
+del /S *.o
+del /S *.err
+z80asm -d -ns -nm -Mo -IXIY -xembedded_sdcc_iy @target/embedded/library/embedded_sdcc_iy.lst
+move /Y embedded_sdcc_iy.lib lib
 del /S *.o
 del /S *.err
