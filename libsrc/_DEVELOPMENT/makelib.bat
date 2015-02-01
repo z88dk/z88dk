7681b4b19f5e1e44b128596bb8181612f7424bb8
aralbrec <alvin_albrecht@hotmail.com>
2015-02-01 21:26:39 +0000

    add appropriate assemble flags to select ix or iy sdcc libraries

--- libsrc/_DEVELOPMENT/makelib.bat
+++ libsrc/_DEVELOPMENT/makelib.bat
@@ -8,11 +8,11 @@ z80asm -d -ns -nm -Mo -xzx_sccz80 @target/zx/library/zx_sccz80.lst
 move /Y zx_sccz80.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -xzx_sdcc_ix @target/zx/library/zx_sdcc_ix.lst
+z80asm -d -ns -nm -Mo -xzx_sdcc_ix -D__SDCC -D__SDCC_IX @target/zx/library/zx_sdcc_ix.lst
 move /Y zx_sdcc_ix.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -IXIY -xzx_sdcc_iy @target/zx/library/zx_sdcc_iy.lst
+z80asm -d -ns -nm -Mo -IXIY -xzx_sdcc_iy -D__SDCC -D__SDCC_IY @target/zx/library/zx_sdcc_iy.lst
 move /Y zx_sdcc_iy.lib lib
 del /S *.o
 del /S *.err
@@ -28,11 +28,11 @@ z80asm -d -ns -nm -Mo -xembedded_sccz80 @target/embedded/library/embedded_sccz80
 move /Y embedded_sccz80.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -xembedded_sdcc_ix @target/embedded/library/embedded_sdcc_ix.lst
+z80asm -d -ns -nm -Mo -xembedded_sdcc_ix -D__SDCC -D__SDCC_IX @target/embedded/library/embedded_sdcc_ix.lst
 move /Y embedded_sdcc_ix.lib lib
 del /S *.o
 del /S *.err
-z80asm -d -ns -nm -Mo -IXIY -xembedded_sdcc_iy @target/embedded/library/embedded_sdcc_iy.lst
+z80asm -d -ns -nm -Mo -IXIY -xembedded_sdcc_iy -D__SDCC -D__SDCC_IY @target/embedded/library/embedded_sdcc_iy.lst
 move /Y embedded_sdcc_iy.lib lib
 del /S *.o
 del /S *.err
