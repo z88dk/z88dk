89ed5bc77ba8894837087ec55180f26a5e300cc4
aralbrec <alvin_albrecht@hotmail.com>
2015-02-01 18:03:52 +0000

    sdcc primitives

---
+++ libsrc/_DEVELOPMENT/l/sdcc/__divuschar.asm
@@ -0,0 +1,35 @@
+
+SECTION code_l_sdcc
+
+PUBLIC __divuschar
+
+EXTERN l_divs_16_16x16
+
+__divuschar:
+
+   ; mixed 8-bit division
+   ;
+   ; enter : stack = divisor (unsigned byte), dividend (signed byte), ret
+   ;
+   ; exit  : hl = quotient
+   ;         de = remainder
+
+   ; note: the fast integer math library has a fast 8x8 divide but it
+   ;       is unknown at this time whether sdcc expects 16-bit results     
+
+   ld hl,3
+   ld d,h                      ; d = 0
+   add hl,sp
+   
+   ld e,(hl)                   ; de = signed divisor (+ve)
+   dec hl
+   ld l,(hl)                   ; l = signed dividend
+   
+   ; must promote to 16-bits
+   
+   ld a,l
+   rlca
+   sbc a,a
+   ld h,a                      ; hl = signed dividend
+   
+   jp l_divs_16_16x16
---
+++ libsrc/_DEVELOPMENT/l/sdcc/__sdcc_call_hl.asm
@@ -0,0 +1,8 @@
+
+SECTION code_l_sdcc
+
+PUBLIC __sdcc_call_hl
+
+EXTERN l_jphl
+
+defc __sdcc_call_hl = l_jphl
