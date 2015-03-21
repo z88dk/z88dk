6afd16080b5a9bc80a88c8472fe722e3ad958d67
aralbrec <alvin_albrecht@hotmail.com>
2015-03-21 00:51:29 +0000

    correct include path

--- src/copt/vscmake.bat
+++ src/copt/vscmake.bat
@@ -1,9 +1,9 @@
 @echo off
-rem $Id: vscmake.bat,v 1.4 2006-12-11 17:46:54 stefano Exp $
+rem $Id: vscmake.bat,v 1.5 2015-03-21 00:51:29 aralbrec Exp $
 
 echo *****************
 echo * Building copt *
 echo *****************
-cl /Fecopt /Iregex -DUSE_REGEXP -DLOCAL_REGEXP -D _CRT_SECURE_NO_DEPRECATE -D _CRT_NONSTDC_NO_DEPRECATE *.c regex\reg*.c
+cl /Fecopt /I. -DLOCAL_REGEXP -D _CRT_SECURE_NO_DEPRECATE -D _CRT_NONSTDC_NO_DEPRECATE *.c regex\reg*.c
 move copt.exe ..\..\bin
 del *.obj
