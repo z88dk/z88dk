814a47cb1b32a02841db033c86b081d8e300adc7
aralbrec <alvin_albrecht@hotmail.com>
2015-06-20 07:42:37 +0000

    *** empty log message ***

---
+++ libsrc/_DEVELOPMENT/error/z80/error_erange_lmc.asm
@@ -0,0 +1,21 @@
+
+SECTION code_error
+
+PUBLIC error_erange_lmc
+
+EXTERN error_erange_mc
+
+   pop hl
+
+error_erange_lmc:
+
+   ; set dehl = -1
+   ; set errno = ERANGE
+   ; set carry flag
+   
+   call error_erange_mc
+   
+   ld e,l
+   ld d,h
+   
+   ret
