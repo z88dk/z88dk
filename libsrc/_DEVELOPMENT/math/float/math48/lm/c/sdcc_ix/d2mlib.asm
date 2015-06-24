425422897dfdda140f209d29fb47fb013e2f66ff
aralbrec <alvin_albrecht@hotmail.com>
2015-06-24 02:56:30 +0000

    neglected to expose alternate entry point

--- libsrc/_DEVELOPMENT/math/float/math48/lm/c/sdcc_ix/d2mlib.asm
+++ libsrc/_DEVELOPMENT/math/float/math48/lm/c/sdcc_ix/d2mlib.asm
@@ -1,8 +1,9 @@
 
 SECTION code_fp_math48
 
-PUBLIC d2mlib
+PUBLIC d2mlib, dx2mlib
 
-EXTERN cm48_sdccixp_d2m48
+EXTERN cm48_sdccixp_d2m48, cm48_sdccixp_dx2m48
 
-defc d2mlib = cm48_sdccixp_d2m48
+defc d2mlib  = cm48_sdccixp_d2m48
+defc dx2mlib = cm48_sdccixp_dx2m48
