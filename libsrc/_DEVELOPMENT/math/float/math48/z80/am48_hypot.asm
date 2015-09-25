bcdaf88bc2c3ed670b587547a77284d93603974e
aralbrec <alvin_albrecht@hotmail.com>
2015-09-25 18:18:42 +0000

    hypot computes sqrt(x*x+y*y) in a manner that avoids overflow

--- libsrc/_DEVELOPMENT/math/float/math48/z80/am48_hypot.asm
+++ libsrc/_DEVELOPMENT/math/float/math48/z80/am48_hypot.asm
@@ -6,6 +6,7 @@ SECTION code_fp_math48
 PUBLIC am48_hypot
 
 EXTERN am48_fabs, am48_dequate, am48_dmul, am48_dadd, am48_sqrt
+EXTERN am48_dpopret, am48_dconst_1, am48_ddiv
 
 am48_hypot:
 
@@ -26,17 +27,19 @@ am48_hypot:
    ;
    ; uses  : af, bc, de, hl, af', bc', de', hl'
 
-   ; zero has special encoding
-
    ld a,l
    or a
    jp z, am48_fabs             ; if x == 0 return |y|
    
+   res 7,b                     ; x = |x|
+      
    exx
    
    ld a,l
    or a
-   jp z, am48_fabs             ; if y == 0 return |x|
+   ret z                       ; if y == 0 return |x|
+
+   res 7,b                     ; y = |y|
 
    ; compare magnitudes
    
@@ -55,39 +58,30 @@ cmp_mag:
    ;  a = |exp diff|
 
    cp 21
-   jp nc, am48_fabs            ; if |y| << |x| return |x|
+   ret nc                      ; if |y| << |x| return |x|
 
-   push bc                     ; save y
-   push de
-   push hl
-   
-   exx
+   ; sqrt(x*x+y*y) = y*sqrt(x*x/(y*y)+1)
 
-   call am48_dequate
-   call am48_dmul              ; AC'= x^2
-
-   pop hl                      ; AC = y
-   pop de
-   pop bc
-   
+   call am48_ddiv              ; AC'= x/y
    ret c                       ; if overflow
    
-   exx
-   
-   push bc                     ; save x^2
+   push bc
    push de
-   push hl
+   push hl                     ; push y
    
    exx
    
-   call am48_dequate
-   call am48_dmul              ; AC'= y^2
+   call am48_dequate           ; AC = AC'= x/y
    
-   pop hl                      ; AC = x^2
-   pop de
-   pop bc
+   call am48_dmul              ; AC'= (x/y)*(x/y)
+   jp c, am48_dpopret          ; if overflow
 
-   call am48_dadd              ; AC = x^2 + y^2
-   ret c                       ; if overflow
+   call am48_dconst_1          ; AC = 1
+   call am48_dadd              ; AC'= x*x/(y*y)+1
+   call am48_sqrt              ; AC'= sqrt(...)
    
-   jp am48_sqrt
+   pop hl
+   pop de
+   pop bc                      ; AC = y
+
+   jp am48_dmul
