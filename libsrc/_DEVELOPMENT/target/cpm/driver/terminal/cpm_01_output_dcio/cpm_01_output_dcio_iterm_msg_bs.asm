3c94dfc0b1fd0aa3f0cf5034421d6c2c2fdf5099
aralbrec <alvin_albrecht@hotmail.com>
2015-10-14 08:25:49 +0000

    must bs, print space, bs

--- libsrc/_DEVELOPMENT/target/cpm/driver/terminal/cpm_01_output_dcio/cpm_01_output_dcio_iterm_msg_bs.asm
+++ libsrc/_DEVELOPMENT/target/cpm/driver/terminal/cpm_01_output_dcio/cpm_01_output_dcio_iterm_msg_bs.asm
@@ -12,6 +12,14 @@ cpm_01_output_dcio_iterm_msg_bs:
    ; backspace
    ; can use:  af, bc, de, hl, ix
 
+   call backspace
+
+   ld c,0x06                   ; bdos direct console i/o
+   ld e,' '
+   call asm_cpm_bdos_alt
+
+backspace:
+
    ld c,0x06                   ; bdos direct console i/o
    ld e,CHAR_BS
    jp asm_cpm_bdos_alt
