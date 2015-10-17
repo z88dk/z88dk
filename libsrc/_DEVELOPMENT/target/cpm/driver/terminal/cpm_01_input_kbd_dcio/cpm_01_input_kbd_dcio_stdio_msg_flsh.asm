d92eb0c1db81c3cbe740e765b6be9ce41829b8f4
aralbrec <alvin_albrecht@hotmail.com>
2015-10-17 05:27:02 +0000

    consume pending chars from cpm on fflush(stdin)

--- libsrc/_DEVELOPMENT/target/cpm/driver/terminal/cpm_01_input_kbd_dcio/cpm_01_input_kbd_dcio_stdio_msg_flsh.asm
+++ libsrc/_DEVELOPMENT/target/cpm/driver/terminal/cpm_01_input_kbd_dcio/cpm_01_input_kbd_dcio_stdio_msg_flsh.asm
@@ -3,14 +3,21 @@ SECTION code_fcntl
 
 PUBLIC cpm_01_input_kbd_dcio_stdio_msg_flsh
 
-EXTERN console_01_input_stdio_msg_flsh
+EXTERN __CPM_DCIO
+EXTERN asm_cpm_bdos_alt, console_01_input_stdio_msg_flsh
 
-defc cpm_01_input_kbd_dcio_stdio_msg_flsh = console_01_input_stdio_msg_flsh
+;;defc cpm_01_input_kbd_dcio_stdio_msg_flsh = console_01_input_stdio_msg_flsh
 
-; cpm_01_input_kbd_dcio_stdio_msg_flsh:
-; 
-;    ; get rid of any pending chars in cpm
-;    ; (What if someone is holding down a key? That's why this is not being used)
-;    
-;    call asm_in_wait_nokey
-;    jp console_01_input_stdio_msg_flsh
+cpm_01_input_kbd_dcio_stdio_msg_flsh:
+ 
+   ; get rid of any pending chars in bdos
+   ; (What if someone is holding down a key?)
+   
+   ld c,__CPM_DCIO             ; direct console i/o
+   ld e,0xff                   ; input
+   call asm_cpm_bdos_alt
+   
+   or a
+   jr nz, cpm_01_input_kbd_dcio_stdio_msg_flsh
+
+   jp console_01_input_stdio_msg_flsh
