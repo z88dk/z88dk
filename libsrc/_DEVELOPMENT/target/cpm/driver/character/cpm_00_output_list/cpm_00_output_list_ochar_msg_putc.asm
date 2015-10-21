931034f7ee0d1c01918d798b064a17b1837a7094
aralbrec <alvin_albrecht@hotmail.com>
2015-10-21 05:22:10 +0000

    stdrdr, stdlst, stdpun streams for cp/m

---
+++ libsrc/_DEVELOPMENT/target/cpm/driver/character/cpm_00_output_list/cpm_00_output_list_ochar_msg_putc.asm
@@ -0,0 +1,20 @@
+
+SECTION code_fcntl
+
+PUBLIC cpm_00_output_list_ochar_msg_putc
+
+EXTERN __CPM_WLST, asm_cpm_bdos_alt
+
+cpm_00_output_list_ochar_msg_putc:
+
+   ;   enter   :  c = char
+   ;   exit    : carry set if error
+   ;   can use : af, bc, de, hl, af'
+
+   ld e,c
+   ld c,__CPM_WLST
+   
+   call asm_cpm_bdos_alt
+   
+   or a
+   ret
---
+++ libsrc/_DEVELOPMENT/target/cpm/driver/character/cpm_00_output_punch/cpm_00_output_punch_ochar_msg_putc.asm
@@ -0,0 +1,20 @@
+
+SECTION code_fcntl
+
+PUBLIC cpm_00_output_punch_ochar_msg_putc
+
+EXTERN __CPM_WPUN, asm_cpm_bdos_alt
+
+cpm_00_output_punch_ochar_msg_putc:
+
+   ;   enter   :  c = char
+   ;   exit    : carry set if error
+   ;   can use : af, bc, de, hl, af'
+
+   ld e,c
+   ld c,__CPM_WPUN
+   
+   call asm_cpm_bdos_alt
+   
+   or a
+   ret
