7febc84a18340672c97fae505855999d65e54a76
aralbrec <alvin_albrecht@hotmail.com>
2015-10-24 15:51:58 +0000

    optionally use self-modifying code to jr into unrolled ldi loop; causes code to be moved into data section so that it is copied into ram at startup for rom models.

--- libsrc/_DEVELOPMENT/l/z80/l_fast_ldir.asm
+++ libsrc/_DEVELOPMENT/l/z80/l_fast_ldir.asm
@@ -1,10 +1,46 @@
 
-SECTION code_l
+INCLUDE "clib_cfg.asm"
 
-PUBLIC l_fast_ldir, l_fast_ldir_0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+IF __CLIB_OPT_FASTCOPY & $c0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+SECTION smc_l
+
+;; self-modifying code allowed
 
 ; call to this subroutine is faster
-; than ldir if number of loops >= 8
+; than raw ldir if number of loops >= 16
+
+PUBLIC l_fast_memcpy_smc
+
+l_fast_memcpy_smc:
+
+   ld a,$10
+   sub c
+   and $0f
+   inc a
+   add a,a
+   ld (joffset+1),a
+
+joffset:
+
+   jr l_fast_ldir_0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ELSE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+SECTION code_l
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ENDIF
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; call directly into this loop is faster
+; than raw ldir if number of loops >= 6
+
+PUBLIC l_fast_ldir, l_fast_ldir_0
 
 l_fast_ldir_0:
 
