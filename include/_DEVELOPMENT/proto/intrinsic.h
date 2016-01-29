4f9ae597ee0d63f872902e23021b62f1a95cdc8d
aralbrec <alvin_albrecht@hotmail.com>
2016-01-29 04:52:30 +0000

    intrinsics are z80 instructions inlined in C source such that they do not interfere with peephole optimization in sdcc

---
+++ include/_DEVELOPMENT/proto/intrinsic.h
@@ -0,0 +1,10 @@
+include(__link__.m4)
+
+#ifndef _INTRINSIC_H
+#define _INTRINSIC_H
+
+__OPROTO(`a,b,c,d,e,h,l',`a,b,c,d,e,h,l',void,,intrinsic_di,void)
+__OPROTO(`a,b,c,d,e,h,l',`a,b,c,d,e,h,l',void,,intrinsic_ei,void)
+__OPROTO(`a,b,c,d,e,h,l',`a,b,c,d,e,h,l',void,,intrinsic_halt,void)
+
+#endif
