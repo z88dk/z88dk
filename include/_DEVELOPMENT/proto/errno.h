aa3ce8aac97f5f09bd07f0c808ada66c80f46d21
aralbrec <alvin_albrecht@hotmail.com>
2016-02-13 14:01:25 +0000

    EWOULDBLOCK, EAGAIN, ESTAT

--- include/_DEVELOPMENT/proto/errno.h
+++ include/_DEVELOPMENT/proto/errno.h
@@ -7,20 +7,23 @@ extern int errno;
 
 // keep consistent with "clib_constants.inc"
 
-#define EOK        0
-#define EACCES     1
-#define EBADF      2
-#define EBDFD      3
-#define EDOM       4
-#define EFBIG      5
-#define EINVAL     6
-#define EMFILE     7
-#define ENFILE     8
-#define ENOLCK     9
-#define ENOMEM    10
-#define ENOTSUP   11
-#define EOVERFLOW 12
-#define ERANGE    13
+#define EOK            0
+#define EACCES         1
+#define EBADF          2
+#define EBDFD          3
+#define EDOM           4
+#define EFBIG          5
+#define EINVAL         6
+#define EMFILE         7
+#define ENFILE         8
+#define ENOLCK         9
+#define ENOMEM        10
+#define ENOTSUP       11
+#define EOVERFLOW     12
+#define ERANGE        13
+#define ESTAT         14
+#define EWOULDBLOCK   15
+#define EAGAIN        15
 
 // target specific errors included below
 
