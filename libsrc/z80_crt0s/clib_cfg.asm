caac7f75ce5c700828559fd6a9ff46a0161f6eb8
suborb <dom@suborbital.org.uk>
2016-03-04 11:06:27 +0000

    Pull in Alvin's work on sdcc on the new library into the old library.
    This way we can compile programs with the old library with sdcc with the
    --fomit-frame-pointer option.
    
    working so far: enigma.c on CPM

---
+++ libsrc/z80_crt0s/clib_cfg.asm
@@ -0,0 +1,9 @@
+; This option affects how multiplications and divisions
+; of integers and longs are performed by the compiler
+; and the library.
+
+; Setting the value of this flag appropriately allows
+; selection of an integer math lib that ranges from
+; very fast and very large to very slow and very small.
+
+defc __CLIB_OPT_IMATH = 0
