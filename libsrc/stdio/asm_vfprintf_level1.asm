3a82e982a70b8ab5c3d87b2f1d7f4b2d099aeec7
suborb <dom@suborbital.org.uk>
2016-03-29 22:14:38 +0000

    Rename some files

---
+++ libsrc/stdio/asm_vfprintf_level1.asm
@@ -0,0 +1,10 @@
+
+
+		PUBLIC	asm_vfprintf_level1
+
+
+		SECTION	code_clib
+
+asm_vfprintf_level1:
+		defc   printflevel = 1
+		INCLUDE "stdio/asm_printf_core.asm"
---
+++ libsrc/stdio/asm_vfprintf_level2.asm
@@ -0,0 +1,10 @@
+
+
+		PUBLIC	asm_vfprintf_level2
+
+
+		SECTION	code_clib
+
+asm_vfprintf_level2:
+		defc   printflevel = 2
+		INCLUDE "stdio/asm_printf_core.asm"
