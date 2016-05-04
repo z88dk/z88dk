127b4be9e8c7b7fd3e9eb5eb41ce21027fdc9624
aralbrec <alvin_albrecht@hotmail.com>
2016-05-04 04:43:58 +0000

    library entry points for 64-bit division and multiplication

---
+++ libsrc/_DEVELOPMENT/math/integer/l_mulu_64_32x32.asm
@@ -0,0 +1,33 @@
+
+INCLUDE "clib_cfg.asm"
+
+SECTION code_clib
+SECTION code_math
+
+PUBLIC l_mulu_64_32x32
+
+   ; multiplication of two 32-bit numbers into a 64-bit product
+   ;
+   ; enter : dehl = 32-bit multiplicand (more zeros = better performance)
+   ;         dehl'= 32-bit multiplicand
+   ;
+   ; exit  : dehl dehl' = 64-bit product
+   ;         carry reset
+   ;
+   ; uses  : af, bc, de, hl, bc', de', hl'
+
+IF __CLIB_OPT_IMATH <= 50
+
+   EXTERN l_small_mul_64_32x32
+   
+   defc l_mulu_64_32x32 = l_small_mul_64_32x32
+
+ENDIF
+
+IF __CLIB_OPT_IMATH > 50
+
+   EXTERN l_fast_mulu_64_32x32
+   
+   defc l_mulu_64_32x32 = l_fast_mulu_64_32x32
+
+ENDIF
---
+++ libsrc/_DEVELOPMENT/math/integer/l_mulu_64_64x64.asm
@@ -0,0 +1,41 @@
+
+INCLUDE "clib_cfg.asm"
+
+SECTION code_clib
+SECTION code_math
+
+PUBLIC l_mulu_64_64x64
+
+   ; 64-bit multiplication
+   ;
+   ; enter :      +-------------------------------
+   ;              | +15 
+   ;              | ...  multiplicand AB (8 bytes)
+   ;              | + 8 
+   ;         ix = |-------------------------------
+   ;              | + 7
+   ;              | ...  multiplicand CD (8 bytes)
+   ;              | + 0
+   ;              +-------------------------------
+   ;
+   ; exit  : ix structure unchanged
+   ;         dehl' dehl = 64-bit product
+   ;         carry reset
+   ;
+   ; uses  : af, bc, de, hl, bc', de', hl'
+   
+IF __CLIB_OPT_IMATH <= 50
+
+   EXTERN l_small_mul_64_64x64
+   
+   defc l_mulu_64_64x64 = l_small_mul_64_64x64
+
+ENDIF
+
+IF __CLIB_OPT_IMATH > 50
+
+   EXTERN l_fast_mulu_64_64x64
+   
+   defc l_mulu_64_64x64 = l_fast_mulu_64_64x64
+
+ENDIF
