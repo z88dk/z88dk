c07356bb82ee8a6a5de6069ef36e30fafd249998
aralbrec <alvin_albrecht@hotmail.com>
2016-05-11 17:57:42 +0000

    64-bit by 8-bit multiply into 72-bit product for strtoull() family

---
+++ libsrc/_DEVELOPMENT/math/integer/l_mulu_72_64x8.asm
@@ -0,0 +1,33 @@
+
+INCLUDE "clib_cfg.asm"
+
+SECTION code_clib
+SECTION code_math
+
+PUBLIC l_mulu_72_64x8
+
+   ; multiplication of a 64-bit number and an 8-bit number into 72-bit result
+   ;
+   ; enter :   dehl'dehl = 64-bit multiplicand
+   ;                   a = 8-bit multiplicand
+   ;
+   ; exit  : a dehl'dehl = 72-bit product
+   ;         carry reset
+   ;
+   ; uses  : af, bc, de, hl, bc', de', hl'
+   
+IF __CLIB_OPT_IMATH <= 50
+
+   EXTERN l_small_mul_72_64x8
+   
+   defc l_mulu_72_64x8 = l_small_mul_72_64x8
+
+ENDIF
+
+IF __CLIB_OPT_IMATH > 50
+
+   EXTERN l_fast_mulu_72_64x8
+   
+   defc l_mulu_72_64x8 = l_fast_mulu_72_64x8
+
+ENDIF
