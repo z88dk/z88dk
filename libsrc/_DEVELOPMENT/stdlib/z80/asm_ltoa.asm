9a0ddb1ce5daa6528fe84a7d4e165f1a4a5f4f4e
aralbrec <alvin_albrecht@hotmail.com>
2016-05-12 01:04:47 +0000

    odd return values simplified

--- libsrc/_DEVELOPMENT/stdlib/z80/asm_ltoa.asm
+++ libsrc/_DEVELOPMENT/stdlib/z80/asm_ltoa.asm
@@ -6,7 +6,7 @@
 ; char *ltoa(unsigned long num, char *buf, int radix)
 ;
 ; Write number to ascii buffer in radix indicated and zero
-; terminate.  Does not skip initial whitespace.
+; terminate.
 ;
 ; If radix==10, the number is treated as signed and a minus
 ; sign may be written to the buffer.
@@ -19,7 +19,7 @@ SECTION code_stdlib
 PUBLIC asm_ltoa
 PUBLIC asm0_ltoa
 
-EXTERN asm2_ultoa, asm3_ultoa
+EXTERN error_zc, error_einval_zc
 EXTERN l_valid_base, asm1_ultoa, l_neg_dehl
 
 asm_ltoa:
@@ -33,21 +33,21 @@ asm_ltoa:
    ;         carry reset no errors
    ;
    ; error : (*) if buf == 0
-   ;             carry set, dehl = 0
+   ;             carry set, hl = 0
    ;
    ;         (*) if radix is invalid
-   ;             carry set, dehl = 0, errno=EINVAL
+   ;             carry set, hl = 0, errno=EINVAL
    ;
    ; uses  : af, bc, de, hl, bc', de', hl'
 
    ld a,ixh                    ; check for NULL buf
    or ixl
-   jp z, asm2_ultoa
+   jp z, error_zc
 
 asm0_ltoa:                     ; bypasses NULL check of buf
 
    call l_valid_base           ; radix in [2,36]?
-   jp nc, asm3_ultoa
+   jp nc, error_einval_zc
 
    ; base 10 is signed
    
