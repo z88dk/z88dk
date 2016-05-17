c6cbaa91df1120e506d124fed01481fd29033187
suborb <dom@suborbital.org.uk>
2016-05-17 20:49:47 +0000

    Update

--- lib/svi_crt0.asm
+++ lib/svi_crt0.asm
@@ -2,51 +2,30 @@
 ;
 ;       Stefano Bodrato - Apr. 2001
 ;
-;       $Id: svi_crt0.asm,v 1.16 2016-05-15 20:15:44 dom Exp $
+;       $Id: svi_crt0.asm,v 1.17 2016-05-17 20:49:47 dom Exp $
 ;
 
 
-		MODULE  svi_crt0
+	MODULE  svi_crt0
 
 ;
 ; Initially include the zcc_opt.def file to find out lots of lovely
 ; information about what we should do..
 ;
 
-		defc    crt0 = 1
-		INCLUDE "zcc_opt.def"
+	defc    crt0 = 1
+	INCLUDE "zcc_opt.def"
 
 ;--------
 ; Some scope definitions
 ;--------
 
-		EXTERN    _main
+	EXTERN    _main
 
-		PUBLIC    cleanup
-		PUBLIC    l_dcal
+	PUBLIC    cleanup
+	PUBLIC    l_dcal
 
-
-		PUBLIC	snd_tick	; Sound variable
-		PUBLIC	bit_irqstatus	; current irq status when DI is necessary
-
-
-		PUBLIC    exitsp
-		PUBLIC    exitcount
-
-		PUBLIC	heaplast	; Near malloc heap variables
-		PUBLIC	heapblocks
-
-		PUBLIC    __sgoioblk
-
-		; Graphics stuff
-		PUBLIC	pixelbyte	; Temp store for non-buffered mode
-		PUBLIC    base_graphics   ; Graphical variables
-		PUBLIC    coords          ; Current xy position
-
-		; MSX platform specific stuff
-		;
-		PUBLIC    msxbios
-		;; PUBLIC    brksave
+	PUBLIC    msxbios
 
 
 
@@ -67,21 +46,11 @@ start:
 ; Optional definition for auto MALLOC init
 ; it assumes we have free space between the end of 
 ; the compiled program and the stack pointer
-	IF DEFINED_USING_amalloc
-		INCLUDE "amalloc.def"
-	ENDIF
+IF DEFINED_USING_amalloc
+	INCLUDE "amalloc.def"
+ENDIF
 
-IF !DEFINED_nostreams
-IF DEFINED_ANSIstdio
-; Set up the std* stuff so we can be called again
-	ld	hl,__sgoioblk+2
-	ld	(hl),19	;stdin
-	ld	hl,__sgoioblk+6
-	ld	(hl),21	;stdout
-	ld	hl,__sgoioblk+10
-	ld	(hl),21	;stderr
-ENDIF
-ENDIF
+	call	crt0_init_bss
 	call	$53		; Hide function key menu
         call    _main
 	
@@ -107,58 +76,6 @@ start1:
 l_dcal:
         jp      (hl)
 
-; Now, define some values for stdin, stdout, stderr
-
-__sgoioblk:
-IF DEFINED_ANSIstdio
-	INCLUDE	"stdio_fp.asm"
-ELSE
-        defw    -11,-12,-10
-ENDIF
-
-
-        INCLUDE "crt0_runtime_selection.asm"
-
-; ---------------
-; Misc Variables
-; ---------------
-;;defltdsk:       defb    0	; Default disc
-base_graphics:  defw    0	; Location of current screen buffer
-coords:         defw    0       ; Current graphics xy coordinates
-pixelbyte:	defb	0
-
-
-IF DEFINED_NEED1bitsound
-snd_tick:       defb	0	; Sound variable
-bit_irqstatus:	defw	0
-ENDIF
-
-
-
-;Atexit routine
-
-exitsp:
-                defw    0
-exitcount:
-                defb    0
-
-; Heap stuff
-
-heaplast:	defw	0
-heapblocks:	defw	0
-
-IF DEFINED_USING_amalloc
-EXTERN ASMTAIL
-PUBLIC _heap
-; The heap pointer will be wiped at startup,
-; but first its value (based on ASMTAIL)
-; will be kept for sbrk() to setup the malloc area
-_heap:
-                defw ASMTAIL	; Location of the last program byte
-                defw 0
-ENDIF
-
-
 ; ---------------
 ; MSX specific stuff
 ; ---------------
@@ -168,35 +85,17 @@ msxbios:
 	push	ix
 	ret
 
+IF NEED_floatpack
+        INCLUDE	"float.asm"
+ENDIF
 
 ; Signature in resulting binary
 
-         defm  "Small C+ SVI"
-	 defb	0
+	defm	"Small C+ SVI"
+	defb	0
 
 
-;All the float stuff is kept in a different file...for ease of altering!
-;It will eventually be integrated into the library
-;
-;Here we have a minor (minor!) problem, we've no idea if we need the
-;float package if this is separated from main (we had this problem before
-;but it wasn't critical..so, now we will have to read in a file from
-;the directory (this will be produced by zcc) which tells us if we need
-;the floatpackage, and if so what it is..kludgey, but it might just work!
-;
-;Brainwave time! The zcc_opt file could actually be written by the
-;compiler as it goes through the modules, appending as necessary - this
-;way we only include the package if we *really* need it!
+        INCLUDE "crt0_runtime_selection.asm"
+	INCLUDE	"crt0_section.asm"
 
-IF NEED_floatpack
-        INCLUDE         "float.asm"
-
-;seed for random number generator - not used yet..
-fp_seed:        defb    $80,$80,0,0,0,0
-;Floating point registers...
-extra:          defs    6
-fa:             defs    6
-fasign:         defb    0
-
-ENDIF
 
