7301a1d515f1ec3f1638c84cff53f60b50c38c13
suborb <dom@suborbital.org.uk>
2016-05-17 20:45:04 +0000

    And SECTION this as well

--- lib/c7420_crt0.asm
+++ lib/c7420_crt0.asm
@@ -2,7 +2,7 @@
 ;
 ;       Stefano Bodrato 2015
 ;
-;       $Id: c7420_crt0.asm,v 1.4 2016-05-15 20:15:44 dom Exp $
+;       $Id: c7420_crt0.asm,v 1.5 2016-05-17 20:45:04 dom Exp $
 ;
 
 
@@ -25,20 +25,6 @@
         PUBLIC    l_dcal          ;jp(hl)
 
 
-        PUBLIC    exitsp          ;atexit() variables
-        PUBLIC    exitcount
-
-       	PUBLIC	heaplast	;Near malloc heap variables
-        PUBLIC	heapblocks
-
-        PUBLIC    __sgoioblk      ;stdio info block
-
-        PUBLIC    base_graphics   ;Graphical variables
-        PUBLIC	coords		;Current xy position
-
-        PUBLIC	snd_tick	;Sound variable
-        PUBLIC	bit_irqstatus	; current irq status when DI is necessary
-
 ; Now, getting to the real stuff now!
 
         IF      !myzorg
@@ -49,18 +35,12 @@
 
 
 start:
-
-		;ex		de,hl ; preserve HL
-		
         ld      (start1+1),sp	;Save entry stack
         ld      hl,-64
         add     hl,sp
         ld      sp,hl
         ld      (exitsp),sp
 		
-		;push    de ; save HL
-		;push    ix
-
 ; Optional definition for auto MALLOC init
 ; it assumes we have free space between the end of 
 ; the compiled program and the stack pointer
@@ -68,21 +48,10 @@ start:
 		INCLUDE "amalloc.def"
 	ENDIF
 
-IF !DEFINED_nostreams
-IF DEFINED_ANSIstdio
-; Set up the std* stuff so we can be called again
-	ld	hl,__sgoioblk+2
-	ld	(hl),19	;stdin
-	ld	hl,__sgoioblk+6
-	ld	(hl),21	;stdout
-	ld	hl,__sgoioblk+10
-	ld	(hl),21	;stderr
-ENDIF
-ENDIF
-		;di
+	call	crt0_init_bss
+
         call    _main
-		;ei
-		push	hl
+	push	hl
 cleanup:
 ;
 ;       Deallocate memory which has been allocated here!
@@ -95,75 +64,23 @@ IF DEFINED_ANSIstdio
 ENDIF
 ENDIF
 
-		;pop     ix		; We preserved HL and IX
-		;pop     hl		; ..let's restore them !
-		pop		hl
+	pop	hl
 start1:
         ld      sp,0
-		ld		a,l
-        jp		$19f9	; $1994 for french version ??
-						; perhaps we should first spot the right location,
+	ld	a,l
+        jp	$19f9	; $1994 for french version ??
+			; perhaps we should first spot the right location,
 						; looking around for the 47h, AFh sequence
 
 
 l_dcal:	jp	(hl)		;Used for function pointer calls
 
 
-;-----------
-; Define the stdin/out/err area. For the z88 we have two models - the
-; classic (kludgey) one and "ANSI" model
-;-----------
-__sgoioblk:
-IF DEFINED_ANSIstdio
-	INCLUDE	"stdio_fp.asm"
-ELSE
-        defw    -11,-12,-10
-ENDIF
-
-
-        INCLUDE "crt0_runtime_selection.asm"
-
-;-----------
-; Now some variables
-;-----------
-coords:         defw    0       ; Current graphics xy coordinates
-base_graphics:  defw    0   ; Address of the Graphics map
-
-
-exitsp:         defw    0       ; Address of where the atexit() stack is
-exitcount:      defb    0       ; How many routines on the atexit() stack
-
-heaplast:       defw    0       ; Address of last block on heap
-heapblocks:     defw    0       ; Number of blocks
-
-IF DEFINED_USING_amalloc
-EXTERN ASMTAIL
-PUBLIC _heap
-; The heap pointer will be wiped at startup,
-; but first its value (based on ASMTAIL)
-; will be kept for sbrk() to setup the malloc area
-_heap:
-                defw ASMTAIL	; Location of the last program byte
-                defw 0
-ENDIF
-
-IF DEFINED_NEED1bitsound
-snd_tick:       defb	0	; Sound variable
-bit_irqstatus:	defw	0
-ENDIF
-
-;		defm  "Small C+ C7420"
-;		defb	0
-
-;-----------------------
-; Floating point support
-;-----------------------
 IF NEED_floatpack
         INCLUDE         "float.asm"
-fp_seed:       defb    $80,$80,0,0,0,0	;FP seed (unused ATM)
-extra:         defs    6		;FP register
-fa:            defs    6		;FP Accumulator
-fasign:        defb    0		;FP register
-
 ENDIF
+
+        INCLUDE "crt0_runtime_selection.asm"
+	INCLUDE	"crt0_section.asm"
+
 
