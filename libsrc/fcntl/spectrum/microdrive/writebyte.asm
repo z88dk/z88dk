e2fd8042f321b589c6f3097be5740d0d60ffe9f5
suborb <dom@suborbital.org.uk>
2016-06-23 20:42:35 +0000

    More cleanup

--- libsrc/fcntl/spectrum/microdrive/writebyte.asm
+++ libsrc/fcntl/spectrum/microdrive/writebyte.asm
@@ -3,11 +3,14 @@
 ;
 ; Stefano - 2/3/2005
 ;
-; $Id: writebyte.asm,v 1.2 2015-01-19 01:32:43 pauloscustodio Exp $
+; $Id: writebyte.asm,v 1.3 2016-06-23 20:42:35 dom Exp $
 
+	SECTION code_clib
 	PUBLIC	writebyte
+	PUBLIC	_writebyte
 
 .writebyte
+._writebyte
 	ld	hl,-1
 	ld	d,h
 	ld	e,l
