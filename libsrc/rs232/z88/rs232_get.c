bad77bbfa6acfc263c0ade87ba05364e8f34594c
suborb <dom@suborbital.org.uk>
2016-06-23 20:22:34 +0000

    Sort some stack misbalancing

--- libsrc/rs232/z88/rs232_get.c
+++ libsrc/rs232/z88/rs232_get.c
@@ -12,19 +12,22 @@
 #include <rs232.h>
 
 
-u8_t rs232_get(i8_t *char)
-{	/* fastcall so implicit push */
+//u8_t rs232_get(i8_t *char)
 #asm
+	SECTION code_clib
+.rs232_get
+._rs232_get
 	INCLUDE	"serintfc.def"
+	push	hl	;save address
 	ld	l,SI_GBT
 	ld	bc,0	;timeout
 	call_oz(os_si)	;preserves ix
-	pop	de
+	pop	de	;address
 	ld	hl,RS_ERR_NO_DATA
 	ret	c
 	ld	(de),a
 	ld	hl,RS_ERR_OK
+	ret
 #endasm
-}
 
 
--- libsrc/rs232/z88/rs232_put.c
+++ libsrc/rs232/z88/rs232_put.c
@@ -12,19 +12,20 @@
 #include <rs232.h>
 
 
-u8_t rs232_put(i8_t char)
-{	/* Fastcall so implicit push */
+//u8_t rs232_put(i8_t char)
 #asm
+	SECTION code_clib
+.rs232_put
+._rs232_put
 	INCLUDE	"serintfc.def"
 	ld	a,l	;get byte
 	ld	l,SI_PBT
 	ld	bc,0	;timeout
 	call_oz(os_si)		;preserves ix
 	ld	hl,RS_ERR_OK
-	pop	bc	;remove implicit push
 	ret	nc
 	ld	hl,RS_ERR_OVERFLOW
+	ret
 #endasm
-}
 
 
