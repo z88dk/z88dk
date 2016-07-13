7c490f802f6f34bbf574b28c6099bf3e755abdfe
pauloscustodio <pauloscustodio@gmail.com>
2016-07-13 18:31:58 +0000

    Extend the batch file to create the library after building the z88dk executables

--- vsbuild.cmd
+++ vsbuild.cmd
@@ -1,3 +1,16 @@
-rem Build tools with Visual Studio solution
+setlocal
+
+rem Build tools
 if not exist bin mkdir bin
 devenv win32\z88dk.sln /Build Release
+
+rem Build library
+set PATH=%CD%\bin;%PATH%
+set Z80_OZFILES=%CD%\lib\
+set ZCCCFG=%CD%\lib\config\
+
+make -C %CD%\libsrc
+make -C %CD%\libsrc install
+make -C %CD%\libsrc\_DEVELOPMENT
+
+endlocal
