4669cc91f65b5777322edef2a3c46860da218bc7
pauloscustodio <pauloscustodio@gmail.com>
2016-07-14 13:32:48 +0000

    Add --nolib option to build only the tools and skip building the libraries

--- vsbuild.cmd
+++ vsbuild.cmd
@@ -4,6 +4,8 @@ rem Build tools
 if not exist bin mkdir bin
 devenv win32\z88dk.sln /Build Release
 
+if "%1"=="--nolib" goto :exit
+
 rem Build library
 set PATH=%CD%\bin;%PATH%
 set Z80_OZFILES=%CD%\lib\
@@ -13,4 +15,5 @@ make -C %CD%\libsrc
 make -C %CD%\libsrc install
 make -C %CD%\libsrc\_DEVELOPMENT
 
+:exit
 endlocal
