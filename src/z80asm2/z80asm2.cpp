a3389fb6f888fa8ca860553f6e76cf90c5f13d75
pauloscustodio <pauloscustodio@gmail.com>
2016-07-21 23:45:01 +0000

    Simplify main()

--- src/z80asm2/z80asm2.cpp
+++ src/z80asm2/z80asm2.cpp
@@ -10,25 +10,33 @@
 #include "messages.hpp"
 #include "global.hpp"
 
+static void usage()
+{
+	g_messages.information(MES_COPYRIGHT);
+	g_messages.plaintext("");
+	g_messages.information(MES_USAGE);
+}
+
+static void help()
+{
+	g_messages.information(MES_COPYRIGHT);
+	g_messages.plaintext("");
+	g_messages.information(MES_USAGE);
+	g_messages.plaintext("");
+	g_messages.information(MES_HELP);
+}
+
 int main(int argc, char *argv[])
 {
 	init_messages();
 	init_global(argv[0]);
 
-	if (argc <= 1) {
-		g_messages.information(MES_COPYRIGHT);
-		g_messages.plaintext("");
-		g_messages.information(MES_USAGE);
-	}
+	if (argc <= 1)
+		usage();
 	else {
 		std::string arg1 = argv[1];
-		if (argc == 2 && (arg1 == "-h" || arg1 == "--help")) {
-			g_messages.information(MES_COPYRIGHT);
-			g_messages.plaintext("");
-			g_messages.information(MES_USAGE);
-			g_messages.plaintext("");
-			g_messages.information(MES_HELP);
-		}
+		if (argc == 2 && (arg1 == "-h" || arg1 == "--help"))
+			help();
 		else {
 			// TODO: parse command line
 			g_errors.error(ERR_CMD_LINE_ARGS);
