5183cc99d1627867e790a127460b933391492099
aralbrec <alvin_albrecht@hotmail.com>
2016-08-22 02:16:28 +0000

    sms target for new c lib

---
+++ libsrc/_DEVELOPMENT/arch/sms/z80/asm_sms_outi.asm
@@ -0,0 +1,155 @@
+
+SECTION code_crt_common
+
+PUBLIC asm_sms_outi
+
+   ; call asm_sms_outi - 2*n
+   ; n = number of outi <= 128
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+   outi
+
+asm_sms_outi:
+
+   ret
