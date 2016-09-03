04a13feab51e7b5d171b209ee04089a212d65132
pauloscustodio <pauloscustodio@gmail.com>
2016-09-03 11:48:44 +0000

    Restart

--- src/z80asm2/ext/stlplus3/source/make_monolithic.bat
+++
@@ -1,6 +0,0 @@
-copy /Y ..\containers\*.?pp .
-copy /Y ..\portability\*.?pp .
-copy /Y ..\subsystems\*.?pp .
-copy /Y ..\strings\*.?pp .
-copy /Y ..\persistence\*.?pp .
-copy /Y stlplus3.header stlplus3.hpp
