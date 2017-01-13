96b51733c3142572b46937ddc0f935e888505735
suborb <dom@suborbital.org.uk>
2017-01-13 22:47:33 +0000

    Match the 1.99B release - removing dead files that CVS import failed to catch

--- libsrc/fcntl/nc100/open_z88.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-
-int open_z88(far char *nam, int flags, mode_t mode, char *outbuf, size_t extlen)
-{
-  return open(nam, flags, mode);
-}
--- libsrc/sprites/software/sp2/readme.txt
+++
@@ -1,1 +0,0 @@
-Placeholder for low resolution sprite engine
