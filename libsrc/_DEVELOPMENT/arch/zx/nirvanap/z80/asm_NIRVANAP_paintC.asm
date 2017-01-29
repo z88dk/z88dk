bb1574f2e645164a4c6a5a28d258bfc3b49aee7f
aralbrec <alvin_albrecht@hotmail.com>
2017-01-29 00:34:42 -0700

    Some nirvana+ functions incorporated

--- libsrc/_DEVELOPMENT/arch/zx/nirvanap/z80/asm_NIRVANAP_paintC.asm
+++ libsrc/_DEVELOPMENT/arch/zx/nirvanap/z80/asm_NIRVANAP_paintC.asm
@@ -1,7 +1,12 @@
 ; ----------------------------------------------------------------
-; Z88DK INTERFACE LIBRARY FOR NIRVANA+ ENGINE - by Einar Saukas
+; Z88DK LIBRARY FOR NIRVANA+ ENGINE - by Einar Saukas
 ;
-; See "nirvana+.h" for further details
+; Paint specified 8x8 block with a sequence of 4 attribute values (in 211T)
+;
+; Params:
+;     BC = attributes address
+;     D = pixel line (16-192, even values only)
+;     E = char column (0-31)
 ; ----------------------------------------------------------------
 
 SECTION code_clib
@@ -9,4 +14,44 @@ SECTION code_nirvanap
 
 PUBLIC asm_NIRVANAP_paintC
 
-defc asm_NIRVANAP_paintC = 56418   ; execute 'NIRVANAP_paintC'
+EXTERN __NIRVANAP_deltas, __NIRVANAP_attribs
+
+asm_NIRVANAP_paintC:
+
+; calculate initial routine attribute address
+        ld      h, 0
+        ld      l, d
+        ld      d, __NIRVANAP_deltas/256
+        inc     e
+        ld      a, (de)
+        ld      de, __NIRVANAP_attribs
+        add     hl, de
+        add     a, (hl)
+        ld      e, a
+        inc     l
+        adc     a, (hl)
+        sub     e
+        ld      d, a
+        ex      de, hl
+
+; update attributes
+        ld      de, 82
+
+        ld      a, (bc)
+        ld      (hl), a
+        inc     bc
+        add     hl, de
+
+        ld      a, (bc)
+        ld      (hl), a
+        inc     bc
+        add     hl, de
+
+        ld      a, (bc)
+        ld      (hl), a
+        inc     bc
+        add     hl, de
+		  
+        ld      a, (bc)
+        ld      (hl), a
+        ret
