fa166521678acbd0a3df3c8442eda8cc3a8d0d8f
aralbrec <alvin_albrecht@hotmail.com>
2017-02-17 23:43:22 -0700

    Correct path information in arch.h

--- include/_DEVELOPMENT/proto/arch.h
+++ include/_DEVELOPMENT/proto/arch.h
@@ -10,23 +10,23 @@ include(__link__.m4)
 #endif
 
 #ifdef __RC2014
-#include <../../../libsrc/_DEVELOPMENT/target/cpm/config_rc2014.h>
+#include <../../../libsrc/_DEVELOPMENT/target/rc2014/config_rc2014.h>
 #endif
 
 #ifdef __SMS
-#include <../../../libsrc/_DEVELOPMENT/target/cpm/config_sms.h>
+#include <../../../libsrc/_DEVELOPMENT/target/sms/config_sms.h>
 #endif
 
 #ifdef __EMBEDDED_Z80
-#include <../../../libsrc/_DEVELOPMENT/target/cpm/config_z80.h>
+#include <../../../libsrc/_DEVELOPMENT/target/z80/config_z80.h>
 #endif
 
 #ifdef __EMBEDDED_Z180
-#include <../../../libsrc/_DEVELOPMENT/target/cpm/config_z180.h>
+#include <../../../libsrc/_DEVELOPMENT/target/z180/config_z180.h>
 #endif
 
 #ifdef __SPECTRUM
-#include <../../../libsrc/_DEVELOPMENT/target/cpm/config_zx.h>
+#include <../../../libsrc/_DEVELOPMENT/target/zx/config_zx.h>
 #endif
 
 #endif
