fa7e22e226a8ba6da39658dbf40f41e7dd9ed76d
aralbrec <alvin_albrecht@hotmail.com>
2017-02-17 23:23:57 -0700

    Classic library's configuration file for new c lib functions changed to config_private.inc

--- libsrc/z80_crt0s/clib_cfg.asm
+++
@@ -1,9 +0,0 @@
-; This option affects how multiplications and divisions
-; of integers and longs are performed by the compiler
-; and the library.
-
-; Setting the value of this flag appropriately allows
-; selection of an integer math lib that ranges from
-; very fast and very large to very slow and very small.
-
-defc __CLIB_OPT_IMATH = 0
