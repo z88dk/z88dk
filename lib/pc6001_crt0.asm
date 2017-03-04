7d3d6c8f11eeff5f673954465635895503f83538
Stefano <stefano_bodrato@hotmail.com>
2017-03-04 23:03:29 +0100

    ROM cartridge support for PC6001

--- lib/pc6001_crt0.asm
+++ lib/pc6001_crt0.asm
@@ -4,7 +4,7 @@
 ;
 ;       If an error occurs eg break we just drop back to BASIC
 ;
-;       $Id: pc6001_crt0.asm,v 1.11 2016-07-11 21:19:38 dom Exp $
+;       $Id: pc6001_crt0.asm,v 1.11 2016/07/11 21:19:38 dom Exp $
 ;
 
 
@@ -28,6 +28,7 @@
         PUBLIC    l_dcal          ;jp(hl)
 
 
+
 IF (startup=2)
 	defc    CRT_ORG_CODE  = $8437  ; PC6001 - 32k (Answer "2" to "How many pages?")
 ENDIF
@@ -37,7 +38,20 @@ IF (startup=3)
 ENDIF
 
 IF (startup=4)
-	defc    CRT_ORG_CODE  = $4004	 ; ROM
+	defc    CRT_ORG_CODE  = $4000	 ; ROM
+IF !DEFINED_CRT_ORG_BSS
+	defc CRT_ORG_BSS =  $c500   ; Static variables are kept in RAM
+	defc DEFINED_CRT_ORG_BSS = 1
+ENDIF
+	defc	__crt_org_bss = CRT_ORG_BSS
+	; In ROM mode we MUST setup the stack
+	defc	STACKPTR = $ffff
+	; If we were given a model then use it
+	IF DEFINED_CRT_MODEL
+		defc __crt_model = CRT_MODEL
+	ELSE
+		defc __crt_model = 1
+	ENDIF
 ENDIF
 
 IF      !CRT_ORG_CODE
@@ -49,6 +63,12 @@ ENDIF
 
 
 		org     CRT_ORG_CODE
+
+IF (startup=4)
+	defb $41
+	defb $42
+	defw start
+ENDIF
 
 start:
 		;di
@@ -122,6 +142,7 @@ l_dcal:
 
 	defm  "Small C+ PC6001"
 	defb   0
+	
         INCLUDE "crt0_runtime_selection.asm"
 	INCLUDE	"crt0_section.asm"
 
