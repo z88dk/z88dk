629f659c58f9a1d4153a6891421d8fc2f59f22fb
aralbrec <alvin_albrecht@hotmail.com>
2017-03-12 17:51:45 -0600

    Fix #108: printf_handle_f no longer uses dldpsh; there is no way to use that function in that manner that would satisfy all combinations of compiler, math lib and restricted targets

--- libsrc/stdio/__printf_handle_f.asm
+++ libsrc/stdio/__printf_handle_f.asm
@@ -1,24 +1,24 @@
 
-	MODULE	__printf_handle_f
-	PUBLIC	__printf_handle_f
-	PUBLIC	__printf_handle_e
+   MODULE   __printf_handle_f
+   PUBLIC   __printf_handle_f
+   PUBLIC   __printf_handle_e
 
-	EXTERN	fa
-	EXTERN	l_jphl
-	EXTERN	dldpsh
-	EXTERN	__printf_get_buffer_address
-	EXTERN	__printf_print_the_buffer
-	EXTERN	ftoa
-	EXTERN	ftoe
-	EXTERN	strlen
-	EXTERN	__convert_sdccf2reg
+   EXTERN   fa
+   EXTERN   l_jphl
+   EXTERN   dldpsh
+   EXTERN   __printf_get_buffer_address
+   EXTERN   __printf_print_the_buffer
+   EXTERN   ftoa
+   EXTERN   ftoe
+   EXTERN   strlen
+   EXTERN   __convert_sdccf2reg
 
 __printf_handle_e:
-	set	5,(ix-4)
+        set   5,(ix-4)
 __printf_handle_f:
         push    hl              ;save fmt
-	bit	0,(ix+6)
-	jr	nz,is_sccz80
+        bit   0,(ix+6)
+        jr nz,is_sccz80
 
         ex      de,hl           ;hl=where tp pick up from
         ld      e,(hl)          ;mantissa
@@ -27,19 +27,19 @@ __printf_handle_f:
         inc     hl
         ld      a,(hl)
         inc     hl
-        ld      b,(hl)		;exponent
+        ld      b,(hl)     ;exponent
         inc     hl
         push    hl              ; save ap
         ld      h,b
         ld      l,a
 
-	push	ix		;save callers
-	call	__convert_sdccf2reg	
-	push	hl	;get it on the stack in 6 byte format
-	push	de
-	ld	hl,0
-	push	hl
-	jr	rejoin
+        push  ix    ;save callers
+        call  __convert_sdccf2reg  
+        push  hl ;get it on the stack in 6 byte format
+        push  de
+        ld hl,0
+        push  hl
+        jr rejoin
 ; If we've got %f then lets assume we've got sccz80 rather than sdcc
 is_sccz80:
         dec     de
@@ -53,8 +53,14 @@ is_sccz80:
         inc     de
 
         push    ix              ;save ix - ftoa will corrupt it
-        ex      de,hl		;hl=address of parameter
-        call    dldpsh
+
+        ld      hl,-6
+        add     hl,sp
+        ld      sp,hl
+        ex      de,hl           ;hl=address of parameter, de=slot on stack
+        ld      bc,6
+        ldir                    ;stack parameter
+
 rejoin:
         ld      c,(ix-8)
         ld      b,(ix-7)
@@ -75,14 +81,12 @@ set_prec:
 call_fp_converter:
         call    l_jphl
         pop     bc              ;the buffer
-        pop     bc		;prec
-        pop     bc		;flt
-        pop     bc		;flt
-        pop     bc		;flt
+        pop     bc      ;prec
+        pop     bc      ;flt
+        pop     bc      ;flt
+        pop     bc      ;flt
         pop     ix              ;get ix back
         call    __printf_get_buffer_address
         call    strlen          ;get the length of it
-	ld	(ix-10),l
-	jp	__printf_print_the_buffer
-
-
+        ld      (ix-10),l
+        jp      __printf_print_the_buffer
