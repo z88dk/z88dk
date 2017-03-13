740270b97d09f6e194ffbe1fcdefd7027206376f
Stefano <stefano_bodrato@hotmail.com>
2017-03-13 13:41:19 +0100

    BANK and CPU switching now in the same code block

--- lib/c128_crt0.asm
+++ lib/c128_crt0.asm
@@ -2,7 +2,7 @@
 ;
 ;       Stefano Bodrato - 22/08/2001
 ;
-;	$Id: c128_crt0.asm,v 1.28 2016-07-15 21:03:25 dom Exp $
+;	$Id: c128_crt0.asm,v 1.28 2016/07/15 21:03:25 dom Exp $
 ;
 
 
@@ -29,14 +29,51 @@
 
 ; Now, getting to the real stuff now!
 
-IF      !DEFINED_CRT_ORG_CODE
-        defc    CRT_ORG_CODE  = $3000
-ENDIF
+        IF      !DEFINED_CRT_ORG_CODE
+                defc    CRT_ORG_CODE  = $3000	; no, use a fixed entry location at $3000, 'appmake' is not ready for different values!
+        ENDIF
+
         org     CRT_ORG_CODE
 
 
 start:
 
+	; Write anything to the proper preconfiguration register to switch to BANK 0
+	defb $8D	; STA ..
+	defw $FF01
+;ENDIF
+
+	; Prepare the "springboard" for the z80
+	defb $A9	; LDA ..
+	defb 195	; ..opcode for JP (z80)
+	defb $8D	; STA ..
+	defw 65518
+	
+	defb $A9	; LDA ..
+	defb z80start&$FF
+	defb $8D	; STA ..
+	defw 65519
+	
+	defb $A9	; LDA ..
+	defb z80start/256
+	defb $8D	; STA ..
+	defw 65520
+	
+	; Prepare the "springboard" for the 6502
+	defb $A9	; LDA ..
+	defb 88		; ..opcode for CLI
+	defb $8D	; STA ..
+	defw 4352
+	
+	defb $A9	; LDA ..
+	defb 96		; ..opcode for RTS
+	defb $8D	; STA ..
+	defw 4353
+	
+	defb $4C	; JMP
+	defw 65488
+
+z80start:
 	di
 	
 	;ld	bc,$d030
@@ -50,7 +87,7 @@ start:
 	ld	bc,$d018
 	;ld	a,$17	; Standard display address ($400)..
 	ld	a,$87	; Display addres at $2000...
-	out	(c),a	; ...and alternate (upper+lower) char set
+	out	(c),a	; ...and alternate (upper+lowercase) char set
 
 	ld	a,11		;dark grey
 	ld	bc,$d020
@@ -103,18 +140,26 @@ ENDIF
 	;out	(c),a	; back to slow speed clock
 
 start1:
-        ld      sp,0
-        jp	$FFE0
+        ld  sp,0
+        jp  $FFE0
 
 l_dcal:
-        jp      (hl)
+        jp  (hl)
 
 
 
 	defm  "Small C+ C128"
 	defb  0
 
-        INCLUDE "crt0_runtime_selection.asm"
+
+	
+; If we were given an address for the BSS then use it
+IF DEFINED_CRT_ORG_BSS
+	defc	__crt_org_bss = CRT_ORG_BSS
+ENDIF
+
+
+	INCLUDE "crt0_runtime_selection.asm"
 
 	INCLUDE "crt0_section.asm"
 
