28b43b3f1b43536ef8fe62f47cac7226faa7a38c
aralbrec <alvin_albrecht@hotmail.com>
2017-03-16 21:35:11 -0600

    Incorrect calculation of vic was leading to an index outside array bounds

--- libsrc/_DEVELOPMENT/EXAMPLES/malloc_test.c
+++ libsrc/_DEVELOPMENT/EXAMPLES/malloc_test.c
@@ -1,5 +1,5 @@
-// zcc +zx -vn -startup=4 -O3 -clib=new malloc_test.c -o malloc_test -create-app -DPRINTF
-// zcc +zx -vn -startup=4 -SO3 -clib=sdcc_iy --max-allocs-per-node200000 malloc_test.c -o malloc_test -create-app -DPRINTF
+// zcc +zx -vn -DPRINTF -startup=4 -O3 -clib=new malloc_test.c -o malloc_test -create-app
+// zcc +zx -vn -DPRINTF -startup=4 -SO3 -clib=sdcc_iy --max-allocs-per-node200000 malloc_test.c -o malloc_test -create-app
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,12 +12,14 @@
    #define PRINTF2(a,b)       printf(a,b)
    #define PRINTF3(a,b,c)     printf(a,b,c)
    #define PRINTF4(a,b,c,d)   printf(a,b,c,d)
+   #define IOCTL(a,b,c)       ioctl(a,b,c)
    #pragma output CLIB_OPT_PRINTF = 0x202
 #else
-   #define PRINTF(a)
+   #define PRINTF1(a)
    #define PRINTF2(a,b)
    #define PRINTF3(a,b,c)
    #define PRINTF4(a,b,c,d)
+   #define IOCTL(a,b,c)
 #endif
 
 #ifndef N
@@ -26,20 +28,20 @@
 
 void         *p[N];
 unsigned int sz[N];
-unsigned int errors;
+unsigned int errors, unables;
 
 void main(void)
 {
    static unsigned char vic;
    static unsigned int  i;
    
-   ioctl(1, IOCTL_OTERM_PAUSE, 0);
+   IOCTL(1, IOCTL_OTERM_PAUSE, 0);
    
 intrinsic_label(TIMER_START);
 
    for (i=0; i<N*1000; ++i)
    {
-      vic = (unsigned char)((long)rand() * N / RAND_MAX);
+      vic = (unsigned char)((unsigned long)rand() * N / ((unsigned int)RAND_MAX + 1));
       
       if (p[vic])
       {
@@ -61,11 +63,14 @@ intrinsic_label(TIMER_START);
             PRINTF4("Slot %2u : malloc at %5u %u bytes\n", vic, p[vic], sz[vic]);
          }
          else
+         {
             PRINTF2("Cannot allocate %u bytes\n", sz[vic]);
+            unables++;
+         }
       }
    }
 
 intrinsic_label(TIMER_STOP);
 
-   PRINTF3("\n\n%s (%u errors)\n\n\n\n", errors ? "FAIL" : "PASS", errors);
+   PRINTF4("\n\n%s (%u errors, %u unables)\n\n\n\n", errors ? "FAIL" : "PASS", errors, unables);
 }
