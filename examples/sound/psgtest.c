3a6190d807cf02bc66ef3ad99c7ba96aed047476
Stefano <stefano_bodrato@hotmail.com>
2017-03-21 08:16:58 +0100

    Create psgtest.c

---
+++ examples/sound/psgtest.c
@@ -0,0 +1,125 @@
+/*
+;
+;  Emulating bit_play with the PSG (no poliphony!)
+;  play a melody (integer approx to optimize speed and size)
+;
+;  by Stefano Bodrato - 2017
+;
+;  Syntax: "TONE(#/b)(+/-)(duration)"
+;
+*/
+
+#include <psg.h>
+#include <sound.h>
+
+void psg_play(unsigned char melody[])
+{
+int sound;
+int duration=2;
+float x;
+
+
+while ( *melody != 0 )
+   {
+   switch (*melody++) {
+	case 'C':
+		if (*melody=='#') {
+			sound=277;
+			melody++;
+			}
+		else
+			sound=262;
+	break;
+	case 'D':
+		if (*melody=='#') {
+			sound=311;
+			melody++;
+			}
+		else if (*melody=='b') {
+			sound=277;
+			melody++;
+			}
+		else
+			sound=294;
+	break;
+	case 'E':
+		if (*melody=='b') {
+			sound=311;
+			melody++;
+			}
+		else
+			sound=330;
+	break;
+	case 'F':
+		if (*melody=='#') {
+			sound=370;
+			melody++;
+			}
+		else
+			sound=349;
+	break;
+	case 'G':
+		if (*melody=='#') {
+			sound=415;
+			melody++;
+			}
+		else if (*melody=='b') {
+			sound=370;
+			melody++;
+			}
+		else
+			sound=392;
+	break;
+	case 'A':
+		if (*melody=='#') {
+			sound=466;
+			melody++;
+			}
+		else if (*melody=='b') {
+			sound=415;
+			melody++;
+			}
+		else
+			sound=440;
+	break;
+	case 'B':
+		if (*melody=='b') {
+			sound=466;
+			melody++;
+			}
+		else
+			sound=494;
+	break;
+	case '+':
+		sound*=2;
+	break;
+	case '-':
+		sound/=2;
+	}
+	if (*melody>'0' && *melody<='9') duration=(*melody++)-48;
+	if ((*melody >= 'A' && *melody <= 'H') || *melody==0) {
+		psg_tone(chan1, psgT(sound));
+		for (x=0.0; x< BEEP_TSTATES/28000.0*duration; x++) {}
+		psg_tone(chan1, 0);
+	}
+   }
+}
+
+
+int main () {
+	psg_init();
+	psg_channels(chanAll, chanNone); // set all channels to tone generation
+
+// Fra Martino
+	psg_play("C4DECCDECEFG8E4FG8G2AGFE4CG2AGFE4CDG-CCDG-CC");
+
+// ZX DEMO
+	psg_play("C8DEb4DC8");
+	psg_play("C8DEb4DC8");
+	psg_play("Eb8FGG");
+	psg_play("Eb8FGG");
+	psg_play("G8Ab4G8FEb4DC");
+	psg_play("G8Ab4G8FEb4DC");
+	psg_play("C8G-C");
+	psg_play("C8G-C");
+}
