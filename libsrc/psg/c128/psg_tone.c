7ece2bacb6fc1047203fa83b0647b03ea14bfd71
Stefano <stefano_bodrato@hotmail.com>
2017-03-21 08:27:14 +0100

    Update psg_tone.c

--- libsrc/psg/c128/psg_tone.c
+++ libsrc/psg/c128/psg_tone.c
@@ -14,19 +14,16 @@
 void psg_tone(unsigned char channels, int period) {
 
 	if (channels & 1) {
-		envelopesid(sidVoice1,0,0,15,0);
 		freqsid(sidVoice1,period);
 		attacksid(sidVoice1,sidWaveTri);
 	}
 	
 	if (channels & 2) {
-		envelopesid(sidVoice2,0,0,15,0);
 		freqsid(sidVoice2,period);
 		attacksid(sidVoice2,sidWaveTri);
 	}
 
 	if (channels & 4) {
-		envelopesid(sidVoice3,0,0,15,0);
 		freqsid(sidVoice3,period);
 		attacksid(sidVoice3,sidWaveTri);
 	}
