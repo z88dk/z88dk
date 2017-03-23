d5e029257bbc18fa9c4691700face9e422041af4
Stefano <stefano_bodrato@hotmail.com>
2017-03-23 16:42:22 +0100

    Commodore 128 variant

--- libsrc/stdlib/delay.c
+++ libsrc/stdlib/delay.c
@@ -9,13 +9,30 @@
 
 #include <stdlib.h>
 #include <time.h>
+
 #ifdef __ZX80__
 #include <zx81.h>
+#endif
+
+#ifdef __C128__
+#include <c128/cia.h>
 #endif
 
 
 void   delay (long msecs)
 {
+
+#ifdef __C128__
+long x;
+
+  setintctrlcia(cia2,ciaClearIcr); /* disable all cia 2 interrupts */
+  settimerbcia(cia2,timervalcia(1000),ciaCountA);  /* timer b counts timer a underflows */
+  settimeracia(cia2,timervalcia(1000),ciaCPUCont); /* set timer a 1/1000 sec */
+  for (x=0; x<msecs; x++)
+	while ((inp(cia2+ciaIntCtrl) & 0x02) == 0) {}      /* wait for count down */
+
+#else
+
 	long start = clock();  
 	long per   = msecs * CLOCKS_PER_SEC / 1000;
 
@@ -27,5 +44,8 @@ void   delay (long msecs)
 	    ;
 #endif
 	}
+
+#endif
+
 }
 
