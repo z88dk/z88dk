2d3088f60cb8102450c28998f4fb485296e5574a
suborb <dom@suborbital.org.uk>
2017-04-14 21:52:15 +0100

    Revert changes

--- src/config.h
+++ src/config.h
@@ -1,3 +1,4 @@
-#define PREFIX "/usr/local/lib/z88dk"
-#define UNIX 1
-#define Z88DK_VERSION "20170407-4e73685"
+#define WIN32 1
+#define Z88DK_EXPAND_STRING(X) Z88DK_STRING(X)		
+#define Z88DK_STRING(X) #X
+#define Z88DK_VERSION "MSC." Z88DK_EXPAND_STRING(_MSC_FULL_VER) " " __DATE__
