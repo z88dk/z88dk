d5f3da19b39b66e0cabd2a4d8dae53db70e83b64
aralbrec <alvin_albrecht@hotmail.com>
2017-04-14 08:29:47 -0600

    sms needs memsetw() to write 16-bit words to vram because character codes are 16-bit

---
+++ libsrc/_DEVELOPMENT/arch/sms/vram/z80/asm_sms_memsetw_vram.asm
@@ -0,0 +1,47 @@
+; ========================================================================
+; 
+; void *sms_memsetw_vram(void *dst, unsigned int c, unsigned int n)
+;
+; memset VRAM word at a time; VRAM addresses are assumed to be stable.
+;
+; ========================================================================
+
+INCLUDE "config_private.inc"
+
+SECTION code_clib
+SECTION code_crt_common
+
+PUBLIC asm_sms_memsetw_vram
+
+EXTERN asm_sms_set_vram_write_de
+
+asm_sms_memsetw_vram:
+
+   ; memset vram by word
+   ;
+   ; enter : de = void *dst in vram
+   ;         hl = unsigned int c
+   ;         bc = unsigned int n > 0
+   ;
+   ; exit  : hl = void *dst, &byte after last written in vram
+   ;         de = unsigned int c
+   ;
+   ; uses  : af, bc, de, hl
+
+   call asm_sms_set_vram_write_de
+   ex de,hl
+   
+loop:
+
+   ld a,e
+   out (__IO_VDP_DATA),a
+   
+   cpi                         ; hl++, bc--
+   
+   ld a,d
+   out (__IO_VDP_DATA),a
+
+   nop
+   jp pe, loop
+
+   ret
