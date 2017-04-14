abec7527af9b31d246c8d37163bc317792326534
aralbrec <alvin_albrecht@hotmail.com>
2017-04-14 08:27:26 -0600

    sms default font

---
+++ libsrc/_DEVELOPMENT/font/sms/_sms_default_font.asm
@@ -0,0 +1,8 @@
+SECTION rodata_font
+SECTION rodata_font_8x8
+
+PUBLIC _sms_default_font
+
+_sms_default_font:
+
+   BINARY "sms_default_font.bin"
---
+++ libsrc/_DEVELOPMENT/font/sms/_sms_default_font_zx7.asm
@@ -0,0 +1,8 @@
+SECTION rodata_font
+SECTION rodata_font_8x8
+
+PUBLIC _sms_default_font_zx7
+
+_sms_default_font_zx7:
+
+   BINARY "sms_default_font.bin.zx7"
