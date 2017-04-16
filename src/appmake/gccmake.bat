946336c613094553640aebb95ccd7055c78da650
suborb <dom@suborbital.org.uk>
2017-04-16 16:01:38 +0100

    Fix #171: Remove dead and old win32 project files

--- src/appmake/gccmake.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem $Id: gccmake.bat,v 1.1 2012-11-05 08:15:51 stefano Exp $
-
-echo ********************
-echo * Building appmake *
-echo ********************
-gcc -o appmake -DWIN32 *.c
-copy appmake.exe ..\..\bin
-del appmake.exe
