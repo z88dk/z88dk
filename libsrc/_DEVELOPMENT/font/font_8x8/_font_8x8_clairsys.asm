29752aee7305b6bc85b4a33b88391f74c9659791
aralbrec <alvin_albrecht@hotmail.com>
2017-04-17 09:24:25 -0600

    Correct font definitions to export label marking their end

--- libsrc/_DEVELOPMENT/font/font_8x8/_font_8x8_clairsys.asm
+++ libsrc/_DEVELOPMENT/font/font_8x8/_font_8x8_clairsys.asm
@@ -2,7 +2,7 @@ SECTION rodata_font
 SECTION rodata_font_8x8
 
 PUBLIC _font_8x8_clairsys
-PUBLIC _font_8x8_clairsys
+PUBLIC _font_8x8_clairsys_end
 
 _font_8x8_clairsys:
 
--- libsrc/_DEVELOPMENT/font/font_8x8/_font_8x8_clairsys_bold.asm
+++ libsrc/_DEVELOPMENT/font/font_8x8/_font_8x8_clairsys_bold.asm
@@ -2,7 +2,7 @@ SECTION rodata_font
 SECTION rodata_font_8x8
 
 PUBLIC _font_8x8_clairsys_bold
-PUBLIC _font_8x8_clairsys_bold
+PUBLIC _font_8x8_clairsys_bold_end
 
 _font_8x8_clairsys_bold:
 
