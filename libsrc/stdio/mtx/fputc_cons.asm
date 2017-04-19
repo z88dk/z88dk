5eed9c497d345154dea389b3041ea75be6bd2041
Stefano <stefano_bodrato@hotmail.com>
2017-04-19 09:07:26 +0200

    #136,  Memotech MTX

--- libsrc/stdio/mtx/fputc_cons.asm
+++ libsrc/stdio/mtx/fputc_cons.asm
@@ -3,7 +3,7 @@
 ;
 ;	(HL)=char to display
 ;
-;	$Id: fputc_cons.asm,v 1.4 2016-05-15 20:15:45 dom Exp $
+;	$Id: fputc_cons.asm,v 1.4 (git imported) Exp $
 ;
 
 	SECTION code_clib
@@ -20,14 +20,10 @@ ELSE
 	cp	13
 ENDIF
 	jr	nz,nocrlf
-	ld	c,a
+	ld	c,13
 	rst 10h
 	defb 192
-IF STANDARDESCAPECHARS
-	ld	a,13
-ELSE
 	ld	a,10
-ENDIF
 .nocrlf
 	ld	c,a
 	rst 10h
