72d2629670c65549725089cec136e3222261d9e6
aralbrec <alvin_albrecht@hotmail.com>
2017-04-19 22:44:32 -0600

    Copy and paste errors exported wrong function names

--- libsrc/_DEVELOPMENT/arch/sms/vdp/c/sccz80/sms_vdp_set_read_address.asm
+++ libsrc/_DEVELOPMENT/arch/sms/vdp/c/sccz80/sms_vdp_set_read_address.asm
@@ -3,8 +3,8 @@
 SECTION code_clib
 SECTION code_arch
 
-PUBLIC sms_vdp_set_read_address_fastcall
+PUBLIC sms_vdp_set_read_address
 
 EXTERN asm_sms_vdp_set_read_address
 
-defc sms_vdp_set_read_address_fastcall = asm_sms_vdp_set_read_address
+defc sms_vdp_set_read_address = asm_sms_vdp_set_read_address
