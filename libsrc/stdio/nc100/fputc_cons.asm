3bc5cd1af98a000278c84cc63e89ba3fa7250a86
Stefano <stefano_bodrato@hotmail.com>
2017-04-21 10:54:24 +0200

    #136,  Amstrad NC

--- libsrc/stdio/nc100/fputc_cons.asm
+++ libsrc/stdio/nc100/fputc_cons.asm
@@ -4,7 +4,7 @@
 ;	fputc_cons(char c)
 ;
 ;
-;	$Id: fputc_cons.asm,v 1.5 2016-05-15 20:15:45 dom Exp $
+;	$Id: fputc_cons.asm,v 1.5+ (now on GIT) $
 ;
 
 
@@ -22,12 +22,9 @@ ELSE
 	cp	13
 ENDIF
 	jr	nz,fputc_cons1
-	call	$B833	;txtoutput
-IF STANDARDESCAPECHARS
 	ld	a,13
-ELSE
+	call	$B833	;txtoutput
 	ld	a,10
-ENDIF
 .fputc_cons1
 	jp	$B833	;txtoutput
 
