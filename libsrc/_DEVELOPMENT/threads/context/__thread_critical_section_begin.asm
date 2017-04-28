94460476f09607603c8035dc03d355c4ffba87a7
aralbrec <alvin_albrecht@hotmail.com>
2017-04-28 00:26:57 -0600

    Distinguish Z80 and Z180

--- libsrc/_DEVELOPMENT/threads/context/__thread_critical_section_begin.asm
+++ libsrc/_DEVELOPMENT/threads/context/__thread_critical_section_begin.asm
@@ -1,9 +1,20 @@
+INCLUDE "config_private.inc"
 
 SECTION code_clib
 SECTION code_threads
 
 PUBLIC __thread_critical_section_begin
 
-EXTERN asm_z80_push_di
+IFDEF __Z80
 
-defc __thread_critical_section_begin = asm_z80_push_di
+   EXTERN asm_z80_push_di
+   defc __thread_critical_section_begin = asm_z80_push_di
+
+ENDIF
+
+IFDEF __Z180
+
+   EXTERN asm_z180_push_di
+   defc __thread_critical_section_begin = asm_z180_push_di
+
+ENDIF
--- libsrc/_DEVELOPMENT/threads/context/__thread_critical_section_end.asm
+++ libsrc/_DEVELOPMENT/threads/context/__thread_critical_section_end.asm
@@ -1,9 +1,20 @@
+INCLUDE "config_private.inc"
 
 SECTION code_clib
 SECTION code_threads
 
 PUBLIC __thread_critical_section_end
 
-EXTERN asm_z80_pop_ei
+IFDEF __Z80
 
-defc __thread_critical_section_end = asm_z80_pop_ei
+   EXTERN asm_z80_pop_ei
+   defc __thread_critical_section_end = asm_z80_pop_ei
+
+ENDIF
+
+IFDEF __Z180
+
+   EXTERN asm_z180_pop_ei
+   defc __thread_critical_section_end = asm_z180_pop_ei
+
+ENDIF
