952063030b7401fcacc3dbac8c52a5fd3207fabb
aralbrec <alvin_albrecht@hotmail.com>
2017-05-03 00:49:36 -0600

    Restore im2 macros that generate interrupt service routines; neither zsdcc nor sccz80 can generate all forms of interrupt service routines

--- include/_DEVELOPMENT/sdcc/im2.h
+++ include/_DEVELOPMENT/sdcc/im2.h
@@ -7,40 +7,103 @@
 
 #include <stdint.h>
 
-extern void im2_init(void *im2_table_address) __preserves_regs(b,c,d,e);
-extern void im2_init_fastcall(void *im2_table_address) __preserves_regs(b,c,d,e,h,l) __z88dk_fastcall;
+extern void im2_init(void *im2_table_address) __preserves_regs(b,c,d,e,iyl,iyh);
+extern void im2_init_fastcall(void *im2_table_address) __preserves_regs(b,c,d,e,h,l,iyl,iyh) __z88dk_fastcall;
 #define im2_init(a) im2_init_fastcall(a)
 
 
-extern void *im2_install_isr(uint8_t vector,void *isr) __preserves_regs(b,c);
-extern void *im2_install_isr_callee(uint8_t vector,void *isr) __preserves_regs(b,c) __z88dk_callee;
+extern void *im2_install_isr(uint8_t vector,void *isr) __preserves_regs(b,c,iyl,iyh);
+extern void *im2_install_isr_callee(uint8_t vector,void *isr) __preserves_regs(b,c,iyl,iyh) __z88dk_callee;
 #define im2_install_isr(a,b) im2_install_isr_callee(a,b)
 
 
-extern void *im2_create_generic_isr(uint8_t num_callbacks,void *address);
-extern void *im2_create_generic_isr_callee(uint8_t num_callbacks,void *address) __z88dk_callee;
+extern void *im2_create_generic_isr(uint8_t num_callbacks,void *address) __preserves_regs(iyl,iyh);
+extern void *im2_create_generic_isr_callee(uint8_t num_callbacks,void *address) __preserves_regs(iyl,iyh) __z88dk_callee;
 #define im2_create_generic_isr(a,b) im2_create_generic_isr_callee(a,b)
 
 
-extern void *im2_create_generic_isr_8080(uint8_t num_callbacks,void *address);
-extern void *im2_create_generic_isr_8080_callee(uint8_t num_callbacks,void *address) __z88dk_callee;
+extern void *im2_create_generic_isr_8080(uint8_t num_callbacks,void *address) __preserves_regs(iyl,iyh);
+extern void *im2_create_generic_isr_8080_callee(uint8_t num_callbacks,void *address) __preserves_regs(iyl,iyh) __z88dk_callee;
 #define im2_create_generic_isr_8080(a,b) im2_create_generic_isr_8080_callee(a,b)
 
 
-extern void im2_append_generic_callback(uint8_t vector,void *callback);
-extern void im2_append_generic_callback_callee(uint8_t vector,void *callback) __z88dk_callee;
+extern void im2_append_generic_callback(uint8_t vector,void *callback) __preserves_regs(iyl,iyh);
+extern void im2_append_generic_callback_callee(uint8_t vector,void *callback) __preserves_regs(iyl,iyh) __z88dk_callee;
 #define im2_append_generic_callback(a,b) im2_append_generic_callback_callee(a,b)
 
 
-extern void im2_prepend_generic_callback(uint8_t vector,void *callback);
-extern void im2_prepend_generic_callback_callee(uint8_t vector,void *callback) __z88dk_callee;
+extern void im2_prepend_generic_callback(uint8_t vector,void *callback) __preserves_regs(iyl,iyh);
+extern void im2_prepend_generic_callback_callee(uint8_t vector,void *callback) __preserves_regs(iyl,iyh) __z88dk_callee;
 #define im2_prepend_generic_callback(a,b) im2_prepend_generic_callback_callee(a,b)
 
 
-extern int im2_remove_generic_callback(uint8_t vector,void *callback);
-extern int im2_remove_generic_callback_callee(uint8_t vector,void *callback) __z88dk_callee;
+extern int im2_remove_generic_callback(uint8_t vector,void *callback) __preserves_regs(iyl,iyh);
+extern int im2_remove_generic_callback_callee(uint8_t vector,void *callback) __preserves_regs(iyl,iyh) __z88dk_callee;
 #define im2_remove_generic_callback(a,b) im2_remove_generic_callback_callee(a,b)
 
 
+
+#ifdef __CLANG
+
+#define IM2_DEFINE_ISR(name)       error clang does not support IM2_DEFINE_ISR;
+#define IM2_DEFINE_ISR_8080(name)  error clang does not support IM2_DEFINE_ISR_8080;
+
+#endif
+
+#ifdef __SCCZ80
+
+#define IM2_DEFINE_ISR(name)  void name(void) \
+{ \
+asm("\tEXTERN\tasm_im2_push_registers\n" \
+"\tEXTERN\tasm_im2_pop_registers\n" \
+"\n" \
+"\tcall\tasm_im2_push_registers\n" \
+"\tcall\t__im2_isr_" #name "\n" \
+"\tcall\tasm_im2_pop_registers\n" \
+"\n" \
+"\tei\n" \
+"\treti\n" \
+); \
+} \
+void _im2_isr_##name(void)
+
+#define IM2_DEFINE_ISR_8080(name)  void name(void) \
+{ \
+asm("\tEXTERN\tasm_im2_push_registers_8080\n" \
+"\tEXTERN\tasm_im2_pop_registers_8080\n" \
+"\n" \
+"\tcall\tasm_im2_push_registers_8080\n" \
+"\tcall\t__im2_isr_8080_" #name "\n" \
+"\tcall\tasm_im2_pop_registers_8080\n" \
+"\n" \
+"\tei\n" \
+"\treti\n" \
+); \
+} \
+void _im2_isr_8080_##name(void)
+
+#endif
+
+#ifdef __SDCC
+
+#define IM2_DEFINE_ISR(name)  void name(void) __naked \
+{ \
+	__asm \
+	EXTERN	asm_im2_push_registers \
+   EXTERN	asm_im2_pop_registers \
+	\
+	call	asm_im2_push_registers \
+	call   __im2_isr_##name \
+	call   asm_im2_pop_registers \
+	\
+	ei \
+	reti \
+	__endasm; \
+} \
+void _im2_isr_##name(void)
+
+#define IM2_DEFINE_ISR_8080(name)  void name(void) __critical __interrupt(0)
+
+#endif
 
 #endif
