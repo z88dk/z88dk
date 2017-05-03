7ac73dfd7d3833c18d199bc3308010317b828781
aralbrec <alvin_albrecht@hotmail.com>
2017-05-02 20:01:32 -0600

    #201 c interrupt state functions updated for the z180

--- libsrc/_DEVELOPMENT/z180/z180/asm_z180_get_int_state.asm
+++ libsrc/_DEVELOPMENT/z180/z180/asm_z180_get_int_state.asm
@@ -24,43 +24,8 @@ asm_cpu_get_int_state:
    ;
    ; uses  : af, hl
 
-   IF __Z80 & __Z80_NMOS
-   
-      ; nmos z80 bug prevents use of "ld a,i" to gather IFF2 into p/v flag
-      ; see http://www.z80.info/zip/ZilogProductSpecsDatabook129-143.pdf
-      
-      ; this is zilog's suggested solution, note status in carry flag not p/v
-      
-      ld hl,0
-      
-      push hl
-      pop hl                   ; zero written underneath SP
-      
-      scf
-      
-      ld a,i
-      jp pe, continue          ; carry set if ints enabled
-      
-      dec sp
-      dec sp
-      pop hl                   ; have a look at zero word underneath SP
-      
-      ld a,h
-      or l
-      jr z, continue           ; int did not occur, ints are disabled, carry reset
-      
-      scf                      ; int occurred, set carry
+   ld a,i
 
-   ELSE
-   
-      ; cmos z80 has no bug
-      
-      ld a,i
-
-   ENDIF
-
-continue:
-   
    push af
    pop hl
 
