a712dfc8653629c765a0d985f457485149377b82
Stefano <stefano_bodrato@hotmail.com>
2017-06-13 14:10:55 +0200

    Sharp PC-G8xx

---
+++ libsrc/stdio/ansi/g800/f_ansi_bel.asm
@@ -0,0 +1,22 @@
+;
+; 	ANSI Video handling for the Sharp PC G-800 family
+;
+;	Stefano Bodrato - 2017
+;
+;
+; 	BEL - chr(7)   Beep it out
+;
+;
+;	$Id: f_ansi_bel.asm$
+;
+
+        SECTION  code_clib
+	PUBLIC	ansi_BEL
+
+
+; No sound, for now!
+
+.ansi_BEL
+
+        ret
+
