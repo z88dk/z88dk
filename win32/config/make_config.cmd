b7d4db3c166845daf751b1c84c1260d067a2b866
aralbrec <alvin_albrecht@hotmail.com>
2017-06-30 21:55:55 -0600

    Ensure vs build's src/config.h is created every time a compile occurs

--- win32/config/make_config.cmd
+++ win32/config/make_config.cmd
@@ -4,14 +4,14 @@ set dir=%~dp0\..\..\src
 pushd %dir%
 set today=%date%
 set today=%today:-=%
-if not exist config.h (
+rem if not exist config.h (
   echo #define PREFIX "/usr/local/share/z88dk"	>  config.h
-  echo #define UNIX 1							>> config.h
+  echo #define WIN32 1							>> config.h
   echo #define EXEC_PREFIX ""					>> config.h
-  echo #define Z88DK_VERSION "build-%today%"	>> config.h
+  echo #define Z88DK_VERSION "MSC build-%today%"	>> config.h
   echo Created %dir%\config.h
-) else (
-  echo File exists %dir%\config.h
-)
+rem ) else (
+rem   echo File exists %dir%\config.h
+rem )
 popd
 endlocal
