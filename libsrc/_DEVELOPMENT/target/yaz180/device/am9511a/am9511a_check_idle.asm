7c54e4c7012d62eb0a0e64f78f587ba2005a5b93
Phillip Stevens <phillip.stevens@gmail.com>
2017-09-05 11:04:59 +1000

    yaz180 - am9511a driver c interface (simple) (#345)
    * rc2014 basic heap & stack relocation
    
    * yaz180 3rd commit
    
    * adjust rc2014 bss config
    
    * yaz180 4th commit
    
    * minor rc2014 & yaz180 corrections
    
    * extend config_target for integrated hardware
    
    * IDE via PIO 82C55 - infrastructure
    
    * use eval() in config_target.m4
    
    * rc2014 - add leading underscores to config_target.m4
    
    * rc2014 - add leading __IO_
    
    * yaz180 - convert config_target defines to double underscore
    
    * yaz180 - handle ide errors cleanly
    
    * add asm diskio functions
    
    * use ccf where carry flag is cleared
    
    * Revert "use ccf where carry flag is cleared" - complement (not clear).
    
    This reverts commit 751efb1e7278716c137b66e18e83af1343e3f93e.
    
    * yaz180 - ccf is complement not clear
    
    * yaz180 - c interface for diskio
    
    * yaz180 - 8255 IDE interface - WIP
    
    * yaz180 - C interface for 8255 IDE interface - WIP
    
    * yaz180 - IDE asm to working state
    
    * yaz180 - minor copy_word improvement
    
    * yaz180 - APU operation definitions
    
    * yaz180 - am9511a driver
    
    * yaz180 - am9511a driver readme
    
    * yaz180 - am9511a driver readme update
    
    * yaz180 - am9511a driver readme update
    
    * Update readme.md
    
    markdown fixes
    
    * Update readme.md
    
    * command language simplification
    
    * yaz180 - adjust APU status handling
    
    * yaz180 - am9511a c interface

--- libsrc/_DEVELOPMENT/target/yaz180/device/am9511a/am9511a_check_idle.asm
+++ libsrc/_DEVELOPMENT/target/yaz180/device/am9511a/am9511a_check_idle.asm
@@ -5,34 +5,32 @@
 ;       and we'll be interrupted for Command entry.
 ;       Use after the first APU_ISR call.
 ;
-;       A = contents of APUError (aggregation of any errors found)
-;       SCF if no errors
+;       L = contents of (APUStatus || APUError)
+;       SCF if no errors (aggregation of any errors found)
+;
+;       APUError is zeroed on return
+;       Uses AF, HL
 
     INCLUDE "config_private.inc"
 
     SECTION code_driver
 
-    PUBLIC _am9511a_chk_idle
+    PUBLIC asm_am9511a_chk_idle
 
     EXTERN APUStatus, APUError
 
-    _am9511a_chk_idle:
-        push bc
+    asm_am9511a_chk_idle:
+        ld a, (APUStatus)       ; get the status of the APU (but don't disturb APU)
+        tst __IO_APU_STATUS_BUSY    ; check busy bit is set,
+        jr nz, asm_am9511a_chk_idle ; so we wait
 
-    am9511a_check_loop:
-        ld a, (APUStatus)       ; get the status of the APU
-        or a                    ; check it is zero (NOP) command
-        jr nz, am9511a_check_loop   ; otherwise wait
-
-        ld bc, __IO_APU_PORT_STATUS ; the address of the APU status port in bc
-        in a, (c)               ; read the APU status port
-        and __IO_APU_STATUS_BUSY    ; busy bit set?
-        jr nz, am9511a_check_loop   ; yes, then wait
-
-        pop bc        
-
-        ld a, (APUError)        ; get the aggregated errors collected
-        or a
-        ret nz                  ; return with no carrry if errors
+        ld hl, APUError
+        or (hl)                 ; collect the aggregated errors, with APUStatus
+        tst __IO_APU_STATUS_ERROR   ; any errors?
+        ld (hl), 0              ; clear any aggregated errors in APUError
+        ld h, 0
+        ld l, a
+        ret nz                  ; return with no carry if errors
         scf                     ; set carry flag
-        ret                     ; return with APUError in a, carry set if no errors
+        ret                     ; return with (APUStatus || APUError) with carry set if no errors
+
