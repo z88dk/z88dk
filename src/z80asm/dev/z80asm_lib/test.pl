182fdf2870346eb24a87ad6dfe79c6bc71d95956
Paulo Custodio <pauloscustodio@gmail.com>
2017-09-09 13:29:23 +0100

    Use ticks to test z80asm*.lib
    Assemble the opcode with --cpu=z80, obtaining the z80 opcode, and with --cpu=r2k obtaining the
    call to the emulation routine in z80asm*.lib and compare the results.
    
    Uses ticks to run the code and output the register values.
    
    Add tests to cpd and cpi.

---
+++ src/z80asm/dev/z80asm_lib/test.pl
@@ -0,0 +1,61 @@
+#------------------------------------------------------------------------------
+# Z88DK Z80 Macro Assembler
+#
+# Test z80asm-*.lib
+#
+# Copyright (C) Paulo Custodio, 2011-2017
+# License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+# Repository: https://github.com/z88dk/z88dk
+#------------------------------------------------------------------------------
+use strict;
+use warnings;
+use v5.10;
+use Test::More;
+require '../../t/testlib.pl';
+
+# CPUs not supported by ticks: z80-zxn z180 r3k
+my @CPUS = (qw( z80 r2k ));
+
+# CPI
+for my $cpu (@CPUS) {
+	for my $carry (0, 1) {
+		for my $data (1, 2, 3) {
+			my $a = 2;
+			for my $bc (2, 1) {
+				for my $op (qw(cpi cpd)) {
+					note "cpu: $cpu, data: $data, a: $a, bc: $bc, op: $op";
+					my $carry_set = $carry ? "scf" : "and a";
+					my $r = ticks(<<END, "--cpu=$cpu -b -m -l");
+						extern __tail
+						defc data = 0x100
+								$carry_set
+								ld hl, data
+								ld (hl), $data
+								ld a, $a
+								ld bc, $bc
+								
+								$op
+								
+								jp __tail
+END
+					# system "z88dk-dis -m$cpu -x test.map test.bin";
+					# use Data::Dump 'dump'; say dump($r);
+					
+					is $r->{F_S}, ($a <  $data 		? 1 : 0), "S";
+					is $r->{F_Z}, ($a == $data 		? 1 : 0), "Z";
+					is $r->{F_H}, ($a <  $data 		? 1 : 0), "Hf";
+					is $r->{F_PV}, ($r->{BC} == 0 	? 0 : 1), "PV";
+					is $r->{F_N}, 1,						  "N";
+					is $r->{F_C}, $carry,					  "C";					
+					is $r->{HL}, $op eq 'cpi' ? 0x101 : 0x0FF,"HL";
+					is $r->{BC}, $bc - 1,					  "BC";
+				}
+			}
+		}
+
+	}
+}
+
+unlink_tests();
+done_testing();
+
--- src/z80asm/t/testlib.pl
+++ src/z80asm/t/testlib.pl
@@ -10,8 +10,15 @@
 use strict;
 use warnings;
 use v5.10;
-
 use Test::More;
+use File::Basename;
+
+# add path to z80asm top directory
+my $IS_WIN32 = $^O =~ /MSWin32/;
+my $root = dirname(dirname(__FILE__));
+$root =~ s!/!\\!g if $IS_WIN32;
+$ENV{PATH} = $root . ($IS_WIN32 ? ';' : ':') . $ENV{PATH};
+
 
 sub slurp {
 	my($file) = @_;
@@ -41,14 +48,14 @@ sub run {
 	my $goterr = slurp("test.stderr");
 	
 	if ($out eq "IGNORE") {
-		diag "test.stdout: ", $gotout;
+		note "test.stdout: ", $gotout;
 	}
 	else {
 		is norm_nl($gotout), norm_nl($out), "test.stdout";
 	}
 	
 	if ($err eq "IGNORE") {
-		diag "test.stderr: ", $goterr;
+		note "test.stderr: ", $goterr;
 	}
 	else {
 		is norm_nl($goterr), norm_nl($err), "test.stderr";
@@ -78,7 +85,7 @@ sub hexdump {
 
 sub unlink_tests {
 	if ($ENV{KEEP}) {
-		diag "kept test files";
+		note "kept test files";
 	}
 	else {
 		if (Test::More->builder->is_passing) {
@@ -91,20 +98,68 @@ sub unlink_tests {
 
 sub z80asm {
 	my($source, $options) = @_;
-	$options //= "-b -l";
+	$options //= "-b";
 	
 	spew("test.asm", $source);
 	run("z80asm $options test.asm");
 }
 
 sub ticks {
-	my($source) = @_;
-	z80asm($source);
-	run("ticks test.bin -output test.out", 0, "IGNORE");
+	my($source, $options) = @_;
+	z80asm($source, $options);
+	my $cpu = ($options =~ /--cpu=(\S+)/) ? $1 : "z80";
+	my $end = -s "test.bin";
+	run("ticks test.bin -m$cpu -end ".sprintf("%04X", $end)." -output test.out", 0, "IGNORE");
 	my $bin = slurp("test.out");
-	my @mem = map {ord} split //, substr($bin, 0, 0x10000);
+	my $mem = substr($bin, 0, 0x10000); $mem =~ s/\0+$//;
+	my @mem = map {ord} split //, $mem;
 	my @regs = map {ord} split //, substr($bin, 0x10000);
-	return [\@mem, \@regs];
+	my $ret = {
+		mem 	=> \@mem, 
+	};
+	$ret->{F} = shift @regs;	$ret->{F_S}  = ($ret->{F} & 0x80) ? 1 : 0;
+								$ret->{F_Z}  = ($ret->{F} & 0x40) ? 1 : 0;
+								$ret->{F_H}  = ($ret->{F} & 0x10) ? 1 : 0;
+								$ret->{F_PV} = ($ret->{F} & 0x04) ? 1 : 0;
+								$ret->{F_N}  = ($ret->{F} & 0x02) ? 1 : 0;
+								$ret->{F_C}  = ($ret->{F} & 0x01) ? 1 : 0;
+	$ret->{A} = shift @regs;
+	$ret->{C} = shift @regs;
+	$ret->{B} = shift @regs;	$ret->{BC} = ($ret->{B} << 8) | $ret->{C};
+	$ret->{L} = shift @regs;
+	$ret->{H} = shift @regs;	$ret->{HL} = ($ret->{H} << 8) | $ret->{L};
+	my $PCl = shift @regs;
+	my $PCh = shift @regs;		$ret->{PC} = ($PCh << 8) | $PCl;
+	my $SPl = shift @regs;
+	my $SPh = shift @regs;		$ret->{SP} = ($SPh << 8) | $SPl;
+	$ret->{I} = shift @regs;
+	$ret->{R} = shift @regs;
+	$ret->{E} = shift @regs;
+	$ret->{D} = shift @regs;	$ret->{DE} = ($ret->{D} << 8) | $ret->{E};
+	$ret->{C_} = shift @regs;
+	$ret->{B_} = shift @regs;	$ret->{BC_} = ($ret->{B_} << 8) | $ret->{C_};
+	$ret->{E_} = shift @regs;
+	$ret->{D_} = shift @regs;	$ret->{DE_} = ($ret->{D_} << 8) | $ret->{E_};
+	$ret->{L_} = shift @regs;
+	$ret->{H_} = shift @regs;	$ret->{HL_} = ($ret->{H_} << 8) | $ret->{L_};
+	$ret->{F_} = shift @regs;	$ret->{F__S}  = ($ret->{F_} & 0x80) ? 1 : 0;
+								$ret->{F__Z}  = ($ret->{F_} & 0x40) ? 1 : 0;
+								$ret->{F__H}  = ($ret->{F_} & 0x10) ? 1 : 0;
+								$ret->{F__PV} = ($ret->{F_} & 0x04) ? 1 : 0;
+								$ret->{F__N}  = ($ret->{F_} & 0x02) ? 1 : 0;
+								$ret->{F__C}  = ($ret->{F_} & 0x01) ? 1 : 0;
+	$ret->{A_} = shift @regs;
+	my $IYl = shift @regs;
+	my $IYh = shift @regs;		$ret->{IY} = ($IYh << 8) | $IYl;
+	my $IXl = shift @regs;
+	my $IXh = shift @regs;		$ret->{IX} = ($IXh << 8) | $IXl;
+	$ret->{IFF} = shift @regs;
+	$ret->{IM} = shift @regs;
+	my $MPl = shift @regs;
+	my $MPh = shift @regs;		$ret->{MP} = ($MPh << 8) | $MPl;
+	@regs == 8 or die;
+	
+	return $ret;
 }
 
 1;
