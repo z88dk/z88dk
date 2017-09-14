7aa0a3409a82209528513db67637d6b249be4021
Stefano <stefano_bodrato@hotmail.com>
2017-09-14 12:37:59 +0200

    #370, #355

---
+++ libsrc/stdio/ansi/gotoxy_callee.asm
@@ -0,0 +1,32 @@
+; void gotoxy_callee(uint x, uint y)
+; 09.2017 stefano
+
+SECTION code_clib
+PUBLIC gotoxy_callee
+PUBLIC _gotoxy_callee
+PUBLIC ASMDISP_GOTOXY_CALLEE
+
+EXTERN ansi_COLUMN
+EXTERN ansi_ROW
+
+.gotoxy_callee
+._gotoxy_callee
+
+   pop af
+   pop de
+   pop bc
+   push af
+
+.asmentry
+
+   ; c = x    e = y
+   ld	a,c
+   dec	a
+   ld	(ansi_COLUMN),a
+   dec	a
+   ld	a,e
+   ld	(ansi_ROW),a
+
+   ret
+
+DEFC ASMDISP_GOTOXY_CALLEE = # asmentry - gotoxy_callee
