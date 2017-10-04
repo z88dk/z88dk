a97ac4f8b98b28d2e2b0c94e75d80b1c101eab98
Phillip Stevens <phillip.stevens@gmail.com>
2017-10-04 23:53:48 +1100

    rc2014 - IDE 9% faster

--- libsrc/_DEVELOPMENT/target/rc2014/device/8255/8255_write_block.asm
+++ libsrc/_DEVELOPMENT/target/rc2014/device/8255/8255_write_block.asm
@@ -1,16 +1,9 @@
 
+INCLUDE "config_private.inc"
+
 SECTION code_driver
 
 PUBLIC ide_write_block
-
-EXTERN __IO_PIO_IDE_LSB, __IO_PIO_IDE_MSB, __IO_PIO_IDE_CTL, __IO_PIO_IDE_CONFIG
-EXTERN __IO_PIO_IDE_RD, __IO_PIO_IDE_WR
-
-EXTERN __IO_IDE_WR_LINE, __IO_IDE_RD_LINE, __IO_IDE_RST_LINE
-
-EXTERN __IO_IDE_DATA, __IO_IDE_ERROR, __IO_IDE_SEC_CNT
-EXTERN __IO_IDE_COMMAND, __IO_IDE_STATUS
-EXTERN __IO_IDE_CONTROL, __IO_IDE_ALT_STATUS
 
     ;Write a block of 512 bytes (one sector) from (HL++) to
     ;the drive 16 bit data register
@@ -24,6 +17,22 @@ ide_write_block:
     ld d, __IO_IDE_DATA
     out (c), d              ;drive address onto control lines
     ld e, $0                ;keep iterative count in e
+
+IF (__IO_PIO_IDE_CTL = __IO_PIO_IDE_MSB+1) & (__IO_PIO_IDE_MSB = __IO_PIO_IDE_LSB+1)
+ide_wrblk2: 
+    ld d, __IO_IDE_DATA|__IO_IDE_WR_LINE
+    out (c), d              ;and assert write pin
+    ld bc, __IO_PIO_IDE_LSB ;drive lower lines with lsb
+    outi                    ;write the lower byte (HL++)
+    inc c                   ;drive upper lines with msb
+    outi                    ;write the upper byte (HL++)
+    inc c                   ;drive control port
+    ld d, __IO_IDE_DATA
+    out (c), d              ;deassert write pin
+    dec e                   ;keep iterative count in e
+    jr nz, ide_wrblk2
+
+ELSE
 ide_wrblk2: 
     ld d, __IO_IDE_DATA|__IO_IDE_WR_LINE
     out (c), d              ;and assert write pin
@@ -36,6 +45,8 @@ ide_wrblk2:
     out (c), d              ;deassert write pin
     dec e                   ;keep iterative count in e
     jr nz, ide_wrblk2
+
+ENDIF
 ;   ld bc, __IO_PIO_IDE_CTL ;remembering what's in bc
     ld d, $0
     out (c), d              ;deassert all control pins
