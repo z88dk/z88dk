8a273c1b130dc4cc2654ef6b8eeee0af0ac8ac07
Stefano <stefano_bodrato@hotmail.com>
2017-10-14 22:20:21 +0200

    Create joystick.asm

---
+++ libsrc/games/aquarius/joystick.asm
@@ -0,0 +1,57 @@
+;
+;	Generic game device library - Aquarius port
+;	Stefano Bodrato - 2017
+;
+;	$Id: joystick.asm $
+;
+
+
+        SECTION code_clib
+	PUBLIC    joystick
+	PUBLIC    _joystick
+	EXTERN	getk
+
+.joystick
+._joystick
+	;__FASTALL__ : joystick no. in HL
+		
+	;ld	a,l
+
+	call	getk
+	ld	a,l
+	ld	l,0
+	cp	' '
+	jr	nz,no_fire1_a
+	set	4,l
+	jr	j_done
+.no_fire1_a
+	cp	'1'
+	jr	nz,no_fire2_a
+	set	5,l
+	jr	j_done
+.no_fire2_a
+	cp	9
+	jr	nz,no_up_a
+	set	3,l
+	jr	j_done
+.no_up_a
+	cp	10
+	jr	nz,no_down_a
+	set	2,l
+	jr	j_done
+.no_down_a
+	cp	8
+	jr	nz,no_left_a
+	set	1,l
+	jr	j_done
+.no_left_a
+	cp	7
+	jr	nz,no_right_a
+	set	0,l
+.no_right_a
+
+.j_done
+
+	ret
+
+	
