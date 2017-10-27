2910a8824bcd55c99b88f0d5f75c72789664ab51
Stefano <stefano_bodrato@hotmail.com>
2017-10-27 21:45:00 +0200

    txt32 (mode G1) for SVI, font left commented

--- libsrc/msx/msx_set_mode.asm
+++ libsrc/msx/msx_set_mode.asm
@@ -20,6 +20,7 @@
 IF FORsvi
     INCLUDE	"msx/vdp.inc"
     INCLUDE "svibios.def"
+    INCLUDE "svibasic.def"
 ENDIF
 
 msx_set_mode:
@@ -46,13 +47,64 @@ ELSE
 txt32:
 	ld    hl,INIGRP		; (Graphics 2)
 	call  setmode
+
 	; Now bend the configuration to Graphics 1 (change reg#0)
 	xor   a
+	ld    (RG0SAV),a
 	di
 	out   (VDP_CMD),a
-	xor   a
+	nop     ; ..do we have timing constraints ?
 	or    $80
 	ei
 	out   (VDP_CMD),a
+
+	; Bend register #3
+	ld	a,$80
+	di
+	out   (VDP_CMD),a
+	ld    a,3  ; reg3
+	or    $80
+	ei
+	out   (VDP_CMD),a
+	
+	; Bend register #4
+	xor	  a
+	di
+	out   (VDP_CMD),a
+	ld    a,4  ; reg4  -  PT./TXT/MCOL-GEN.TAB.
+	or    $80
+	ei
+	out   (VDP_CMD),a
+	
+	
+	ld	hl,$1800		; SCREEN 1 name table
+	ld	bc,32*24
+	ld	a,c	; ' '
+	ld ix,FILVRM
+	call	msxbios
+	
+	LD A,(FORCLR)
+	ADD A,A
+	ADD A,A
+	ADD A,A
+	ADD A,A
+	LD HL,BAKCLR
+	OR (HL)
+	ld	hl,$2000		; SCREEN 1 color table
+	ld	bc,32
+;	ld a,$1F
+	ld ix,FILVRM
+	call	msxbios
+
+;	ld	hl,font
+;	ld	de,0			; SCREEN 1 character pattern table
+;	ld	bc,768
+;	ld	ix,LDIRVM
+;	call	msxbios
+
 	ret
+	
+;.font
+;binary "../stdio/ansi/FONT8.BIN"
+
 ENDIF
