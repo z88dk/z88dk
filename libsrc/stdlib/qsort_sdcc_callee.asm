c4cdfb71afbf43c8520e856dd575950afb03ab88
suborb <dom@suborbital.org.uk>
2017-11-13 21:08:49 +0000

    Issue #312: Remove optimisations in library code

--- libsrc/stdlib/qsort_sdcc_callee.asm
+++ libsrc/stdlib/qsort_sdcc_callee.asm
@@ -65,11 +65,7 @@ asm_qsort:
    ld (_base),bc
 
    ; t1 = nel * width;
-IF __CPU_Z80_ZXN__
-   mul
-ELSE
    call  l_mult
-ENDIF
    ld (_t1),hl
 
    ; for (ngap = nel / 2; ngap > 0; ngap /= 2) {
