a658f168864f7b4d0292e81be04ebb8120acf8a1
Stefano <stefano_bodrato@hotmail.com>
2017-11-28 23:55:29 +0100

    Update w_move.asm

--- libsrc/graphics/w_move.asm
+++ libsrc/graphics/w_move.asm
@@ -6,13 +6,15 @@
 ;
 
         SECTION code_clib
-                PUBLIC    move
-                PUBLIC    _move
-                EXTERN     swapgfxbk
-                EXTERN	__graphics_end
+			PUBLIC    move
+			PUBLIC    _move
+			EXTERN     swapgfxbk
+			EXTERN	__graphics_end
 
-                EXTERN     w_line_r
-                EXTERN     w_setxy
+			EXTERN	__pen
+			EXTERN     w_line_r
+			EXTERN     w_plotpixel
+			EXTERN     w_setxy
 
 
 .move
@@ -24,8 +26,16 @@
 		ld	d,(ix+3)
 		ld	l,(ix+4)
 		ld	h,(ix+5)
-                call    swapgfxbk
-                ld      ix,w_setxy
-                call    w_line_r
-                jp      __graphics_end
+		
+		call    swapgfxbk
+				
+		ld	a,(__pen)
+		ld	ix,w_setxy
+		rla
+		jr	nc,pen_up
+		ld	ix,w_plotpixel
+.pen_up
+
+		call    w_line_r
+		jp      __graphics_end
 
