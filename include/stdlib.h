e96ecd696f87b5a49cc081f5c309d2c283ad46e3
Stefano <stefano_bodrato@hotmail.com>
2017-12-13 07:30:27 +0100

    #546,

--- include/stdlib.h
+++ include/stdlib.h
@@ -303,11 +303,19 @@ extern unsigned int  __LIB__  wpeek(const void *addr) __z88dk_fastcall;
 // ACCURATE T-STATE DELAY
 extern void   __LIB__    t_delay(unsigned int tstates) __z88dk_fastcall;   // at least 141 T
 
+#ifdef __Z88__
+extern void   __LIB__ __SAVEFRAME__     sleep (int secs) __z88dk_fastcall;
+/* Very non standard! sleep for centisecs! (z88 and others)*/
+extern void   __LIB__    __SAVEFRAME__  csleep(unsigned int centiseconds) __z88dk_fastcall;
+extern void   __LIB__     delay (unsigned long milliseconds) __z88dkfastcall;
+#else
 extern void   __LIB__ __SAVEFRAME__     sleep (int secs);
 /* Very non standard! sleep for centisecs! (z88 and others)*/
 extern void   __LIB__    __SAVEFRAME__  csleep(unsigned int centiseconds);
 extern void   __LIB__     delay (unsigned long milliseconds);
+#endif
 
+extern void __LIB__ delay (unsigned long milliseconds);
 
 
 /*********/
