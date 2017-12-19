3c197cc2176e95f6ea68bec428b1cd72f6fc407a
Stefano <stefano_bodrato@hotmail.com>
2017-12-19 14:06:44 +0100

    #453

--- libsrc/graphics/msx/gen_clg.asm
+++ libsrc/graphics/msx/gen_clg.asm
@@ -2,22 +2,30 @@
 ;	z88dk library: Generic VDP support code
 ;
 ;
-;	$Id: gen_clg.asm,v 1.4 2016-06-21 20:16:35 dom Exp $
+;	$Id: gen_clg.asm $
 ;
 
 	SECTION	code_clib
         PUBLIC  clg
         PUBLIC  _clg
         
+IF FORsc3000
+        EXTERN	msx_color
+        EXTERN	FILVRM
+ELSE
         EXTERN	msx_set_mode
         EXTERN	msx_color
         EXTERN	FILVRM
+ENDIF
 
 .clg
 ._clg
+IF FORsc3000
+	call $39E2
+ELSE
 	ld    hl,2		; set graphics mode
 	call  msx_set_mode
-
+ENDIF
 	ld	hl,15
 	push hl		; border
 	push hl		; paper
