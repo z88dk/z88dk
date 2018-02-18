fab5c675d13358c4712484b3234ac97983600cd3
suborb <dom@suborbital.org.uk>
2018-02-18 14:39:21 +0000

    Update esxdos open to handle O_APPEND,O_TRUNC and the correct r/w flags

--- libsrc/fcntl/spectrum/esxdos/open.c
+++ libsrc/fcntl/spectrum/esxdos/open.c
@@ -9,14 +9,27 @@ int open(char *name, int flags, mode_t mode) __naked
     push    ix
     ld      hl,6	;flags
     add     hl,sp
-    ld      b,(hl)
+    ld      a,(hl)	; esxdos flags are +1 on C flags
+    inc     a
+    and     3
     inc     hl
+    ld      c,8		; esx_mode_open_creat
+    bit     0,(hl)	;O_APPEND
+    jr      z,not_o_append
+    ld      c,0		; esx_mode_open_exist
+not_o_append:
+    bit     1,(hl)	;O_TRUNC
+    jr      z,not_o_trunc
+    ld	    c,$0c		; esx_mode_creat_trunc
+not_o_trunc:
+    or      c		; add in open mode
+    ld      b,a
     inc     hl
     ld      e,(hl)	;filename
     inc     hl
     ld      d,(hl)
     ex      de,hl
-    ld      a,'*'
+    ld      a,$2a	;*
     call    asm_esxdos_f_open
     jr      nc,end
     ld      h,0
