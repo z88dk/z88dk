31db9afa19281eb326c8f1b30c2b4317b31feab2
suborb <dom@suborbital.org.uk>
2018-02-21 21:17:56 +0000

    Remove csleep() - now a macro for msleep(x/10) on platforms apart from the z88.
    Implement sleep() in terms of msleep(). Remove the explicit
    t_delay code and point to the newlib equivalent.
    
    z80 sleep is still the old method - though I'm not convinced about
    it totally

--- libsrc/stdlib/t_delay.asm
+++ libsrc/stdlib/t_delay.asm
@@ -3,51 +3,9 @@
 SECTION code_clib
 PUBLIC t_delay
 PUBLIC _t_delay
+EXTERN asm_cpu_delay_tstate
 
-; Z80 delay routine
-; by Jan Bobrowski, license GPL
-; wait hl T (including call; hl>=141)
-; destroys: af, bc, hl
 
-.t_delay
-._t_delay
+defc t_delay = asm_cpu_delay_tstate
+defc _t_delay = asm_cpu_delay_tstate
 
-        ld      bc,-141
-        add     hl,bc
-        ld      bc,-23
-        
-.loop
-
-        add     hl,bc
-        jr      c, loop
-        
-        ld      a,l
-        add     a,15
-        jr      nc, g0
-        
-        cp      8
-        jr      c, g1
-        
-        or      0
-        
-.g0
-
-        inc     hl
-
-.g1
-
-        rra
-        jr      c, b0
-        nop
-
-.b0
-
-        rra
-        jr      nc, b1
-        or      0
-        
-.b1
-
-        rra
-        ret     nc
-        ret
