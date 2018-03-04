e6666b175766c8ac28d4a2085a5cfec8ccf1ba0c
Phillip Stevens <phillip.stevens@gmail.com>
2018-03-04 17:06:16 +1100

    yaz180 - make yabios discern between asci options

--- libsrc/_DEVELOPMENT/target/yaz180/driver/yabios/asm_common1_data.asm
+++ libsrc/_DEVELOPMENT/target/yaz180/driver/yabios/asm_common1_data.asm
@@ -100,6 +100,11 @@ APUStatus:              defb    0
 APUError:               defb    0
 _APULock:               defb    $FE             ; mutex for APU
 
+; currently active console interface, only bit 0 is distinguished currently with TTY=0 CRT=1.
+PUBLIC  _bios_ioByte
+
+_bios_ioByte:   defb    0               ; intel I/O byte
+
 PUBLIC asci0RxCount, asci0RxIn, asci0RxOut, _asci0RxLock
 
 asci0RxCount:   defb    0               ; Space for Rx Buffer Management 
