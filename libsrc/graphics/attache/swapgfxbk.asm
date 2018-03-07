eacc0fca5e934ca32e4cc0184ac266c9adcf215a
Stefano <stefano_bodrato@hotmail.com>
2018-03-07 15:47:55 +0100

    Create swapgfxbk.asm

---
+++ libsrc/graphics/attache/swapgfxbk.asm
@@ -0,0 +1,33 @@
+;
+;       Otrona Attachè graphics lib
+;       Stefano Bodrato 2018
+;
+;
+;	Video memory paging.
+;
+;
+;	$Id: swapgfxbk.asm $
+;
+
+
+	        SECTION code_clib
+                PUBLIC    swapgfxbk
+                PUBLIC    _swapgfxbk
+
+                PUBLIC    swapgfxbk1
+                PUBLIC    _swapgfxbk1
+
+
+.swapgfxbk
+._swapgfxbk
+
+.swapgfxbk1
+._swapgfxbk1
+
+	ret
+
+
+
+
+
+
