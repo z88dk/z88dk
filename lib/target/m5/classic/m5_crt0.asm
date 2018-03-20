cbb744be427eea6cbe05045e015339cf9092bd09
suborb <dom@suborbital.org.uk>
2018-03-20 19:26:36 +0000

    Fix #656: Missing quote preventing m5 from compiling

--- lib/target/m5/classic/m5_crt0.asm
+++ lib/target/m5/classic/m5_crt0.asm
@@ -48,7 +48,7 @@ ENDIF
         defc    TAR__clib_exit_stack_size = 32
         defc    TAR__register_sp = -1
 	defc	__CPU_CLOCK = 3580000
-        INCLUDE "crt/classic/crt_rules.inc
+        INCLUDE "crt/classic/crt_rules.inc"
         org     CRT_ORG_CODE
 
 start:
