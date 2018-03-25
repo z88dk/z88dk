79acc827cd66ee2b3d60df6605b7001ca6638ee9
aralbrec <aralbrec@users.noreply.github.com>
2018-03-24 21:07:49 -0600

    Update am48_dcmp_s.asm

--- libsrc/_DEVELOPMENT/math/float/math48/z80/am48_dcmp_s.asm
+++ libsrc/_DEVELOPMENT/math/float/math48/z80/am48_dcmp_s.asm
@@ -6,6 +6,8 @@ PUBLIC am48_dcmp_s
 
 EXTERN am48_dcmp
 
+am48_dcmp_s:
+
    ; compare double on stack to AC' (effective double - AC')
    ;
    ; enter : AC'   = double y
@@ -20,7 +22,6 @@ EXTERN am48_dcmp
    ;
    ; uses  : AF, BC, DE, HL
 
-am48_dcmp_s:
    pop af
    
    pop hl                      ; AC = x
