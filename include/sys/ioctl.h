cbe2df7c9f5f454b63327e8e0b47f566403a6c39
suborb <dom@suborbital.org.uk>
2018-05-05 16:24:25 +0100

    Rername ioctl to a more pleasant name. Commit the +ace changes

--- include/sys/ioctl.h
+++ include/sys/ioctl.h
@@ -2,13 +2,14 @@
 #define SYS_IOCTL_H
 
 #include <sys/types.h>
+#include <sys/compiler.h>
 
-extern int __LIB__ fputc_cons_generic_ioctl(uint16_t cmd, void *arg) __smallc;
+extern int __LIB__ console_ioctl(uint16_t cmd, void *arg) __smallc;
 
 #define IOCTL_GENCON_RAW_MODE	  1  /* Set raw terminal mode (int *) */
 #define IOCTL_GENCON_CONSOLE_SIZE 2  /* Get console size (int *) = (d<<8|w)  */
 #define IOCTL_GENCON_SET_FONT32   3  /* Set the address for the 32 column font (int *) */
 #define IOCTL_GENCON_SET_FONT64   4  /* Set the address for the 64 column font (int *) */
-#define IOCTL_GENCON_SET_UDGS     4  /* Set the address for the 64 column udgs (int *) */
+#define IOCTL_GENCON_SET_UDGS     5  /* Set the address for the 64 column udgs (int *) */
 
 #endif
--- libsrc/stdio/fputc_cons_generic_ioctl.c
+++ libsrc/stdio/fputc_cons_generic_ioctl.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 
-int fputc_cons_generic_ioctl(uint16_t cmd, void *arg) __naked 
+int console_ioctl(uint16_t cmd, void *arg) __naked 
 {
 #asm
 	EXTERN		generic_console_ioctl
