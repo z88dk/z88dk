806736a7f34e94c970e82b316fcb87478b03d826
suborb <dom@suborbital.org.uk>
2018-05-08 21:34:15 +0100

    Issue #732: Add an untested +mc1000 generic console.
    Supports mode 0 (text) and mode 1 (hires 256x192) - changable via
    ioctl.
    
    mode 1 supports udgs

---
+++ libsrc/stdio/mc1000/generic_console_ioctl.asm
@@ -0,0 +1,55 @@
+
+	MODULE	generic_console_ioctl
+	PUBLIC	generic_console_ioctl
+
+	SECTION	code_clib
+	INCLUDE	"ioctl.def"
+
+	EXTERN	generic_console_cls
+	EXTERN	__mc1000_font
+	EXTERN	__mc1000_udg
+	EXTERN	__mc1000_mode
+	EXTERN	__console_h
+
+
+; a = ioctl
+; de = arg
+generic_console_ioctl:
+	ex	de,hl
+	ld	c,(hl)	;bc = where we point to
+	inc	hl
+	ld	b,(hl)
+	cp	IOCTL_GENCON_SET_FONT32
+	jr	nz,check_set_udg
+	ld	(__mc1000_font),bc
+success:
+	and	a
+	ret
+check_set_udg:
+	cp	IOCTL_GENCON_SET_UDGS
+	jr	nz,check_mode
+	ld	(__mc1000_udg),bc
+	jr	success
+check_mode:
+	cp	IOCTL_GENCON_SET_MODE
+	jr	nz,failure
+	ld	a,c
+	and	a		;mode 0 = text
+	ld	h,16
+	ld	l,0
+	jr	z,set_mode
+	cp	1
+	ld	l,0x9e
+	ld	h,24
+	jr	nz,failure
+set_mode:
+	ld	a,h
+	ld	(__console_h),a
+	ld	a,l
+	ld	(__mc1000_mode),a
+	out	($80),a
+	call	generic_console_cls
+	jr	success
+failure:
+	scf
+	ret
