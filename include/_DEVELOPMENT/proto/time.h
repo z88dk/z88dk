66685c343df80bcde97e299ad1132aeaa05c906a
aralbrec <alvin_albrecht@hotmail.com>
2018-05-29 22:33:31 -0600

    newlib: start time.h with a few functions to convert between fat time and broken down time

--- include/_DEVELOPMENT/proto/time.h
+++ include/_DEVELOPMENT/proto/time.h
@@ -11,23 +11,23 @@ include(__link__.m4)
 
 struct tm
 {
-   int16_t tm_sec;     // 0-60 seconds (leap seconds accommodated)
-   int16_t tm_min;     // 0-59 minutes
-   int16_t tm_hour;    // 0-23 hour (since midnight)
-   int16_t tm_mday;    // 1-31 day of month
-   int16_t tm_mon;     // 0-11 month (since January)
-   int16_t tm_year;    // years since 1900 (signed)
+   uint8_t  tm_sec;     // 0-59 seconds (leap seconds may be accommodated)
+   uint8_t  tm_min;     // 0-59 minutes
+   uint8_t  tm_hour;    // 0-23 hour (since midnight)
+   uint8_t  tm_mday;    // 1-31 day of month
+   uint8_t  tm_mon;     // 0-11 month (since January)
+   int16_t  tm_year;    // years since 1900 (signed)
    
    // following is not filled in by some time functions
    
-   int16_t tm_wday;    // 0-6   day (since Sunday)
-   int16_t tm_yday;    // 0-365 day (since January 1)
-   int16_t tm_isdst;   // daylight savings time <0 (not avail), 0 (no), >0 (yes)
+   uint8_t  tm_wday;    // 0-6   day (since Sunday)
+   uint16_t tm_yday;    // 0-365 day (since January 1)
+   int8_t   tm_isdst;   // daylight savings time <0 (not avail), 0 (no), >0 (yes)
    
    // following is bsd/gnu extension and may not be filled in by some targets
    
-   int32_t tm_gmtoff;  // seconds to add to UTC for local time
-   const unsigned char tm_zone[6];   // name of timezone
+   // int32_t tm_gmtoff;  // seconds to add to UTC for local time
+   // const unsigned char tm_zone[6];   // name of timezone
 };
 
 // MSDOS Time for FAT
