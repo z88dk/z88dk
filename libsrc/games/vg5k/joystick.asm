6309371fe776ff826cb883279e3c0275bf253b3c
suborb <dom@suborbital.org.uk>
2018-06-25 22:05:17 +0100

    Issue #779: Add inkey() support for vg5k. Read joystick ports as well

--- libsrc/games/vg5k/joystick.asm
+++ libsrc/games/vg5k/joystick.asm
@@ -4,7 +4,18 @@
 ;
 ;	$Id: joystick.asm,v 1.3 2016-06-16 19:33:59 dom Exp $
 ;
-
+; 	Joystick 1 on 0x07
+; 	Joystick 2 on 0x08
+;
+;	Active low:
+;
+;	0x01 = up
+;	0x02 = right
+;	0x04 = down
+; 	0x08 = left
+;	0x10 = fire
+;
+; 
 
         SECTION code_clib
 	PUBLIC    joystick
@@ -14,44 +25,32 @@
 .joystick
 ._joystick
 	;__FASTALL__ : joystick no. in HL
-		
-	;ld	a,l
-
-	call	getk
 	ld	a,l
-	ld	l,0
-	cp	' '
-	jr	nz,no_fire1_a
-	set	4,l
-	jr	j_done
-.no_fire1_a
-	cp	'1'
-	jr	nz,no_fire2_a
-	set	5,l
-	jr	j_done
-.no_fire2_a
-	cp	9
-	jr	nz,no_up_a
+	ld	bc,0x07
+	cp	1
+	jr	nz,read_joystick
+	inc	bc	; bc = 0x08
+read_joystick:
+	ld	hl,0	;return value
+	in	a,(c)
+	rrca
+	jr	c,no_up
 	set	3,l
-	jr	j_done
-.no_up_a
-	cp	10
-	jr	nz,no_down_a
-	set	2,l
-	jr	j_done
-.no_down_a
-	cp	8
-	jr	nz,no_left_a
-	set	1,l
-	jr	j_done
-.no_left_a
-	cp	7
-	jr	nz,no_right_a
+no_up:
+	rrca
+	jr	c,no_right
 	set	0,l
-.no_right_a
-
-.j_done
-
+no_right:
+	rrca
+	jr	c,no_down
+	set	2,l
+no_down:
+	rrca
+	jr	c,no_left
+	set	1,l
+no_left:
+	rrca
+	ret	c
+	set	4,l	;fire
 	ret
 
-	
