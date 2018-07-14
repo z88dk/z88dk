6147692da082e48baf5f1fefb992ffc5f7a4e827
suborb <dom@suborbital.org.uk>
2018-07-14 17:24:34 +0100

    Looks like rotate (hl) didn't actually rotate (hl).
    Add a CP/M loading bodge so we can run zexall/zexdoc

--- src/ticks/hook.c
+++ src/ticks/hook.c
@@ -9,6 +9,25 @@ void PatchZ80(void)
 {
     int   val;
 
+    // CP/M Emulation bodge
+    if ( pc == 7 ) {
+       if ( c == 0x02 ) {
+           fputc(e, stdout);
+	   fflush(stdout);
+       } else if ( c == 0x09 ) {
+           // Print string
+           int addr = d << 8 | e;
+           int tp;
+           while ( ( tp = *get_memory_addr(addr)) ) {
+               if ( tp == '$' ) break;
+               fputc(tp, stdout);
+		addr++;
+           }
+	   fflush(stdout);
+      }
+       return;
+    }
+
     if ( hooks[a] != NULL ) {
         hooks[a]();
     } else {
