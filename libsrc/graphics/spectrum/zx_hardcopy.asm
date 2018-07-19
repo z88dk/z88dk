8553128774f64643be1c1a75488162f79b3a077f
Stefano <stefano_bodrato@hotmail.com>
2018-07-19 10:33:36 +0200

    $853

--- libsrc/graphics/spectrum/zx_hardcopy.asm
+++ libsrc/graphics/spectrum/zx_hardcopy.asm
@@ -13,5 +13,10 @@
 
 .zx_hardcopy
 ._zx_hardcopy
-
+		ld	hl, $0A02
+		ld	a,(hl)
+		cp	$f3		; DI instruction, should be in that position only for the TS2068
+		jr	nz,no_ts2068
+		jp	(hl)
+.no_ts2068
 		jp	$0EAC
