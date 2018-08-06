7b25c2d8456fe96b8f8aab8ea734ad90d659540e
aralbrec <alvin_albrecht@hotmail.com>
2018-08-06 10:08:04 -0600

    recompile

--- libsrc/_DEVELOPMENT/EXAMPLES/zxn/firmware/loader/Winmake.bat
+++ libsrc/_DEVELOPMENT/EXAMPLES/zxn/firmware/loader/Winmake.bat
@@ -4,6 +4,7 @@ setlocal
 set CFLAGS=-clib=sdcc_iy -SO3 --max-allocs-per-node200000 --opt-code-size
 
 @rem zcc +zxn -v -c %CFLAGS% fat.c
+
 zcc +zxn -v -m -startup=31 %CFLAGS% -pragma-include:zpragma.inc @zproject.lst -o load
 appmake +rom -b load -o loader.bin --rombase 0 --romsize 0x2000
 
