221a79c8df9977049c9c1455c8e40032184bd405
Stefano <stefano_bodrato@hotmail.com>
2018-08-07 13:52:33 +0200

    Update zx81.h

--- include/zx81.h
+++ include/zx81.h
@@ -345,6 +345,7 @@ extern void __LIB__    zx_setfloat_callee(char *variable, double_t value) __smal
 //////////////
 
 extern void __LIB__  zx_lprintc(int chr);
+extern void __LIB__  zx_lprintc5(int chr);
 extern void __LIB__  zx_hardcopy();
 // Print out a 256 bytes buffer (8 rows)
 extern void __LIB__  zx_print_buf(char *buf) __z88dk_fastcall;
