663315e33955a9d4bb669badf220b4877f2b0a12
Phillip Stevens <phillip.stevens@gmail.com>
2018-08-18 11:41:32 +1000

    z80 - add small 16x16 multiply

---
+++ libsrc/_DEVELOPMENT/math/integer/small/l_small_mul_32_16x16.asm
@@ -0,0 +1,48 @@
+
+; 2018 feilipu
+
+SECTION code_clib
+SECTION code_math
+
+PUBLIC l_small_mul_32_16x16
+
+l_small_mul_32_16x16:
+
+    ; multiplication of two 16-bit numbers into a 16-bit product
+    ;
+    ; enter : de = 16-bit multiplicand
+    ;         hl = 16-bit multiplicand
+    ;
+    ; exit  : dehl = 32-bit product
+    ;         carry reset
+    ;
+    ; uses  : af, bc, de, hl
+
+    ld c,l
+    ld b,h
+
+    ld a,16
+    ld hl,0
+
+loop_0:
+
+    ; bc = 16-bit multiplicand
+    ; de = 16-bit multiplicand
+    ;  a = iterations
+
+    add hl,hl
+
+    rl e
+    rl d
+
+    jr NC,loop_1
+    add hl,bc
+    jr NC,loop_1
+    inc de
+
+loop_1:
+    dec a
+    jr NZ,loop_0
+
+    or a
+    ret
