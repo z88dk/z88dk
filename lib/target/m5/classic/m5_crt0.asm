06778ec8c5c898e2c49468a875ba2634a55dbbbf
suborb <dom@suborbital.org.uk>
2018-09-14 20:19:25 +0100

    The RAM/ROM define values so include crt_rules.inc

--- lib/target/m5/classic/m5_crt0.asm
+++ lib/target/m5/classic/m5_crt0.asm
@@ -17,9 +17,8 @@
         defc    TAR__fputc_cons_generic = 1
         defc    CONSOLE_COLUMNS = 32
         defc    CONSOLE_ROWS = 24
-        defc    __CPU_CLOCK = 3580000
+        defc    __CPU_CLOCK = 3579999
 	defc	CRT_KEY_DEL = 12
-	INCLUDE	"crt/classic/crt_rules.inc"
 
 IF startup = 2
 	INCLUDE	"target/m5/classic/rom.asm"
