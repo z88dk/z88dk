98c1f6db227dfb8db8208a2387bd8f8418d15262
Tatsuji Ashitani <ashipong@gmail.com>
2018-09-24 19:33:03 +0900

    [g800] added monographics support

---
+++ libsrc/graphics/g800/draw.asm
@@ -0,0 +1,519 @@
+    SECTION         code_clib
+
+    PUBLIC    draw
+    PUBLIC    _draw
+    PUBLIC    undraw
+    PUBLIC    _undraw
+    PUBLIC    xordraw
+    PUBLIC    _xordraw
+
+    PUBLIC    drawto
+    PUBLIC    _drawto
+    PUBLIC    undrawto
+    PUBLIC    _undrawto
+    PUBLIC    xordrawto
+    PUBLIC    _xordrawto
+
+    PUBLIC  drawr
+    PUBLIC  _drawr
+    PUBLIC  undrawr
+    PUBLIC  _undrawr
+    PUBLIC  xordrawr
+    PUBLIC  _xordrawr
+
+    EXTERN plotpixel   ; in:  l=y, h=x
+    EXTERN respixel    ; in:  l=y, h=x
+    EXTERN xorpixel    ; in:  l=y, h=x
+    EXTERN last_pos
+
+;-----------------------------------------------------
+; draw
+;-----------------------------------------------------
+
+draw:
+_draw:
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld d,(ix+8); x0
+    ld e,(ix+6); y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+
+    push iy
+    ld iy,call_plotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+undraw:
+_undraw:
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld d,(ix+8); x0
+    ld e,(ix+6); y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+
+    push iy
+    ld iy,call_unplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+xordraw:
+_xordraw:
+
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld d,(ix+8); x0
+    ld e,(ix+6); y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+
+    push iy
+    ld iy,call_xorplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+;-----------------------------------------------------
+; drawto
+;-----------------------------------------------------
+
+drawto:
+_drawto:
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+    push iy
+    ld iy,call_plotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+undrawto:
+_undrawto:
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+    push iy
+    ld iy,call_unplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+xordrawto:
+_xordrawto:
+
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); x1
+    ld l,(ix+2); y1
+
+    push hl
+    push iy
+    ld iy,call_xorplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+;-----------------------------------------------------
+; drawr
+;-----------------------------------------------------
+drawr:
+_drawr:
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); xr
+    ld l,(ix+2); yr
+
+    ld a,h
+    add d
+    ld h,a
+
+    ld a,l
+    add e
+    ld l,a
+
+    push hl
+    push iy
+    ld iy,call_plotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+
+undrawr:
+_undrawr:
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); xr
+    ld l,(ix+2); yr
+
+    ld a,h
+    add d
+    ld h,a
+
+    ld a,l
+    add e
+    ld l,a
+
+    push hl
+    push iy
+    ld iy,call_unplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+xordrawr:
+_xordrawr:
+    push ix
+    ld ix,2
+    add ix,sp
+
+    ld de,(last_pos); x0/y0
+    ld h,(ix+4); xr
+    ld l,(ix+2); yr
+
+    ld a,h
+    add d
+    ld h,a
+
+    ld a,l
+    add e
+    ld l,a
+
+    push hl
+    push iy
+    ld iy,call_xorplotpixel
+    call myplot_main
+    pop iy
+
+    pop hl
+    ld (last_pos),hl
+
+    pop ix
+
+    ret
+
+
+;----------------------------------------------------------
+myplot_main:
+
+    ; convert from 2nd/3rd quadrant to 1st/4th quadrant
+    call conv_quad
+
+    ; store x1/y1
+    ld a,h
+    ld (x1_),a
+    ld a,l
+    ld (y1_),a
+
+
+    ; b = dx
+    ld a,h
+    sub d
+    ld b,a
+
+    ; c =dy
+    ld a,l
+    sub e
+    ld c,a
+
+    ; if dy<0 goto aminus
+    cp 0
+    jp m, aminus
+
+    ; signy = 1
+    ld a,1
+    ld (signy_),a
+    jp ext_signy
+
+aminus:
+    ; signy = -1
+    ld a,-1
+    ld (signy_),a
+
+    ; dy= signy*dy
+    ld a,c
+    neg
+    ld c,a
+
+ext_signy:
+    ; if dy>dx goto over45
+    ld a,b
+    sub c
+    jp m, over45
+
+;-------------------------------------
+; under 45 degrees at 1st/4th quadrant
+;-------------------------------------
+
+under45:
+
+; d0 = 2 * dy;
+    sla c
+    ld a,c
+    ld (d0_),a
+
+; D = d0 - dx;
+    sub b
+    ld (D_),a
+
+; plot(x0,y0)
+    ld h,d
+    ld l,e
+    call myplotpixel
+
+;    for (x = x0 + 1; x <= x1; x++) {
+    inc h ; x=x0+1
+
+loop_under45:
+; if (x1<x){goto exit_draw}
+    ld a,(x1_)
+    cp h
+    jp m, exit_draw
+
+
+;      if (D <= 0) goto plot45
+    ld a,(D_)
+    cp 0
+    jp m, plot45
+    jp z, plot45
+
+;        y = y + signy;
+    ld a,l
+    ld IX,signy_
+    add a,(IX)
+    ld l,a
+
+;        D -= 2 * dx;
+    ld d,b;
+    sla d ; 2*dx
+    ld a,(D_)
+    sub d
+    ld (D_),a
+
+plot45:
+;      D += d0;
+    ld a,(D_)
+    ld IX,d0_
+    add a,(IX)
+    ld (D_),a
+     call myplotpixel
+
+    inc h
+    jp loop_under45
+
+;-------------------------------------
+; over 45 degrees at 1st/4th quadrant
+;-------------------------------------
+
+over45:
+; d0 = 2 * dx;
+    sla b
+    ld a,b
+    ld (d0_),a
+
+; D = d0 - dy;
+    sub c
+    ld (D_),a;
+
+; plot(x0,y0) ; x=x0;
+    ld h,d
+    ld l,e
+    call myplotpixel
+
+;    for (y = y0 + 1; signy * (y - y1) <= 0; y += signy) {
+    inc l ; y=y0+1
+
+loop_over45:
+
+ext_y_p_signy:
+    ld a,(y1_)
+    sub l
+    push af ; a= y1-y
+
+    ld a,(signy_)
+    cp 0
+    jp m, aminus2 ;if a<0 goto aminus2
+    pop af
+    jp cmp
+
+aminus2:
+    pop af
+    neg
+
+cmp:
+    cp 0
+    jp m, exit_draw ; if (signy*(y-y1)>0){goto exit_draw}
+
+    ld a,(D_)
+    cp 0
+
+;      if (D <= 0)  goto plot_over45
+    jp m, plot_over45
+    jp z, plot_over45
+
+;        x = x + 1
+    inc h
+
+;        D -= 2 * dy;
+    ld d,c;
+    sla d ; 2*dy
+    ld a,(D_)
+    sub d
+    ld (D_),a
+
+plot_over45:
+;      D += d0;
+    ld a,(D_)
+    push IX
+    ld IX,d0_
+    add a,(IX)
+    pop IX
+    ld (D_),a
+    call myplotpixel
+
+; y+=signy
+    ld a,(signy_)
+    cp 0
+    jp m, aminus1 ;if a<0 goto aminus1
+    inc l ; y++ (y+=signy)
+    jp ext_y_p_signy
+
+aminus1:
+    dec l ;y-- (y+=signy)
+    jp loop_over45
+;-------
+
+exit_draw:
+  ret
+
+myplotpixel:
+  jp (iy)
+;nop
+exitplotpixel:
+  ret
+
+call_plotpixel:
+  call plotpixel
+  jp exitplotpixel
+
+call_unplotpixel:
+  call respixel
+  jp exitplotpixel
+
+call_xorplotpixel:
+  call xorpixel
+  jp exitplotpixel
+
+
+; convert from 2nd/3rd quadrant to 1st/4th quadrant
+conv_quad:
+  ld a,d
+  cp h
+  ret m          ; return if x0<x1
+  ex de,hl
+  ret
+
+signy_:
+  defb 0
+
+x1_:
+  defb 0
+y1_:
+  defb 0
+x_:
+  defb 0
+D_:
+  defb 0
+d0_:
+  defb 0
+
