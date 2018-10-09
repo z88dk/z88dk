6fa5a4cf0fb2032e82e2d48dbc0f4e7069e2986c
Stefano <stefano_bodrato@hotmail.com>
2018-10-09 09:54:34 +0200

    Update diffusion.c

--- examples/graphics/diffusion.c
+++ examples/graphics/diffusion.c
@@ -16,9 +16,9 @@ void main()
 	ymax=(float)getmaxy()+((float)getmaxx()/4.0);
 	
 	b=3.8; c=0.9; d=3.18;
-	x=0.4; y=0.2
+	x=0.4; y=0.2;
 
-	for(f=1; f<15000; f++) {
+	for (f=1; f<15000; f++) {
 		plot ((int)(x*xmax),(int)(y*ymax));
 		x1=x; y1=y;
 		x=b*x1*(1.0-c*x1-y1);
