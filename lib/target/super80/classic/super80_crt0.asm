8f99417b59a752c70eb194bdc4c69231100fe182
suborb <dom@suborbital.org.uk>
2018-10-16 23:00:52 +0100

    Issue #1008: Start on the super80 support
    This gets gencon working on the vanilla super80 at the least.

---
+++ lib/target/super80/classic/super80_crt0.asm
@@ -0,0 +1,135 @@
+;
+;	Startup for the Dick Smith Super80
+;
+
+
+        module super80_crt0 
+
+
+;--------
+; Include zcc_opt.def to find out some info
+;--------
+
+        defc    crt0 = 1
+        INCLUDE "zcc_opt.def"
+
+;--------
+; Some scope definitions
+;--------
+
+        EXTERN    _main           ;main() is always external to crt0 code
+
+        PUBLIC    cleanup         ;jp'd to by exit()
+        PUBLIC    l_dcal          ;jp(hl)
+
+        defc    CONSOLE_COLUMNS = 32
+        defc    CONSOLE_ROWS = 16
+
+
+        defc    TAR__no_ansifont = 1
+        defc    CRT_KEY_DEL = 12
+        defc    __CPU_CLOCK = 2000000
+
+
+	defc	CRT_ORG_CODE = 0x0000
+
+        defc    TAR__clib_exit_stack_size = 0
+        defc    TAR__register_sp = 0xbdff
+
+        INCLUDE "crt/classic/crt_rules.inc"
+
+	org	  CRT_ORG_CODE
+
+if (ASMPC<>$0000)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	di
+	jp	program
+
+	defs	$0008-ASMPC
+if (ASMPC<>$0008)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart08
+
+	defs	$0010-ASMPC
+if (ASMPC<>$0010)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart10
+
+	defs	$0018-ASMPC
+if (ASMPC<>$0018)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart18
+
+	defs	$0020-ASMPC
+if (ASMPC<>$0020)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart20
+
+    defs	$0028-ASMPC
+if (ASMPC<>$0028)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart28
+
+	defs	$0030-ASMPC
+if (ASMPC<>$0030)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	jp	restart30
+
+	defs	$0038-ASMPC
+if (ASMPC<>$0038)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+	; IM1 interrupt routine
+	reti
+
+	defs	$0066 - ASMPC
+if (ASMPC<>$0066)
+        defs    CODE_ALIGNMENT_ERROR
+endif
+nmi:
+	; Should jump to pause
+	retn
+
+; Restart routines, nothing sorted yet
+restart10:
+restart08:
+restart18:
+restart20:
+restart28:
+restart30:
+	ret
+
+program:
+        INCLUDE "crt/classic/crt_init_sp.asm"
+        INCLUDE "crt/classic/crt_init_atexit.asm"
+	call    crt0_init_bss
+	ld	(exitsp),sp
+	im	1
+    	ei
+; Optional definition for auto MALLOC init
+; it assumes we have free space between the end of
+; the compiled program and the stack pointer
+IF DEFINED_USING_amalloc
+    INCLUDE "crt/classic/crt_init_amalloc.asm"
+ENDIF
+	call	_main
+cleanup:
+	di
+	halt
+	jp	cleanup
+
+
+l_dcal: jp      (hl)            ;Used for function pointer calls
+
+
+
+        INCLUDE "crt/classic/crt_runtime_selection.asm"
+
+        INCLUDE "crt/classic/crt_section.asm"
---
+++ libsrc/stdio/super80/generic_console.asm
@@ -0,0 +1,152 @@
+;
+;
+; Nibble	Composite	RGB
+;0	Black	Black
+;1	Grey	Black
+;2	Blue	Blue
+;3	L.Yellow	L.Blue
+;4	Green	Green
+;5	L.Magenta	Bright Green
+;6	Cyan	Cyan
+;7	L.Red	Turquoise
+;8	Red	Dark Red
+;9	Dark Cyan	Red
+;A	Magenta	Purple
+;B	L.Green	Magenta
+;C	Yellow	Lime
+;D	Dark Blue	Yellow
+;E	White	Off White
+;F	Black	Bright White
+
+
+
+		SECTION		code_clib
+
+		PUBLIC		generic_console_cls
+		PUBLIC		generic_console_vpeek
+		PUBLIC		generic_console_scrollup
+		PUBLIC		generic_console_printc
+		PUBLIC		generic_console_ioctl
+                PUBLIC          generic_console_set_ink
+                PUBLIC          generic_console_set_paper
+                PUBLIC          generic_console_set_inverse
+
+		EXTERN		CONSOLE_COLUMNS
+		EXTERN		CONSOLE_ROWS
+		EXTERN		__super80_attr
+
+		defc		DISPLAY = $be00
+		defc		COLOUR_MAP = $fe00
+
+generic_console_ioctl:
+	scf
+generic_console_set_inverse:
+	ret
+
+generic_console_set_ink:
+	and	15
+	rla
+	rla
+	rla
+	rla	
+	ld	e,a
+	ld	a,(__super80_attr)
+	and	@00001111
+	or	e
+	ld	(__super80_attr),a
+	ret
+
+	
+generic_console_set_paper:
+	and	15
+	ld	e,a
+	ld	a,(__super80_attr)
+	and	@11110000
+	or	e
+	ld	(__super80_attr),a
+	ret
+
+generic_console_cls:
+	ld	hl, DISPLAY
+	ld	de, DISPLAY +1
+	ld	bc, +(CONSOLE_COLUMNS * CONSOLE_ROWS) - 1
+	ld	(hl),32
+	ldir
+	ld	hl, COLOUR_MAP
+	ld	de, COLOUR_MAP+1
+	ld	bc, +(CONSOLE_COLUMNS * CONSOLE_ROWS) - 1
+	ld	a,(__super80_attr)
+	ld	(hl),a
+	ldir
+	ret
+
+; c = x
+; b = y
+; a = character to print
+; e = raw
+generic_console_printc:
+	call	xypos
+	ld	(hl),a
+	set	6,h		;Move to fe range
+	ld	a,(__super80_attr)
+	ld	(hl),a
+	ret
+
+
+;Entry: c = x,
+;       b = y
+;Exit:  nc = success
+;        a = character,
+;        c = failure
+generic_console_vpeek:
+        call    xypos
+        ld      a,(hl)
+        and     a
+        ret
+
+
+xypos:
+	ld	l,b
+	ld	h,0
+	add	hl,hl		;x32
+	add	hl,hl
+	add	hl,hl
+	add	hl,hl
+	add	hl,hl
+	ld	b,$be
+	add	hl,bc			;hl now points to address in display
+	ret
+
+
+generic_console_scrollup:
+	push	de
+	push	bc
+	ld	hl, DISPLAY + CONSOLE_COLUMNS
+	ld	de, DISPLAY
+	ld	bc,+ ((CONSOLE_COLUMNS) * (CONSOLE_ROWS-1))
+	ldir
+	ex	de,hl
+	ld	b,CONSOLE_COLUMNS
+generic_console_scrollup_3:
+	ld	(hl),32
+	inc	hl
+	djnz	generic_console_scrollup_3
+	ld	hl, COLOUR_MAP + CONSOLE_COLUMNS
+	ld	de, COLOUR_MAP
+	ld	bc,+ ((CONSOLE_COLUMNS) * (CONSOLE_ROWS-1))
+	ldir
+	ex	de,hl
+	ld	b,CONSOLE_COLUMNS
+	ld	a,(__super80_attr)
+generic_console_scrollup_4:
+	ld	(hl),a
+	inc	hl
+	djnz	generic_console_scrollup_4
+	pop	bc
+	pop	de
+	ret
+
+
+	SECTION data_clib
+
+__super80_attr:	defb	0x0e		;white on black
