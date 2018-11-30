a285a7767702302dba1d09d6bc4fd40fc5ff77de
Stefano <stefano_bodrato@hotmail.com>
2018-11-30 08:11:27 +0100

    Update w_xordraw.asm

--- libsrc/graphics/w_xordraw.asm
+++ libsrc/graphics/w_xordraw.asm
@@ -5,57 +5,40 @@
 ;
 ;       Stubs Written by D Morris - 30/9/98
 ;
-;       Wide resolution (WORD based parameters) version by Stefano Bodrato
-;
-;	$Id: w_xordraw.asm,v 1.1 2016-11-10 07:34:54 stefano Exp $
+;       Wide resolution (int type parameters) version by Stefano Bodrato
 ;
 
+;
+;	$Id: w_xordraw.asm $
+;
 
-        SECTION code_graphics
-                PUBLIC    xordraw
-                PUBLIC    _xordraw
-                EXTERN     swapgfxbk
-                EXTERN     swapgfxbk1
-                EXTERN	__graphics_end
+;
+; CALLER LINKAGE FOR FUNCTION POINTERS
+; ----- void  xordraw(int x, int y, int x2, int y2)
 
-                EXTERN     w_line_r
-                EXTERN     w_xorpixel
+SECTION code_graphics
+PUBLIC xordraw
+PUBLIC _xordraw
+EXTERN xordraw_callee
+EXTERN ASMDISP_XORDRAW_CALLEE
 
 
 .xordraw
 ._xordraw
-		push	ix
-		ld	ix,4
-		add	ix,sp
-		ld	l,(ix+6)
-		ld	h,(ix+7)
-		ld	e,(ix+4)
-		ld	d,(ix+5)
+		pop af
+		pop de
+		pop	hl
+		exx			; w_xorpixel and swapgfxbk must not use the alternate registers, no problem with w_line_r
+		pop hl
+		pop de
 		
+		push de
+		push hl
+		exx
 		push hl
 		push de
-		push ix
-
-		call    swapgfxbk
-		call	w_xorpixel
-		call    swapgfxbk1
-
-		pop ix
-		ld	l,(ix+0)
-		ld	h,(ix+1)
-		pop bc
-		or a
-		sbc hl,bc
-		ex de,hl
 		
-		ld	l,(ix+2)
-		ld	h,(ix+3)
-		pop bc
-		or a
-		sbc hl,bc
-
-		call    swapgfxbk
-		ld      ix,w_xorpixel
-		call    w_line_r
-		jp      __graphics_end
-
+		push af		; ret addr
+		
+   jp xordraw_callee + ASMDISP_XORDRAW_CALLEE
+ 
