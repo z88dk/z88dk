6aa0b2b0ea9face00ec8f78c8b0d4ac724dadb16
Stefano <stefano_bodrato@hotmail.com>
2018-12-12 10:53:59 +0100

    Update undrawr_callee.asm

--- libsrc/graphics/retrofit/undrawr_callee.asm
+++ libsrc/graphics/retrofit/undrawr_callee.asm
@@ -3,10 +3,6 @@
 ;
 ;       Written around the Interlogic Standard Library
 ;
-;       Stubs Written by D Morris - 30/9/98
-;
-;       Wide resolution (int type parameters) version by Stefano Bodrato
-;
 
 ;
 ;	$Id: undrawr_callee.asm $
@@ -17,7 +13,7 @@
 ; ----- void  undrawr(int x2, int y2)
 
 
-SECTION smc_clib
+SECTION code_graphics
 
 PUBLIC     undrawr_callee
 PUBLIC    _undrawr_callee
