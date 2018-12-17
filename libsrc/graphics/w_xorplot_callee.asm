342256a2b037b998ff7ae0f297e7119d192d0605
Stefano <stefano_bodrato@hotmail.com>
2018-12-17 15:03:02 +0100

    Create w_xorplot_callee.asm

---
+++ libsrc/graphics/w_xorplot_callee.asm
@@ -0,0 +1,41 @@
+;
+;       Z88 Graphics Functions - Small C+ stubs
+;
+;       Written around the Interlogic Standard Library
+;
+;       Stubs Written by D Morris - 30/9/98
+;
+;
+;	$Id: w_xorplot_callee.asm $
+;
+
+
+; CALLER LINKAGE FOR FUNCTION POINTERS
+; ----- void  xorplot(int x, int y)
+
+
+        SECTION code_graphics
+		
+                PUBLIC    xorplot_callee
+                PUBLIC    _xorplot_callee
+				
+                EXTERN     swapgfxbk
+                EXTERN    __graphics_end
+
+                EXTERN     w_xorpixel
+
+.xorplot_callee
+._xorplot_callee
+
+   pop af
+   pop de	; y
+   pop hl	; x
+   push af
+
+.asmentry
+		push	ix
+                call    swapgfxbk
+                call    w_xorpixel
+                jp      __graphics_end
+
+DEFC ASMDISP_XORPLOT_CALLEE = # asmentry - xorplot_callee
