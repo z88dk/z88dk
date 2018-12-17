91f5ec45ea6a771286347e553e2bfb18e66f9ea6
Stefano <stefano_bodrato@hotmail.com>
2018-12-17 15:05:41 +0100

    Create w_plot_callee.asm

---
+++ libsrc/graphics/w_plot_callee.asm
@@ -0,0 +1,41 @@
+;
+;       Z88 Graphics Functions - Small C+ stubs
+;
+;       Written around the Interlogic Standard Library
+;
+;       Stubs Written by D Morris - 30/9/98
+;
+;
+;	$Id: w_plot_callee.asm $
+;
+
+
+; CALLER LINKAGE FOR FUNCTION POINTERS
+; ----- void  plot(int x, int y)
+
+
+        SECTION code_graphics
+		
+                PUBLIC    plot_callee
+                PUBLIC    _plot_callee
+				
+                EXTERN     swapgfxbk
+                EXTERN    __graphics_end
+
+                EXTERN     w_plotpixel
+
+.plot_callee
+._plot_callee
+
+   pop af
+   pop de	; y
+   pop hl	; x
+   push af
+
+.asmentry
+		push	ix
+                call    swapgfxbk
+                call    w_plotpixel
+                jp      __graphics_end
+
+DEFC ASMDISP_PLOT_CALLEE = # asmentry - plot_callee
