e7bcedcf8052fb6a93403b97b3c93e246f62bb14
Stefano <stefano_bodrato@hotmail.com>
2018-12-19 07:31:19 +0100

    Update multipoint.asm

--- libsrc/graphics/multipoint.asm
+++ libsrc/graphics/multipoint.asm
@@ -4,59 +4,38 @@
 ;       Stefano Bodrato 19/7/2007
 ;
 ;
-;       $Id: multipoint.asm,v 1.4 2016-04-13 21:09:09 dom Exp $
+;       $Id: multipoint.asm $
 ;
 
+;
+; Pick a vertical or horizontal bit bar, up to 16 bits long
+;
+; Usage: multipoint(int hv, int length, int x, int y)
+;
 
-;Usage: multipoint(int vh, int length, struct *pixel)
-;pick a vertical or horizontal bit bar, up to 16 bits long
+; CALLER LINKAGE FOR FUNCTION POINTERS
 
 
         SECTION code_graphics
                 PUBLIC    multipoint
                 PUBLIC    _multipoint
 
-                EXTERN     pointxy
-                EXTERN     swapgfxbk
-                EXTERN     swapgfxbk1
-
+		EXTERN multipoint_callee
+		EXTERN ASMDISP_MULTIPOINT_CALLEE
+		
 
 .multipoint
 ._multipoint
-		push	ix
-                ld      ix,2
-                add     ix,sp
-                ld      l,(ix+2)
-                ld      h,(ix+4)
-                ld      b,(ix+6)
 
-                call    swapgfxbk
-
-                ld      de,0
-                bit     0,(ix+8)
-                jr      z,horizontal                
-.vertical
-                sla     e
-                rl      d
-                call    pointxy
-                jr      z,jv
-                inc     de
-.jv
-                inc     l
-                djnz    vertical
-                jr      exit
-.horizontal
-                sla     e
-                rl      d
-                call    pointxy
-                jr      z,jh
-                inc     de
-.jh
-                inc     h
-                djnz    horizontal
-.exit
-                call    swapgfxbk1
-		pop	ix
-                ld      h,d
-                ld      l,e
-                ret
+	pop	af	; ret addr
+	pop hl	; y
+	pop de	; x
+	ld	h,e
+	pop bc
+	ld	b,c	; length
+	pop de
+	ld	c,e	; h/v
+	push	af	; ret addr
+	
+	
+   jp multipoint_callee + ASMDISP_MULTIPOINT_CALLEE
