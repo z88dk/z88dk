93f0ed9323a8b62781ba5cd52576651e34314c83
Stefano <stefano_bodrato@hotmail.com>
2019-01-11 10:10:11 +0100

    Update w_clga.asm

--- libsrc/graphics/w_clga.asm
+++ libsrc/graphics/w_clga.asm
@@ -1,80 +1,37 @@
-
 ;
-;       Z88 Graphics Functions - Small C+ stubs
+; CLGA  -  clear rect area
 ;
-;       Written around the Interlogic Standard Library
-;
-;       Stubs Written by D Morris - 30/9/98
-;
-;
-;	$Id: w_clga.asm,v 1.1 2016-10-18 06:52:34 stefano Exp $
+; Generic high resolution version
 ;
 
+;
+;	$Id: w_clga.asm $
+;
 
-;Usage: clga(struct *pixels)
-
-	INCLUDE "graphics/grafix.inc"
- 	SECTION code_graphics
-	PUBLIC    clga
-	PUBLIC    _clga
-
-	EXTERN	w_respixel
-	EXTERN	w_line_r
-
-	EXTERN	swapgfxbk
-	EXTERN	swapgfxbk1
-	EXTERN	__graphics_end
+SECTION code_graphics
+PUBLIC clga
+PUBLIC _clga
+EXTERN clga_callee
+EXTERN ASMDISP_CLGA_CALLEE
 
 	
 .clga
 ._clga
-		push	ix
-		ld	ix,4
-		add	ix,sp
-		ld	l,(ix+6)
-		ld	h,(ix+7)
-		ld	e,(ix+4)
-		ld	d,(ix+5)
+
+		pop af
 		
-		ld	c,(ix+0)
-		ld	b,(ix+1)
-		inc bc
-		
-		ex (sp),hl
-		ld	l,(ix+2)
-		ld	h,(ix+3)
-		ex (sp),hl
-		
-.loop
-		push hl
-		push de
-		push bc
-		call    swapgfxbk
-		call	w_respixel
-		call    swapgfxbk1
-		pop bc
+		pop de
+		pop	hl
+		exx			; w_plotpixel and swapgfxbk must not use the alternate registers, no problem with w_line_r
 		pop de
 		pop hl
-		ex (sp),hl
 		
 		push hl
 		push de
-		push bc
-		ld de,0
-		call    swapgfxbk
-		ld      ix,w_respixel
-		call    w_line_r
-		call    swapgfxbk1
-		pop bc
-		pop de
-		pop hl
-		ex (sp),hl
-		inc de
-		dec bc
-		ld a,b
-		or c
-		jr nz,loop
-
-		pop ix
-		ret
-
+		exx
+		push hl
+		push de
+		
+		push af		; ret addr
+		
+   jp clga_callee + ASMDISP_CLGA_CALLEE
