467e9813de3a6084ff2de5e076278c238478c41a
Stefano <stefano_bodrato@hotmail.com>
2019-01-23 17:09:24 +0100

    #1073

--- libsrc/graphics/w_xorcircle_callee.asm
+++ libsrc/graphics/w_xorcircle_callee.asm
@@ -50,7 +50,9 @@
 		
 .asmentry
                 ld      ix,w_xorpixel
+		push	af
                 call    swapgfxbk
+		pop	af
                 call    w_draw_circle
                 jp      __graphics_end
 
