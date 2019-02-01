74166796c3e47eb2805ff84a7ef2d1f859be0afc
Stefano <stefano_bodrato@hotmail.com>
2019-02-01 11:13:52 +0100

    #960 +workaround for emulators

--- libsrc/graphics/px8/w_respixl.asm
+++ libsrc/graphics/px8/w_respixl.asm
@@ -3,14 +3,12 @@
         SECTION code_clib
         PUBLIC    w_respixel
 
-        EXTERN     l_cmp
+        EXTERN     l_graphics_cmp
 
         EXTERN    __gfx_coords
-        EXTERN    px8_conout
+		EXTERN     w_pixeladdress
+        EXTERN     pix_return
 
-;
-;       $Id: w_respixl.asm, Stefano - 2017 $
-;
 
 ; ******************************************************************
 ;
@@ -24,43 +22,31 @@
 ;
 ; in:  hl,de    = (x,y) coordinate of pixel
 ;
-; registers changed after return:
-;  ......../ixiy same
-;  afbcdehl/.... different
-;
+
 .w_respixel
                         push    hl
                         ld      hl,maxy
-                        call    l_cmp
+                        call    l_graphics_cmp
                         pop     hl
                         ret     nc               ; Return if Y overflows
 
                         push    de
                         ld      de,maxx
-                        call    l_cmp
+                        call    l_graphics_cmp
                         pop     de
                         ret     c               ; Return if X overflows
                         
                         ld      (__gfx_coords),hl     ; store X
                         ld      (__gfx_coords+2),de   ; store Y: COORDS must be 2 bytes wider
-                        push	hl
-						push	de
-						
-						ld	c,27		; ESCape
-						call px8_conout
-						
-						ld	c,0xc7		; PSET/PRESET
-						call px8_conout
-						ld	c,0		; PRESET
-						call px8_conout
-						pop de
-						ld c,e
-						call px8_conout	; y
-						pop hl
-						push hl
-						ld c,h
-						call px8_conout	; x (msb)
-						pop hl
-						ld c,l
-						jp px8_conout	; x (lsb)
-
+                        
+                        call    w_pixeladdress
+                        ld      b,a
+                        ld      a,1
+                        jr      z, or_pixel     ; pixel is at bit 0...
+.plot_position          rlca
+                        djnz    plot_position
+.or_pixel               ex      de,hl
+						cpl
+                        and      (hl)
+                        ld      (hl),a
+                        jp		pix_return
