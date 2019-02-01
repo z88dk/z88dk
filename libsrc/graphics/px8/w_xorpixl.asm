e7a04ef40ad9b94daea5318cda60f89c731013ee
Stefano <stefano_bodrato@hotmail.com>
2019-02-01 11:13:20 +0100

    #960, workaround for emulators

--- libsrc/graphics/px8/w_xorpixl.asm
+++ libsrc/graphics/px8/w_xorpixl.asm
@@ -3,13 +3,12 @@
         SECTION code_clib
         PUBLIC    w_xorpixel
 
-        EXTERN     w_plotpixel
-        EXTERN     w_respixel
-        EXTERN     w_pointxy
+        EXTERN     l_graphics_cmp
 
-;
-;       $Id: w_xorpixl.asm $
-;
+        EXTERN    __gfx_coords
+		EXTERN     w_pixeladdress
+        EXTERN     pix_return
+
 
 ; ******************************************************************
 ;
@@ -23,15 +22,31 @@
 ;
 ; in:  hl,de    = (x,y) coordinate of pixel
 ;
-; registers changed after return:
-;  ......../ixiy same
-;  afbcdehl/.... different
-;
+
 .w_xorpixel
-		push hl
-		push de
-		call w_pointxy
-		pop de
-		pop hl
-		jp	z,w_plotpixel
-		jp	w_respixel
+                        push    hl
+                        ld      hl,maxy
+                        call    l_graphics_cmp
+                        pop     hl
+                        ret     nc               ; Return if Y overflows
+
+                        push    de
+                        ld      de,maxx
+                        call    l_graphics_cmp
+                        pop     de
+                        ret     c               ; Return if X overflows
+                        
+                        ld      (__gfx_coords),hl     ; store X
+                        ld      (__gfx_coords+2),de   ; store Y: COORDS must be 2 bytes wider
+                        
+                        call    w_pixeladdress
+                        ld      b,a
+                        ld      a,1
+                        jr      z, or_pixel     ; pixel is at bit 0...
+.plot_position          rlca
+                        djnz    plot_position
+.or_pixel               ex      de,hl
+                        xor      (hl)
+                        ld      (hl),a
+                        jp		pix_return
+
