1dd7f9471fa5b0b839c23dfb0b1e38bb904d7cc2
Stefano <stefano_bodrato@hotmail.com>
2019-02-05 08:21:05 +0100

    #960

--- libsrc/graphics/px8/w_putsprite.asm
+++ libsrc/graphics/px8/w_putsprite.asm
@@ -12,11 +12,11 @@
         SECTION   smc_clib
         PUBLIC    putsprite
         PUBLIC    _putsprite
-        EXTERN    w_pixeladdress
-        EXTERN    pix_return
+		
+		EXTERN    subcpu_call
 
-        EXTERN    swapgfxbk
-        EXTERN    __graphics_end
+        ;EXTERN    swapgfxbk
+        ;EXTERN    __graphics_end
 
         INCLUDE "graphics/grafix.inc"
 
@@ -28,188 +28,169 @@
 .putsprite
 ._putsprite
         
-		push ix
-		
-        ld      hl,4
+        ld      hl,2
         add     hl,sp
         ld      e,(hl)
         inc     hl
-        ld      d,(hl)  ; sprite address
-        push    de
-        pop     ix
+        ld      d,(hl)
+        push    de  		; sprite address
 
         inc     hl
-        ld      e,(hl)
+		ld      a,(hl)
+		ld		(ycoord),a
         inc     hl
-        ld      d,(hl)
         inc     hl
         ld      c,(hl)
         inc     hl
         ld      b,(hl)  ; x and y __gfx_coords
+		
 
         inc     hl
         ld      a,(hl)  ; and/or/xor mode
-        ld      (ortype+1),a    ; Self modifying code
-        ld      (ortype2+1),a   ; Self modifying code
-
-        inc     hl
-        ld      a,(hl)
-        ld      (ortype),a      ; Self modifying code
-        ld      (ortype2),a     ; Self modifying code
-
-        call    swapgfxbk
-        ; @@@@@@@@@@@@
-        ld      h,b
-        ld      l,c
-        ld      (curx),hl
-        ld      (oldx),hl
-        ld      (cury),de
-        call    w_pixeladdress
-        ; @@@@@@@@@@@@
-         ld       hl,offsets_table
-         ld       c,a
-         ld       b,0
-         add      hl,bc
-         ld       a,(hl)
-         ld       (wsmc1+1),a
-         ld       (wsmc2+1),a
-         ld       (_smc1+1),a
-
-        ld      h,d
-        ld      l,e                 ; display location from pixeladdress
-
-        ld      a,(ix+0)
-        cp      9
-        jp      nc,putspritew
-
-         ld       d,a
-         ld       b,(ix+1)
-._oloop  push     bc                ;Save # of rows
-         ld       b,d               ;Load width
-         ld       c,(ix+2)          ;Load one line of image
-         inc      ix
-._smc1   ld       a,1               ;Load pixel mask
-._iloop  sla      c                 ;Test leftmost pixel
-         jp       nc,_noplot        ;See if a plot is needed
-         ld       e,a
-.ortype
-        nop     ; changed into nop / cpl
-         nop    ; changed into and/or/xor (hl)
-         ld       (hl),a
-         ld       a,e
-._noplot rrca
-         call      c,edge_reached
-
-        djnz     _iloop
-
-        push   de
-        ;@@@@@@@@@@
-        ;Go to next line
-        ;@@@@@@@@@@
-		call	pix_return
-         ld      hl,(oldx)
-         ld      (curx),hl
-         ld      de,(cury)
-         inc     de
-         ld      (cury),de
-         call    w_pixeladdress
-         ld      h,d
-         ld      l,e
-        ;@@@@@@@@@@
-        pop     de
-         pop      bc                ;Restore data
-         djnz     _oloop
-		 
-         jp       __graphics_end
-
-
-
-.putspritew
-         ld       d,a
-         ld       b,(ix+1)        
-.woloop  push     bc                ;Save # of rows
-         ld       b,d               ;Load width
-         ld       c,(ix+2)          ;Load one line of image
-         inc      ix
-.wsmc1    ld       a,1               ;Load pixel mask
-.wiloop  sla      c                 ;Test leftmost pixel
-         jp       nc,wnoplot         ;See if a plot is needed
-         ld       e,a
-.ortype2
-        nop     ; changed into nop / cpl
-         nop    ; changed into and/or/xor (hl)
-         ld       (hl),a
-         ld       a,e
-.wnoplot rrca
-		 call      c,edge_reached
-
-.wsmc2   cp       1
-         jp       z,wover_1
-
-         djnz     wiloop
-
-.nextline
-
-        push   de
-        ;@@@@@@@@@@
-        ;Go to next line
-        ;@@@@@@@@@@
-		call	pix_return
-         ld      hl,(oldx)
-         ld      (curx),hl
-         ld      de,(cury)
-         inc     de
-         ld      (cury),de
-         call    w_pixeladdress
-         ld      h,d
-         ld      l,e
-        ;@@@@@@@@@@
-        pop     de
-         pop      bc                ;Restore data
-         djnz     woloop
-		 
-         jp       __graphics_end
-        
+		
+		; 166 - and {10 100 110} -> 1
+		; 182 - or  [10 110 110] -> 2
+		; 174 - xor [10 101 110] -> 3
+		
+		ld	e,1
+		cp	166	; and(hl) opcode
+		jr	z,setmode
+		inc e
+		cp	182	; or(hl) opcode
+		jr	z,setmode
+		inc e
+.setmode
+		ld	a,e
+		ld	(operation),a
 		
 
-.wover_1 ld       c,(ix+2)
-         inc      ix
-         djnz     wiloop
-         dec      ix
-		 
-		 jr nextline
+		ld	a,c
+		and	7
+		ld	(loopcount2+1),a
+		
+		srl	b               ;hl = x / 8
+		rr	c
+		srl	b
+		rr	c
+		srl	b
+		rr	c
+		ld	a,c
+		ld	(xcoord),a
+
+		
+		pop		hl  		; sprite address
+		ld		a,(hl)		; x size in pixels
+		inc		hl
+		add	7
+		srl a
+		srl a
+		srl a
+		ld	(oloop+1),a		; sprite size in bytes
+		
+		inc a				; +1 for possible bit shifting !
+		ld	(width),a
+		ld	e,a				; x size in bytes
+		ld	d,0
+		ld	a,(hl)			; y size
+		ld	(height),a
+		
+		inc		hl
+		push	hl				; sprite data ptr
+
+		
+		ld	h,d
+		ld	l,d			; hl=0
+.multiply
+		add	hl,de
+		dec	a
+		jr	nz,multiply
+		ld	(shiftcount+1),hl
+		
+		ld	de,6
+		add	hl,de					; add packet header (command, parameters..) to data size
+		ld	(packet_sz),hl		; packet size
+		
+
+		ld	a,(height)
+		ld	c,a
+		
+		pop	hl				; <- sprite data
+		ld	de,data
+
+.oloop
+		ld	b,0
+.loopcount
+		ld	a,(hl)
+		ld	(de),a
+		inc hl
+		inc de
+		xor a
+		cp	b
+		jr	z,noloop
+		djnz	loopcount
+.noloop
+		ld	(de),a	; add a blank byte on every row for possible shifting
+		inc de
+		dec c
+		jr	nz,oloop
+		
+		
+		
+.loopcount2
+		ld	b,0
+		xor a
+		push	af
+		cp	b
+		jr	z,noshift
+		
+.shiftcount
+		ld	de,0
+		ld	hl,data
+.shiftloop
+		pop	af
+		ld	a,(hl)
+		rra
+		ld	(hl),a
+		push af
+		inc hl
+		dec	de
+		ld	a,d
+		or	e
+		jr	nz,shiftloop
+		djnz	shiftcount
+.noshift
+		pop	af
+		
+
+		ld	hl,packet_wr
+		jp	subcpu_call
 
 
-.edge_reached
-        ;@@@@@@@@@@
-        ;Go to next byte
-        ;@@@@@@@@@@
-         push bc
-         ex	af,af
-		 call	pix_return
-         ld      hl,(curx)
-         ld      de,8
-         add     hl,de
-         ld      de,(cury)
-         ld      (curx),hl
-         call    w_pixeladdress
-         ld      h,d
-         ld      l,e
-         ex	af,af
-         pop   bc
-        ;@@@@@@@@@@
-		ret
-
-	SECTION  bss_graphics
-.oldx
-         defw   0
-.curx
-         defw   0
-.cury
-         defw   0
 
 
-	SECTION	rodata_clib
-.offsets_table
-         defb   1,2,4,8,16,32,64,128
+		SECTION	data_clib
+
+; master packet for write operation
+packet_wr:
+	defw	sndpkt
+packet_sz:
+	defw	7		; packet sz (=6+data len)
+	defw	xcoord	; packet addr expected back from the slave CPU (1 byte for return code only, we use the foo position of xcoord)
+	defw	1		; size of the expected packet being received (just the return code)
+
+	
+sndpkt:
+	defb	$25		; slave CPU command to write to the graphics memory ($25 = write)
+xcoord:
+	defb	0
+ycoord:
+	defb	0
+width:
+	defb	1
+height:
+	defb	1
+operation:
+	defb	0		; Operation (only in WR mode)
+data:
+	defs	255		; warning !  picture size is limited, x*y must be <= 255 !
 
