9fc70195ca9c543c787893c3555d2edeb716aca5
Stefano <stefano_bodrato@hotmail.com>
2019-02-08 13:42:58 +0100

    Update fgetc_cons.asm

--- libsrc/stdio/spectrum/fgetc_cons.asm
+++ libsrc/stdio/spectrum/fgetc_cons.asm
@@ -1,43 +1,36 @@
 ;
-; Devilishly simple Spectrum Routines
+;	Devilishly simple Spectrum Routines
 ;
-; getkey() Wait for keypress
+;	getkey() Wait for keypress
 ;
-; 17/5/99 djm
+;	17/5/99 djm
 ;
-; 22/3/2000 djm Rechristened getchar
-; 1/4/2000  djm Rechristened fgetc_cons
+;	22/3/2000 djm Rechristened getchar
+;	1/4/2000  djm Rechristened fgetc_cons
 ;
 ;
-; $Id: fgetc_cons_rom.asm $
+;	$Id: fgetc_cons.asm,v 1.5 2016-03-09 22:25:54 dom Exp $
 ;
 
 
-    SECTION code_clib
-    PUBLIC  fgetc_cons
-    PUBLIC  _fgetc_cons
-
-    EXTERN in_Inkey
+		SECTION	code_clib
+		PUBLIC	fgetc_cons
+		PUBLIC	_fgetc_cons
 
 .fgetc_cons
 ._fgetc_cons
-  
-  call  in_Inkey
-  ld  a,l
-  and a
-  jr  nz,_fgetc_cons
-
-.fgetc_wait
-  call  in_Inkey
-  ld  a,l
-  and a
-  jr  z,fgetc_wait
-
+	xor	a
+	ld	(23560),a
+.getkey1
+	ld	a,(23560)
+	and	a
+	jr	z,getkey1
 IF STANDARDESCAPECHARS
-  cp  13
-  jr  nz,not_return
-  ld  l,10
+	cp	13
+	jr	nz,not_return
+	ld	a,10
 .not_return
 ENDIF
-
-  ret
+	ld	l,a
+	ld	h,0
+	ret
