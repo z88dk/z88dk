3a39a098eec7b95d25cdf913e944b157b9beeee3
suborb <dom@suborbital.org.uk>
2019-04-25 21:17:36 +0100

    mbf32: Simplify fabs, implement modf

--- libsrc/math/mbf32/c/sccz80/fabs.asm
+++ libsrc/math/mbf32/c/sccz80/fabs.asm
@@ -2,13 +2,7 @@
         SECTION code_fp_mbf32
 
         PUBLIC  fabs
-        EXTERN  ___mbf32_setup_single
-        EXTERN  ___mbf32_ABS
-        EXTERN  ___mbf32_return
-	EXTERN	msbios
 
 fabs:
-	call	___mbf32_setup_single
-	ld	ix,___mbf32_ABS
-	call	msbios
-	jp	___mbf32_return
+	res	7,e
+	ret
