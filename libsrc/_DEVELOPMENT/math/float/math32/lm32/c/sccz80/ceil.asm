531f39b0a525ded839fb185350aa8d6a3d569852
suborb <dom@suborbital.org.uk>
2019-05-12 00:35:33 +0100

    (math32) Create asm floor/ceil routines
    Fix issue with m32_floatXXu - needs swapped registers
    
    Use digi conversions directly from sccz80 rather than writing my
    own.

---
+++ libsrc/_DEVELOPMENT/math/float/math32/lm32/c/sccz80/ceil.asm
@@ -0,0 +1,6 @@
+
+	SECTION	code_fp_math32
+	PUBLIC	ceil
+	EXTERN	m32_ceil
+
+	defc	ceil = m32_ceil
---
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_f2long.asm
@@ -0,0 +1,38 @@
+
+
+	SECTION		code_fp_math32
+
+	PUBLIC		m32_f2slong
+	PUBLIC		m32_f2ulong
+	PUBLIC		m32_f2sint
+	PUBLIC		m32_f2uint
+
+	EXTERN		l_long_neg
+	EXTERN		m32_fszero_fastcall
+
+; Convert floating point number to long
+m32_f2sint:
+m32_f2uint:
+m32_f2slong:
+m32_f2ulong:
+        ld      a,d		;Holds sign + 7bits of exponent
+        rl	e
+        rla                     ;a = Exponent
+        and     a
+	jp	z,m32_fszero_fastcall	;Expontent was 0, return 0
+        cp      $7e + 24
+	jp	nc,m32_fszero_fastcall	;Number too large
+	; e register is rotated by bit, restore the hidden bit and rotate back
+	scf
+	rr	e
+loop:
+	srl	e		;Fill with 0
+	rr	h
+	rr	l
+	inc	a
+	cp	$7e + 24
+	jr	nz,loop
+	rl	d		;Check sign bit
+	ld	d,0
+	jp	c,l_long_neg
+	ret
