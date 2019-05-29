6767f2348abcc66eb278399fa15fe5c162a7288f
Phillip Stevens <phillip.stevens@gmail.com>
2019-05-30 05:01:13 +1000

    m32 - hypotf update

--- libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_fshypot.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_fshypot.asm
@@ -6,17 +6,16 @@ SECTION code_fp_math32
 
 PUBLIC cm32_sccz80_fshypot
 
-EXTERN m32_fshypot
+EXTERN cm32_sccz80_fsreadl, m32_fshypot
 
     ; find the hypotenuse of two sccz80 floats
     ;
-    ; enter : stack = sccz80_float left, ret
-    ;          DEHL = sccz80_float right
+    ; enter : stack = sccz80_float left, sccz80_float right, ret
     ;
     ; exit  :  DEHL = sccz80_float(left+right)
     ;
     ; uses  : af, bc, de, hl, af', bc', de', hl'
 
-DEFC  cm32_sccz80_fshypot = m32_fshypot ; enter stack = d32_float left
-                                        ;        DEHL = d32_float right
-                                        ; return DEHL = d32_float
+cm32_sccz80_fshypot:
+    call cm32_sccz80_fsreadl
+    jp m32_fshypot
