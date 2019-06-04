459ea34760e9e842ea14bd57d6357489b672dce3
Stefano <stefano_bodrato@hotmail.com>
2019-06-04 10:25:50 +0200

    OSCA: simpler preservation of argv ptr

--- lib/target/osca/classic/osca_crt0.asm
+++ lib/target/osca/classic/osca_crt0.asm
@@ -125,17 +125,19 @@ ENDIF
 	
 start:
         di
-	ld	b,h
-	ld	c,l
+		
+	ld	(cmdline+1),hl
+;	ld	b,h
+;	ld	c,l
 
         ld      (start1+1),sp
         INCLUDE "crt/classic/crt_init_sp.asm"
         INCLUDE "crt/classic/crt_init_atexit.asm"
-	push	bc		
+;	push	bc		
 	call	crt0_init_bss
-	pop	bc
+;	pop	bc
         ld      (exitsp),sp
-        push	bc  ; keep ptr to arg list
+;       push	bc  ; keep ptr to arg list
 
 ; Optional definition for auto MALLOC init
 ; it assumes we have free space between the end of 
@@ -173,7 +175,10 @@ ENDIF
         
 	; Push pointers to argv[n] onto the stack now
 	; We must start from the end 
-	pop	hl	; command line back again
+	;pop	hl	; command line back again
+cmdline:
+	ld		hl,0	; SMC
+	
 	ld	bc,0
 	ld	a,(hl)
 	and	a
