6386ef2922792a0f4f9b6864b690dcadf0608180
Phillip Stevens <phillip.stevens@gmail.com>
2019-06-08 21:36:19 +1000

    m32 - commentfix

--- libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fsfrexp.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fsfrexp.asm
@@ -17,7 +17,7 @@
 ;       fl.f = x;
 ;       /* Find the exponent (power of 2) */
 ;       i  = ( fl.l >> 23) & 0x000000ff;
-;       i -= 0x7f;
+;       i -= 0x7e;
 ;       *pw2 = i;
 ;       fl.l &= 0x807fffff; /* strip all exponent bits */
 ;       fl.l |= 0x3f000000; /* mantissa between 0.5 and 1 */
