2900ff655e685732c174b20f9f20d41a97d55de9
Phillip Stevens <phillip.stevens@gmail.com>
2019-06-09 05:52:25 +1000

    m32 - div2f more typos

--- libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/asm_div2f.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/asm_div2f.asm
@@ -2,16 +2,18 @@
 ; float _div2f (float number) __z88dk_fastcall
 
 SECTION code_fp_math32
+
 PUBLIC  asm_div2f
-EXTERN  m32_div2_fastcall
+
+EXTERN  m32_fsdiv2_fastcall
 
     ; Divide a float by 2
     ;
     ; enter : stack = ret
     ;          DEHL = sccz80_float number
     ;
-    ; exit  :  DEHL = |sccz80_float|
+    ; exit  : DEHL = sccz80_float/2
     ;
     ; uses  : de, hl
 
-defc asm_div2f = m32_div2_fastcall
+defc asm_div2f = m32_fsdiv2_fastcall
--- libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/asm_fmul2f.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/asm_fmul2f.asm
@@ -1,17 +1,19 @@
 
-; float _fmul2f (float number) __z88dk_fastcall
+; float _mul2f (float number) __z88dk_fastcall
 
 SECTION code_fp_math32
-PUBLIC  asm_fmul2f
-EXTERN  m32_fmul2_fastcall
+
+PUBLIC  asm_mul2f
+
+EXTERN  m32_fsmul2_fastcall
 
     ; Multiply a float by 2
     ;
     ; enter : stack = ret
     ;          DEHL = sccz80_float number
     ;
-    ; exit  : DEHL = sccz80_float/2
+    ; exit  : DEHL = sccz80_float*2
     ;
     ; uses  : de, hl
 
-defc asm_fmul2f = m32_fmul2_fastcall
+defc asm_mul2f = m32_fsmul2_fastcall
