55bc2eccc10fb79bd7cc0d4db817d248919d09fb
Stefano <stefano_bodrato@hotmail.com>
2019-06-10 21:45:10 +0200

    Create close.c

---
+++ libsrc/fcntl/trs80/close.c
@@ -0,0 +1,36 @@
+
+/*
+ *  FCNTL library for the TRS-80
+ *	Stefano Bodrato - 2019
+ *
+ *	int close(int handle)
+ *	closes file and frees memory
+ *
+ *	$Id: close.c $
+ */
+
+#include <fcntl.h>
+
+//#include <stdio.h>
+#include <malloc.h>
+#include <trsdos.h>
+
+
+
+int close(int handle)
+{
+
+	struct TRSDOS_FILE *trs80_file;
+
+
+	trs80_file = (void *) handle;
+
+	/* Let's close the file only if TRSDOS thinks it is open */
+	if (trs80_file->fcb->type & 0x80) {
+		trsdos(DOS_CLOSE, 0, trs80_file->fcb);
+	}
+	free(handle);
+
+	return 0;
+
+}
