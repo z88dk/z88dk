ac87d38d4c28f72fb89c9a9ab0e2de2e9e159fb0
Phillip Stevens <phillip.stevens@gmail.com>
2019-06-11 20:25:35 +1000

    m32 - pow callee whitespace

--- libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_pow_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_pow_callee.asm
@@ -6,22 +6,22 @@ EXTERN _m32_powf
 
 
 cm32_sccz80_pow_callee:
-	pop	af	; return
-	pop	hl	; RHS
-	pop de
-	exx
-	pop hl  ; LHS
-	pop de
-	push af ; return
-	exx
-	push de ; RHS
-	push hl
-	exx
-	push de ; LHS
-	push hl
-	call _m32_powf
-	pop bc
-	pop bc
-	pop bc
-	pop bc
-	ret     ; return
+    pop af  ; return
+    pop hl  ; RHS
+    pop de
+    exx
+    pop hl  ; LHS
+    pop de
+    push af ; return
+    exx
+    push de ; RHS
+    push hl
+    exx
+    push de ; LHS
+    push hl
+    call _m32_powf
+    pop bc
+    pop bc
+    pop bc
+    pop bc
+    ret     ; return
--- libsrc/_DEVELOPMENT/math/float/math32/c/sdcc/cm32_sdcc_pow_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/c/sdcc/cm32_sdcc_pow_callee.asm
@@ -6,21 +6,21 @@ EXTERN _m32_powf
 
 
 cm32_sdcc_pow_callee:
-	pop	af	; return
-	pop	hl	; LHS
-	pop de
-	exx
-	pop hl  ; RHS
-	pop de
-	push af ; return
-	push de ; RHS
-	push hl
-	exx
-	push de ; LHS
-	push hl
-	call _m32_powf
-	pop bc
-	pop bc
-	pop bc
-	pop bc
-	ret     ; return
+    pop af  ; return
+    pop hl  ; LHS
+    pop de
+    exx
+    pop hl  ; RHS
+    pop de
+    push af ; return
+    push de ; RHS
+    push hl
+    exx
+    push de ; LHS
+    push hl
+    call _m32_powf
+    pop bc
+    pop bc
+    pop bc
+    pop bc
+    ret     ; return
