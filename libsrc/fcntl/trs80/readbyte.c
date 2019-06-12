482009b62b0b26de77e5f623a9fc1731a20f58ef
Stefano <stefano_bodrato@hotmail.com>
2019-06-12 14:45:19 +0200

    C, NC, conditions in readbyte()

--- libsrc/fcntl/trs80/readbyte.c
+++ libsrc/fcntl/trs80/readbyte.c
@@ -12,19 +12,22 @@
 //#include <fcntl.h>
 
 // "stdio.h" contains definition for EOF
-#include <stdio.h>
+//#include <stdio.h>
 #include <trsdos.h>
-
 
 int readbyte(int handle)
 {
 
 	struct TRSDOS_FILE *trs80_file;
-	
-	
+	int buffer;
+
 	trs80_file = (void *) handle;
 	
-	// trsdos_get() picks a byte or -1 (= EOF)
-	return (trsdos_get(trs80_file->fcb));
+	buffer = (trsdos_get(&trs80_file->fcb));
+
+	if (buffer == -1)
+		return_c -1;
+	else
+		return_nc (unsigned char)buffer;
 
 }
