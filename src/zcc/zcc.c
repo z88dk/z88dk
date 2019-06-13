b29e089f4f02293c9499c9671231c994b205f7fd
suborb <dom@suborbital.org.uk>
2019-06-13 20:50:54 +0100

    (zcc/cfg) Add the option to the right compiler

--- src/zcc/zcc.c
+++ src/zcc/zcc.c
@@ -844,7 +844,7 @@ int main(int argc, char **argv)
     parse_configfile_line(buffer);
     snprintf(buffer,sizeof(buffer),"ALIAS --hardware-keyboard -pragma-redirect:fgetc_cons=fgetc_cons_inkey");
     parse_configfile_line(buffer);
-    snprintf(buffer,sizeof(buffer),"ALIAS --math32 -Cs-fp-mode=ieee -lmath32");
+    snprintf(buffer,sizeof(buffer),"ALIAS --math32 -Cc-fp-mode=ieee -lmath32");
     parse_configfile_line(buffer);
 
     gc = 1;            /* Set for the first argument to scan for */
