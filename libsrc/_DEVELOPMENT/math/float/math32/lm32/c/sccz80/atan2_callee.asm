ce1ecbaf8dfceb29b96e0cbb76d0ecd652f1aa16
Phillip Stevens <phillip.stevens@gmail.com>
2019-06-15 19:27:04 +1000

    m32 - fix atan2 again

--- libsrc/_DEVELOPMENT/math/float/math32/lm32/c/sccz80/atan2_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/lm32/c/sccz80/atan2_callee.asm
@@ -1,14 +1,14 @@
 
 	SECTION	code_fp_math32
-	PUBLIC	pow_callee
+	PUBLIC	atan2_callee
 	EXTERN	cm32_sccz80_atan2_callee
 
-	defc	pow_callee = cm32_sccz80_atan2_callee
+	defc	atan2_callee = cm32_sccz80_atan2_callee
 
 
 ; SDCC bridge for Classic
 IF __CLASSIC
 PUBLIC _atan2_callee
-defc _atan2_callee = pow_callee
+defc _atan2_callee = atan2_callee
 ENDIF
 
