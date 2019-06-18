f507dcefe43235eda363cded865f50cc794cdc78
Stefano <stefano_bodrato@hotmail.com>
2019-06-18 13:34:29 +0200

    #1173 switch to mode 2 if in text mode

--- libsrc/video/tms9918/graphics/clg.asm
+++ libsrc/video/tms9918/graphics/clg.asm
@@ -9,7 +9,20 @@
 	SECTION	code_clib
         PUBLIC  clg
         PUBLIC  _clg
+		
 	EXTERN	__tms9918_cls
+	EXTERN	__tms9918_screen_mode
+	EXTERN	msx_set_mode
 
-	defc	clg = __tms9918_cls
-	defc	_clg = __tms9918_cls
+clg:
+_clg:
+	ld    a,(__tms9918_screen_mode)
+	and   a
+	jr    nz,notext
+	
+	; Enter in GFX mode 2 by default if in text mode
+	ld    hl,2
+	call  msx_set_mode
+	
+notext:
+	jp    __tms9918_cls
