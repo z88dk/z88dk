08cc2079b40b69a3f33ed5d430d155e7319154d0
Stefano <stefano_bodrato@hotmail.com>
2019-06-26 18:31:49 +0200

    #1173

--- libsrc/video/tms9918/graphics/bkrestore.asm
+++ libsrc/video/tms9918/graphics/bkrestore.asm
@@ -10,10 +10,13 @@
         
         PUBLIC  bkrestore
         PUBLIC  _bkrestore
+		
         EXTERN  bkpixeladdress
 
         EXTERN  l_tms9918_disable_interrupts
         EXTERN  l_tms9918_enable_interrupts
+        EXTERN  swapgfxbk
+        EXTERN  __graphics_end
 
         INCLUDE "video/tms9918/vdp.inc"
 
@@ -22,67 +25,67 @@
 ._bkrestore
 
 ; __FASTCALL__ : sprite ptr in HL
-        push    ix                ;save callers        
-        push    hl
-        pop     ix
+		push    ix                ;save callers        
+		push    hl
+		pop     ix
+		call    swapgfxbk
 
-        ld      h,(ix+2)
-        ld      l,(ix+3)
+		ld      h,(ix+2)
+		ld      l,(ix+3)
 
-        ld      a,(ix+0)
-        ld      b,(ix+1)
-        
-        dec     a
-        srl     a
-        srl     a
-        srl     a
-        inc     a
-        inc     a                ; INT ((Xsize-1)/8+2)
-        ld      (rbytes+1),a
+		ld      a,(ix+0)
+		ld      b,(ix+1)
+
+		dec     a
+		srl     a
+		srl     a
+		srl     a
+		inc     a
+		inc     a                ; INT ((Xsize-1)/8+2)
+		ld      (rbytes+1),a
 
 .bkrestores
-        push    bc
+		push    bc
 
-        push    hl
-        call    bkpixeladdress
-        pop     hl
+		call    bkpixeladdress
 
 .rbytes
-	xor	a
-.rloop	ex	af,af
-        call    l_tms9918_disable_interrupts
+		ld		a,0	; <-SMC!
+.rloop
+		ex      af,af
+		call    l_tms9918_disable_interrupts
 IF VDP_CMD < 0
-	ld	a,e
-	ld	(-VDP_CMD),a
-        ld      a,d                ; MSB of video mem ptr
-        and     @00111111        ; masked with "write command" bits
-        or      @01000000
-	ld	(-VDP_CMD),a
-	ld	a,(ix+4)
-	ld	(-VDP_DATA),a
+		ld      a,e
+		ld      (-VDP_CMD),a
+		ld      a,d                ; MSB of video mem ptr
+		and     @00111111        ; masked with "write command" bits
+		or      @01000000
+		ld      (-VDP_CMD),a
+		ld      a,(ix+4)
+		ld      (-VDP_DATA),a
 ELSE
-	ld	bc,VDP_CMD
-	out	(c),e
-        ld      a,d                ; MSB of video mem ptr
-        and     @00111111        ; masked with "write command" bits
-        or      @01000000
-	out	(c),a
-	ld	bc,VDP_DATA
-	ld	a,(ix+4)
-	out	(c),a
+		ld      bc,VDP_CMD
+		out     (c),e
+		ld      a,d                ; MSB of video mem ptr
+		and     @00111111        ; masked with "write command" bits
+		or      @01000000
+		out     (c),a
+		ld      bc,VDP_DATA
+		ld      a,(ix+4)
+		out     (c),a
 ENDIF
-        call    l_tms9918_enable_interrupts
-       	ex	de,hl
-	ld	bc,8
-	add	hl,bc
-	ex	de,hl 
-        inc     ix
-	ex	af,af
-	dec	a
-	jr	nz,rloop
+		call    l_tms9918_enable_interrupts
+		ex      de,hl
+		ld      bc,8
+		add     hl,bc
+		ex      de,hl 
+		inc     ix
+		ex      af,af
+		dec     a
+		jr      nz,rloop
 
-        inc     l
-        pop     bc
-        djnz    bkrestores
-        pop     ix        ;restore callers
-        ret
+		inc     l
+		pop     bc
+		djnz    bkrestores
+
+		jp     __graphics_end        ;restore callers
