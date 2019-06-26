50e62e62f36c509051681706a35b91aa5e49a752
Stefano <stefano_bodrato@hotmail.com>
2019-06-26 09:21:44 +0200

    #1173

--- libsrc/video/tms9918/graphics/clg.asm
+++ libsrc/video/tms9918/graphics/clg.asm
@@ -1,31 +1,45 @@
 ;
-;       Clear Graphics Screen
+;	TMS9918 generic variant
 ;
-;       MSX version by Stefano Bodrato, December 2007
+;	GFX - a small graphics library 
+;	Copyright (C) 2004  Rafael de Oliveira Jannone
+;
+;	extern clg();
+;
+;	Init and clear graphics screen
 ;
 ;	$Id: clg.asm $
 ;
 
-	SECTION	code_clib
-        PUBLIC  clg
-        PUBLIC  _clg
-		
-	EXTERN	__tms9918_cls
-	EXTERN	__tms9918_screen_mode
+	SECTION code_clib
+
+	PUBLIC	clg
+	PUBLIC	_clg
+	
+	EXTERN	msx_set_border
 	EXTERN	msx_set_mode
+	EXTERN	FILVRM
 	EXTERN	__tms9918_attribute
 
 clg:
 _clg:
-	ld    a,(__tms9918_screen_mode)
-	and   a
-	jr    nz,notext
+
+	ld	hl,2
+	call msx_set_mode
 	
-	; Enter in GFX mode 2 by default if in text mode
-	ld	a,$1F	; BLACK INK, WHITE PAPER
-	ld	(__tms9918_attribute),a
-	ld    hl,2
-	call  msx_set_mode
+	ld a,$1F   	; black on white attributes
+	ld (__tms9918_attribute),a
+    ld    hl,8192    ; set VRAM attribute area
+    ld    bc,6144
+    push  bc
+    call  FILVRM
+    pop   bc
+    xor   a
+    ld    h,a		; clear VRAM graphics page
+    ld    l,a
+    call    FILVRM
 	
-notext:
-	jp    __tms9918_cls
+	ld	l,$0F
+	call msx_set_border
+
+	ret
