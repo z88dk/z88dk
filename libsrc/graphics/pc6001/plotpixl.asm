fdc0d6a7a99dbd65d3782d909ceb4cdf561d7920
Stefano <stefano_bodrato@hotmail.com>
2019-07-07 14:56:28 +0200

    #1222

--- libsrc/graphics/pc6001/plotpixl.asm
+++ libsrc/graphics/pc6001/plotpixl.asm
@@ -6,12 +6,14 @@
 		EXTERN	plot_MODE1
 		EXTERN	plot_MODE2
 
+                EXTERN  __gfx_coords
 		EXTERN	__pc6001_mode
 
 		INCLUDE	"target/pc6001/def/pc6001.def"
 
 
 plotpixel:
+                ld	(__gfx_coords),hl
 		ld	a,(__pc6001_mode)
 		cp	MODE_1
 		jp	z,plot_MODE1
