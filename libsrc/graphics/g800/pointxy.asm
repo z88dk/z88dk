f5eb014facaf1746e673e5620e280a667036148a
Stefano <stefano_bodrato@hotmail.com>
2019-07-15 11:56:02 +0200

    #1080

--- libsrc/graphics/g800/pointxy.asm
+++ libsrc/graphics/g800/pointxy.asm
@@ -13,13 +13,15 @@ pointxy:
 
       call sety
       call getpat
+
       call setx
+      in a,(0x41) ;dummy read
       in a,(0x41) ;read data
 
       and b
       pop bc
 
-      cp 0
+      ;cp 0
       ret
 
 
