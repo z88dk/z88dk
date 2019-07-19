dfa06de99fd4b895e2c557bfb4bb5ff3a1131bcb
Phillip Stevens <phillip.stevens@gmail.com>
2019-07-20 00:03:34 +1000

    m32 - sgnabs comments fix

--- libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/__dtoa_sgnabs.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/lm32/z80/__dtoa_sgnabs.asm
@@ -6,15 +6,15 @@ PUBLIC __dtoa_sgnabs
 
 EXTERN m32__dtoa_sgnabs
 
-    ; enter : x = dehl = floating point number
+    ; enter : x = dehl' = floating point number
     ;
-    ; exit  : dehl = |x|
+    ; exit  : dehl' = |x|
     ;         a = sgn(x) = 1 if negative, 0 otherwise
     ;
     ; uses  : af
 
 
-DEFC  __dtoa_sgnabs = m32__dtoa_sgnabs          ; enter stack = ret
-                                                ;        DEHL = d32_float
-                                                ; return DEHL = d32_float
-                                                ;           A = sign
+DEFC  __dtoa_sgnabs = m32__dtoa_sgnabs          ; enter stack  = ret
+                                                ;        DEHL' = d32_float
+                                                ; return DEHL'  = d32_float
+                                                ;           A  = sign
