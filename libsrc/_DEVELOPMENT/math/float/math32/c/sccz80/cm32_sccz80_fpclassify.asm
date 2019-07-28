ca29c31bac1dc57b59414a75dcd16a8e17ae70c0
Phillip Stevens <phillip.stevens@gmail.com>
2019-07-28 14:57:12 +1000

    m32 - fpclassify C linkage

--- libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_fpclassify.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/c/sccz80/cm32_sccz80_fpclassify.asm
@@ -3,10 +3,11 @@
 SECTION code_fp_math32
 PUBLIC cm32_sccz80_fpclassify
 
-EXTERN cm32_sccz80_fsread1, _m32_fpclassify
+EXTERN cm32_sccz80_fsread1, m32_fpclassify
 
 cm32_sccz80_fpclassify:
-    exx
     call cm32_sccz80_fsread1
-    exx
-    jp _m32_fpclassify
+    call m32_fpclassify
+    ld l,a
+    ld h,0
+    ret
--- libsrc/_DEVELOPMENT/math/float/math32/c/sdcc/cm32_sdcc_fpclassify.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/c/sdcc/cm32_sdcc_fpclassify.asm
@@ -3,15 +3,16 @@
 SECTION code_fp_math32
 PUBLIC cm32_sdcc_fpclassify
 
-EXTERN _m32_fpclassify
+EXTERN m32_fpclassify
 
 cm32_sdcc_fpclassify:
-        exx
-        pop     bc
-        pop     hl
-        pop     de
-        push    de
-        push    hl
-        push    bc
-        exx
-        jp      _m32_fpclassify
+    pop bc                      ; pop ret
+    pop hl
+    pop de
+    push de
+    push hl
+    push bc                     ; push ret
+    call m32_fpclassify
+    ld l,a
+    ld h,0
+    ret
