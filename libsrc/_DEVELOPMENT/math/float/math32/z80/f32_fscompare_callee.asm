3d476e053e0bfcccbcfad7ddbe09b9fd8e1fd380
Phillip Stevens <phillip.stevens@gmail.com>
2019-08-01 20:16:44 +1000

    m32 - f32_compare_callee -0 handling

--- libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fscompare_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fscompare_callee.asm
@@ -3,71 +3,136 @@ SECTION code_fp_math32
 
 PUBLIC  m32_compare_callee
 
-; Compare two IEEE floats. NB. Needs to handle -0 == +0
-;       Entry: dehl=secondary
-;              onstack (sp+4: under two return addresses) = primary
+; Compare two IEEE floats.
+;       Entry: dehl  = right
+;              stack = left, ret, ret
 ;
-;       Exit:     z=number is zero
-;              (nz)=number is non-zero
-;                 c=number is negative 
-;                nc=number is positive
+;       Exit:      Z = number is zero
+;               (NZ) = number is non-zero
+;                  C = number is negative 
+;                 NC = number is positive
+;
+;       Uses: af, bc, de, hl, bc', de', hl'
 
 .m32_compare_callee
-    pop     bc      ;return address from this function
-                    ;this leaves return address to real program
+                    ;right
+    pop af          ;return address from this function
+                    ;return address to real program
                     ;and the primary on the stack
 
     exx             ;left
-    pop     bc
-    pop     hl
-    pop     de
-    push    bc      ;return address to program
-    ld      a,l
+    pop bc
+    pop hl
+    pop de
+    push bc         ;return address to program
+    push af         ;return address from this function
+
+    sla e
+    rl d
+    jr Z,zero_left  ;left is zero (exponent is zero)
+    rr d
+    rr e
+
+    ld a,l
+
     exx             ;right
-    push    bc      ;return address from function
-    sub     l
-    ld      c,a    
+    sla e
+    rl d
+    jr Z,zero_right ;right is zero (exponent is zero)
+    rr d
+    rr e
+
+    sub l
+    ld c,a
+
     exx             ;left
-    ld      a,h
+    ld a,h
+
     exx             ;right
-    sbc     a,h
-    ld      b,a
+    sbc a,h
+    ld b,a
+
     exx             ;left
-    ld    a,e
+    ld a,e
+
     exx             ;right
-        sbc     a,e
+    sbc a,e
+
     exx             ;left
-    ld    c,a
-    ld      a,d
+    ld c,a
+    ld a,d
+
     exx             ;right
-    sbc     a,d
+    sbc a,d
+
     exx             ;left
-    ld      b,a
+    ld b,a
 
     ; left dehl = float, bc = highword of result
     ; (exx) right dehl = float, bc = low word of result
-    ld    a,b
-    or    c
-
-    bit    7,b
-    jr    z,consider_positive
+    ld a,b
+    or c
+    bit 7,b
+    jr Z,consider_positive
 
     ; Calculate zero state of result
+.consider_negative
     exx
-    or    b
-    or    c
+    or b
+    or c
+
     exx
-    ld    hl,1    
+    ld hl,1    
     scf
     ret
 
-consider_positive:
+.consider_positive
     exx
-    or    b
-    or    c
+    or b
+    or c
+
     exx
     scf
     ccf
-    ld    hl,1
+    ld hl,1
     ret
-    
+
+.zero_left
+    ; left dehl = 0
+    ; right dehl' = float
+    exx
+    ld a,d
+    or e
+    bit 7,d
+    jr Z,zero_negative
+
+.zero_positive
+    exx
+    or h
+    or l
+
+    exx
+    scf
+    ccf
+    ld hl,1
+    ret
+
+.zero_right
+    ; left dehl' = float
+    ; right dehl = 0
+    exx
+    ld a,d
+    or e
+    bit 7,d
+    jr Z,zero_positive
+
+.zero_negative
+    exx
+    or h
+    or l
+
+    exx
+    ld hl,1    
+    scf
+    ret
+
