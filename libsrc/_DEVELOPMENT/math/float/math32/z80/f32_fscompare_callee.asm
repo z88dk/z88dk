ed075f8ce3e0e03a9069cb21598202c7319ca914
Phillip Stevens <phillip.stevens@gmail.com>
2019-08-01 21:41:21 +1000

    m32 - f32_compare_callee -0 optimisation

--- libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fscompare_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fscompare_callee.asm
@@ -4,6 +4,9 @@ SECTION code_fp_math32
 PUBLIC  m32_compare_callee
 
 ; Compare two IEEE floats.
+;
+; IEEE float is considered zero if exponent is zero.
+;
 ;       Entry: dehl  = right
 ;              stack = left, ret, ret
 ;
@@ -15,57 +18,57 @@ PUBLIC  m32_compare_callee
 ;       Uses: af, bc, de, hl, bc', de', hl'
 
 .m32_compare_callee
-                    ;right
-    pop af          ;return address from this function
-                    ;return address to real program
-                    ;and the primary on the stack
+                        ;right
+    pop af              ;return address from this function
+                        ;return address to real program
+                        ;and the primary on the stack
 
-    exx             ;left
+    exx                 ;left
     pop bc
     pop hl
     pop de
-    push bc         ;return address to program
-    push af         ;return address from this function
+    push bc             ;return address to program
+    push af             ;return address from this function
 
     sla e
     rl d
-    jr Z,zero_left  ;left is zero (exponent is zero)
+    jr Z,zero_left      ;left is zero (exponent is zero)
     rr d
     rr e
 
     ld a,l
 
-    exx             ;right
+    exx                 ;right
     sla e
     rl d
-    jr Z,zero_right ;right is zero (exponent is zero)
+    jr Z,zero_right     ;right is zero (exponent is zero)
     rr d
     rr e
 
     sub l
     ld c,a
 
-    exx             ;left
+    exx                 ;left
     ld a,h
 
-    exx             ;right
+    exx                 ;right
     sbc a,h
     ld b,a
 
-    exx             ;left
+    exx                 ;left
     ld a,e
 
-    exx             ;right
+    exx                 ;right
     sbc a,e
 
-    exx             ;left
+    exx                 ;left
     ld c,a
     ld a,d
 
-    exx             ;right
+    exx                 ;right
     sbc a,d
 
-    exx             ;left
+    exx                 ;left
     ld b,a
 
     ; left dehl = float, bc = highword of result
@@ -75,7 +78,7 @@ PUBLIC  m32_compare_callee
     bit 7,b
     jr Z,consider_positive
 
-    ; Calculate zero state of result
+    ; Calculate whether result is zero (equal)
 .consider_negative
     exx
     or b
@@ -92,47 +95,36 @@ PUBLIC  m32_compare_callee
     or c
 
     exx
+    ld hl,1    
     scf
     ccf
-    ld hl,1
     ret
 
 .zero_left
     ; left dehl = 0
     ; right dehl' = float
     exx
-    ld a,d
-    or e
-    bit 7,d
-    jr Z,zero_negative
+    sla e
+    rl d
+    jr Z,comp_positive  ;right is zero
+    jr C,comp_positive  ;sign of right is negative
 
-.zero_positive
-    exx
-    or h
-    or l
-
-    exx
-    scf
-    ccf
+.comp_negative
     ld hl,1
+    scf
     ret
 
 .zero_right
     ; left dehl' = float
     ; right dehl = 0
     exx
-    ld a,d
-    or e
-    bit 7,d
-    jr Z,zero_positive
+    sla e
+    rl d
+    jr C,comp_negative  ;sign of left is negative
 
-.zero_negative
-    exx
-    or h
-    or l
-
-    exx
-    ld hl,1    
+.comp_positive
+    ld hl,1
     scf
+    ccf
     ret
 
