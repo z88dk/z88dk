b68920791b9d31c97445f4bf1c20252613b65746
Stefano <stefano_bodrato@hotmail.com>
2019-08-05 08:58:11 +0200

    #1251

--- libsrc/fcntl/cpm/dir_move_first.c
+++ libsrc/fcntl/cpm/dir_move_first.c
@@ -2,7 +2,8 @@
  *  CP/M Directory browsing
  * 
  *  Stefano, 5 Jun 2013
- *  07/2019: Dirty workaround to stabilize dir_move_next when file operations happen.
+ *  07/2019: Workaround to stabilize dir_move_next when file operations happen.
+ *  08/2019: Dealing with the bdos() function in a correct way
  *
  *
  *  $Id: dir_move_first.c $
@@ -21,7 +22,7 @@ int dir_move_first()
 {
 	fc_dirbuf=dirbuf;
 	bdos(CPM_SDMA,fc_dirbuf);
-	parsefcb(&fc_dir,"????????.???");
-	dirbuf[133]=0;
-	return (fc_dirpos=bdos(CPM_FFST,&fc_dir));
+	parsefcb(&fc_dir,"*.*");
+	fc_dirpos=bdos(CPM_FFST,&fc_dir);
+	return (fc_dirpos==-1?0x24:0);	// Not knowing what to pass for non-zero, let's simulate FLOS error code $24 (= Reached end of directory)
 }
