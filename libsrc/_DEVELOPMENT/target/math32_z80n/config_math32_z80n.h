badc4df72d5ce5fd83a769d0cd887a85d302067a
Phillip Stevens <phillip.stevens@gmail.com>
2019-09-13 00:42:31 +1000

    math32 - z80n - config repair

--- libsrc/_DEVELOPMENT/target/math32_z80n/config_math32_z80n.h
+++ libsrc/_DEVELOPMENT/target/math32_z80n/config_math32_z80n.h
@@ -42,15 +42,11 @@
 #define __Z80_NMOS  0x01
 #define __Z80_CMOS  0x02
 
-#define __CPU_Z80N__ 0x01
-
 #define __USE_Z80N_OPCODES_NEXTREG  1
 #define __USE_Z80N_OPCODES_MLT  2
 #define __USE_Z80N_OPCODES_LDIR  4
 #define __USE_Z80N_OPCODES_DISPLAY  8
 #define __USE_Z80N_OPCODES_OTHER  16
-
-#define __CPU_CLOCK  3500000
 
 #define __CPU_INFO  0x00
 
