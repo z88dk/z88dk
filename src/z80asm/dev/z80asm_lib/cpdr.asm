47057f3c09eccd6a4c906c542f5793e338d8489d
Paulo Custodio <pauloscustodio@gmail.com>
2019-09-15 09:43:40 +0100

    (#1275, #1276) Add call to emulation routine on ex(sp),hl
    On a GameBoy, call automatically the emulation for ex(sp),hl.
    Remove IF/ELSE in library code.

--- src/z80asm/dev/z80asm_lib/cpdr.asm
+++ src/z80asm/dev/z80asm_lib/cpdr.asm
@@ -4,7 +4,6 @@
 
 SECTION code_crt0_sccz80
 PUBLIC __z80asm__cpdr
-EXTERN __z80asm__exsphl
 
 .__z80asm__cpdr
  
@@ -14,11 +13,7 @@ EXTERN __z80asm__exsphl
    
 ; scf clears N and H - must set carry the hard way
    push af
-IF __CPU_GBZ80__
-   call __z80asm__exsphl
-ELSE
    ex (sp),hl
-ENDIF
 IF __CPU_8080__ || __CPU_8085__
    ld  a,l
    or  @00000001
@@ -53,11 +48,7 @@ ENDIF
  
 .joinbc0
  
-IF __CPU_GBZ80__
-   call __z80asm__exsphl
-ELSE
    ex (sp),hl
-ENDIF
 IF __CPU_8080__ || __CPU_8085__
    ld a,l
    and @11111010
@@ -77,11 +68,7 @@ ENDIF
    or c
    jr z, joinbc0
   
-IF __CPU_GBZ80__
-   call __z80asm__exsphl
-ELSE
    ex (sp),hl
-ENDIF
 IF __CPU_8080__ || __CPU_8085__
    ld a,l
    and @11111110
@@ -93,10 +80,6 @@ ELSE
 ENDIF
 
 .retflags
-IF __CPU_GBZ80__
-   call __z80asm__exsphl
-ELSE
    ex (sp),hl
-ENDIF
    pop af
    ret
