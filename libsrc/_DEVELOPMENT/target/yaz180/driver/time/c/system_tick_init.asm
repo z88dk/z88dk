85a660cd7655bb2cb99b491f736c7c7a0255f271
Phillip Stevens <phillip.stevens@gmail.com>
2019-10-22 12:23:11 +1100

    yaz180 - adjust system time framework

---
+++ libsrc/_DEVELOPMENT/target/yaz180/driver/time/c/system_tick_init.asm
@@ -0,0 +1,16 @@
+SECTION code_driver
+
+PUBLIC _system_tick_init, system_tick_init
+
+EXTERN asm_system_tick_init
+
+; initialize the system tick
+
+_system_tick_init:
+    pop af
+    pop hl
+    push hl
+    push af
+    jp asm_system_tick_init
+
+defc system_tick_init = asm_system_tick_init
