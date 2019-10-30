9dfadba03b646e9e2739e265102782ae836bb21c
Stefano <stefano_bodrato@hotmail.com>
2019-10-30 08:18:22 +0100

    #1311

--- libsrc/fcntl/cpm/lseek.c
+++ libsrc/fcntl/cpm/lseek.c
@@ -5,7 +5,7 @@
  *	1	SEEK_CUR from current position
  *	2	SEEK_END from end of file (always -ve)
  *
- *	$Id: lseek.c $
+ *	$Id: lseek.c,v 1.6 2013-06-06 08:58:32 stefano Exp $
 */
 
 #include <fcntl.h>
@@ -19,30 +19,35 @@ long lseek(int fd,long posn, int whence)
 	char buffer[1];
 
 	if(fd >= MAXFILE)
-		return -1;
+		return -1L;
+	
 	fc = &_fcb[fd];
+	
 	switch(whence) {
-
 	default:
 		pos = posn;
 		break;
-
 	case 1:
 		pos = fc->rwptr + posn;
 		break;
-
 	case 2:
-		while (read(fd,buffer,1) != EOF) {
-			if (buffer[0]==__STDIO_EOFMARKER) break;
+		if (fc->mode & _IOTEXT) {
+			while (read(fd,buffer,1) != EOF) {
+					if (buffer[0]==__STDIO_EOFMARKER) break;
+			}
+			pos = fc->rwptr-1;
+		} else {
+			bdos(CPM_CFS,fc);
+			pos = (unsigned long) (fc->ranrec[0] + 256 * fc->ranrec[1]) * 128L;
+			if (fc->ranrec[2]&1)
+				pos += 8388608L;
 		}
-		pos = fc->rwptr-1;
-		break;
 	}
 
 	if (pos < 0L)
-		return -1;
+		return -1L;
 	
 	fc->rwptr = pos;
-	return fc->rwptr;
+	return pos;
 	
 }
