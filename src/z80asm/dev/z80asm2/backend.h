1eb5d491ee6c22d4dd015db7f7f5ad8cc13f5e1e
Paulo Custodio <pauloscustodio@gmail.com>
2019-11-12 14:53:17 +0000

    Update backend.h

--- src/z80asm/dev/z80asm2/backend.h
+++ src/z80asm/dev/z80asm2/backend.h
@@ -1,10 +1,9 @@
-#------------------------------------------------------------------------------
-# z80asm restart
-#
-# Copyright (C) Paulo Custodio, 2011-2019
-# License: http://www.perlfoundation.org/artistic_license_2_0
-# Repository: https://github.com/z88dk/z88dk
-#------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// z80asm restart
+// Copyright (C) Paulo Custodio, 2011-2019
+// License: http://www.perlfoundation.org/artistic_license_2_0
+// Repository: https://github.com/z88dk/z88dk
+//-----------------------------------------------------------------------------
 #pragma once
 
 void init_backend(const char* output_filename);
