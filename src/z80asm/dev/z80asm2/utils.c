fe4026d53417093da04cdd58bedbf8498a2953ce
Paulo Custodio <pauloscustodio@gmail.com>
2019-11-12 14:47:48 +0000

    Update utils.c

--- src/z80asm/dev/z80asm2/utils.c
+++ src/z80asm/dev/z80asm2/utils.c
@@ -1,3 +1,10 @@
+#------------------------------------------------------------------------------
+# z80asm restart
+#
+# Copyright (C) Paulo Custodio, 2011-2019
+# License: http://www.perlfoundation.org/artistic_license_2_0
+# Repository: https://github.com/z88dk/z88dk
+#------------------------------------------------------------------------------
 #include "utils.h"
 
 #include <stdbool.h>
