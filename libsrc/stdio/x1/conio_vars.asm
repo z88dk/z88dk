b18f718796efd2a032ccfb5166c0feaacc1ee5d9
suborb <dom@suborbital.org.uk>
2019-11-14 17:53:56 +0000

    (x1) Issue #787: Rework appmake to use disc utilities.
    Add CP/M extension library supporting graphics, gencon etc

--- libsrc/stdio/x1/conio_vars.asm
+++ libsrc/stdio/x1/conio_vars.asm
@@ -8,9 +8,10 @@
     PUBLIC  __x1_attr
     PUBLIC  __x1_mode
     PUBLIC  __x1_pcg_mode
+    EXTERN  CLIB_DEFAULT_SCREEN_MODE
 
 .__x1_attr       defb $7, $0	; White on Black
 
-.__x1_mode	defb	0
+.__x1_mode	defb	CLIB_DEFAULT_SCREEN_MODE
 
 .__x1_pcg_mode defb    0       ;Bit 0 = custom font, bit 1 = custom udgs
