c60fff50dde8a123f28acb5d65d7c31373d24628
suborb <dom@suborbital.org.uk>
2019-11-19 22:40:48 +0000

    Issue #1330: Add lores graphics using character redefinition

--- libsrc/target/fp1100/graphics/w_plotpixl.asm
+++ libsrc/target/fp1100/graphics/w_plotpixl.asm
@@ -5,9 +5,18 @@
 
     SECTION code_clib
     PUBLIC  w_plotpixel
+    EXTERN  __fp1100_mode
     defc    NEEDplot = 1
 
-.w_plotpixel			
+.w_plotpixel
+    ld      a,(__fp1100_mode)
+    bit     1,a
+    jr      z,hires
+    ld      h,l
+    ld      l,e
+    INCLUDE "graphics/generic_console/pixel.inc"
+
+hires:
     INCLUDE "w_pixel.inc"
 
 
--- libsrc/target/fp1100/graphics/w_pointxy.asm
+++ libsrc/target/fp1100/graphics/w_pointxy.asm
@@ -6,10 +6,19 @@
 
 
         SECTION code_clib
-	PUBLIC	w_pointxy
+        PUBLIC  w_pointxy
+        EXTERN  __fp1100_mode
         defc    NEEDpoint= 1
 
 
-.w_pointxy			
+.w_pointxy
+    ld      a,(__fp1100_mode)
+    bit     1,a
+    jr      z,hires
+    ld      h,l
+    ld      l,e
+    INCLUDE "graphics/generic_console/pixel.inc"
+
+hires:
 	INCLUDE "w_pixel.inc"
 
