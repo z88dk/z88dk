d796b8fa41e5e0c80ab7514b95786fe08fda1095
Sandor Vass <vass.sanyi@gmail.com>
2019-11-22 13:54:07 +0100

    di/ei is removed as (INTINC) is incremented in TVC's interrupt routine (is not changed outside of an ISR)

--- libsrc/time/tvc/clock.asm
+++ libsrc/time/tvc/clock.asm
@@ -14,10 +14,7 @@
         include "target/tvc/def/tvc.def"
 .clock
 ._clock
-;    INTINC is 0b1dh
-    di
 	ld hl,(INTINC) ; count of 20.096ms from start (can count up to almost 22mins)
-    ei
 	ex de,hl
     ld h,0
     ld l,h
