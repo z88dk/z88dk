0e43cd17eae83183ce834f40af11764ba7cd95ea
Phillip Stevens <phillip.stevens@gmail.com>
2019-11-23 23:26:55 +1100

    scz180 - diskio for hbios, rom config

--- include/_DEVELOPMENT/proto/arch/scz180/diskio.h
+++ include/_DEVELOPMENT/proto/arch/scz180/diskio.h
@@ -93,11 +93,11 @@ typedef enum {
 // IDE DISK COMMANDS
 //
 
-__DPROTO(`b,c,d,e,iyh,iyl',`b,c,d,e,iyh,iyl',DSTATUS,,disk_initialize,BYTE pdrv)
-__DPROTO(`d,e,iyh,iyl',`d,e,iyh,iyl',DSTATUS,,disk_status,BYTE pdrv)
+__DPROTO(`iyh,iyl',`iyh,iyl',DSTATUS,,disk_initialize,BYTE pdrv)
+__DPROTO(`iyh,iyl',`iyh,iyl',DSTATUS,,disk_status,BYTE pdrv)
 __DPROTO(`iyh,iyl',`iyh,iyl',DRESULT,,disk_read,BYTE pdrv, BYTE* buff, DWORD sector, UINT count)
 __DPROTO(`iyh,iyl',`iyh,iyl',DRESULT,,disk_write,BYTE pdrv, const BYTE* buff, DWORD sector, UINT count)
-__DPROTO(`d,e,iyh,iyl',`d,e,iyh,iyl',DRESULT,,disk_ioctl,BYTE pdrv, BYTE cmd, void* buff)
+__DPROTO(`iyh,iyl',`iyh,iyl',DRESULT,,disk_ioctl,BYTE pdrv, BYTE cmd, void* buff)
 
 //
 // CSIO SD COMMANDS
