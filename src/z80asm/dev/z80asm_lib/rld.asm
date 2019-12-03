de7283110084f9963072367098ec5eede25f5316
Paulo Custodio <pauloscustodio@gmail.com>
2019-12-03 23:17:58 +0000

    Implement rld and rrd emulation for 8080/8085

--- src/z80asm/dev/z80asm_lib/rld.asm
+++ src/z80asm/dev/z80asm_lib/rld.asm
@@ -1,7 +1,14 @@
 ; Substitute for z80 rld instruction
 ; aralbrec 06.2007
+; CPU   Min T Max T
+; 8080  201   232
+; 8085  197   226
+; gbz80 164   188
+; r2k   108   125
+; z180   18    18
+; z80    18    18
+; z80n   18    18
 
-IF !__CPU_8080__ && !__CPU_8085__
 SECTION code_crt0_sccz80
 PUBLIC __z80asm__rld
 
@@ -15,6 +22,37 @@ PUBLIC __z80asm__rld
 
 .dorld
    
+IF __CPU_INTEL__		; a = xi, (hl) = jk --> a = xj, (hl) = ki
+   push de
+   push hl
+   
+   ld l, (hl)			
+   ld h, 0				; hl = 00jk
+
+   ld d, a				; d = xi
+   and 0xf0
+   ld e, a				; e = x0
+   ld a, d
+   and 0x0f
+   ld d, a              ; d = a = 0i
+   
+   add hl,hl
+   add hl,hl
+   add hl,hl
+   add hl,hl			; a = 0i, hl = 0jk0
+   
+   add a, l
+   ld l, a				; a = 0i, hl = 0jki
+   ld a, h				; a = 0j, hl = 0jki
+   add a, e				; a = xj, hl = 0jki
+   
+   ld e, l				; a = xj, e = ki
+   
+   pop hl
+   ld (hl), e			; a = xj, (hl) = ki
+   
+   pop de
+ELSE   
    rlca
    rlca
    rlca
@@ -35,7 +73,7 @@ PUBLIC __z80asm__rld
    rla
    rl (hl)
    adc a,0
+ENDIF
    
    or a
    ret
-ENDIF
--- src/z80asm/dev/z80asm_lib/rrd.asm
+++ src/z80asm/dev/z80asm_lib/rrd.asm
@@ -1,7 +1,14 @@
 ; Substitute for z80 rrd instruction
 ; aralbrec 06.2007
+; CPU   Min T Max T
+; 8080  229   260
+; 8085  224   253
+; gbz80 136   160
+; r2k    94   111
+; z180   18    18
+; z80    18    18
+; z80n   18    18
 
-IF !__CPU_8080__ && !__CPU_8085__
 SECTION code_crt0_sccz80
 PUBLIC __z80asm__rrd
 
@@ -15,6 +22,43 @@ PUBLIC __z80asm__rrd
 
 .dorrd
    
+IF __CPU_INTEL__		; a = xi, (hl) = jk --> a = xk, (hl) = ij
+   push de
+   push hl
+   
+   ld l, (hl)			
+   ld h, 0				; hl = 00jk
+
+   ld d, a				; d = xi
+   and 0xf0
+   ld e, a				; e = x0
+   ld a, d
+   and 0x0f
+   ld d, a              ; d = a = 0i
+
+   ld a, l				; a = jk
+   and 0x0f				; a = 0k
+   add a, e				; a = xk
+   ld e, a				; e = xk
+   
+   add hl,hl
+   add hl,hl
+   add hl,hl
+   add hl,hl			; e = xk, a = xk, hl = 0jk0
+   
+   ld a, d
+   add a, a
+   add a, a
+   add a, a
+   add a, a				; a = i0
+   add a, h				; a = ij
+   
+   pop hl
+   ld (hl), a			; (hl) = ij
+   
+   ld a, e				; a = xk
+   pop de
+ELSE
    srl a
    rr (hl)
    
@@ -33,6 +77,7 @@ PUBLIC __z80asm__rrd
    rra
    rra
    
+ENDIF
+
    or a
    ret
-ENDIF
