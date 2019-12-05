1442efea904b1deb8cb6f9b95b28b1d08bbe1294
Phillip Stevens <phillip.stevens@gmail.com>
2019-12-05 19:35:37 +1100

    scz180 - csio SD interface

--- libsrc/_DEVELOPMENT/target/scz180/device/csio/sdcc/csio_sd_cs_lower_fastcall.asm
+++ libsrc/_DEVELOPMENT/target/scz180/device/csio/sdcc/csio_sd_cs_lower_fastcall.asm
@@ -7,6 +7,7 @@ EXTERN asm_sd_cs_lower
 
     ;Lower the SC130 SD card CS using the GPIO address
     ;
+    ;input (H)L = SD CS selector of 1 or 2
     ;uses AF
 
 defc _sd_cs_lower_fastcall = asm_sd_cs_lower
--- libsrc/_DEVELOPMENT/target/scz180/device/csio/sdcc/csio_sd_cs_raise_fastcall.asm
+++ libsrc/_DEVELOPMENT/target/scz180/device/csio/sdcc/csio_sd_cs_raise_fastcall.asm
@@ -7,6 +7,7 @@ EXTERN asm_sd_cs_raise
 
     ;Raise the SC180 SD card CS using the GPIO address
     ;
+    ;input (H)L = SD CS selector of 1 or 2
     ;uses AF
 
 defc _sd_cs_raise_fastcall = asm_sd_cs_raise
