384b45fb5e11aa8422230099dcd301040fe038ec
Phillip Stevens <phillip.stevens@gmail.com>
2019-12-27 13:52:38 +1100

    scz180 - move default SD to #0

--- libsrc/_DEVELOPMENT/target/scz180/device/csio/sccz80/csio_sd_cs_lower.asm
+++ libsrc/_DEVELOPMENT/target/scz180/device/csio/sccz80/csio_sd_cs_lower.asm
@@ -7,7 +7,7 @@ EXTERN asm_sd_cs_lower
 
     ;Lower the SC130 SD card CS using the GPIO address
     ;
-    ;input (H)L = SD CS selector of 1 or 2
+    ;input (H)L = SD CS selector of 0 or 1
     ;uses AF
 
 defc sd_cs_lower = asm_sd_cs_lower
