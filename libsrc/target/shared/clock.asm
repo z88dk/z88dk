71cec314207d684618dc4471515199cf3e01c957
suborb <dom@suborbital.org.uk>
2020-01-05 20:39:10 +0000

    Fix up for zx dependencies

--- libsrc/target/shared/clock.asm
+++ libsrc/target/shared/clock.asm
@@ -1,12 +1,5 @@
 ;
 ;	clock()
-;	Goodness knows it this is right..ages since
-;	I used a spectrum...
-;
-;	djm 12/1/2000
-;
-; ------
-; $Id: clock.asm,v 1.5 2016-06-10 21:14:50 dom Exp $
 ;
 
 	SECTION code_clib
