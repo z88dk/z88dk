cdc20ceb21d1d1703009c69492eb5efcc5411e6b
suborb <dom@suborbital.org.uk>
2020-01-13 22:16:07 +0000

    (pmd85) A faster method to calculate plot position

--- libsrc/target/pmd85/graphics/w_pixladdr.asm
+++ libsrc/target/pmd85/graphics/w_pixladdr.asm
@@ -13,7 +13,8 @@
 .w_pixeladdress
 	; Display is 288 pixels wide, 256 pixels high
 	push	de
-	ld	de,-6
+	; If we divide by 24
+	ld	de,-24
 	ld	c,255	;Number of cells
 divloop:
 	inc	c
@@ -22,8 +23,20 @@ divloop:
 	cp	255
 	jp	nz,divloop
 	ld	a,l
-	add	6
-	ld	b,a	;Pixel position
+	add	24
+	; a = pixel position
+	ld	e,a
+	ld	d,0
+	ld	hl,lookup_table
+	add	hl,de
+	add	hl,de
+	ld	a,c		;24 pixels = 4 chars, so *4
+	add	a
+	add	a
+	add	(hl)		;Byte offset
+	ld	c,a
+	inc	hl
+	ld	b,(hl)		;pixel position
 	pop	hl	;Get y position back, we need to *64
 	add	hl,hl	;*2
 	add	hl,hl	;*4
@@ -40,3 +53,31 @@ divloop:
 	and	a
 	ret
 
+
+	SECTION	rodata_clib
+
+lookup_table:
+	defb	0,0
+	defb	0,1
+	defb	0,2
+	defb	0,3
+	defb	0,4
+	defb	0,5
+	defb	1,0
+	defb	1,1
+	defb	1,2
+	defb	1,3
+	defb	1,4
+	defb	1,5
+	defb	2,0
+	defb	2,1
+	defb	2,2
+	defb	2,3
+	defb	2,4
+	defb	2,5
+	defb	3,0
+	defb	3,1
+	defb	3,2
+	defb	3,3
+	defb	3,4
+	defb	3,5
