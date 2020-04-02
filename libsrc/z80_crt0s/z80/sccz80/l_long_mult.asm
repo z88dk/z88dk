4623aac7fbb37d0f20819aee461449e3f1d72409
suborb <dom@suborbital.org.uk>
2020-04-02 22:20:35 +0100

    (crt0) Delegate to newlib math so can be replaced with fastmath

--- libsrc/z80_crt0s/z80/sccz80/l_long_mult.asm
+++ libsrc/z80_crt0s/z80/sccz80/l_long_mult.asm
@@ -4,71 +4,24 @@
 ;       Multiply 32 bit numbers
 ;
 ;       Entry: dehl=arg1
-;       Staci: return address, arg2
+;       Stack: return address, arg2
 ;
 ;       Exit:  dehl=result
 
-; djm note I don't particularly like the use of ix as a pointer to the
-; second argument, however it makes it quite a short routine, alternate
-; method might be to use bc and bc' to hold arg2 and use a as a loop
-; counter - please feel free to experiment!
 
-; aralbrec  advice taken!
-
-
-                SECTION   code_crt0_sccz80
-PUBLIC	l_long_mult
-
-; dehl = arg1
-; stack = arg2, ret
+       SECTION   code_crt0_sccz80
+       PUBLIC    l_long_mult
+       EXTERN    l_mulu_32_32x32
 
 .l_long_mult
+   ; dehl = arg1
+   ; stack = arg2, ret
 
-   pop af
-
-   push hl
-   exx
-   pop de
-   pop bc
-   ld hl,0
    exx
    pop bc
-   ld hl,0
 
-   push af
-
-   ; dede' = arg1
-   ; bcbc' = arg2
-   ; hlhl' = res
-
-   ld a,32
-
-.l_long_mult1
-
-   srl b                       ; arg2 >>= 1
-   rr c
-   exx
-   rr b
-   rr c
-   jr nc, l_long_mult2
-   
-   add hl,de                   ; res += arg1
-   exx
-   adc hl,de
-   exx
-
-.l_long_mult2
-
-   sla e                       ; arg1 <<= 1
-   rl d
-   exx
-   rl e
-   rl d
-   
-   dec a
-   jp nz, l_long_mult1
-
-   push hl
-   exx
+   pop hl
    pop de
-   ret
+
+   push bc
+   jp l_mulu_32_32x32
