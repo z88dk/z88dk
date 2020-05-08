152ca2b7c38d88b02911003643e2b42aedf1846f
Phillip Stevens <phillip.stevens@gmail.com>
2020-05-08 23:56:01 +1000

    classic -cpm whitespace

--- libsrc/target/cpm/fcntl/write.c
+++ libsrc/target/cpm/fcntl/write.c
@@ -1,6 +1,6 @@
 /*
  *  Write to a file
- * 
+ *
  *  27/1/2002 - djm
  *
  *  May, 2020 - feilipu - added sequential write
@@ -16,14 +16,14 @@
 
 
 ssize_t write(int fd, void *buf, size_t len)
-{   
+{
     char    buffer[SECSIZE+2];
     unsigned char uid;
     struct fcb *fc;
     int    cnt,size,offset;
 
     if ( fd >= MAXFILE )
-	return -1;
+    return -1;
 
     fc = &_fcb[fd];
     cnt = len;
@@ -32,62 +32,62 @@ ssize_t write(int fd, void *buf, size_t len)
     switch ( fc->use ) {
 #ifdef DEVICES
     case U_PUN:
-		while ( len-- ) {
-			bdos(CPM_WPUN,*buf++);
-		}
-		return cnt;
-		break;
-	case U_LST:
-		offset = CPM_WLST;
-	case U_CON:
-		while ( len-- ) {
-			bdos(offset,*buf++);
-		}
-		return cnt;
-		break;
+        while ( len-- ) {
+            bdos(CPM_WPUN,*buf++);
+        }
+        return cnt;
+        break;
+    case U_LST:
+        offset = CPM_WLST;
+    case U_CON:
+        while ( len-- ) {
+            bdos(offset,*buf++);
+        }
+        return cnt;
+        break;
 #endif
     case U_WRITE:
     case U_RDWR:
-		uid = getuid();
-		offset = fc->rwptr%SECSIZE;
-		while ( len ) {
-			setuid(fc->uid);
-			if ( offset == 0 && len >= SECSIZE ) {
+        uid = getuid();
+        offset = fc->rwptr%SECSIZE;
+        while ( len ) {
+            setuid(fc->uid);
+            if ( offset == 0 && len >= SECSIZE ) {
                 size = SECSIZE;
                 bdos(CPM_SDMA,buf);
                 if ( bdos(CPM_WRIT,fc) ) {
                     return cnt-len;
                 }
-			} else {
-			    if ( ( size = SECSIZE - offset ) > len )
-			        size = len;
-				_putoffset(fc->ranrec,fc->rwptr/SECSIZE);
-			    if ( size == SECSIZE ) {
-			        bdos(CPM_SDMA,buf);
-			    } else {  /* Not the required size, read in the extent */
-			        bdos(CPM_SDMA,buffer);
-			        /* Blank out the buffer to indicate EOF */
-			        buffer[0] = 26;         /* ^Z */
-			        memcpy(buffer+1,buffer,SECSIZE-1);
-			        bdos(CPM_RRAN,fc);
-			        memcpy(buffer+offset,buf,size);
-			    }
-			    if ( bdos(CPM_WRAN,fc) ) {
-			        setuid(uid);
-			        return -1;   /* Not sure about this.. */
-			    }
-			}
-			buf += size;
-			fc->rwptr += size;
-			len -= size;
-			setuid(uid);
-		}
-		setuid(uid);
-		return cnt-len;	
-		break;
+            } else {
+                if ( ( size = SECSIZE - offset ) > len )
+                    size = len;
+                _putoffset(fc->ranrec,fc->rwptr/SECSIZE);
+                if ( size == SECSIZE ) {
+                    bdos(CPM_SDMA,buf);
+                } else {  /* Not the required size, read in the extent */
+                    bdos(CPM_SDMA,buffer);
+                    /* Blank out the buffer to indicate EOF */
+                    buffer[0] = 26;         /* ^Z */
+                    memcpy(buffer+1,buffer,SECSIZE-1);
+                    bdos(CPM_RRAN,fc);
+                    memcpy(buffer+offset,buf,size);
+                }
+                if ( bdos(CPM_WRAN,fc) ) {
+                    setuid(uid);
+                    return -1;   /* Not sure about this.. */
+                }
+            }
+            buf += size;
+            fc->rwptr += size;
+            len -= size;
+            setuid(uid);
+        }
+        setuid(uid);
+        return cnt-len;
+        break;
     default:
-		return -1;
-		break;
+        return -1;
+        break;
     }
 }
 
