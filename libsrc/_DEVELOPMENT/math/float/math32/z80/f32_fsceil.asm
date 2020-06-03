d3ef7466dbb1d7f854120f2a8e860e4b955bac2d
Phillip Stevens <phillip.stevens@gmail.com>
2020-06-03 20:29:34 +1000

    math32 - fix ceil

--- libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fsceil.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_fsceil.asm
@@ -1,14 +1,13 @@
-SECTION code_clib
-SECTION	code_fp_math32
 
-EXTERN	m32_f2ulong
-EXTERN	m32_float32u
-EXTERN	m32_float32
-EXTERN	m32_unity
-EXTERN	m32_fsadd_callee
+SECTION code_fp_math32
 
-PUBLIC  m32_ceil_fastcall
-PUBLIC	_m32_ceilf
+EXTERN m32_f2ulong
+EXTERN m32_float32u
+EXTERN m32_float32
+EXTERN m32_fsadd_callee
+
+PUBLIC m32_ceil_fastcall
+PUBLIC _m32_ceilf
 
 
 ; float ceilf(float f) __z88dk_fastcall;
@@ -16,20 +15,21 @@ PUBLIC	_m32_ceilf
 
 ; Entry: dehl = floating point number
 .m32_ceil_fastcall
-	bit	7,d
-	push	af			;Save sign flag
-	call	m32_f2ulong		;Exits dehl = number
-	pop	af
-	jr	Z,was_positive
-	call	m32_float32
-	ret
-.was_positive
-	call	m32_float32u
-	; Add 1
-	push	de
-	push	hl
-	call	m32_unity
-	call	m32_fsadd_callee
-	ret
+    bit 7,d
+    push af                     ;Save sign flag
+    call m32_f2ulong            ;Exits dehl = number
+    pop af
+    jr Z,was_positive
+    call m32_float32
+    ret
 
+.was_positive
+    call m32_float32u
+    ; Add 1
+    push de
+    push hl
+    ld de,$3f80
+    ld hl,$0000
+    call m32_fsadd_callee
+    ret
 
