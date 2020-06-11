4ddb54d1924b46cc14bf0f278df767f51e9c973c
Phillip Stevens <phillip.stevens@gmail.com>
2020-06-11 19:03:56 +1000

    math16 - ftof16 conversion

--- libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
+++ libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
@@ -1,6 +1,9 @@
 
     SECTION code_fp_math16
 
+    PUBLIC l_f16_ftof16
+    PUBLIC l_f16_f16tof
+
     PUBLIC f16_f16_f32
     PUBLIC f16_f32_f16
 
@@ -10,19 +13,25 @@
     PUBLIC f16_f24_f32
     PUBLIC f16_f32_f24
 
+    EXTERN l_ret
+
     EXTERN asm_f24_f32
     EXTERN asm_f32_f24
 
     EXTERN asm_f16_f24
     EXTERN asm_f24_f16
-    
+
 .f16_f16_f32
     call asm_f24_f32
     jp asm_f16_f24
 
+
 .f16_f32_f16
     call asm_f24_f16
     jp asm_f32_f24
+    
+defc l_f16_f16tof = l_ret
+defc l_f16_ftof16 = l_ret
 
 defc f16_f16_f24 = asm_f16_f24
 defc f16_f24_f16 = asm_f24_f16
