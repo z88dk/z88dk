8d9f5635feabb56bb90c5c9f26c5250089165853
Phillip Stevens <phillip.stevens@gmail.com>
2020-06-11 20:00:28 +1000

    math16 - f48 conversion

--- libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
+++ libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
@@ -7,6 +7,9 @@
     PUBLIC f16_f16_f32
     PUBLIC f16_f32_f16
 
+    PUBLIC f16_f16_f48
+    PUBLIC f16_f48_f16
+
     PUBLIC f16_f16_f24
     PUBLIC f16_f24_f16
 
@@ -14,26 +17,38 @@
     PUBLIC f16_f32_f24
 
     EXTERN l_ret
-
+    
+    EXTERN asm_f32_f48
+    EXTERN asm_f48_f32
+    
     EXTERN asm_f24_f32
     EXTERN asm_f32_f24
 
     EXTERN asm_f16_f24
     EXTERN asm_f24_f16
 
+defc l_f16_f16tof = l_ret
+defc l_f16_ftof16 = l_ret  
+
+.f16_f16_f48
+    call asm_f32_f48
 .f16_f16_f32
     call asm_f24_f32
     jp asm_f16_f24
 
+.f16_f48_f16
+    call asm_f24_f16
+    call asm_f32_f24
+    jp asm_f48_f32
 
 .f16_f32_f16
     call asm_f24_f16
     jp asm_f32_f24
-    
-defc l_f16_f16tof = l_ret
-defc l_f16_ftof16 = l_ret
 
 defc f16_f16_f24 = asm_f16_f24
 defc f16_f24_f16 = asm_f24_f16
 defc f16_f24_f32 = asm_f24_f32
 defc f16_f32_f24 = asm_f32_f24
+defc f16_f32_f24 = asm_f32_f48
+defc f16_f24_f32 = asm_f48_f32
+
