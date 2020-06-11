39dd280f8ce543ba6d572e3c12176fd94968525d
Phillip Stevens <phillip.stevens@gmail.com>
2020-06-11 20:11:46 +1000

    math16 - fix f16 convert

--- libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
+++ libsrc/_DEVELOPMENT/math/float/math16/lm16/c/sccz80/l_f16_conv_float.asm
@@ -4,11 +4,11 @@
     PUBLIC l_f16_ftof16
     PUBLIC l_f16_f16tof
 
-    PUBLIC f16_f16_f32
-    PUBLIC f16_f32_f16
-
     PUBLIC f16_f16_f48
     PUBLIC f16_f48_f16
+
+    PUBLIC f16_f16_f32
+    PUBLIC f16_f32_f16
 
     PUBLIC f16_f16_f24
     PUBLIC f16_f24_f16
@@ -49,6 +49,6 @@ defc f16_f16_f24 = asm_f16_f24
 defc f16_f24_f16 = asm_f24_f16
 defc f16_f24_f32 = asm_f24_f32
 defc f16_f32_f24 = asm_f32_f24
-defc f16_f32_f24 = asm_f32_f48
-defc f16_f24_f32 = asm_f48_f32
+defc f16_f32_f48 = asm_f32_f48
+defc f16_f48_f32 = asm_f48_f32
 
