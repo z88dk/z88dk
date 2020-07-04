4afee32a760df69bd7609ed1690185b9e956a410
suborb <dom@suborbital.org.uk>
2020-07-04 17:18:43 +0100

    Issue #1466: math16 is an adjunct library so should always be available.
    Move the M_ constants to route math.h header since the value of pi
    doesn't change.

--- include/math.h
+++ include/math.h
@@ -6,11 +6,33 @@
 #include <limits.h>
 #include <float.h>
 
-/* $Id: math.h,v 1.18 2016-07-16 22:00:08 dom Exp $ */
+#define M_E        2.718282
+#define M_INVLN2   1.442694  /* 1 / log(2) */
+#define M_LOG2E    1.442694
+#define M_IVLN10   0.434294  /* 1 / log(10) */
+#define M_LOG10E   0.434294
+#define M_LOG2_E   0.693146
+#define M_LN2      0.693146
+#define M_LN10     2.302585
+#define M_PI       3.141592
+#define M_TWOPI    6.283184
+#define M_PI_2     1.570796
+#define M_PI_4     0.785396
+#define M_3PI_4    2.356194
+#define M_SQRTPI   1.772454
+#define M_1_PI     0.318310
+#define M_2_PI     0.636620
+#define M_4_PI     1.273240
+#define M_1_SQRTPI 0.564190
+#define M_2_SQRTPI 1.128379
+#define M_SQRT2    1.414214
+#define M_SQRT3    1.732051
+#define M_SQRT1_2  0.707107
 
-#ifdef __MATH_MATH16
+// math16 is an adjunct library so is always available
 #include <math/math_math16.h>
-#elif __MATH_MATH32
+
+#if __MATH_MATH32
 #include <math/math_math32.h>
 #elif __MATH_MBF32
 #include <math/math_mbf32.h>
--- include/math/math_math32.h
+++ include/math/math_math32.h
@@ -45,27 +45,6 @@
 #define SQR10           (double_t)3.162277660168
 #define TWObyPI         (double_t)0.63661977
 
-#define M_E        2.718282
-#define M_INVLN2   1.442694  /* 1 / log(2) */
-#define M_LOG2E    1.442694
-#define M_IVLN10   0.434294  /* 1 / log(10) */
-#define M_LOG10E   0.434294
-#define M_LOG2_E   0.693146
-#define M_LN2      0.693146
-#define M_LN10     2.302585
-#define M_PI       3.141592
-#define M_TWOPI    6.283184
-#define M_PI_2     1.570796
-#define M_PI_4     0.785396
-#define M_3PI_4    2.356194
-#define M_SQRTPI   1.772454
-#define M_1_PI     0.318310
-#define M_2_PI     0.636620
-#define M_1_SQRTPI 0.564190
-#define M_2_SQRTPI 1.128379
-#define M_SQRT2    1.414214
-#define M_SQRT3    1.732051
-#define M_SQRT1_2  0.707107
 
 
 /* Trigonometric functions */
