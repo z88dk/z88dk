853d7b53f520d0344dbb73a546f8e926e104b14b
PaintedBlck <PaintedBlck@gmail.com>
2020-07-04 21:52:14 +0200

    Urgent fix - build.sh - Impossible to use parameters -b nor -l

--- build.sh
+++ build.sh
@@ -4,97 +4,157 @@
 # Build z88dk on unix systems
 #
 
+function show_help_and_exit {
+
+  if [[ -n $1 ]]; then rc=$1; else rc=0; fi
+
+  echo ""
+  echo "Usage: $0 [-b][-c][-C][-e][-h][-k][-l][-p][-t]"
+  echo ""
+  echo "  -b    Don't build binaries"
+  echo "  -c    Clean build environment"
+  echo "  -C    Clean build environment and binaries (including bin/*)"
+  echo "        Chosing this option makes a manual rebuild of zsdcc and"
+  echo "        szdcpp necessary in the win32 environment"
+  echo "  -e    Build examples"
+  echo "  -h    This help information"
+  echo "  -k    Keep building ignoring errors"
+  echo "  -l    Don't build libraries"
+  echo "  -p    TARGET Build specified targets"
+  echo "  -t    Run tests"
+  echo ""
+  echo "Default is to build binaries and libraries"
+  echo ""
+
+  exit $rc
+}
+
+
 set -e      # -e: exit on error; -u: exit on undefined variable
             # -e can be overidden by -k option
 
+
+do_build=1                              # Set initial (default)  values (build binaries and libraries)
 do_clean=0
-do_tests=0
-do_build=1
+do_clean_bin=0
 do_examples=0
 do_libbuild=1
+do_tests=0
 
-while getopts "hkctep:" arg; do
+
+while getopts "bcCehklpt" arg; do       # Handle all given arguments
   case "$arg" in
-    k) set +e                  ;;  # keep building ignoring errors
-    c) do_clean=1              ;;  # clean before building
-    t) do_tests=1              ;;  # Run tests as well
-    e) do_examples=1           ;;  # Build examples as well 
-    p) export TARGETS=$OPTARG  ;;
-    b) do_build=0              ;;  # Don't build
-    l) do_libbuild=0           ;;  # Don't build libraries
-    h | *) 
-    echo "Usage: $0 [-e][-k][-c][-t][-nb][-nl]"
-    echo
-    echo "-k\tKeep building ignoring errors"
-    echo "-c\tClean before building"
-    echo "-t\tRun tests"
-    echo "-e\tBuild examples"
-    echo "-p\tTARGET Build specified targets"
-    echo "-b\tDon't build binaries"
-    echo "-l\tDon't build libraries"
-    echo ""
-    echo "Default is to build binaries and libraries"
-    exit 1 
-    ;;
+    b)     do_build=0              ;;   # Don't build
+    c)     do_clean=1              ;;   # clean except bin/*
+    C)     do_clean_bin=1          ;;   # Clean including bin/*
+    e)     do_examples=1           ;;   # Build examples as well
+    k)     set +e                  ;;   # keep building ignoring errors
+    l)     do_libbuild=0           ;;   # Don't build libraries
+    p)     export TARGETS=$OPTARG  ;;
+    t)     do_tests=1              ;;   # Run tests as well
+    h | *) show_help_and_exit 0    ;;   # Show help on demand
   esac
 done
 
-if [ $do_clean = 1 ]; then
-  make clean
-  # dont remove bin, as zsdcc and szdcpp must be built by hand in win32
-  #rm -rf bin/*
+                                        # If there will be no action at all with the given parameters, then show help and exit with error
+if [[ $do_clean     = 0 ]]         \
+&& [[ $do_clean_bin = 0 ]]         \
+&& [[ $do_build     = 0 ]]         \
+&& [[ $do_libbuild  = 0 ]]         \
+&& [[ $do_tests     = 0 ]]         \
+&& [[ $do_examples  = 0 ]]; then
+  show_help_and_exit 1
+fi
+                                        # Only execute the most complete clean of the two options
+if [[ $do_clean     = 1 ]]         \
+&& [[ $do_clean_bin = 1 ]]; then
+  do_clean=0
 fi
 
-mkdir -p bin
-PATH=`pwd`/bin:$PATH
-export PATH
+if [ $do_clean = 1 ]; then              # Dont remove bin, as zsdcc and szdcpp must be built by hand in win32
+  make clean
+fi
 
-ZCCCFG=`pwd`/lib/config/
+
+if [ $do_clean_bin = 1 ]; then          # Remove bin => zsdcc and zdcpp must be built again by hand in win32
+  make clean
+  echo "rm -rf bin"
+  rm -rf bin
+fi
+
+                                        # If there was only cleaning to do then don't change paths, global variables, ...
+if [[ $do_build    != 1 ]]         \
+&& [[ $do_libbuild != 1 ]]         \
+&& [[ $do_tests    != 1 ]]         \
+&& [[ $do_examples != 1 ]]; then
+  exit 0
+fi
+
+
+if [ -z "$CC" ]; then                   # Insert default value for CC if CC is empty
+  CC="gcc"
+  export CC
+fi
+
+
+if [ -z "$CFLAGS" ]; then               # Insert default value for CFLAGS if CFLAGS is empty
+  CFLAGS="-g -O2"
+  export CFLAGS
+fi
+
+
+case `uname -s` in                      # Insert default values for MAKE and INSTALL following used OS
+  SunOS)
+    MAKE="gmake"
+    INSTALL="ginstall"
+    export INSTALL
+    ;;
+  OpenBSD|NetBSD|FreeBSD)
+    MAKE="gmake"
+    INSTALL="install"
+    export INSTALL
+    ;;
+  *)
+    MAKE="make"
+    INSTALL="install"
+    export INSTALL
+    ;;
+esac
+
+
+path=`pwd`/bin                          # Add bin directory to path if it's not already there
+mkdir -p $path                          # Guarantee that the directory exists
+if [[ $PATH != *$path* ]]; then
+  PATH=$path:$PATH
+  export PATH
+fi
+
+
+ZCCCFG=`pwd`/lib/config/                # Set ZCCCFG to the lib config directory
+mkdir -p $ZCCCFG                        # Guarantee that the directory exists
 export ZCCCFG
 
 
-if [ -z "$CFLAGS" ]; then
-   CFLAGS="-g -O2"
-fi
-
-export CC
-export CFLAGS
-
-case `uname -s` in
-    SunOS)
-        MAKE="gmake"
-        INSTALL="ginstall"
-        ;;
-    OpenBSD|NetBSD|FreeBSD)
-        MAKE="gmake"
-        INSTALL="install"
-        ;;
-    *)
-        MAKE="make"
-        INSTALL="install"
-        ;;
-esac
-
-export INSTALL
-
-
-if [ $do_build = 1 ]; then
-    $MAKE 
+if [ $do_build = 1 ]; then              # Build binaries or not...
+  $MAKE
 fi
 
 
-if [ $do_libbuild = 1 ]; then
-    $MAKE -C libsrc clean
-    $MAKE -C libsrc 
-    $MAKE -C libsrc install 
-    $MAKE -C libsrc/_DEVELOPMENT 
-    $MAKE -C include/_DEVELOPMENT 
-fi
-if [ $do_tests = 1 ]; then
-    $MAKE -C testsuite 
-    $MAKE -C test 
-fi
-if [ $do_examples = 1 ]; then
-    $MAKE -C examples
+if [ $do_libbuild = 1 ]; then           # Build libraries or not...
+  $MAKE -C libsrc clean
+  $MAKE -C libsrc
+  $MAKE -C libsrc install
+  $MAKE -C libsrc/_DEVELOPMENT
+  $MAKE -C include/_DEVELOPMENT
 fi
 
+
+if [ $do_tests = 1 ]; then              # Build tests or not...
+  $MAKE -C testsuite
+  $MAKE -C test
+fi
+
+
+if [ $do_examples = 1 ]; then           # Build examples or not...
+  $MAKE -C examples
+fi
