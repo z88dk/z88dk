e96011fb7a8ff4377060a0da9b0531afc2ba6864
suborb <dom@suborbital.org.uk>
2020-07-22 20:39:37 +0100

    (daimath32) And that was missing as well...

--- libsrc/math/dai32math/c/sccz80/modf.asm
+++ libsrc/math/dai32math/c/sccz80/modf.asm
@@ -1,85 +1,36 @@
 
 
-	SECTION		code_fp_dai32
+	SECTION code_fp_dai32
 
-	PUBLIC		modf
-	EXTERN		floor
-	EXTERN		ceil
-	EXTERN		msbios
-	EXTERN		___dai32_xfsub
-	EXTERN		___dai32_fpac
-	EXTERN		l_glong
-	EXTERN		___dai32_return
+	PUBLIC  modf
+	EXTERN  ___dai32_xload
+	EXTERN  ___dai32_xsave
+	EXTERN  ___dai32_xfint
+	EXTERN  ___dai32_xfrac
+	EXTERN  ___dai32_return
+    EXTERN  ___dai32_tempval
 
 ; double modf(double value, double *iptr)
 modf:
-	ld	hl,4
-	add	hl,sp
-	call	l_glong
-IF __CPU_INTEL__
-	ld	a,d
-	rra
-	jp	c,isnegative
-ELSE
-	bit	7,d
-	jr	nz,isnegative
-ENDIF
-	push	de
-	push	hl	
-	call	floor
-	pop	bc
-	pop	bc
-	jr	rejoin
-isnegative:
-	push	de
-	push	hl
-	call	ceil
-	pop	bc
-	pop	bc
-rejoin:
-	ld	c,l
-	ld	b,h
-	ld	hl,2
-	add	hl,sp
-	ld	a,(hl)
-	inc	hl
-	ld	h,(hl)
-	ld	l,a
-	; Now store the float
-	ld	(hl),c
-	inc	hl
-	ld	(hl),b
-	inc	hl
-	ld	(hl),e
-	inc	hl
-	ld	(hl),d
-IF __CPU_INTEL__|__CPU_GBZ80__
-	ex	de,hl
-	ld	(___dai32_fpac+2),hl
-	ld	l,c
-	ld	h,b
-	ld	(___dai32_fpac),hl
-ELSE
-	ld	(___dai32_fpac),bc
-	ld	(___dai32_fpac+2),de
-ENDIF
-	ld	hl,4
-	add	hl,sp	
-	ld	e,(hl)
-	inc	hl
-	ld	d,(hl)
-	inc	hl
-	ld	c,(hl)
-	inc	hl
-	ld	b,(hl)
-	call	___dai32_xfsub		; fpac = floor - value
-        ld      hl,(___dai32_fpac+2)
-	ex	de,hl
-        ld      hl,(___dai32_fpac)
-	; Now flip the sign
-	ld	a,d
-	xor	$80
-	ld	d,a
-	ret
-	
+    ld      hl,4
+    add     hl,sp
+    call    ___dai32_xload
+    call    ___dai32_xfint
+    ld      hl,2
+    add     hl,sp
+    ld      a,(hl)
+    inc     hl
+    ld      h,(hl)
+    ld      l,a
+    call    ___dai32_xsave
+
+    ; And now do the fractional part
+    ld      hl,4
+    add     hl,sp
+    call    ___dai32_xload
+    call    ___dai32_xfrac
+    jp      ___dai32_return
+
+
+
 
