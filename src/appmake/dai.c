f7153233c4679b05bfda0011ad58e6a75d9c8ed7
suborb <dom@suborbital.org.uk>
2020-07-25 18:12:26 +0100

    (dai) Create a .wav file.
    The program name can be read, but not loaded?

--- src/appmake/dai.c
+++ src/appmake/dai.c
@@ -12,7 +12,7 @@ static int               origin       = -1;
 static int               exec         = -1;
 static char              help         = 0;
 
-static void writebyte_dai(uint8_t byte, FILE *fp, uint8_t *cksump);
+static void writebyte_dai(uint8_t byte, FILE *fp, FILE *wavfile, uint8_t *cksump);
 
 /* Options that are available for this module */
 option_t dai_options[] = {
@@ -25,6 +25,58 @@ option_t dai_options[] = {
     {  0,   NULL,        NULL,                               OPT_NONE,  NULL }
 };
 
+
+
+// Leader: 283.5us high, 296us low, 283.5us high, 212.5us low
+// Bit 0 = 283.5us high, 296us low, 463.5us high, 392.5us low
+// Bit 1 = 463.5us high, 476us low, 283.5us high, 212.5us low
+// Trailer: 193.5us high, 206us low, 283.5us high, 212.5us low
+//
+// Time periods: 193.5, 206, 212.5, 283.5, 296, 463.5 476
+
+
+static int lengths[4][4] = {
+    // Leader
+    { 13, 13, 13, 13 },
+    // Bit 0
+    { 13, 13, 21, 21 },
+    // Bit 1
+    { 21, 21, 13, 13 },
+    // Trailer
+    { 9, 9, 12, 13 }
+};
+
+static uint8_t    h_lvl = 0xff;
+static uint8_t    l_lvl = 0x00;
+
+static void dai_bit(FILE* fpout, int index)
+{
+    int  i;
+
+    for ( i = 0; i < lengths[index][0]; i++ ) {
+        fputc(h_lvl, fpout);
+    }
+    for ( i = 0; i < lengths[index][1]; i++ ) {
+        fputc(l_lvl, fpout);
+    }
+    for ( i = 0; i < lengths[index][2]; i++ ) {
+        fputc(h_lvl, fpout);
+    }
+    for ( i = 0; i < lengths[index][3]; i++ ) {
+        fputc(l_lvl, fpout);
+    }
+}
+
+static void dai_rawout(FILE *fpout, unsigned char b)
+{
+    unsigned char c[8] = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
+    int i;
+
+    for (i = 0; i < 8; i++)
+        dai_bit(fpout, (b & c[i]) ? 2 : 1);
+}
+
+
 int dai_exec(char *target)
 {
     char    filename[FILENAME_MAX+1];
@@ -32,6 +84,7 @@ int dai_exec(char *target)
     struct  stat binname_sb;
     FILE   *fpin;
     FILE   *fpout;
+    FILE   *fpwav;
     uint8_t cksum;
     int     i,c;
     int     size;
@@ -72,6 +125,18 @@ int dai_exec(char *target)
         exit_log(1,"Can't open output file %s\n", filename);
     }
 
+    suffix_change(filename,".raw");
+    if ( ( fpwav = fopen(filename, "wb")) == NULL ) {
+        exit_log(1,"Can't open output file %s\n", filename);
+    }
+
+    // Now the leader for the wav file
+    for ( i = 0; i < 1860; i++ ) {
+        dai_bit(fpwav, 0);
+    }
+    // Bit1 to indicate end of leader
+    dai_bit(fpwav, 2);
+
     /* Header 
      * defb 0x30 = basic, 0x31=binary, 0x32=datatype
      * defwBE length of name
@@ -87,54 +152,67 @@ int dai_exec(char *target)
      * defs data
      * defb cksum:1
      */
+    dai_rawout(fpwav, 0x55);
+
+
     size = binname_sb.st_size;
-    writebyte(0x30, fpout);
+    writebyte_dai(0x31, fpout,fpwav,&cksum);  // File type
     cksum = 0x56;
     size = strlen(blockname);
-    writebyte_dai((size >> 8) & 0xff,fpout,&cksum);
-    writebyte_dai(size & 0xff,fpout,&cksum);
-    writebyte(cksum,fpout);
+    writebyte_dai((size >> 8) & 0xff,fpout,fpwav,&cksum);
+    writebyte_dai(size & 0xff,fpout,fpwav, &cksum);
+    writebyte_dai(cksum,fpout,fpwav,&cksum);
     cksum = 0x56;
     for ( i = 0; i < size; i++) {
-        writebyte_dai(blockname[i], fpout, &cksum);
+        writebyte_dai(blockname[i], fpout, fpwav, &cksum);
     }
-    writebyte(cksum,fpout);
-    writebyte_dai(0x00, fpout, &cksum);  // Address length
-    writebyte_dai(0x02, fpout, &cksum);
-    writebyte(cksum,fpout);
+    writebyte_dai(cksum,fpout,fpwav,&cksum);
+    writebyte_dai(0x00, fpout, fpwav, &cksum);  // Address length
+    writebyte_dai(0x02, fpout, fpwav, &cksum);
+    writebyte_dai(cksum,fpout, fpwav, &cksum);
     cksum = 0x56;
-    writebyte_dai(origin & 0xff, fpout, &cksum);
-    writebyte_dai((origin >>8) & 0xff, fpout, &cksum);
-    writebyte(cksum,fpout);
+    writebyte_dai(origin & 0xff, fpout, fpwav, &cksum);
+    writebyte_dai((origin >>8) & 0xff, fpout, fpwav, &cksum);
+    writebyte_dai(cksum,fpout, fpwav, &cksum);
     cksum = 0x56;
     size = binname_sb.st_size;
-    writebyte_dai((size >> 8) & 0xff,fpout,&cksum);
-    writebyte_dai(size & 0xff,fpout,&cksum);
-    writebyte(cksum,fpout);
+    writebyte_dai((size >> 8) & 0xff,fpout,fpwav,&cksum);
+    writebyte_dai(size & 0xff,fpout,fpwav,&cksum);
+    writebyte_dai(cksum,fpout, fpwav, &cksum);
     i = 0;
     while ( i < size ) {
         c = getc(fpin);
-        writebyte_dai(c, fpout, &cksum);
+        writebyte_dai(c, fpout, fpwav, &cksum);
         i++;
     }
-    writebyte(cksum,fpout);
+    writebyte_dai(cksum,fpout, fpwav, &cksum);
+
+    // And the trailer
+    for ( i = 0; i < 74; i++ ) {
+        dai_bit(fpwav, 3);
+    }
+
+    fclose(fpwav);
     fclose(fpin);
     fclose(fpout);
+
+    // And now convert raw to a .wav
+    raw2wav(filename);
     
     return 0;
 }
 
 
 // Initial value of cksum should be 0x56
-static void writebyte_dai(uint8_t byte, FILE *fp, uint8_t *cksump)
+static void writebyte_dai(uint8_t byte, FILE *fp, FILE *fpwav, uint8_t *cksump)
 {
    uint8_t cksum = *cksump;
 
-   cksum ^= byte;
-
-   cksum = (( cksum << 1 ) & 0xfe) | ((cksum & 0x80) >> 7);
-
-   *cksump = cksum;
-
+   dai_rawout(fpwav,byte);
    fputc(byte, fp);
+
+   cksum ^= byte;
+   cksum = (( cksum << 1 ) & 0xfe) | ((cksum & 0x80) >> 7);
+   *cksump = cksum;
 }
+
