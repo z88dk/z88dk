649cd49e1b3af7b6492923d60555594da4dad5f5
suborb <dom@suborbital.org.uk>
2020-07-26 19:27:35 +0100

    Fix #1539: Try to handle CP/M arguments with a [ on CPM/3
    In this case the command line doesn't begin with a space so we
    have to include the first character.

--- lib/crt/classic/crt_command_line.asm
+++ lib/crt/classic/crt_command_line.asm
@@ -8,166 +8,128 @@
 ; Exit:	  bc = argc
 ;         hl = argv
 
-	ld	de,0	;NULL pointer at end, just in case
-	push	de
+    GLOBAL  asm_isspace
+    ld      de,0	;NULL pointer at end of array, just in case
+    push    de
 ; Try to find the end of the arguments
 argv_loop_1:
-	ld	a,(hl)
-	cp	' '
-IF __CPU_8080__
-	jp	nz,argv_loop_2
-ELSE
-	jr	nz,argv_loop_2
-ENDIF
-	ld	(hl),0
-	dec	hl
-	dec	c
-IF __CPU_8080__
-	jp	nz,argv_loop_1
-ELSE
-	jr	nz,argv_loop_1
-ENDIF
+    ld      a,(hl)          ;Strip off trailing spaces
+    cp      ' '
+    jr      nz,argv_loop_2
+    ld      (hl),0
+    dec     hl
+    dec     c
+    jr      nz,argv_loop_1
 ; We've located the end of the last argument, try to find the start
 argv_loop_2:
-	ld	a,(hl)
-	cp	' '
-IF __CPU_8080__
-	jp	nz,argv_loop_3
-ELSE
-	jr	nz,argv_loop_3
-ENDIF
-	;ld	(hl),0
-	inc	hl
-
+    ld      a,(hl)
+    cp      ' '
+    jr      nz,argv_loop_3
+    ;ld	(hl),0
+    inc     hl
 IF CRT_ENABLE_STDIO
-IF !DEFINED_noredir
-IF !DEFINED_nostreams
-	EXTERN freopen
-	xor	a
-	add	b
-IF __CPU_8080__
-	jp	nz,no_redir_stdout
-ELSE
-	jr	nz,no_redir_stdout
-ENDIF
-	ld	a,(hl)
-	cp	'>'
-IF __CPU_8080__
-	jp	nz,no_redir_stdout
-ELSE
-	jr	nz,no_redir_stdout
-ENDIF
-	push	hl
-	inc	hl
-	cp	(hl)
-	dec	hl
-	ld	de,redir_fopen_flag	; "a" or "w"
-IF __CPU_8080__
-	jp	nz,noappendb
-ELSE
-	jr	nz,noappendb
-ENDIF
-	ld	a,'a'
-	ld	(de),a
-	inc	hl
+  IF !DEFINED_noredir
+    IF !DEFINED_nostreams
+        EXTERN freopen
+        xor     a
+        add     b
+        jr      nz,no_redir_stdout
+        ld      a,(hl)
+        cp      '>'
+        jr      nz,no_redir_stdout
+        push    hl
+        inc     hl
+        cp      (hl)
+        dec     hl
+        ld      de,redir_fopen_flag	; "a" or "w"
+        jr      nz,noappendb
+        ld      a,'a'
+        ld      (de),a
+        inc     hl
 noappendb:
-	inc	hl
-		
-	push	bc
-	push	hl					; file name ptr
-	push	de
-	ld	de,__sgoioblk+10		; file struct for stdout
-	push	de
-	call	freopen
-	pop	de
-	pop	de
-	pop	hl
-	pop	bc
-
-	pop	hl
-		
-	dec	hl
-IF __CPU_8080__
-	jp	argv_zloop
-ELSE
-	jr	argv_zloop
-ENDIF
+        inc     hl
+        	
+        push    bc
+        push    hl					; file name ptr
+        push    de
+        ld      de,__sgoioblk+10		; file struct for stdout
+        push    de
+        call    freopen
+        pop     de
+        pop     de
+        pop     hl
+        pop     bc
+        pop     hl
+        dec     hl
+        jr      argv_zloop
 no_redir_stdout:
 
-	ld	a,(hl)
-	cp	'<'
-IF __CPU_8080__
-	jp	nz,no_redir_stdin
-ELSE
-	jr	nz,no_redir_stdin
-ENDIF
-	push	hl
-	inc	hl
-	ld	de,redir_fopen_flagr
-	
-	push	bc
-	push	hl					; file name ptr
-	push	de
-	ld	de,__sgoioblk		; file struct for stdin
-	push	de
-	call	freopen
-	pop	de
-	pop	de
-	pop	hl
-	pop	bc
-
-	pop	hl
-		
-	dec	hl
-IF __CPU_8080__
-	jp	argv_zloop
-ELSE
-	jr	argv_zloop
-ENDIF
+        ld      a,(hl)
+        cp      '<'
+        jr      nz,no_redir_stdin
+        push    hl
+        inc     hl
+        ld      de,redir_fopen_flagr
+        push    bc
+        push    hl					; file name ptr
+        push    de
+        ld      de,__sgoioblk		; file struct for stdin
+        push    de
+        call    freopen
+        pop     de
+        pop     de
+        pop     hl
+        pop     bc
+        pop     hl
+        dec	    hl
+        jr      argv_zloop
 no_redir_stdin:
+    ENDIF
+  ENDIF
 ENDIF
-ENDIF
-ENDIF
-
-	push	hl
-	inc	b
-	dec	hl
-
+    push    hl
+    inc     b
+    dec     hl
 ; skip extra blanks
 argv_zloop:
-	ld	(hl),0
-	dec	c
-IF __CPU_8080__
-	jp	z,argv_done
-ELSE
-	jr	z,argv_done
-ENDIF
-	dec	hl
-	ld	a,(hl)
-	cp	' '
-IF __CPU_8080__
-	jp	z,argv_zloop
-ELSE
-	jr	z,argv_zloop
-ENDIF
-	inc c
-	inc hl
+    ld      (hl),0
+    dec     hl
+    dec     c
+    jr      z,argv_done
+    ld      a,(hl)
+    cp      ' '
+    jr      z,argv_zloop
+    inc     c
+    inc     hl
 
 argv_loop_3:
-	dec	hl
-	dec	c
-IF __CPU_8080__
-	jp	nz,argv_loop_2
-ELSE
-	jr	nz,argv_loop_2
-ENDIF
+    dec     hl
+    dec     c
+    jr      nz,argv_loop_2
 
 argv_done:
-	ld	hl,end	;name of program (NULL)
-	push	hl
-	inc	b
-	ld	hl,0
-	add	hl,sp	;address of argv
-	ld	c,b
-	ld	b,0
+    pop     de              ;Get last argument pushed, if it's not the same as hl we have an extra arg
+    push    de
+    ld      a,h
+    sub     d
+    jr      nz,argv_push_extra_arg
+    ld      a,l
+    sub     e
+    jr      z,argv_done_2
+argv_push_extra_arg:
+    ld      a,(hl)          ;We don't want to push an empty space argument
+    and     a
+    jr      z,argv_done_2
+    call    asm_isspace     ;And we don't want to push just whitespace
+    jr      nc,argv_done_2
+    push    hl
+    inc     b
+argv_done_2:
+    ld      hl,end	;name of program (NULL)
+    push    hl
+    inc     b
+    ld      hl,0
+    add     hl,sp	;address of argv
+    ld      c,b
+    ld      b,0
 
