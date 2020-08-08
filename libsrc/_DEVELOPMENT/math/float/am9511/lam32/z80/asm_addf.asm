10dac64bdb86afadbf73dc5589eefd8d11951d8d
Phillip Stevens <phillip.stevens@gmail.com>
2020-08-08 17:14:21 +1000

    am9511 - fix float comment

--- libsrc/_DEVELOPMENT/math/float/am9511/lam32/z80/asm_addf.asm
+++ libsrc/_DEVELOPMENT/math/float/am9511/lam32/z80/asm_addf.asm
@@ -17,6 +17,6 @@ EXTERN asm_am9511_add_callee
     ;
     ; uses  : af, bc, de, hl, af', bc', de', hl'
 
-DEFC  asm_addf = asm_am9511_add_callee  ; enter stack = am9511_float left
-                                        ;        DEHL = am9511_float right
-                                        ; return DEHL = am9511_float
+DEFC  asm_addf = asm_am9511_add_callee  ; enter stack = IEEE-754 float left
+                                        ;        DEHL = IEEE-754 float right
+                                        ; return DEHL = IEEE-754 float
