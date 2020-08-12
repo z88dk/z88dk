d0161a8a94cdc0310d1d5aa7118acb428d7c9c8a
suborb <dom@suborbital.org.uk>
2020-08-12 21:29:45 +0100

    Issue #1550: Add banked_call for +zx

---
+++ libsrc/target/zx/banked_call.asm
@@ -0,0 +1,36 @@
+MODULE banked_call
+
+SECTION code_driver
+PUBLIC banked_call
+EXTERN l_jphl
+
+INCLUDE "target/zx/def/sysvar.def"
+
+banked_call:
+    pop     hl     ;Return address
+    ld      a,(SV_BANKM)
+    push    af
+    ld      e,(hl)          ; Fetch the call address
+    inc     hl
+    ld      d,(hl)
+    inc     hl
+    ld      a,(hl+)         ; ...and page
+    inc     hl              ; Yes this should be here
+    push    hl              ; Push the real return address
+    ld      bc,32765
+    di
+    ld      (SV_BANKM),a
+    out     (c),a
+    ei
+    ld      l,e
+    ld      h,d
+    call    l_jphl
+    pop     bc              ; Get the return address
+    pop     af              ; Pop the old bank
+    ld      bc,32765
+    di
+    ld      (SV_BANKM),a
+    out     (c),a
+    ei
+    push    bc
+    ret
