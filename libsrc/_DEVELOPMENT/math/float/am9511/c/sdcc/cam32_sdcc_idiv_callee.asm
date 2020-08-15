5bb849ee42cdc46fc15a4e9a1b43cc990807abb6
Phillip Stevens <phillip.stevens@gmail.com>
2020-08-15 21:03:49 +1000

    am9511 -  __divsint oops

--- libsrc/_DEVELOPMENT/math/float/am9511/c/sdcc/cam32_sdcc_idiv_callee.asm
+++ libsrc/_DEVELOPMENT/math/float/am9511/c/sdcc/cam32_sdcc_idiv_callee.asm
@@ -1,17 +1,17 @@
 
-; int __ldiv_callee (int left, int right)
+; int __idiv_callee (int left, int right)
 
 SECTION code_clib
 SECTION code_fp_am9511
 
-PUBLIC _am9511_ldiv_callee
-PUBLIC cam32_sdcc_ldivs_callee, cam32_sdcc_ldivu_callee
+PUBLIC _am9511_idiv_callee
+PUBLIC cam32_sdcc_idivs_callee, cam32_sdcc_idivu_callee
 
-EXTERN cam32_sdcc_ireadr_callee, asm_am9511_ldiv_callee
+EXTERN cam32_sdcc_ireadr_callee, asm_am9511_idiv_callee
 
-DEFC _am9511_ldiv_callee = cam32_sdcc_ldivs_callee
+DEFC _am9511_idiv_callee = cam32_sdcc_idivs_callee
 
-.cam32_sdcc_ldivs_callee
+.cam32_sdcc_idivs_callee
 
     ; divide sdcc int by sdcc int
     ;
@@ -22,11 +22,11 @@ DEFC _am9511_ldiv_callee = cam32_sdcc_ldivs_callee
     ; uses  : af, bc, de, hl, af', bc', de', hl'
 
     call cam32_sdcc_ireadr_callee
-    jp asm_am9511_ldiv_callee   ; enter stack = sdcc_int left, ret
+    jp asm_am9511_idiv_callee   ; enter stack = sdcc_int left, ret
                                 ;        DEHL = sdcc_int right
 
 
-.cam32_sdcc_ldivu_callee
+.cam32_sdcc_idivu_callee
 
     ; divide sdcc int by sdcc int
     ;
@@ -38,6 +38,6 @@ DEFC _am9511_ldiv_callee = cam32_sdcc_ldivs_callee
 
     call cam32_sdcc_ireadr_callee
     res 7,h                     ; unsigned divisor
-    jp asm_am9511_ldiv_callee   ; enter stack = sdcc_int left, ret
+    jp asm_am9511_idiv_callee   ; enter stack = sdcc_int left, ret
                                 ;        DEHL = sdcc_int right
                                 ; return DEHL = sdcc_int
