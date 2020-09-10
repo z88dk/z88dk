2599b489d750a3016a248e14c174d33a3e3b6306
suborb <dom@suborbital.org.uk>
2020-09-10 19:55:37 +0100

    Issue #1567: Working vpeek for +ondra

--- libsrc/target/ondra/stdio/generic_console_vpeek.asm
+++ libsrc/target/ondra/stdio/generic_console_vpeek.asm
@@ -15,9 +15,25 @@ generic_console_vpeek:
         ld      sp,hl
         push    hl                ;save buffer
 
-	; Do something
+        ; Calculate screen address
+        ld      a,c
+        cpl
+        ld      d,a
+        ld      a,b
+        add     a
+        add     a
+        cpl
+        ld      e,a
 
-
+        ld      b,8
+loop:
+        ld      a,(de)
+        ld      (hl),a
+        inc     hl
+        rlc     e
+        dec     e
+        rrc     e
+        djnz    loop
         pop     de                ;buffer
         ld      hl,(generic_console_font32)
         call    screendollar
