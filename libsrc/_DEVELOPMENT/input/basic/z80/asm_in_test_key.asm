7f7a1814db718d3ca0652eb6c57a58dc1a3cc63e
Phillip Stevens <phillip.stevens@gmail.com>
2020-09-11 17:13:47 +1000

    rc2014 - BASIC in_key functions

--- libsrc/_DEVELOPMENT/input/basic/z80/asm_in_test_key.asm
+++ libsrc/_DEVELOPMENT/input/basic/z80/asm_in_test_key.asm
@@ -1,6 +1,6 @@
 
 ; ===============================================================
-; feilipu Jan 2020
+; feilipu Sept 2020
 ; ===============================================================
 ; 
 ; int in_test_key(void)
@@ -14,9 +14,6 @@ SECTION code_input
 
 PUBLIC asm_in_test_key
 
-EXTERN __BF_CIOIST
-EXTERN asm_hbios
-
 .asm_in_test_key
 
     ; exit : NZ flag set if a key is pressed
@@ -24,8 +21,7 @@ EXTERN asm_hbios
     ;
     ; uses : potentially all (ix, iy saved for sdcc)
 
-    ld bc,__BF_CIOIST<<8|0      ; direct console i/o test
-    call asm_hbios              ; result in A
+    rst 18h                     ; result in A
     ld l,a
     ld h,0                      ; make sure H is reset
     or a                        ; reset carry
