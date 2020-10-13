9b7a998c707a9b3ae817aadd21f3af9dc2390609
suborb <dom@suborbital.org.uk>
2020-10-13 23:22:00 +0100

    Disentangle so we don't break targets without gencon

--- libsrc/stdio/conio/console_coords.asm
+++ libsrc/stdio/conio/console_coords.asm
@@ -7,11 +7,9 @@
 
 		PUBLIC		__console_w
 		PUBLIC		__console_h
-		PUBLIC		generic_console_caps
 
 		EXTERN		CONSOLE_COLUMNS
 		EXTERN		CONSOLE_ROWS
-                EXTERN          CLIB_GENCON_CAPS
 
 __console_x:	defb	0
 __console_y:	defb	0
@@ -21,6 +19,4 @@ __console_y:	defb	0
 
 __console_w:	defb	CONSOLE_COLUMNS
 __console_h:	defb	CONSOLE_ROWS
-generic_console_caps:
-                defb    CLIB_GENCON_CAPS
 
