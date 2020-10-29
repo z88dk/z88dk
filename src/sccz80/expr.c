0cfe29414533eccf43b1628d359e4433241a8829
suborb <dom@suborbital.org.uk>
2020-10-29 22:14:30 +0000

    Need to handle it for non-constants as well

--- src/sccz80/expr.c
+++ src/sccz80/expr.c
@@ -649,7 +649,10 @@ int heirb(LVALUE* lval)
                     }
                 } else {
                     /* non-constant subscript, calc at run time */
-                    if (ispointer(lval->ltype) ) {
+                    if ( ispointer(lval->ltype) && lval->ltype->ptr->kind == KIND_ARRAY) {
+                        LVALUE tmp = {0};
+                        mult_const(&tmp,lval->ltype->ptr->size / lval->ltype->ptr->ptr->size);
+                    } else if (ispointer(lval->ltype) ) {
                         scale(lval->ltype->ptr->kind, lval->ltype->ptr->tag);
                     } else if ( lval->ltype->kind == KIND_ARRAY ) {
                         LVALUE tmp = {0};
