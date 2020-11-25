eb3ee0e2bc9672fc085ca3a11007a90477e5fe38
Stefano <stefano_bodrato@hotmail.com>
2020-11-25 16:57:09 +0100

    #1644

---
+++ libsrc/stdio/fileno.c
@@ -0,0 +1,15 @@
+/*
+ *  int fileno(FILE *fp)
+ *
+ *  Get a file file descriptor from a file pointer 
+ *  Stefano, 25/11/2020
+ *
+ *  $Id: fileno.c $
+ */
+
+#include <stdio.h>
+
+extern int __LIB__	fileno(FILE *fp) __z88dk_fastcall
+{
+	return (fp->desc.fd);
+}
