92785322926c0566fe1fda2ad63d49893620e2a9
Stefano <stefano_bodrato@hotmail.com>
2020-12-03 10:11:24 +0100

    OSCA fcntl:  fix for "Object not found" link error

--- libsrc/target/osca/fcntl/flosmulti/rename.c
+++ libsrc/target/osca/fcntl/flosmulti/rename.c
@@ -4,15 +4,16 @@
  *
  *	Stefano Bodrato - March 2012
  *
- *	$Id: rename.c,v 1.1 2012-03-21 10:20:23 stefano Exp $
+ *	$Id: rename.c$
  */
 
-//#include <stdio.h>
+#include <stdio.h>
 #include <flos.h>
 
 
 int rename(char *oldname, char *newname)
 {
+	erase_file(newname);
 	if (rename_file(oldname, newname) == 0) return 0;
     return (-1);
 }
