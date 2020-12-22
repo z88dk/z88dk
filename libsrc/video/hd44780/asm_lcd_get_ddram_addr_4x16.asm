9fb1dcd5d8fb50f598c1dd51feb3d3fe129a1e7c
suborb <dom@suborbital.org.uk>
2020-12-22 23:47:19 +0000

    (hd44780) Add the correct mapper for 4x16

--- libsrc/video/hd44780/asm_lcd_get_ddram_addr_4x16.asm
+++ libsrc/video/hd44780/asm_lcd_get_ddram_addr_4x16.asm
@@ -1,13 +1,13 @@
 
 SECTION code_driver
 
-PUBLIC asm_lcd_get_ddram_addr_4x20
+PUBLIC asm_lcd_get_ddram_addr_4x16
 
-; Calculate DDRAM address for 4x20 HD44780 
-; Row 0: 0x00 - 0x13
-; Row 1: 0x40 - 0x53
-; Row 2: 0x14 - 0x27
-; Row 3: 0x54 - 0x67
+; Calculate DDRAM address for 4x16 HD44780
+; Row 0: 0x00 - 0x0f
+; Row 1: 0x40 - 0x4f
+; Row 2: 0x10 - 0x1f
+; Row 3: 0x50 - 0x5f
 ;
 ; Entry: 
 ;   b = y
@@ -16,7 +16,7 @@ PUBLIC asm_lcd_get_ddram_addr_4x20
 ;   l = ddram address
 ; Preserves:
 ;  de
-asm_lcd_get_ddram_addr_4x20:
+asm_lcd_get_ddram_addr_4x16:
 	ld	hl,hd44780_4x20_rows
 row_inc:
 	ld	a,(hl)
@@ -28,5 +28,5 @@ row_inc:
 
 ; Offsets for the start of each row
 hd44780_4x20_rows:
-	defb	$00, $40, $14, $54
+	defb	$00, $40, $10, $50
 
