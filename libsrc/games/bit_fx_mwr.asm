6954512e1d118b1ff65a595cd5790651540e90a1
Stefano <stefano_bodrato@hotmail.com>
2021-01-13 16:51:15 +0100

    Update bit_fx_mwr.asm

--- libsrc/games/bit_fx_mwr.asm
+++ libsrc/games/bit_fx_mwr.asm
@@ -25,12 +25,7 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 
 .bit_fx
 ._bit_fx
-          pop  bc
-          pop  de
-          push de
-          push bc
-          
-          ld    a,e  
+          ld    a,l
           cp    8  
           ret   nc  
           add   a,a  
