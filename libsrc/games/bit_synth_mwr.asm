cf139c99880ec70ebd886206f72d24569f11a153
Stefano <stefano_bodrato@hotmail.com>
2021-01-19 17:10:34 +0100

    #1672

--- libsrc/games/bit_synth_mwr.asm
+++ libsrc/games/bit_synth_mwr.asm
@@ -1,9 +1,10 @@
-; $Id: bit_synth_mwr.asm $
+; $Id: bit_synth.asm $
 ;
 ; void bit_synth(int duration, int frequency1, int frequency2, int frequency3, int frequency4);
 ;
 ; 1 bit sound library - version for "memory write" I/O architectures
 ;
+; Generic platform sound library.
 ; synthetizer - this is a sort of "quad sound" routine.
 ; It is based on 4 separate counters and a delay.
 ; Depending on the parameters being passed it is able to play
@@ -18,13 +19,48 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
           SECTION    smc_clib
           PUBLIC     bit_synth
           PUBLIC     _bit_synth
+          PUBLIC     bit_synth_callee
+          PUBLIC     _bit_synth_callee
+
+          ;  ASM driven bending for special effects  ;)
+          PUBLIC     bit_synth_sub
+		  ; FR*_tick should hold the XOR instruction.
+		  ; FR*_tick+1 should be set to 0 or to sndbit_mask
+          PUBLIC     FR1_tick
+          PUBLIC     FR2_tick
+          PUBLIC     FR3_tick
+          PUBLIC     FR4_tick
+		  ; FR_*+1 needs to be loaded with the 4 sound periods
+          PUBLIC     FR_1
+          PUBLIC     FR_2
+          PUBLIC     FR_3
+          PUBLIC     FR_4
+		  
           INCLUDE  "games/games.inc"
 
           EXTERN      bit_open_di
           EXTERN      bit_close_ei
 
+
+.bit_synth_callee
+._bit_synth_callee
+		pop hl
+		ld	(ret_addr1+1),hl
+		call bit_synth
+		pop  de
+		pop  de
+		pop  de
+		pop  de
+		pop  de
+.ret_addr1
+		jp   0
+
 .bit_synth
 ._bit_synth
+
+		pop hl
+		ld	(ret_addr+1),hl
+		  push    ix
           ld      ix,2
           add     ix,sp
           ld      a,(ix+8)
@@ -58,7 +94,16 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 .FR4_blank
           ld      (FR1_tick+1),a
 
+
           call    bit_open_di
+          call    bit_synth_sub
+          call    bit_close_ei
+		  pop     ix
+.ret_addr
+          jp      0
+
+
+.bit_synth_sub
           ld      h,1
           ld      l,h
           ld      d,h
@@ -73,6 +118,7 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 .FR1_tick
           xor     sndbit_mask
           ld      (sndbit_port),a
+
 .FR_1
           ld      h,80
 .jump
@@ -81,6 +127,7 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 .FR2_tick
           xor     sndbit_mask
           ld      (sndbit_port),a
+
 .FR_2
           ld      l,81
 .jump2
@@ -89,6 +136,7 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 .FR3_tick
           xor     sndbit_mask
           ld      (sndbit_port),a
+
 .FR_3
           ld      d,162
 .jump3
@@ -97,13 +145,14 @@ IF !__CPU_GBZ80__ && !__CPU_INTEL__
 .FR4_tick
           xor     sndbit_mask
           ld      (sndbit_port),a
+
 .FR_4
           ld      e,163
           
           dec     c
           jr      nz,loop2
           djnz    loop
-          call	bit_close_ei
+		  ret
 
-          ret
+
 ENDIF
