9353e044dd4e000f9a9666933e27a138745c645d
suborb <dom@suborbital.org.uk>
2021-01-21 22:37:12 +0000

    (pc88) Move initialisation to im1_init() rather than piggy backing on an interrupt being registered

--- libsrc/target/pc88/interrupt/add_raster_int.asm
+++ libsrc/target/pc88/interrupt/add_raster_int.asm
@@ -8,29 +8,3 @@
         defc            add_raster_int = im1_install_isr
         defc            _add_raster_int = im1_install_isr
 
-
-	SECTION bss_crt
-
-fw_interrupt:	defw	0
-
-	SECTION	code_crt_init
-
-	EXTERN	im1_install_isr
-	EXTERN	asm_im1_handler
-	
-	di
-	ld	hl,($f302)	;System handler
-	ld	(fw_interrupt),hl
-	push	hl
-	call	im1_install_isr
-	pop	bc
-	ld	hl,asm_im1_handler
-	ld	($f302),hl
-	ei
-
-	SECTION code_crt_exit
-	di
-	ld	hl,(fw_interrupt)
-	ld	($f302),hl
-	ei
-
