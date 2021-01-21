08449c651402d380a7aea7f649ff6ed5c2ac2211
Phillip Stevens <phillip.stevens@gmail.com>
2021-01-21 14:23:19 +1100

    freertos - update task.h

--- include/_DEVELOPMENT/sdcc/freertos/task.h
+++ include/_DEVELOPMENT/sdcc/freertos/task.h
@@ -132,7 +132,7 @@ typedef struct xMEMORY_REGION
 typedef struct xTASK_PARAMETERS
 {
     TaskFunction_t pvTaskCode;
-    const char * const pcName;
+    const char * pcName;     /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
     configSTACK_DEPTH_TYPE usStackDepth;
     void * pvParameters;
     UBaseType_t uxPriority;
