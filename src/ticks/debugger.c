057e203fc321b43ac4bd6f02ebe8af185f7c84ad
suborb <dom@suborbital.org.uk>
2021-02-08 22:50:06 +0000

    (debug) Fix z80asm tests + more symbol resolution for the debugger

--- src/ticks/debugger.c
+++ src/ticks/debugger.c
@@ -110,6 +110,7 @@ static int cmd_list(int argc, char **argv);
 static int cmd_help(int argc, char **argv);
 static int cmd_quit(int argc, char **argv);
 static void print_hotspots();
+static const char *resolve_to_label(int addr);
 
 
 
@@ -245,18 +246,18 @@ void debugger()
                 continue;
             }
             if ( elem->type == BREAK_PC && elem->value == pc ) {
-                printf("Hit breakpoint %d\n",i);
+                printf("Hit breakpoint %d: @%04x (%s)\n",i,pc,resolve_to_label(pc));
                 dodebug=1;
                 break;
             } else if ( elem->type == BREAK_CHECK8 && *elem->lcheck_ptr == elem->lvalue ) {
-                printf("Hit breakpoint %d (%s = $%02x)\n",i,elem->text, elem->lvalue);
+                printf("Hit breakpoint %d (%s = $%02x): @%04x (%s)\n",i,elem->text, elem->lvalue,pc,resolve_to_label(pc));
                 elem->enabled = 0;
                 dodebug=1;
                 break;
             } else if ( elem->type == BREAK_CHECK16 &&
                         *elem->lcheck_ptr == elem->lvalue  &&
                          *elem->hcheck_ptr == elem->hvalue  ) {
-                printf("Hit breakpoint %d (%s = $%02x%02x)\n",i,elem->text, elem->hvalue, elem->lvalue);
+                printf("Hit breakpoint %d (%s = $%02x%02x): @%04x (%s)\n",i,elem->text, elem->hvalue, elem->lvalue,pc,resolve_to_label(pc));
                 elem->enabled = 0;
                 dodebug=1;
                 break;
@@ -381,6 +382,45 @@ static int parse_number(char *str, char **end)
     return strtol(str, end, base);
 }
 
+/* Parse an address operand. 
+ *
+ * It may be one of:
+ * 1. A number address
+ * 2. A symbol
+ * 3. A line expression
+ */
+static int parse_address(char *arg)
+{
+    int  where;
+    char *end;
+
+    where = parse_number(arg, &end);
+    if ( end == arg ) {
+        where = symbol_resolve(arg);
+        if ( where == -1 ) {
+            // And now try to resolve a line expression
+            where = debug_resolve_source(arg);
+        }
+    }
+    return where;
+}
+
+/* Map an address into a convenient label */
+static const char *resolve_to_label(int addr)
+{
+    static char tbuf[1024];
+    const char *sym;
+    
+    if ( (sym = find_symbol(addr, SYM_ADDRESS) ) != NULL ) {
+        return sym;
+    }
+    symbol_find_lower(addr,SYM_ADDRESS,tbuf,sizeof(tbuf));
+
+    if ( tbuf[0] == 0) return "<unknown>";
+
+    return tbuf;
+}
+
 
 
 static int cmd_disassemble(int argc, char **argv)
@@ -390,14 +430,8 @@ static int cmd_disassemble(int argc, char **argv)
     int   where = pc;
 
     if ( argc == 2 ) {
-        char *end;
-        where = parse_number(argv[1], &end);
-        if ( end == argv[1] ) {
-            where = symbol_resolve(argv[1]);
-            if ( where == -1 ) {
-                where = pc;
-            }
-        }
+        where = parse_address(argv[1]);
+        if ( where == -1 ) where = pc;
     }
 
     while ( i < 10 ) {
@@ -469,11 +503,9 @@ static int cmd_watch(int argc, char **argv)
         /* Just show the breakpoints */
         LL_FOREACH(watchpoints, elem) {
             if ( elem->type == BREAK_READ) {
-                const char *sym = find_symbol(elem->value, SYM_ADDRESS);
-                printf("%d:\t(read) @$%04x (%s) %s\n",i, elem->value,sym ? sym : "<unknown>", elem->enabled ? "" : " (disabled)");
+                printf("%d:\t(read) @$%04x (%s) %s\n",i, elem->value,resolve_to_label(elem->value), elem->enabled ? "" : " (disabled)");
             } else if ( elem->type == BREAK_WRITE) {
-                const char *sym = find_symbol(elem->value, SYM_ADDRESS);
-                printf("%d:\t(read) @$%04x (%s) %s\n",i, elem->value,sym ? sym : "<unknown>", elem->enabled ? "" : " (disabled)");
+                printf("%d:\t(write) @$%04x (%s) %s\n",i, elem->value,resolve_to_label(elem->value), elem->enabled ? "" : " (disabled)");
             } 
             i++;
         }
@@ -482,30 +514,17 @@ static int cmd_watch(int argc, char **argv)
         char *end;
         const char *sym;
         breakpoint *elem;
-        int value = parse_number(argv[2], &end);
+        int value = parse_address(argv[2]);
 
-        if ( end != argv[2] ) {
+        if ( value != -1 ) {
             elem = malloc(sizeof(*elem));
             elem->type = breakwrite ? BREAK_WRITE : BREAK_READ;
             elem->value = value;
             elem->enabled = 1;
             LL_APPEND(watchpoints, elem);
-            sym = find_symbol(value, SYM_ADDRESS);
-            printf("Adding %s watchpoint at '%s' $%04x (%s)\n",breakwrite ? "write" : "read", argv[2], value,  sym ? sym : "<unknown>");
+            printf("Adding %s watchpoint at '%s' $%04x (%s)\n",breakwrite ? "write" : "read", argv[2], value,  resolve_to_label(value));
         } else {
-            int value = symbol_resolve(argv[2]);
-
-            if ( value != -1 ) {
-                elem = malloc(sizeof(*elem));
-                elem->type = breakwrite ? BREAK_WRITE : BREAK_READ;
-                elem->value = value;
-                elem->enabled = 1;
-                LL_APPEND(watchpoints, elem);
-                sym = find_symbol(value, SYM_ADDRESS);
-                printf("Adding %s watchpoint at '%s', $%04x (%s)\n",breakwrite ? "write" : "read", argv[2], value, sym ? sym : "<unknown>");
-            } else {
-                printf("Cannot set watchpoint on '%s'\n",argv[1]);
-            }
+            printf("Cannot set watchpoint on '%s'\n",argv[2]);
         }
     } else if ( argc == 3 && strcmp(argv[1],"delete") == 0 ) {
         int num = atoi(argv[2]);
@@ -566,30 +585,17 @@ static int cmd_break(int argc, char **argv)
         char *end;
         const char *sym;
         breakpoint *elem;
-        int value = parse_number(argv[1], &end);
+        int value = parse_address(argv[1]);
 
-        if ( end != argv[1] ) {
+        if ( value != -1 ) {
             elem = malloc(sizeof(*elem));
             elem->type = BREAK_PC;
             elem->value = value;
             elem->enabled = 1;
             LL_APPEND(breakpoints, elem);
-            sym = find_symbol(value, SYM_ADDRESS);
-            printf("Adding breakpoint at '%s' $%04x (%s)\n",argv[1], value,  sym ? sym : "<unknown>");
+            printf("Adding breakpoint at '%s' $%04x (%s)\n",argv[1], value,  resolve_to_label(value));
         } else {
-            int value = symbol_resolve(argv[1]);
-
-            if ( value != -1 ) {
-                elem = malloc(sizeof(*elem));
-                elem->type = BREAK_PC;
-                elem->value = value;
-                elem->enabled = 1;
-                LL_APPEND(breakpoints, elem);
-                sym = find_symbol(value, SYM_ADDRESS);
-                printf("Adding breakpoint at '%s', $%04x (%s)\n",argv[1], value, sym ? sym : "<unknown>");
-            } else {
-                printf("Cannot break on '%s'\n",argv[1]);
-            }
+            printf("Cannot break on '%s'\n",argv[1]);
         }
     } else if ( argc == 3 && strcmp(argv[1],"delete") == 0 ) {
         int num = atoi(argv[2]);
@@ -627,11 +633,7 @@ static int cmd_break(int argc, char **argv)
     } else if ( argc == 5 && strcmp(argv[1], "memory8") == 0 ) {
         // break memory8 <addr> = <value>
         char  *end;
-        int value = parse_number(argv[2], &end);
-
-        if ( end == argv[2] ) {
-            value =  symbol_resolve(argv[2]);
-        }
+        int value = parse_address(argv[2]);
 
         if ( value != -1 ) {
             breakpoint *elem = malloc(sizeof(*elem));
@@ -644,12 +646,8 @@ static int cmd_break(int argc, char **argv)
             printf("Adding breakpoint for %s = $%02x\n", elem->text, elem->lvalue);
         }
     } else if ( argc == 5 && strcmp(argv[1], "memory16") == 0 ) {
-        char  *end;
-        int addr = parse_number(argv[2], &end);
-
-        if ( end == argv[2] ) {
-            addr =  symbol_resolve(argv[2]);
-        }
+        char *end;
+        int addr = parse_address(argv[2]);
 
         if ( addr != -1 ) {
             int value = parse_number(argv[4],&end);
@@ -715,10 +713,7 @@ static int cmd_examine(int argc, char **argv)
 {
     if ( argc == 2 ) {
         char *end;
-        int addr = parse_number(argv[1], &end);
-        if ( end == argv[1] ) {
-            addr =  symbol_resolve(argv[1]);
-        }
+        int addr = parse_address(argv[1]);
 
         if ( addr != -1  ) {
             char  abuf[17];
