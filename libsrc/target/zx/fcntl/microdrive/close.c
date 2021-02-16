f0c34b48dc96f0ffa06c508241ca2d8c175d2853
Stefano <stefano_bodrato@hotmail.com>
2021-02-16 14:39:06 +0100

    Trying to add write support

--- libsrc/target/zx/fcntl/microdrive/close.c
+++ libsrc/target/zx/fcntl/microdrive/close.c
@@ -1,11 +1,11 @@
 /*
  *	Close a file on Microdrive
- *	Stefano Bodrato - Feb. 2005
+ *	Stefano Bodrato - Feb. 2005..2021
  *
  *	int close(int handle)
  *	closes file and frees memory
  *
- *	$Id: close.c,v 1.3 2014-01-20 09:15:31 stefano Exp $
+ *	$Id: close.c $
  */
 
 #include <fcntl.h>
@@ -14,17 +14,36 @@
 //#include <stdio.h>
 #include <malloc.h>
 
-int close(int handle)
+
+int close(int fd)
 {
 
+struct M_CHAN *if1_file;
+int flags;
+
+	if1_file = (void *) fd;
+	flags = (if1_file)->flags & 0xff;
+
+	if ((flags == O_APPEND) || (flags == O_WRONLY))
+	{
+		//printf ("\nClosing file on drive \n",);
+
+		if1_write_record (if1_file->drive, if1_file);
+		
 /* DEBUG - experiments to see how the M_CHAN structure survived
-struct M_CHAN * if1_file;
-if1_file = (char *) handle;
-printf ("-- closing '%s' - %u --",if1_getname( (char *) if1_file->hdname ), handle);
+		#pragma printf %u
+		printf ("\n Cartridge name: %s",if1_getname( (char *) if1_file->hdname));
+		printf ("\n File name: %s",if1_getname( (char *) if1_file->name));
+		printf ("\nRecord flags: ");
+		int x;
+		for (x=7; x >-1; x--)  (if1_file->recflg & (2^x)) ? fputc_cons('1') : fputc_cons('0');
+		printf ("\n");
 */
+		
+		//if1_file->recflg |= 2;	// Set EOF bit on last record
+	}
 
-free(handle);
-return 0;
-
+	free(fd);
+	return 0;
 }
 
