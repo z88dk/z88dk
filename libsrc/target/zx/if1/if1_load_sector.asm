e38e0c2b70448c8ebf80c84da51c67e73e582334
Stefano <stefano_bodrato@hotmail.com>
2021-02-24 16:22:10 +0100

    Update if1_load_sector.asm

--- libsrc/target/zx/if1/if1_load_sector.asm
+++ libsrc/target/zx/if1/if1_load_sector.asm
@@ -9,7 +9,7 @@
 ;       2/3 sector number
 ;       0/1 buffer
 ;
-;       $Id: if1_load_sector.asm,v 1.3 2016-07-01 22:08:20 dom Exp $
+;       $Id: if1_load_sector.asm $
 ;
 
 		SECTION   code_clib
@@ -39,11 +39,9 @@ _if1_load_sector:
                 ld      a,(ix+4)
                 ld      hl,-1
                 and     a               ; drive no. = 0 ?
-                jp	z,if_load_sector_exit               ; yes, return -1
-                dec     a
-                cp      8               ; drive no. >8 ?
-                jr	nc,if_load_sector_exit              ; yes, return -1
-                inc     a
+                jp      z,if_load_sector_exit               ; yes, return -1
+                cp      9               ; drive no. >8 ?
+                jr      nc,if_load_sector_exit              ; yes, return -1
 
                 ld      (driveno),a     ; drive number selected (d_str1)
 
@@ -58,9 +56,10 @@ _if1_load_sector:
                 call    if1_rommap
 
 
-
-                ld      hl,(driveno)    ; drive number selected
-                ld      (5CD6h),hl      ; d_str1
+                ;ld      hl,(driveno)    ; drive number selected
+                ;ld      (5CD6h),hl      ; d_str1
+                ld      a,(driveno)    ; drive number selected
+                ld      (5CD6h),a      ; d_str1
 
                 ld      a,'M'
                 ld      (5CD9h),A       ; l_str1 (device type = "M")
