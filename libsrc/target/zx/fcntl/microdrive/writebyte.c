6966061d3e3cf4c90adf431e9d2be925b4b83a71
Stefano <stefano_bodrato@hotmail.com>
2021-02-26 17:04:35 +0100

    #1703

--- libsrc/target/zx/fcntl/microdrive/writebyte.c
+++ libsrc/target/zx/fcntl/microdrive/writebyte.c
@@ -29,31 +29,30 @@ int writebyte(int fd, int byte)
 
 	if ( (if1_file->position != 0L) && ((int) ((if1_file->position ) % 512) == 0) )
 	{
-		if1_write_record (if1_file->drive, if1_file);
-		if1_file->record++;
+		//if1_write_record (if1_file->drive, if1_file);
+		if1_file->recflg &= 0xFD ;	// Reset EOF bit on current record
+		// This will overwrite/finalize the current sector.
+		if1_write_sector (if1_file->drive, if1_file->sector, if1_file);
+		
+		// TODO:  append the new blank sector
+		
 		if1_file->reclen=0;
 		if1_file->bytecount=0;
+		//if1_file->record++;  <-- this is probably a sector number
 		if1_file->recnum++;
 		bzero (if1_file->data, 512);
-		
-		//DEBUG:
-		//printf ("\nNext record in file: %u  ",if1_file->record + 1);
-/*
-		// EOF flag set ?
-		if (if1_file->recflg && 1) return (EOF);
-		// No, load next record
-		if1_filestatus = if1_load_record(if1_file->drive, if1_file->name, ++if1_file->record, if1_file);
-		
-		//DEBUG:
-		//printf ("  sector: %u  len: %u\n",if1_filestatus,if1_file->reclen);
-		
-		if (if1_filestatus == -1) return (EOF);
-	*/	
 	}
-	//if ( ((int) (if1_file->position) % 512) >= if1_file->reclen ) return EOF;
-	*((unsigned char *) (if1_file->data + ( (int)(if1_file->position++) % 512))) == byte;
+	
+	
+	//if1_file->data[if1_file->bytecount] == byte;
+	*((unsigned char *) (if1_file->data + ( (int)(if1_file->position) % 512))) = byte;
+	
 	if1_file->reclen++;
 	if1_file->bytecount++;
+
+		//fputc_cons ('|');
+		//fputc_cons ( *(unsigned char *) (if1_file->data + ( (int)(if1_file->position) % 512)) );
+	
 	if1_file->position++;
 
 }
