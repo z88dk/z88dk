33ce5f39fe6f76cdc44a882a8fa0fc0cf7275dd3
Stefano <stefano_bodrato@hotmail.com>
2021-03-01 11:28:32 +0100

    #1703

--- libsrc/target/zx/fcntl/microdrive/writebyte.c
+++ libsrc/target/zx/fcntl/microdrive/writebyte.c
@@ -27,25 +27,30 @@ int writebyte(int fd, int byte)
 	if1_file = (void *) fd;
 	//printf ("-- reading '%s' - %u --",if1_getname( (char *) if1_file->hdname ), fd);
 
-	if ( (if1_file->position != 0L) && ((int) ((if1_file->position ) % 512) == 0) )
+	if ( (if1_file->position != 0L) && ((int) ((if1_file->position ) % 512L) == 0L) )
 	{
-		//if1_write_record (if1_file->drive, if1_file);
-		if1_file->recflg &= 0xFD ;	// Reset EOF bit on current record
 		// This will overwrite/finalize the current sector.
 		if1_write_sector (if1_file->drive, if1_file->sector, if1_file);
 		
-		// TODO:  append the new blank sector
-		
 		if1_file->reclen=0;
 		if1_file->bytecount=0;
-		//if1_file->record++;  <-- this is probably a sector number
+		
 		if1_file->recnum++;
-		bzero (if1_file->data, 512);
+		
+		//bzero (if1_file->data, 512);
+
+		/*
+		   When accessing to the Microdrive at sector level, the 'record' field
+		   is used to specify the sector number to be written.
+		   Let's spot a free one and assign the fresh value to the current buffer.
+		*/
+		if1_file->record = if1_find_sector(if1_file->drive);
+		if1_file->sector = if1_file->record;
 	}
-	
+
 	
 	//if1_file->data[if1_file->bytecount] == byte;
-	*((unsigned char *) (if1_file->data + ( (int)(if1_file->position) % 512))) = byte;
+	*((unsigned char *) (if1_file->data + ( (int)(if1_file->position) % 512L))) = byte;
 	
 	if1_file->reclen++;
 	if1_file->bytecount++;
