b0379bab223e4bfd5d6bbc25cdcfe4d57bc45ccb
Stefano <stefano_bodrato@hotmail.com>
2021-03-02 10:55:37 +0100

    Update rename.c

--- libsrc/target/zx/fcntl/microdrive/rename.c
+++ libsrc/target/zx/fcntl/microdrive/rename.c
@@ -3,7 +3,7 @@
  *
  *	Stefano Bodrato - Oct. 2004
  *
- *	$Id: rename.c,v 1.3 2005-03-01 17:50:37 stefano Exp $
+ *	$Id: rename.c $
  */
 
 #include <stdio.h>
@@ -15,21 +15,26 @@ int rename(char *oldname, char *newname)
 int blkcount, currblock;
 struct M_CHAN mybuf;
 
-  // check if "newname" already exists
-  if (if1_load_record (1, newname, 0, &mybuf) != -1 ) return (-1);
+  // check if "newname" already exists (we get the optional drive specifier from the first parameter)
+  if (if1_load_record (if1_driveno(oldname), if1_filename(newname), 0, &mybuf) != -1 ) return (-1);
+
 
   // load first file record and check for its existence
-  if ((currblock = if1_load_record (1, oldname, 0, &mybuf)) == -1 )
+  if ((currblock = if1_load_record (if1_driveno(oldname), if1_filename(oldname), 0, &mybuf)) == -1 )
     return (-1);
+
 
   /* now rename every file record
      we skip the record no. 255 to avoid loops */
      
   for (blkcount=1; blkcount < 255; blkcount++)
     {
-       if1_setname(newname,&mybuf.recname[0]);
-       if (if1_write_sector (1, currblock, &mybuf) == -1) return (-1);
-       currblock = if1_load_record (1, oldname, blkcount, &mybuf);
+	   if1_setname(if1_filename(newname),mybuf.recname);
+	   if1_setname(if1_filename(newname),mybuf.name);
+       if (if1_write_sector (if1_driveno(oldname), currblock, &mybuf) == -1) return (-1);
+	   // EOF ?
+	   if ((mybuf.recflg && 1) != 0) return 0;
+       currblock = if1_load_record (if1_driveno(oldname), if1_filename(oldname), blkcount, &mybuf);
        if (currblock == -1) return 0;
     }
     return (0);
