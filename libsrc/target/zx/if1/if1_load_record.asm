d4a035ea5158020621b568ee3952ada4b85aabf1
Stefano <stefano_bodrato@hotmail.com>
2021-03-03 14:25:46 +0100

    recovering a regression on mdv error handling

--- libsrc/target/zx/if1/if1_load_record.asm
+++ libsrc/target/zx/if1/if1_load_record.asm
@@ -115,17 +115,22 @@ copyname:
                 ld      (ix+0Dh),a      ; CHREC
                 res     0,(ix+18h)      ; set CHFLAG to "read" mode
  
-                ;xor     a
-                ;ld      (if1_sect_ready),a       ; flag for "sector read"
+                xor     a
+                ld      (if1_sect_ready),a       ; flag for "sector read"
 
-                ld      hl,04FBh
+                ;ld      hl,255*5		; set sector counter
+				ld      hl,256*4		; set sector counter (retries slightly reduced)
                 ld      (5CC9h),hl      ; SECTOR
+
+
 
                 ld      a,(driveno)     ; drive number selected
                 call    MOTOR           ; select drive motor
 IF !OLDIF1MOTOR
                 jr      nz,error_exit
 ENDIF
+
+
 
 do_read:
                 call    FETCH_H         ; fetch header
@@ -207,6 +212,7 @@ next_sector:
                 ld      a,l
                 or      h
                 ret
+
 
 		SECTION bss_clib
 ; parameters and variables
