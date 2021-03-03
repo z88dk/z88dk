f52655e0241c2254d65236aff157a4733bedc4c2
Stefano <stefano_bodrato@hotmail.com>
2021-03-03 16:30:23 +0100

    Update writebyte.c

--- libsrc/target/zx/fcntl/microdrive/writebyte.c
+++ libsrc/target/zx/fcntl/microdrive/writebyte.c
@@ -32,31 +32,44 @@ int writebyte(int fd, int byte)
 
 	if ( (if1_file->position != 0L) && ((int) ((if1_file->position ) % 512L) == 0L) )
 	{
-		// This will overwrite/finalize the current sector.
-		if1_write_sector (if1_file->drive, if1_file->sector, if1_file);
+		// If we're on the last record, we will need to append a new one
+		if ((if1_file->recflg & 2) != 0) {
+				
+			// This will overwrite/finalize the current sector.
+			if1_file->recflg &= 0xFD;    // Reset EOF bit
+			if1_write_sector (if1_file->drive, if1_file->sector, if1_file);
 
-	// Microdrive full ?
-	// We alwayse reserve one sector to prevent the 'ERASE' crash bug
-	if (if1_free_sectors(if1_file->drive)<1) {
-		if1_file->hdflag |= 128;
-		return(-1);
-	}
-		
-		if1_file->reclen=0;
-		if1_file->bytecount=0;
-		
-		if1_file->recnum++;
-		
-		//bzero (if1_file->data, 512);
+			// Microdrive full ?
+			// We alwayse reserve one sector to prevent the 'ERASE' crash bug
+			if (if1_free_sectors(if1_file->drive)<1) {
+				if1_file->hdflag |= 128;
+				return(-1);
+			}
+			
+			// Let's update the sector data to initialize the next one
+			bzero (if1_file->data, 512);
+			if1_file->reclen=0;
+			if1_file->bytecount=0;
+			if1_file->recnum++;
+			if1_file->recflg |= 2;    // Set EOF bit
+			/*
+			   When accessing to the Microdrive at sector level, the 'record' field
+			   is used to specify the sector number to be written.
+			   Let's spot a free one and assign the fresh value to the current buffer.
+			*/
+			if1_file->record = if1_find_sector(if1_file->drive);
+			if1_file->sector = if1_file->record;
+				
+		} else {
+			
+			// If we're not at the file tail (EOF), let's pick the next record
+			if1_filestatus = if1_load_record(if1_file->drive, if1_file->name, ++if1_file->record, if1_file);
 
-		/*
-		   When accessing to the Microdrive at sector level, the 'record' field
-		   is used to specify the sector number to be written.
-		   Let's spot a free one and assign the fresh value to the current buffer.
-		*/
-		if1_file->record = if1_find_sector(if1_file->drive);
-		//if1_find_sector_map(if1_file->map)
-		if1_file->sector = if1_file->record;
+			// Now get the current position in LONG format for our C driver
+			if1_file->position = (long) if1_file->record * 512L + (long) if1_file->reclen;
+			
+			// TODO: possible error recovery, e.g. appending a new blank record
+		}
 	}
 
 	
