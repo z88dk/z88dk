ac7a42a1bf2ddab5cad6b9918b93bd3f65dc32e6
Stefano <stefano_bodrato@hotmail.com>
2021-03-15 19:46:07 +0100

    #1715

---
+++ libsrc/target/cpm/gsx/gios_2px.asm
@@ -0,0 +1,44 @@
+
+;
+; Invoke a GSX/GIOS function with 2 coordinates (4 values) in "ptsin"
+;
+
+;
+; $Id: gios_2px.asm $
+;
+
+SECTION code_clib
+
+PUBLIC	gios_2px
+PUBLIC	_gios_2px
+
+EXTERN ASMDISP_GIOS_2PX
+EXTERN gios_2px_callee
+
+EXTERN	gios_ptsin
+
+gios_2px:
+_gios_2px:
+
+	ld	hl,2
+	add	hl,sp
+	ld	de,gios_ptsin+7
+	ld	b,4
+cploop:
+	ld	a,(hl)
+	inc	hl
+	ld	c,a
+	ld	a,(hl)
+	inc	hl
+	ld	(de),a
+	dec de
+	ld	a,c
+	ld	(de),a
+	dec de
+	dec	b
+	jr	nz,cploop
+	ld	a,(hl)		; fn
+	ld	e,a
+	ld	d,0
+   
+	jp gios_2px_callee + ASMDISP_GIOS_2PX
