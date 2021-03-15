d4ebf4f872e647076b9f80d7451e84f72d8ffb42
Stefano <stefano_bodrato@hotmail.com>
2021-03-15 18:19:21 +0100

    Update gsx_pb.asm

--- libsrc/target/cpm/gsx/gsx_pb.asm
+++ libsrc/target/cpm/gsx/gsx_pb.asm
@@ -39,6 +39,7 @@ _gios_pb:
 
 
 
+
 SECTION bss_clib
 
 gios_ctl:
@@ -46,23 +47,29 @@ _gios_ctl:
 	defw 0  ; GSX function, 1-33
 	defw 0  ; number of pts in ptsin
 	defw 0  ; number of pts in ptsout
-	defw 0  ; number of pts in intin
-	defw 0  ; number of pts in intin
+	defw 0  ; number of values in intin
+	defw 0  ; number of values in intout 
 	defw 0  ; for special uses
 
+
+; limited to 200 bytes, e.g.  printable text max. 100 characters
 gios_intin:
 _gios_intin:
 	defs 200
 
+; limited to 160 bytes, e.g. max 40 corners in graphics objects
 gios_ptsin:
 _gios_ptsin:
-	defs 200
+	defs 160
 
-gios_intout:
-_gios_intout:
-	defs 200
-
+; should be max 12 words, thus 24 bytes
 gios_ptsout:
 _gios_ptsout:
-	defs 200
+	defs 30
+
+; should be max 65 words, thus 130 bytes
+gios_intout:
+_gios_intout:
+	defs 140
+
 
