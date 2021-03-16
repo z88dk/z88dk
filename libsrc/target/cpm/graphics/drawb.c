0306e741ff448ad880be6a44232ad70b921ccb04
Stefano <stefano_bodrato@hotmail.com>
2021-03-16 19:11:29 +0100

    Create drawb.c

---
+++ libsrc/target/cpm/graphics/drawb.c
@@ -0,0 +1,18 @@
+/*
+ *	CP/M GSX based graphics libraries
+ *
+ *	drawb(x1,y1,x2,y2)
+ *
+ *	Stefano Bodrato - 2021
+ *
+ *	$Id: drawb.c $
+ */
+
+#include <cpm.h>
+#include <graphics.h>
+
+
+void draw(int x1,int y1,int x2,int y2)
+{
+	gios_drawb(48*x1,128*(255-y1),x2,128*(255-y2));
+}
