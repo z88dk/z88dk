7b456aa11a9ac72fdcf301e92a641131b197ce36
Stefano <stefano_bodrato@hotmail.com>
2021-03-16 19:12:05 +0100

    Create draw.c

---
+++ libsrc/target/cpm/graphics/draw.c
@@ -0,0 +1,18 @@
+/*
+ *	CP/M GSX based graphics libraries
+ *
+ *	draw(x1,y1,x2,y2)
+ *
+ *	Stefano Bodrato - 2021
+ *
+ *	$Id: draw.c $
+ */
+
+#include <cpm.h>
+#include <graphics.h>
+
+
+void draw(int x1,int y1,int x2,int y2)
+{
+	gios_draw(48*x1,128*(255-y1),x2,128*(255-y2));
+}
