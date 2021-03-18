1682521ab93518f5f292d16946beda248b12c2c3
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 08:58:50 +0100

    Update undrawb.c

--- libsrc/target/cpm/graphics/undrawb.c
+++ libsrc/target/cpm/graphics/undrawb.c
@@ -9,12 +9,13 @@
  */
 
 #include <enterprise.h>
-#include <graphics.h>
+//#include <graphics.h>
+extern void __LIB__ undrawb(int tlx, int tly, int width, int height) __smallc;
 
 
 void undrawb(int x1,int y1,int x2,int y2)
 {
 	gios_wmode(W_ERASE);
-	gios_undrawb(48*x1,128*(255-y1),x2,128*(255-y2));
+	gios_drawb(48*x1,128*(255-y1),x2,128*(255-y2));
 	gios_wmode(W_REPLACE);
 }
