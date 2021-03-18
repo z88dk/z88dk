8ca87cf15daf3e958c3d09b480426b84e7d92a2c
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 08:44:19 +0100

    Create xordrawr.c

---
+++ libsrc/target/cpm/graphics/xordrawr.c
@@ -0,0 +1,21 @@
+/*
+ *	CP/M GSX based graphics libraries
+ *
+ *	xordrawr(x,y)
+ *
+ *	Stefano Bodrato - 2021
+ *
+ *	$Id: xordrawr.c $
+ */
+
+#include <cpm.h>
+//#include <graphics.h>
+extern void __LIB__ xordrawr(int px, int py) __smallc;
+
+
+void xordrawr(int x,int y)
+{
+	gios_wmode(W_COMPLEMENT);
+	gios_drawr(48*x,128*(255-y));
+	gios_wmode(W_REPLACE);
+}
