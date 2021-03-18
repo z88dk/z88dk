40eec73efcbbb733a02eb8b787f7fb3a4bc38a3a
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 08:59:13 +0100

    Update undraw.c

--- libsrc/target/cpm/graphics/undraw.c
+++ libsrc/target/cpm/graphics/undraw.c
@@ -9,12 +9,13 @@
  */
 
 #include <cpm.h>
-#include <graphics.h>
+//#include <graphics.h>
+extern void __LIB__ undraw(int x1, int y1, int x2, int y2) __smallc;
 
 
 void undraw(int x1,int y1,int x2,int y2)
 {
 	gios_wmode(W_ERASE);
-	gios_undraw(48*x1,128*(255-y1),x2,128*(255-y2));
+	gios_draw(48*x1,128*(255-y1),x2,128*(255-y2));
 	gios_wmode(W_REPLACE);
 }
