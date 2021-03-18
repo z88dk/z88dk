eafaf732fd07a443aec72a9bf244fbdfd663363b
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 10:40:16 +0100

    Create w_plotpixel.asm

---
+++ libsrc/graphics/retrofit/w_plotpixel.asm
@@ -0,0 +1,47 @@
+;
+;       Z88 Graphics Functions - Small C+ stubs
+;
+;       Written around the Interlogic Standard Library
+;
+
+;
+;	$Id: w_plotpixel.asm $
+;
+
+;
+; fake w_plotpixel calling a function written in C
+; 
+
+
+SECTION code_graphics
+
+PUBLIC     w_plotpixel
+EXTERN    plot
+
+.w_plotpixel
+IF !__CPU_INTEL__
+	push ix
+	exx
+	push bc
+	push hl
+	push de
+	exx
+ENDIF
+	push bc
+	push hl
+	push de
+	
+	call plot
+	
+	pop de
+	pop hl
+	pop bc
+IF !__CPU_INTEL__
+	exx
+	pop de
+	pop hl
+	pop bc
+	exx
+	pop ix
+ENDIF
+	ret
