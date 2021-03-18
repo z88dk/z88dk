5ec71f5b854d1c7a396f38c85e1838defd33febd
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 08:57:57 +0100

    Update undrawto.c

--- libsrc/target/cpm/graphics/undrawto.c
+++ libsrc/target/cpm/graphics/undrawto.c
@@ -9,12 +9,13 @@
  */
 
 #include <cpm.h>
-#include <graphics.h>
+//#include <graphics.h>
+extern void __LIB__ undrawto(int x2, int y2) __smallc;
 
 
 void undrawto(int x,int y)
 {
 	gios_wmode(W_ERASE);
-	gios_undrawto(48*x,128*(255-y));
+	gios_drawto(48*x,128*(255-y));
 	gios_wmode(W_REPLACE);
 }
