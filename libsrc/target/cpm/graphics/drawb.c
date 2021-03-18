6a7806224110f43ad4c8fa930116239a98ac2128
Stefano <stefano_bodrato@hotmail.com>
2021-03-18 09:01:12 +0100

    Update drawb.c

--- libsrc/target/cpm/graphics/drawb.c
+++ libsrc/target/cpm/graphics/drawb.c
@@ -9,10 +9,11 @@
  */
 
 #include <cpm.h>
-#include <graphics.h>
+//#include <graphics.h>
+extern void __LIB__ drawb(int tlx, int tly, int width, int height) __smallc;
 
 
-void draw(int x1,int y1,int x2,int y2)
+void drawb(int x1,int y1,int x2,int y2)
 {
 	gios_drawb(48*x1,128*(255-y1),x2,128*(255-y2));
 }
