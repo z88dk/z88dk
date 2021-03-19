04656354e6c8cfdef1f5f8fb9a32fa3de7c4f585
Stefano <stefano_bodrato@hotmail.com>
2021-03-19 10:32:21 +0100

    Create set_psg.asm

---
+++ libsrc/target/pcw/psg/set_psg.asm
@@ -0,0 +1,33 @@
+;
+;	Amstrad PCW specific routines (DKTronics sound generator)
+;	by Stefano Bodrato, 2021
+;
+;	int set_psg(int reg, int val);
+;
+;	Play a sound by PSG
+;
+;
+;	$Id: set_psg.asm $
+;
+
+	SECTION code_clib
+	PUBLIC	set_psg
+	PUBLIC	_set_psg
+	EXTERN		set_psg_callee
+
+	EXTERN ASMDISP_SET_PSG_CALLEE
+	
+
+set_psg:
+_set_psg:
+
+	pop	bc
+	pop	de
+	pop	hl
+
+	push	hl
+	push	de
+	push	bc
+	
+	jp set_psg_callee + ASMDISP_SET_PSG_CALLEE
+
