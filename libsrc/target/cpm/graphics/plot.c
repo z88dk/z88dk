3d9fa97ccc8011099f6f08b9a7d4a454a3d0b0e4
Stefano <stefano_bodrato@hotmail.com>
2021-03-24 12:12:33 +0100

    Update plot.c

--- libsrc/target/cpm/graphics/plot.c
+++ libsrc/target/cpm/graphics/plot.c
@@ -12,7 +12,11 @@
 //#include <graphics.h>
 extern void __LIB__ plot(int x, int y) __smallc;
 
+extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
+extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
+
+
 void plot(int x, int y)
 {
-	gios_plot(48*x,128*(255-y));
+	gios_plot(gsx_xscale(x),gsx_yscale(y));
 }
