f42e77020544a72d6d01943a455bfc15c04aeccf
Stefano <stefano_bodrato@hotmail.com>
2021-03-24 12:13:35 +0100

    Update undrawb.c

--- libsrc/target/cpm/graphics/undrawb.c
+++ libsrc/target/cpm/graphics/undrawb.c
@@ -8,14 +8,17 @@
  *	$Id: undrawb.c $
  */
 
-#include <enterprise.h>
+#include <cpm.h>
 //#include <graphics.h>
 extern void __LIB__ undrawb(int tlx, int tly, int width, int height) __smallc;
+
+extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
+extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
 
 
 void undrawb(int x1,int y1,int x2,int y2)
 {
 	gios_wmode(W_ERASE);
-	gios_drawb(48*x1,128*(255-y1),x2,128*(255-y2));
+	gios_drawb(gsx_xscale(x1),gsx_yscale(y1),gsx_xscale(x2),gsx_yscale(y2));
 	gios_wmode(W_REPLACE);
 }
