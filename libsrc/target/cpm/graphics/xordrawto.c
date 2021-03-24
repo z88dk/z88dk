7dd9c18bf2db56ce39dd54ec6f2295c3d5439bbd
Stefano <stefano_bodrato@hotmail.com>
2021-03-24 12:16:34 +0100

    Update xordrawto.c

--- libsrc/target/cpm/graphics/xordrawto.c
+++ libsrc/target/cpm/graphics/xordrawto.c
@@ -12,11 +12,14 @@
 //#include <graphics.h>
 extern void __LIB__ xordrawto(int x2, int y2) __smallc;
 
+extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
+extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
+
 
 void xordrawto(int x,int y)
 {
 	gios_wmode(W_COMPLEMENT);
-	gios_drawto(48*x,128*(255-y));
+	gios_drawto(gsx_xscale(x),gsx_yscale(y));
 	gios_wmode(W_REPLACE);
 }
 
