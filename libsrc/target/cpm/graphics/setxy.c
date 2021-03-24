37daa5d643607acfa1a4aaa532cfcfcd51b6241c
Stefano <stefano_bodrato@hotmail.com>
2021-03-24 12:12:03 +0100

    Update setxy.c

--- libsrc/target/cpm/graphics/setxy.c
+++ libsrc/target/cpm/graphics/setxy.c
@@ -12,9 +12,12 @@
 //#include <graphics.h>
 extern void __LIB__ setxy(int x, int y) __smallc;
 
+extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
+extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
+
 
 void setxy(int x, int y)
 {
-	gios_ptsin[2]=48*x;
-	gios_ptsin[3]=128*(255-y);
+	gios_ptsin[2]=gsx_xscale(x);
+	gios_ptsin[3]=gsx_yscale(y);
 }
