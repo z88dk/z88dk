860bb62e8ab735f18e152caa8accc5de378f8aac
suborb <dom@suborbital.org.uk>
2021-03-25 22:43:09 +0000

    (sam) Separate printers into different files

--- libsrc/target/sam/stdio/__sam_printc.asm
+++ libsrc/target/sam/stdio/__sam_printc.asm
@@ -19,23 +19,21 @@
 
     MODULE  __sam_printc
     PUBLIC  __sam_printc
+
     PUBLIC  __sam_xypos_MODE3
     PUBLIC  __sam_xypos_MODE4
 
+    EXTERN  printc_MODE2
+    EXTERN  printc_MODE3
+    EXTERN  printc_MODE4
 
     EXTERN  __zx_32col_udgs
     EXTERN  __zx_32col_font
-    EXTERN  generic_console_flags
     EXTERN  __zx_screenmode
-    EXTERN  __zx_console_attr
-    EXTERN  __sam_MODE3_attr
-    EXTERN  __sam_MODE4_attr
+    EXTERN  generic_console_flags
+   
     EXTERN  SCREEN_BASE
-    EXTERN  __sam_graphics_pagein
-    EXTERN  __sam_graphics_pageout
 
-    EXTERN  THIS_FUNCTION_ONLY_WORKS_WITH_RAM_SUBTYPES
-    defc ___sam_printc_protection = THIS_FUNCTION_ONLY_WORKS_WITH_RAM_SUBTYPES
 
 ; Print on screen for MODE 2,3,4
 ;
@@ -76,183 +74,11 @@ handle_characters:
     jp      z,printc_MODE2
     cp      2
     jp      z,printc_MODE3
-    ;; Fall through into printc_MODE4
-
-; Mode 4: 16 colours per pixel
-; p0 p0 p0 p0 p1 p1 p1 p1
-printc_MODE4:
-    call    __sam_xypos_MODE4
-    call    __sam_graphics_pagein
-    ld      b,8
-printc_MODE4_1:
-    push    bc
-    ld      a,b
-    exx
-    bit     3,b
-    jr      z,printc_MODE4_no_underline
-    cp      1
-    jr      nz,printc_MODE4_no_underline
-    ld      a,255
-    jr      printc_MODE4_not_bold
-printc_MODE4_no_underline:
-    ld      a,(hl)
-    bit     4,b
-    jr      z,printc_MODE4_not_bold
-    rrca
-    or      (hl)
-printc_MODE4_not_bold:
-    xor     c
-    inc     hl
-    exx
-    
-
-    ld      b,4
-    ld      de,(__sam_MODE4_attr)    ;e = ink, d = paper
-printc_MODE4_2:
-    push    bc
-    ; We have two bits per byte, so lets unroll a bit
-    rlca
-    ld      c,d    ;paper
-    jr      nc,printc_MODE4_is_paper
-    ld      c,e    ;ink
-printc_MODE4_is_paper:
-    rlca        ;shift again
-    ld      b,a    ;save it
-    ld      a,d
-    jr      nc,printc_MODE4_is_paper2
-    ld      a,e    ;ink
-printc_MODE4_is_paper2:
-    rrca
-    rrca
-    rrca
-    rrca
-    and     0x0f
-    or      c
-    ld      (hl),a
-    inc     hl
-    ld      a,b
-    pop     bc
-    djnz    printc_MODE4_2
-    ld      de,128 - 4
-    add     hl,de
-    pop     bc
-    djnz    printc_MODE4_1
-    call    __sam_graphics_pageout
-    ret
+    jp      printc_MODE4
 
 
 
 
-;p0 p0 p1 p1 p2 p2 p3
-printc_MODE3:
-    call    __sam_xypos_MODE3
-    call    __sam_graphics_pagein
-    ld      b,8
-printc_MODE3_1:
-    push    bc
-    ld      a,b
-    exx
-    bit     3,b
-    jr      z,printc_MODE3_no_underline
-    cp      1
-    jr      nz,printc_MODE3_no_underline
-    ld      a,255
-    jr      printc_MODE3_not_bold
-printc_MODE3_no_underline:
-    ld      a,(hl)
-    bit     4,b
-    ld      a,(hl)
-    jr      z,printc_MODE3_not_bold
-    rrca
-    or      (hl)
-printc_MODE3_not_bold:
-    xor     c
-    inc     hl
-    exx
-
-    ld      b,2
-printc_MODE3_2:
-    ld      de,(__sam_MODE3_attr)    ;e = ink, b = paper
-    push    bc
-    push    hl
-    ld      l,a
-    ld      b,4    ;4 iterations
-    ld      c,0    ;final byte
-printc_MODE3_3:
-    rl      l
-    ld      a,d
-    jr      nc,is_paper
-    ld      a,e
-is_paper:
-    or      c
-    ld      c,a
-    srl     d
-    srl     d
-    srl     e
-    srl     e
-    djnz    printc_MODE3_3
-    ld      a,l    ;Save the character for a moment
-    pop     hl
-    ld      (hl),c
-    inc     hl
-    pop     bc
-    djnz    printc_MODE3_2
-    ld      de,128 - 2
-    add     hl,de
-    pop     bc
-    djnz    printc_MODE3_1
-    call    __sam_graphics_pageout
-    ret
-
-
-
-; Mode 2 linear 32x192, attributes +8k
-; Entry:  b = y
-;         c = x
-;       hl' = font entry to print
-;        c' = inverse mask
-;        b' = flags
-printc_MODE2:
-    ; Each character row is 8*32 = 256 bytes
-    ld      a,+(SCREEN_BASE / 256)
-    add     b
-    ld      h,a
-    ld      l,c
-    call    __sam_graphics_pagein
-    ld      de,32
-    ld      a,(__zx_console_attr)
-    ld      c,a
-    ld      b,8
-print32_loop:
-    exx
-    bit     4,b
-    ld      a,(hl)
-    jr      z,no_32_bold
-    rrca
-    or      (hl)
-no_32_bold:
-    xor     c
-    inc     hl
-    exx
-    ld      (hl),a
-    set     5,h
-    ld      (hl),c
-    res     5,h
-    add     hl,de
-    djnz    print32_loop
-handle_attributes:
-    ; hl holds address of line of character below, lets go back
-    exx
-    bit     3,b
-    jr      z,mode2_end
-    exx
-    and     a
-    sbc     hl,de
-    ld      a,255
-    ld      (hl),a
-mode2_end:
-    call    __sam_graphics_pageout
-    ret
 
 
 ; convert character x,y to address
