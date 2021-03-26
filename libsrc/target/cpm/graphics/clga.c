92c38b921f8cbaf705e067bb1983810d3462f951
Stefano <stefano_bodrato@hotmail.com>
2021-03-26 14:31:14 +0100

    Update clga.c

--- libsrc/target/cpm/graphics/clga.c
+++ libsrc/target/cpm/graphics/clga.c
@@ -10,7 +10,7 @@
 
 #include <cpm.h>
 //#include <graphics.h>
-extern void __LIB__ clga(int tlx, int tly, int width, int height) __smallc;
+extern void __LIB__ clga(int tlx, int tly, int tlx2, int tly2) __smallc;
 
 extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
 extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
@@ -18,9 +18,29 @@ extern int  __LIB__ gsx_yscale(int y) __z88dk_fastcall;
 
 void clga(int x1,int y1,int x2,int y2)
 {
-	int y;
+	int xa,ya,xb,yb;
+	
+	if (x1<x2) {
+		xa=x1;
+		xb=x2;
+	} else {
+		xa=x2;
+		xb=x1;
+	}
+
+	if (y1<y2) {
+		ya=y2;
+		yb=y1;
+	} else {
+		ya=y1;
+		yb=y2;
+	}
+	
 	gios_wmode(W_ERASE);
-	for(y=y1;y<y2;y++)
-		gios_draw(gsx_xscale(x1),gsx_yscale(y),gsx_xscale(x1+x2-1),gsx_yscale(y));
+	gios_f_style(F_FULL);
+
+	gios_drawb(gsx_xscale(xa+2),gsx_yscale(ya+2),gsx_xscale(xb-2),gsx_yscale(yb-2));
+
+	gios_f_style(F_EMPTY);
 	gios_wmode(W_REPLACE);
 }
