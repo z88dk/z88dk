187bf00c3b7ddd64cdf8ac7b71fc4e573187fcf7
Stefano <stefano_bodrato@hotmail.com>
2021-03-26 12:02:45 +0100

    Update move.c

--- libsrc/target/cpm/graphics/move.c
+++ libsrc/target/cpm/graphics/move.c
@@ -13,16 +13,16 @@
 extern void __LIB__ move(int px, int py) __smallc;
 extern unsigned char _pen[];
 
-extern int  __LIB__ gsx_xscale(int x) __z88dk_fastcall;
+extern int  __LIB__ gsx_xoffs(int x) __z88dk_fastcall;
 extern int  __LIB__ gsx_yoffs(int y) __z88dk_fastcall;
 
 
 void move(int x,int y)
 {
 	if (_pen[0] & 128)
-		gios_drawr(gsx_xscale(x),gsx_yoffs(y));
+		gios_drawr(gsx_xoffs(x),gsx_yoffs(y));
 	else {
-		gios_ptsin[2]=gios_ptsin[2]+gsx_xscale(x);
+		gios_ptsin[2]=gios_ptsin[2]+gsx_xoffs(x);
 		gios_ptsin[3]=gios_ptsin[3]+gsx_yoffs(y);
 	}
 }
