0a37ee86b032a0d0355f8fb0a25cb6d33d38ae06
Phillip Stevens <phillip.stevens@gmail.com>
2021-04-28 00:03:02 +1000

    math32 - revised z80 mantissa multiplication

--- libsrc/_DEVELOPMENT/math/float/math32/z80/f32_z80_mulu_32h_24x24.asm
+++ libsrc/_DEVELOPMENT/math/float/math32/z80/f32_z80_mulu_32h_24x24.asm
@@ -1,5 +1,5 @@
 ;
-;  feilipu, 2019 May
+;  feilipu, 2021 April
 ;
 ;  This Source Code Form is subject to the terms of the Mozilla Public
 ;  License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,134 +12,274 @@ IF __CPU_Z80__
 SECTION code_clib
 SECTION code_fp_math32
 
-EXTERN m32_z80_mulu_de
-
 PUBLIC m32_mulu_32h_24x24
 
 ;------------------------------------------------------------------------------
 ;
-; multiplication of two 24-bit numbers into a 32-bit product
+; multiplication of two 24-bit numbers into a 32-bit high product
 ;
 ; result is calculated for highest 32-bit result
 ; from a 48-bit calculation.
 ;
-; Lower 8 bits intended to provide rounding information for
-; IEEE floating point mantissa calculations.
+; lower 8 bits intended to provide rounding information for
+; IEEE floating point 24-bit mantissa calculations.
 ;
-; enter : abc = lde  = 24-bit multiplier   = x
-;         def = lde' = 24-bit multiplicand = y
-;
-; abc * def
-; = (a*d)*2^32 +
-;   (a*e + b*d)*2^24 +
-;   (b*e + a*f + c*d)*2^16 +
-;   (b*f + c*e)*2^8
-;
-;   NOT CALCULATED
-;   (c*f)*2^0
-;
-; 8 8*8 multiplies in total
+; enter :  lde  = 24-bit multiplicand = x
+;          lde' = 24-bit multiplier   = y
 ;
 ; exit  : hlde  = 32-bit product
 ;
-; uses  : af, bc, de, hl, af', bc', de', hl'
+; uses  : af, bc, de, hl, bc', de', hl'
 
 
 .m32_mulu_32h_24x24
 
-    ld h,l                      ; ab:bc
-    ld l,d
-    ld a,h                      ; a in a
-    
+    push hl                     ; preserve multiplicand
+    push de
     exx
-    ld h,a
-    push hl                     ; ad on stack
-    ld h,l                      ; de:ef
-    ld l,d
-    push hl                     ; de on stack
-    push de                     ; ef on stack
-    ld a,h                      ; d in a
 
+    push hl                     ; preserve multiplier
+    push de
+
+    ld a,e                      ; set up first multiplier from e
     exx
-    ld d,a                      ; dc in de
+
+    ld c,l                      ; e' * lde
+    call mulu_32_24x8           ; result in bcde
+
+    sla e                       ; check for significant bit for rounding
+    jr NC,no_rounding_e
+    set 0,d
+
+.no_rounding_e
+    ld e,d                      ; shift result down 8 bits
+    ld d,c
+    ld c,b
+    ld b,0
+    exx
+
+    pop af                      ; set up second multiplier from d
+    pop hl                      ; recover multiplier msb l
+
+    pop de                      ; recover multiplicand
+    pop bc
+    push bc
+    push de
+
+    push hl                     ; preserve multiplier msb l
+
+                                ; d' * lde
+    call mulu_32_24x8           ; result in bcde
+
+    push bc
+    push de
+    exx
+
+    pop hl                      ; sum terms
+    add hl,de
+    ex de,hl
+
+    pop hl
+    adc hl,bc
     ld b,h
     ld c,l
-    ex (sp),hl                  ; ab on stack, ef in HL
-    push de                     ; dc on stack
-    push bc                     ; ab on stack (again)
-    push hl                     ; ef on stack
 
-    ld d,l
-    ld a,h
-    ld h,e
-    ld e,a
-    call m32_z80_mulu_de        ; b*f 2^8
-    ex de,hl
-    call m32_z80_mulu_de        ; c*e 2^8
+    sla e                       ; check for significant bit for rounding
+    jr NC,no_rounding_d
+    set 0,d
 
-    xor a
-    add hl,de
-    adc a,a
-
-    ld c,h                      ; put 2^8 in bc
-    ld b,a
-
-    pop de                      ; ef
-    pop hl                      ; ab
-    ld a,d
-    ld d,h
-    ld h,a
-    call m32_z80_mulu_de        ; a*f 2^16
-    ex de,hl
-    call m32_z80_mulu_de        ; e*b 2^16
-
-    xor a
-    add hl,bc
-    adc a,a
-    add hl,de
-    adc a,0
-
-    pop de                      ; dc
-    call m32_z80_mulu_de        ; d*c 2^16
-
-    add hl,de
-    adc a,0
-
-    ld c,h                      ; put 2^16 in bca
-    ld b,a
-    ld a,l
-
-    pop de                      ; ab
-    pop hl                      ; de
-
-    push af                     ; l on stack
-
-    ld a,d
-    ld d,h
-    ld h,a
-    call m32_z80_mulu_de        ; d*b 2^24
-    ex de,hl
-    call m32_z80_mulu_de        ; a*e 2^24
-
-    xor a
-    add hl,bc
-    adc a,a
-    add hl,de
-    adc a,0
-
-    pop bc                     ; l in b
+.no_rounding_d
+    ld e,d                      ; shift result down 8 bits
+    ld d,c
     ld c,b
-    ld b,l
-    ld l,h
-    ld h,a
+    ld b,0
+    exx
 
-    pop de                      ; ad
-    call m32_z80_mulu_de        ; a*d 2^32
+    pop hl                      ; set up third multiply from l
+
+    pop de                      ; recover multiplicand
+    pop bc
+
+    ld a,l                      ; l' * lde
+    call mulu_32_24x8           ; result in bcde
+
+    push bc
+    push de
+    exx
+
+    pop hl                      ; sum terms
+    add hl,de
+    ex de,hl
+
+    pop hl
+    adc hl,bc
+
+    ret                         ; exit  : HLDE  = 32-bit high product
+
+
+.mulu_32_24x8
+
+    ;  cde = 24-bit multiplicand
+    ;    a = 8-bit multiplier
+    ;
+    ; bcde = 32-bit result
+    ;
+    ; uses  : af, bc, de, hl
+
+    ; zero multiplier ?
+
+    or a
+    jr Z,exit_zero
+
+    ; setup multiply
+
+    ld b,a
+
+    ld a,c
+    ld h,d
+    ld l,e
+
+    ; eliminate leading zero bits
+
+.loop_00
+
+    sla b
+    jr C,loop_11
+
+    sla b
+    jr C,loop_12
+
+    sla b
+    jr C,loop_13
+
+    sla b
+    jr C,loop_14
+
+    sla b
+    jr C,loop_15
+
+    sla b
+    jr C,loop_16
+
+    sla b
+    jr C,loop_17
+
+    sla b
+    jr C,exit_18
+
+.exit_zero
+    xor a
+    ld b,a
+    ld c,a
+    ld d,a
+    ld e,a
+    ret
+
+.loop_11
+
+    add hl,hl
+    rla
+    rl b
+
+    jr NC,loop_12
 
     add hl,de
+    adc a,c
 
-    ld d,b
-    ld e,c                      ; exit  : HLDE  = 32-bit product
+    jp NC,loop_12
+    inc b
+
+.loop_12
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,loop_13
+
+    add hl,de
+    adc a,c
+
+    jp NC,loop_13
+    inc b
+
+.loop_13
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,loop_14
+
+    add hl,de
+    adc a,c
+
+    jp NC,loop_14
+    inc b
+
+.loop_14
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,loop_15
+
+    add hl,de
+    adc a,c
+
+    jp NC,loop_15
+    inc b
+
+.loop_15
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,loop_16
+
+    add hl,de
+    adc a,c
+
+    jp NC,loop_16
+    inc b
+
+.loop_16
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,loop_17
+
+    add hl,de
+    adc a,c
+
+    jp NC,loop_17
+    inc b
+
+.loop_17
+
+    add hl,hl
+    rla 
+    rl b
+
+    jr NC,exit_18
+
+    add hl,de
+    adc a,c
+
+    jp NC,exit_18
+    inc b
+
+.exit_18
+
+    ; product in bahl
+    ld c,a
+    ex de,hl
+
+    ; product in bcde
     ret
 
 ENDIF
