fbbfe3c359217b73306585beeccf00130d8ffc31
suborb <dom@suborbital.org.uk>
2021-04-28 21:09:44 +0100

    Just call 5 here since it's a minor call

--- libsrc/target/cpm/fcntl/swapuid.c
+++ libsrc/target/cpm/fcntl/swapuid.c
@@ -4,18 +4,17 @@
 
 int swapuid(int id) __z88dk_fastcall __naked  {
 #asm
-    EXTERN  __bdos
     push    hl
     ld      c,CPM_SUID
     ld      e,0xff	;query
-    call    __bdos
+    call    5
     pop     hl
     cp      l
     ret     z           ;Existing id was the same
     ld      e,l         ;It was different, so set it
     push    af
     ld      c,CPM_SUID
-    call    __bdos
+    call    5
     pop     af
     ld      l,a
     ld      h,0
