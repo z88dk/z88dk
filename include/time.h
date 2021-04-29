4ae6134e2c3910fac18e86e2efb3eba6316f08fe
suborb <dom@suborbital.org.uk>
2021-04-29 18:33:50 +0100

    These functions were added last wekk

--- include/time.h
+++ include/time.h
@@ -146,9 +146,8 @@ struct dos_tm
    uint16_t date;
 };
 
-#if 0
-// Not yet ported from newlib to classic
 // dos time affects tm.tm_sec through tm.tm_year
+// Not available on 8080 targets
 
 extern void __LIB__ dostm_from_tm(struct dos_tm *,struct tm *) __smallc;
 extern void __LIB__ dostm_from_tm_callee(struct dos_tm *,struct tm *) __smallc __z88dk_callee;
@@ -158,7 +157,6 @@ extern void __LIB__ dostm_from_tm_callee(struct dos_tm *,struct tm *) __smallc _
 extern void __LIB__ tm_from_dostm(struct tm *,struct dos_tm *) __smallc;
 extern void __LIB__ tm_from_dostm_callee(struct tm *,struct dos_tm *) __smallc __z88dk_callee;
 #define tm_from_dostm(a,b) tm_from_dostm_callee(a,b)
-#endif
 
 
 #endif /* _TIME_H */
