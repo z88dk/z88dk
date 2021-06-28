9f4ceeef0c7193d924213ab456094f1a8ce2296e
suborb <dom@suborbital.org.uk>
2021-06-28 21:41:13 +0100

    Issue #1784: Roll back command line parsing
    This should fix the argc/argv behaviour at the cost of
    breaking redirection..

--- lib/crt/classic/crt_command_line.asm
+++ lib/crt/classic/crt_command_line.asm
@@ -12,30 +12,10 @@ IF CRT_ENABLE_COMMANDLINE = 1
     jr      argv_begin
 
 redir_fopen_flag:		defb	'w',0
-redir_fopen_flagr:		defb	'r',0
-commandline_argv0:
-	DEFINE NEED_name	; The next time we'll include zcc_opt.def
-				;  it will have the programs namestring.
-				; Usage in C file:
-				; #pragma string name xxxx
-	INCLUDE	"zcc_opt.def"
-	UNDEFINE NEED_name
-						defb	0
+redir_fopen_flagr:		defb	'r'
+commandline_argv0:		defb	0
 
 argv_begin:
-	push    bc
-    ld      d,h
-    ld      e,l
-    inc     de
-    push    de
-    lddr
-	inc    hl
-    ld     (hl),' '
-    pop     hl
-    pop     bc
-	inc     c
-	
-	
     ld      de,0	;NULL pointer at end of array, just in case
     push    de
 ; Try to find the end of the arguments
@@ -56,16 +36,10 @@ argv_loop_2:
     inc     c
 IF CRT_COMMANDLINE_REDIRECTION = 1
         EXTERN freopen
-        
-		; This could be useful on those platforms with args possibly 
-		; at the very first buffer byte (no leading spaces, etc..)
-		; (shouldn't it be replicated for redir_stdin, then?)
-		;xor     a
-        ;add     b
-        ;jr      nz,no_redir_stdout
-        ;-----------------------------------
-		
-		ld      a,(hl)
+        xor     a
+        add     b
+        jr      nz,no_redir_stdout
+        ld      a,(hl)
         cp      '>'
         jr      nz,no_redir_stdout
         push    hl
@@ -124,9 +98,8 @@ empty_arg:
 argv_zloop:
     ld      (hl),0      ;Terminate the previous argument
     dec     hl          ;Last character of previous argument
-;    inc     a		; 'flag'
     dec     c
-    jr      z,argv_done
+    jr      z,argv_done 
     ld      a,(hl)
     cp      ' '
     jr      z,argv_zloop ;Skip over multiple spaces
@@ -137,37 +110,32 @@ argv_loop_3:
     dec     c
     jr      nz,argv_loop_2
 
-;    inc     a		; 'flag'
-
 argv_done:
-;    and     a		; 'flag' to discriminate the "no parameter" case
-;    jr      z,argv_done_2
-
     ; We may still have an argument left (if it was at the start of the buffer)
 
-;argv_push_final_arg:
-;    ld      a,(hl)              ;Strip leading spaces
-;    cp      ' '
-;    jr      nz,argv_push_final_arg2
-;    inc     hl
-;    jr      argv_push_final_arg
-;argv_push_final_arg2:
-;    pop     de                  ;Is it the same as the last argument we pushed?
-;    push    de
-;    ld      a,h
-;    sub     d
-;    jr      nz,argv_push_final_arg3
-;    ld      a,l
-;    sub     e
-;    jr      z,argv_done_2
-;argv_push_final_arg3:
-    ;ld      a,(hl)
-    ;and     a
-    ;jr      z,argv_done_2
-    ;push    hl
-    ;inc     b
-;
-;argv_done_2:
+argv_push_final_arg:
+    ld      a,(hl)              ;Strip leading spaces
+    cp      ' '
+    jr      nz,argv_push_final_arg2
+    inc     hl
+    jr      argv_push_final_arg
+argv_push_final_arg2:
+    pop     de                  ;Is it the same as the last argument we pushed?
+    push    de
+    ld      a,h
+    sub     d
+    jr      nz,argv_push_final_arg3
+    ld      a,l
+    sub     e
+    jr      z,argv_done_2
+argv_push_final_arg3:
+    ld      a,(hl)
+    and     a
+    jr      z,argv_done_2
+    push    hl
+    inc     b
+    
+argv_done_2:
     ld      hl,commandline_argv0	;name of program (NULL)
     push    hl
     inc     b
