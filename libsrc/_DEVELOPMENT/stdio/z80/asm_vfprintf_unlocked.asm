0bf9e325f1386644155de9d9243a099742e75f26
Phillip Stevens <phillip.stevens@gmail.com>
2021-07-05 11:00:48 +1000

    newlib - fix 32BITFLOATS again

--- libsrc/_DEVELOPMENT/stdio/z80/asm_vfprintf_unlocked.asm
+++ libsrc/_DEVELOPMENT/stdio/z80/asm_vfprintf_unlocked.asm
@@ -562,6 +562,9 @@ ENDIF
 
 IF (__SCCZ80 | __ASM) && (CLIB_OPT_PRINTF & $3fc00000)
 ; This is used for floats only so we can check the library spec
+
+EXTERN CLIB_32BIT_FLOATS
+
 printf_return_is_6:
    ld a,CLIB_32BIT_FLOATS
    and a
