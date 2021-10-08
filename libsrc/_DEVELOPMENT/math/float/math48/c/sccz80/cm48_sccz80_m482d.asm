9722b4d1bd6f84b50bab3b65e29ace380b8f8d81
Phillip Stevens <phillip.stevens@gmail.com>
2021-10-08 22:31:48 +1100

    sccz80 - save a ret in mlib2d

--- libsrc/_DEVELOPMENT/math/float/math48/c/sccz80/cm48_sccz80_m482d.asm
+++ libsrc/_DEVELOPMENT/math/float/math48/c/sccz80/cm48_sccz80_m482d.asm
@@ -4,11 +4,12 @@ SECTION code_fp_math48
 
 PUBLIC cm48_sccz80_m482d
 
+EXTERN l_ret
+
    ; convert math48 double to sccz80_float
    ;
    ; enter : AC' = math48 double
    ;
    ; exit  : AC' = sccz80 double
 
-.cm48_sccz80_m482d
-   ret
+defc cm48_sccz80_m482d = l_ret
