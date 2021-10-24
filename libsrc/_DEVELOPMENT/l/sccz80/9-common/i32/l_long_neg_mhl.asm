686b0c08075674ae0c4f5d43ab46680cf59d96ab
Phillip Stevens <phillip.stevens@gmail.com>
2021-10-24 16:57:09 +1100

    classic - long pointer operation (hl)= -(hl)

--- libsrc/_DEVELOPMENT/l/sccz80/9-common/i32/l_long_neg_mhl.asm
+++ libsrc/_DEVELOPMENT/l/sccz80/9-common/i32/l_long_neg_mhl.asm
@@ -13,36 +13,26 @@ PUBLIC  l_long_neg_mhl
 ;enter with primary in (hl)
 
 .l_long_neg_mhl
-    ld      a,(hl)
-    cpl
-    inc     a
+    ld      a,0
+    sub     a,(hl)
     ld      (hl),a
 
     inc     hl
 
-    ld      a,(hl)
-    cpl
-    jp      NZ,neg1
-    inc     a
-.neg1
+    ld      a,0
+    sbc     a,(hl)
     ld      (hl),a
 
     inc     hl
 
-    ld      a,(hl)
-    cpl
-    jp      NZ,neg2
-    inc     a
-.neg2
+    ld      a,0
+    sbc     a,(hl)
     ld      (hl),a
 
     inc     hl
 
-    ld      a,(hl)
-    cpl
-    jp      NZ,neg3
-    inc     a
-.neg3
+    ld      a,0
+    sbc     a,(hl)
     ld      (hl),a
 
     ret
