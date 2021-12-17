08f50f8e05880cb792e1e59d21015299f0b8f9fc
Phillip Stevens <phillip.stevens@gmail.com>
2021-12-17 21:30:02 +1100

    rc2014 - setup lba whitespace

--- libsrc/_DEVELOPMENT/target/rc2014/driver/ide/8080/ide_setup_lba.asm
+++ libsrc/_DEVELOPMENT/target/rc2014/driver/ide/8080/ide_setup_lba.asm
@@ -23,19 +23,19 @@ EXTERN ideStatus
 .ide_setup_lba
     push de
     ld a,__IO_IDE_LBA0
-    call ide_write_byte     ;set LBA0 0:7
+    call ide_write_byte         ;set LBA0 0:7
     pop de
     ld e,d
     ld a,__IO_IDE_LBA1
-    call ide_write_byte     ;set LBA1 8:15
+    call ide_write_byte         ;set LBA1 8:15
     ld e,c
     ld a,__IO_IDE_LBA2
-    call ide_write_byte     ;set LBA2 16:23
+    call ide_write_byte         ;set LBA2 16:23
 
     ld a,b
-    and 00001111b           ;lowest 4 bits LBA address used only
-    or  11100000b           ;to enable LBA address master mode
+    and 00001111b               ;lowest 4 bits LBA address used only
+    or  11100000b               ;to enable LBA address master mode
     ld e,a
     ld a,__IO_IDE_LBA3
-    call ide_write_byte     ;set LBA3 24:27 + bits 5:7=111
+    call ide_write_byte         ;set LBA3 24:27 + bits 5:7=111
     ret
