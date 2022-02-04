159ccc9f2be9ee6f6e80de0d1d9f46c970acc448
Craig Hackney <iratahack@digitalxfer.com>
2022-02-03 18:32:03 -0800

    Export l_pop_ei_jp which equates to asm_cpu_pop_ei_jp.

--- libsrc/video/tms9918/l_tms9918_disable_interrupts_im1.asm
+++ libsrc/video/tms9918/l_tms9918_disable_interrupts_im1.asm
@@ -3,10 +3,13 @@
 
 	PUBLIC	l_tms9918_disable_interrupts
 	PUBLIC	l_tms9918_enable_interrupts	
+	PUBLIC	l_tms9918_enable_interrupts_jp
 
 	EXTERN	l_push_di
 	EXTERN	l_pop_ei
+	EXTERN	l_pop_ei_jp
 
 	defc	l_tms9918_disable_interrupts = l_push_di
 	defc	l_tms9918_enable_interrupts = l_pop_ei
+	defc	l_tms9918_enable_interrupts_jp = l_pop_ei_jp
 
