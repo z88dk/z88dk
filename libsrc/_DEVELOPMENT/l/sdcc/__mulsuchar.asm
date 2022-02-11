bc17f93928ac89bc5ae0e4b4aa3fe498df354877
Phillip Stevens <phillip.stevens@gmail.com>
2022-02-12 01:35:47 +1100

    zsdcc - add ___mulsint2slong_callee and friends

--- libsrc/_DEVELOPMENT/l/sdcc/__mulsuchar.asm
+++ libsrc/_DEVELOPMENT/l/sdcc/__mulsuchar.asm
@@ -4,8 +4,9 @@ INCLUDE "config_private.inc"
 SECTION code_clib
 SECTION code_l_sdcc
 
-PUBLIC __mulsuchar
-PUBLIC __mulsuchar_0
+PUBLIC __mulsuchar, __mulsuchar_0
+
+EXTERN l_muls_16_16x16
 
 __mulsuchar:
 
@@ -14,37 +15,23 @@ __mulsuchar:
    ; enter : stack = multiplicand (signed byte), multiplicand (byte), ret
    ;
    ; exit  : hl = 16-bit product
-   
+
    ld hl,3
    add hl,sp
-   
+
    ld e,(hl)
    dec hl
    ld l,(hl)
-   
+
    ; must promote to 16-bits
 
 __mulsuchar_0:
 
    ld h,0
-   
+
    ld a,e
    add a,a
    sbc a,a
    ld d,a
-   
-IF __CLIB_OPT_IMATH <= 50
 
-   EXTERN l_mulu_16_16x16
-
-   jp l_mulu_16_16x16
-
-ENDIF
-
-IF __CLIB_OPT_IMATH > 50
-
-   EXTERN l_fast_muls_16_16x16
-   
-   jp l_fast_muls_16_16x16
-
-ENDIF
+   jp l_muls_16_16x16
