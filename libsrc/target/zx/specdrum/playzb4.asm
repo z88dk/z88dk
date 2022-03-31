c00c12e1ac659f797a5de39a0726901e93fbdd13
Stefano <stefano_bodrato@hotmail.com>
2022-03-31 14:53:05 +0200

    Create playzb4.asm

---
+++ libsrc/target/zx/specdrum/playzb4.asm
@@ -0,0 +1,67 @@
+;
+;Based on the SG C Tools 1.7
+;(C) 1993 Steve Goldsmith
+;
+; SpecDrum ZX Spectrum interface
+; calibrated for a fixed 8khz bitrate
+
+;
+;$Id: playzb4.asm $
+;
+
+; extern void __LIB__ playzb4(uchar *SamStart, ushort SamLen);
+; play 4 bit pulse wave encoded data using sid master volume
+
+	SECTION code_clib
+	
+	PUBLIC	playzb4
+	PUBLIC	_playzb4
+
+playzb4:
+_playzb4:
+
+;call    csv
+;ld      l,(ix+6)        ;sample start addr
+;ld      h,(ix+7)
+;ld      e,(ix+8)        ;sample length
+;ld      d,(ix+9)
+
+pop	bc
+pop	de	;sample length
+pop	hl	;sample start addr
+push	hl
+push	de
+push	bc
+
+
+rep1:
+;; delay 1
+ld   b,32
+rep1b:                   ;repeat
+djnz rep1b
+
+ld      a,(hl)          ; a = sample byte
+and     $f0             ; 4 bit nibble
+out		($df),a
+
+;; delay 2
+ld   b,32
+rep2:                   ;repeat
+djnz rep2
+
+ld      a,(hl)          ; a = sample byte
+rlca                    ; a = a div 16
+rlca                    ;
+rlca                    ;
+rlca                    ;
+and     $f0             ; 4 bit nibble
+out		($df),a
+
+inc     hl              ; hl = hl+1
+dec     e               ;
+jr      nz,rep1         ;
+dec     d               ; de = de-1
+jr      nz,rep1         ;until de = 0
+
+
+ret
