21af7ce1623353cbcb949d8d63eafe32e1e50a2d
dikdom <vass.sanyi@freemail.hu>
2022-04-29 08:51:44 +0200

    circle and circle_callee is properly worked out.

--- libsrc/target/tvc/romgfx/circle.c
+++ libsrc/target/tvc/romgfx/circle.c
@@ -11,30 +11,8 @@
  */
 
 #include <graphics.h>
+#undef circle
 
 void circle(int x0, int y0, int radius, int skip) {
-    int x = radius;
-    int y = 0;
-    int err = 0;
-
-    while (x >= y) {
-        plot(x0 + x, y0 + y);
-        plot(x0 + y, y0 + x);
-        plot(x0 - y, y0 + x);
-        plot(x0 - x, y0 + y);
-        plot(x0 - x, y0 - y);
-        plot(x0 - y, y0 - x);
-        plot(x0 + y, y0 - x);
-        plot(x0 + x, y0 - y);
-
-        if (err <= 0) {
-            y += skip;
-            err += 2*y + 1;
-        }
-
-        if (err > 0) {
-            x -= skip;
-            err -= 2*x + 1;
-        }
-    }
+    circle_callee(x0, y0, radius, skip);
 }
