d4af372f2775ff148474d7bc924dcdf71139161f
Phillip Stevens <phillip.stevens@gmail.com>
2022-08-08 00:04:58 +1000

    freeRTOS - 10.4.6 August 7 snapshot

--- include/_DEVELOPMENT/clang/freertos/FreeRTOSConfig.h
+++ include/_DEVELOPMENT/clang/freertos/FreeRTOSConfig.h
@@ -70,7 +70,7 @@
 #define configSUPPORT_DYNAMIC_ALLOCATION    1
 #define configSUPPORT_STATIC_ALLOCATION     0
 
-#define configUSE_IDLE_HOOK                 1
+#define configUSE_IDLE_HOOK                 0
 #define configUSE_TICK_HOOK                 0
 
 /* Timer definitions. */
--- include/_DEVELOPMENT/proto/freertos/FreeRTOSConfig.h
+++ include/_DEVELOPMENT/proto/freertos/FreeRTOSConfig.h
@@ -68,7 +68,7 @@ include(__link__.m4)
 #define configSUPPORT_DYNAMIC_ALLOCATION    1
 #define configSUPPORT_STATIC_ALLOCATION     0
 
-#define configUSE_IDLE_HOOK                 1
+#define configUSE_IDLE_HOOK                 0
 #define configUSE_TICK_HOOK                 0
 
 /* Timer definitions. */
