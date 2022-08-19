43c1f0891b347b22707e596d708ec27ff02a7e59
Stefano <stefano_bodrato@hotmail.com>
2022-08-19 08:59:50 +0200

    Update swapuid.c

--- libsrc/target/cpm/fcntl/swapuid.c
+++ libsrc/target/cpm/fcntl/swapuid.c
@@ -4,29 +4,19 @@
 
 int swapuid(int id) __z88dk_fastcall __naked  {
 #asm
+	EXTERN __bdos
+
     push    hl
     ld      c,CPM_SUID
     ld      e,0xff	;query
-IF !__CPU_INTEL__
-	push	ix
-	call	5
-	pop	ix
-ELSE
-	call	5
-ENDIF
+	call __bdos
     pop     hl
     cp      l
     ret     z           ;Existing id was the same
     ld      e,l         ;It was different, so set it
     push    af
     ld      c,CPM_SUID
-IF !__CPU_INTEL__
-	push	ix
-	call	5
-	pop	ix
-ELSE
-	call	5
-ENDIF
+	call __bdos
     pop     af
     ld      l,a
     ld      h,0
