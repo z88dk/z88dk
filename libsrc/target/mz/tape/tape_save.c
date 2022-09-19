4954d8bdd46ab267c7049346eb556d099f261199
Stefano <stefano_bodrato@hotmail.com>
2022-09-19 17:55:58 +0200

    Update tape_save.c

--- libsrc/target/mz/tape/tape_save.c
+++ libsrc/target/mz/tape/tape_save.c
@@ -12,29 +12,31 @@
 
 int tape_save(char *name, size_t loadstart,void *start,void *exec,size_t len)
 {
-	struct  mztapehdr hdr;
+	struct  mztapehdr *hdr;
 	int	l,i;
 
+	hdr=(struct  mztapehdr *)0x10F0;
+	
 	l = strlen(name);
 	if ( l > 17 )
 		l = 17;
 	for (i=0 ; i < l ; i++ )
-		hdr.name[i] = name[i];
+		hdr->name[i] = name[i];
 	for ( ; i < 17 ; i++ )
-		hdr.name[i] = 13;
+		hdr->name[i] = 13;
 
 // 01 machine code program file
 // 03 MZ-80 data file
 // 04 MZ-700 data file
-        hdr.type    = 3;
-        hdr.length  = len;
-        hdr.address = loadstart;
-        hdr.exec    = exec;
+        hdr->type    = 3;
+        hdr->length  = len;
+        hdr->address = loadstart;
+        hdr->exec    = exec;
 
-        if ( tape_save_block(&hdr,sizeof(hdr),0xCC) )
+        if ( mztape_save_header(0x10F0,128) )
                 return -1;
 
-        if ( tape_save_block(start,len,0x53) )
+        if ( mztape_save_block(start,len) )
                 return -1;
         return 0;
 }
