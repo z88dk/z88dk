6f15988bbf919f6e61b5a76bce0180d43e6c398a
Stefano <stefano_bodrato@hotmail.com>
2022-09-20 11:43:47 +0200

    Update tape_save_block.c

--- libsrc/target/mz/tape/tape_save_block.c
+++ libsrc/target/mz/tape/tape_save_block.c
@@ -12,16 +12,23 @@
 #include <string.h>
 #include <arch/mz.h>
 
+__at (0x10F0) struct  mztapehdr mz_hdr;
+
 int tape_save_block(void *addr, size_t len, unsigned char type)
 {
-char mz_type[2];
+char mz_type[3];
 
-	mz_type[1]=0;
+	memset(mz_type,0,3);
 	itoa(type,mz_type,16);
 
+	mz_hdr.type    = 3;
+	mz_hdr.length  = 2;
+	mz_hdr.address = mz_type;
 	if (mztape_save_block(mz_type, 2))
 		return(-1);
 
+	mz_hdr.length  = len;
+	mz_hdr.address = addr;
 	if (mztape_save_block(addr, len))
 		return(-1);
 
