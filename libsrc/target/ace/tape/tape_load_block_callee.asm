6f963f193c22bb749653010b7325a73d3e7f3d68
Stefano <stefano_bodrato@hotmail.com>
2022-09-21 09:05:23 +0200

    Create tape_load_block_callee.asm

---
+++ libsrc/target/ace/tape/tape_load_block_callee.asm
@@ -0,0 +1,34 @@
+;
+;      Tape load routine
+;
+;      Stefano 31/5/2010
+;
+;      int __CALLEE__ tape_load_block_callee(void *addr, size_t len, unsigned char type)
+;
+
+PUBLIC tape_load_block_callee
+PUBLIC _tape_load_block_callee
+PUBLIC asm_tape_load_block
+
+.tape_load_block_callee
+._tape_load_block_callee
+
+	pop af
+	pop bc
+	pop de
+	pop hl
+	push af
+
+.asm_tape_load_block
+
+; enter : hl = addr
+;         de = len
+;          c = type
+
+	scf
+	call $18a7
+        ld      hl,-1
+	ret	nc		;error
+	inc	hl		;okay
+	ret
+
