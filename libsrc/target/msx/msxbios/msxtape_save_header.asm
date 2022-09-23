4f7e747a170b3c911e5f1eeb2e28680b1a8e2d5c
Stefano <stefano_bodrato@hotmail.com>
2022-09-23 14:11:57 +0200

    Create msxtape_save_header.asm

---
+++ libsrc/target/msx/msxbios/msxtape_save_header.asm
@@ -0,0 +1,24 @@
+; int msxtape_save_header(void *addr, size_t len)
+; CALLER linkage for function pointers
+
+PUBLIC msxtape_save_header
+PUBLIC _msxtape_save_header
+
+EXTERN asm_msxtape_save_header
+
+.msxtape_save_header
+._msxtape_save_header
+
+;--------------------
+;   in   BC=byte size
+;        HL=adr.
+;--------------------
+
+   pop af
+   pop bc
+   pop hl
+   push hl
+   push bc
+   push af
+   
+   jp asm_msxtape_save_header
