f17f6fed6a75cd425c786f77dc3705809c375280
Stefano <stefano_bodrato@hotmail.com>
2022-09-30 20:16:44 +0200

    Create tape_save.asm

---
+++ libsrc/target/msx/msxbios/tape_save.asm
@@ -0,0 +1,38 @@
+;
+;      Tape save routine
+;
+;      Stefano, 2022
+;
+;      int  tape_save(char *name, size_t loadstart, void *start, void *exec, size_t len);
+;
+
+PUBLIC tape_save
+PUBLIC _tape_save
+
+EXTERN asm_tape_save
+
+
+.tape_save
+._tape_save
+
+
+  POP AF	; ret_addr
+  POP DE	; len
+  POP BC	; exec
+  POP HL	; start
+  EXX
+  POP DE	; loadstart
+  POP HL	; name
+  
+  PUSH HL
+  PUSH DE
+  EXX
+  PUSH HL
+  PUSH BC
+  PUSH DE
+  PUSH AF
+
+  EXX
+  jp asm_tape_save
+
+.asm_tape_save
