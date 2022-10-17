fa357f42a31c3157dab2b8eeb2cf7ebf86a88c24
Stefano <stefano_bodrato@hotmail.com>
2022-10-17 16:02:17 +0200

    Update lseek.c

--- libsrc/target/nc100/fcntl/lseek.c
+++ libsrc/target/nc100/fcntl/lseek.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <nc.h>
+#include <arch/nc.h>
 
 extern unsigned int __LIB__ __FASTCALL__ nc_lsize(unsigned int handle);
 extern unsigned int __LIB__ __FASTCALL__ nc_ltell(unsigned int handle);
