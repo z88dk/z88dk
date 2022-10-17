56de2545bb5d93fc5d8fe65dfddc21b8f28958af
Stefano <stefano_bodrato@hotmail.com>
2022-10-17 15:53:57 +0200

    Update vz.h

--- include/arch/vz.h
+++ include/arch/vz.h
@@ -43,3 +43,22 @@ extern void __LIB__    vz_brick_callee(void *addr, int byte) __smallc __z88dk_ca
 extern void __LIB__    vz_gotoxy_callee(int x, int y) __smallc __z88dk_callee;
 extern void __LIB__    vz_line_callee(int x1, int y1, int x2, int y2, int c) __smallc __z88dk_callee;
 extern char __LIB__   *vz_midstr_callee(char *str, int pos) __smallc __z88dk_callee;
+extern void __LIB__    vz_plot_callee(int x, int y, int c) __smallc __z88dk_callee;
+extern char __LIB__    vz_point_callee(int x, int y) __smallc __z88dk_callee;
+extern void __LIB__    vz_score_callee(void *addr, int byte) __smallc __z88dk_callee;
+extern void __LIB__    vz_sound_callee(int freq, int cycles) __smallc __z88dk_callee;
+extern void __LIB__    vz_soundcopy_callee(char *dst, char *src, int size, int sound1, int sound2) __smallc __z88dk_callee;
+
+// make the CALLEE linkage default, function pointers will use CALLER linkage
+
+#define vz_brick(a,b)            vz_brick_callee(a,b)
+#define vz_gotoxy(a,b)           vz_gotoxy_callee(a,b)
+#define vz_line(a,b,c,d,e)       vz_line_callee(a,b,c,d,e)
+#define vz_midstr(a,b)           vz_midstr_callee(a,b)
+#define vz_plot(a,b,c)           vz_plot_callee(a,b,c)
+#define vz_point(a,b)            vz_point_callee(a,b)
+#define vz_score(a,b)            vz_score_callee(a,b)
+#define vz_sound(a,b)            vz_sound_callee(a,b)
+#define vz_soundcopy(a,b,c,d,e)  vz_soundcopy_callee(a,b,c,d,e)
+
+#endif
