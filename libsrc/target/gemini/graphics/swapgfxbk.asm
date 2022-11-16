eafc4cbc55a1199631210e5f45626338e8a4ad49
Stefano <stefano_bodrato@hotmail.com>
2022-11-16 11:12:36 +0100

    Create swapgfxbk.asm

---
+++ libsrc/target/gemini/graphics/swapgfxbk.asm
@@ -0,0 +1,21 @@
+;
+;
+;	$Id: swapgfxbk.asm $
+;
+
+	SECTION code_clib
+
+	PUBLIC    swapgfxbk
+	PUBLIC    _swapgfxbk
+
+	PUBLIC	swapgfxbk1
+	PUBLIC   _swapgfxbk1
+		
+
+.swapgfxbk
+._swapgfxbk
+		ret
+
+.swapgfxbk1
+._swapgfxbk1
+		ret
