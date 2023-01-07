cc92a75e35d1570bfcb1d5062179e03ccee04531
dom <dom@suborbital.org.uk>
2023-01-07 00:18:50 +0000

    (cpc) I added interrupts a few months ago

--- include/features.h
+++ include/features.h
@@ -1302,7 +1302,7 @@
 // #define __HAVE_PSG_AY_VT2 0
 // #define __HAVE_PSG_SN76489 0
 // #define __HAVE_PSG_SAA1099 0
-// #define __HAVE_INTERRUPTS 0
+#define __HAVE_INTERRUPTS 1
 #define __NEED_INTERRUPT_INIT 0
 #define __HAVE_RS232 1
 #define __HAVE_FILEIO 1
