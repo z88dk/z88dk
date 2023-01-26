c0cec0e32f79e4e33266d23668e8b4b50d92f7d4
desertkun <desertkun@gmail.com>
2023-01-26 22:20:39 +0200

    sccz80: save a byte on each debug call

--- libsrc/_DEVELOPMENT/l/sccz80/8-8080/l_debug_pop_frame.asm
+++ libsrc/_DEVELOPMENT/l/sccz80/8-8080/l_debug_pop_frame.asm
@@ -3,7 +3,11 @@ SECTION code_clib
 SECTION code_l_sccz80
 
 PUBLIC l_debug_pop_frame
+PUBLIC l_debug_pop_frame_bc
 EXTERN __debug_framepointer
+
+l_debug_pop_frame_bc:
+    pop     bc
 
 l_debug_pop_frame:
     pop     bc      ;return code
