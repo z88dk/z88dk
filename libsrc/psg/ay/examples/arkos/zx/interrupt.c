cf0ef355f3a8f11064d9b3305d2e7816c21aed73
ZXjogv <zx@jogv.es>
2023-03-11 00:40:37 +0100

    arkos: zx interrupt example, fix typecast warnings with SDCC

--- libsrc/psg/ay/examples/arkos/zx/interrupt.c
+++ libsrc/psg/ay/examples/arkos/zx/interrupt.c
@@ -31,10 +31,10 @@ M_END_ISR
 
 void init_interrupts( void ) {
     intrinsic_di();
-    im2_Init( 0xd300 );                // place z80 in im2 mode with interrupt vector table located at 0xd300
-    memset( 0xd300, 0xd4, 257 );       // initialize 257-byte im2 vector table with all 0xd4 bytes
-    bpoke( 0xd4d4, JP_OPCODE );        // POKE jump instruction at address 0xd4d4 (interrupt service routine entry)
-    wpoke( 0xd4d5, isr );              // POKE isr address following the jump instruction
+    im2_Init( ( void * ) 0xd300 );                // place z80 in im2 mode with interrupt vector table located at 0xd300
+    memset( ( void * ) 0xd300, 0xd4, 257 );       // initialize 257-byte im2 vector table with all 0xd4 bytes
+    bpoke( ( void * ) 0xd4d4, JP_OPCODE );        // POKE jump instruction at address 0xd4d4 (interrupt service routine entry)
+    wpoke( ( void * )0xd4d5, ( uint16_t ) isr );              // POKE isr address following the jump instruction
     intrinsic_ei();
 }
 
