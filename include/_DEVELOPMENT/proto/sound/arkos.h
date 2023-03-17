56217cb8790db1484491208a927a24692344b678
ZXjogv <zx@jogv.es>
2023-03-17 18:37:31 +0100

    arkos newlib: add hack to header proto to include __LIB__ in function protos when compiling with SCCZ80

--- include/_DEVELOPMENT/proto/sound/arkos.h
+++ include/_DEVELOPMENT/proto/sound/arkos.h
@@ -10,18 +10,23 @@ include(__link__.m4)
 #ifndef __SOUND_ARKOS_H__
 #define __SOUND_ARKOS_H__
 
+#ifdef __SCCZ80
+  #define __LIB_DECORATOR__ __LIB__
+#else
+  #define __LIB_DECORATOR__
+#endif
 
-extern void ply_akg_init( void *song, uint16_t subsong ) __z88dk_callee;
-extern void ply_akg_play( void );
-extern void ply_akg_stop( void );
-extern void ply_akg_initsoundeffects( void *effects_table[] ) __z88dk_fastcall;
-extern void ply_akg_playsoundeffect( uint16_t effect, uint16_t channel, uint16_t inv_volume ) __z88dk_callee;
+extern void __LIB_DECORATOR__ ply_akg_init( void *song, uint16_t subsong ) __z88dk_callee;
+extern void __LIB_DECORATOR__ ply_akg_play( void );
+extern void __LIB_DECORATOR__ ply_akg_stop( void );
+extern void __LIB_DECORATOR__ ply_akg_initsoundeffects( void *effects_table[] ) __z88dk_fastcall;
+extern void __LIB_DECORATOR__ ply_akg_playsoundeffect( uint16_t effect, uint16_t channel, uint16_t inv_volume ) __z88dk_callee;
 
-extern void ply_rom_akg_init( void *song, uint16_t subsong ) __z88dk_callee;
-extern void ply_rom_akg_play( void );
-extern void ply_rom_akg_stop( void );
-extern void ply_rom_akg_initsoundeffects( void *effects_table[] ) __z88dk_fastcall;
-extern void ply_rom_akg_playsoundeffect( uint16_t effect, uint16_t channel, uint16_t inv_volume ) __z88dk_callee;
+extern void __LIB_DECORATOR__ ply_rom_akg_init( void *song, uint16_t subsong ) __z88dk_callee;
+extern void __LIB_DECORATOR__ ply_rom_akg_play( void );
+extern void __LIB_DECORATOR__ ply_rom_akg_stop( void );
+extern void __LIB_DECORATOR__ ply_rom_akg_initsoundeffects( void *effects_table[] ) __z88dk_fastcall;
+extern void __LIB_DECORATOR__ ply_rom_akg_playsoundeffect( uint16_t effect, uint16_t channel, uint16_t inv_volume ) __z88dk_callee;
 
 #ifdef ARKOS_USE_ROM_PLAYER
 #define ply_akg_init(s,ss) ply_rom_akg_init(s,ss)
