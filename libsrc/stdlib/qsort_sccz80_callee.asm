574dd30278e28b923014f34143888ea0c8499195
Stefano <stefano_bodrato@hotmail.com>
2023-05-02 09:23:55 +0200

    Update qsort_sccz80_callee.asm

--- libsrc/stdlib/qsort_sccz80_callee.asm
+++ libsrc/stdlib/qsort_sccz80_callee.asm
@@ -3,7 +3,7 @@
 ; QSORT - compiled, then hand-adjusted
 ; Stefano, 29/10/2010
 ;
-; $Id: qsort_callee.asm,v 1.4 2016-03-04 23:48:13 dom Exp $
+; $Id: qsort_callee.asm $
 ;
 ; Original code taken from the BDS C by Leor Zolman
 ;
@@ -33,12 +33,20 @@ PUBLIC qsort_sccz80_enter
 EXTERN   l_mult
 EXTERN l_le
 EXTERN memswap_callee
+EXTERN l_setix, l_jpix
 
 
 .qsort_sccz80_callee
 ._qsort_sccz80_callee
    pop   af
-   pop   ix ; *compar
+
+IF __CPU_GBZ80__ || __CPU_INTEL__
+	pop	hl	; *compar
+    call l_setix
+ELSE
+	pop	ix	; *compar
+ENDIF
+
    pop   hl ; width
    pop de   ; nel
    pop bc   ; base
@@ -50,20 +58,55 @@ qsort_sccz80_enter:
 
    push de  ; ngap
    push hl ;width
+
+IF __CPU_GBZ80__ || __CPU_INTEL__
+
+   push hl
+IF __CPU_GBZ80__
+   ld hl,_base
+   ld (hl),bc
+ELSE
+   ld h,b
+   ld l,c
+   ld (_base),hl
+ENDIF
+   pop hl
+
+ELSE
    ld (_base),bc
+ENDIF
 
    ; t1 = nel * width;
    call  l_mult
+
+IF __CPU_GBZ80__
+   ld de,_t1
+   ex de,hl
+   ld (hl),de   
+ELSE
    ld (_t1),hl
+ENDIF
 
    ; for (ngap = nel / 2; ngap > 0; ngap /= 2) {
 .i_3
    pop de   ; width
    pop hl  ; ngap
+
+IF __CPU_GBZ80__ || __CPU_INTEL__
+   ld a,h
+   and a
+   rra
+   ld h,a
+   ld a,l
+   rra
+   ld l,a
+   or h
+ELSE
    srl h       ; _ngap/2 ..bit rotation
    rr  l
    ld a,h
    or l
+ENDIF
 
    ret z
    push hl ; ngap
@@ -74,28 +117,67 @@ qsort_sccz80_enter:
    ;ld e,l
    ;ld   hl,(_width)
    call  l_mult
+
+IF __CPU_GBZ80__
+   ld de,_gap
+   ex de,hl
+   ld (hl),de   
+   ex de,hl
+ELSE
    ld (_gap),hl
+ENDIF
+
    ; t2 = gap + width;
 
-   pop de
+   pop de	; width
    push de
 
    push hl
    ;ex   de,hl
    ;ld   hl,(_width)
    add   hl,de
+
+
+IF __CPU_GBZ80__
+   ld de,_t2
+   ex de,hl
+   ld (hl),de
+   ex de,hl
+ELSE
    ld (_t2),hl
+ENDIF
 
    ; jd = (unsigned int) base + gap;
+IF __CPU_GBZ80__
+   ld a,(_base)
+   ld e,a
+   ld a,(_base+1)
+   ld d,a
+ELSE
    ld de,(_base)
+ENDIF
    ex (sp),hl     ; retrieve 'gap'
    add   hl,de
+IF __CPU_GBZ80__
+   ld de,_jd
+   ex de,hl
+   ld (hl),de
+   ex de,hl
+ELSE
    ld (_jd),hl
+ENDIF
    pop hl         ; t2
    jr i_8
    
 .i_6
+IF __CPU_GBZ80__
+   ld a,(_i)
+   ld e,a
+   ld a,(_i+1)
+   ld d,a
+ELSE
    ld de,(_i)
+ENDIF
    pop hl
    push hl
    ;ld   hl,(_width)
@@ -104,52 +186,114 @@ qsort_sccz80_enter:
 
    ; for (i = t2 ....
 .i_8
+IF __CPU_GBZ80__
+   ld de,_i
+   ex de,hl
+   ld (hl),de
+   ex de,hl
+ELSE
    ld (_i),hl
+ENDIF
+IF __CPU_GBZ80__
+   ld a,(_t1)
+   ld e,a
+   ld a,(_t1+1)
+   ld d,a
+ELSE
    ld de,(_t1)
+ENDIF
    ex de,hl
    call  l_le
    jr nc,i_3
 
    ; for (j =  i - t2..
+IF __CPU_GBZ80__
+   ld a,(_i)
+   ld e,a
+   ld a,(_i+1)
+   ld d,a
+ELSE
    ld de,(_i)
+ENDIF
+
+IF __CPU_GBZ80__
+   ld a,(_t2)
+   ld l,a
+   ld a,(_t2+1)
+   ld h,a
+ELSE
    ld hl,(_t2)
+ENDIF
 
 .i_11
    ex de,hl    ; same subtraction is used twice in the for loop
    and   a
    sbc   hl,de
+
+IF __CPU_GBZ80__
+   ld de,_j
+   ex de,hl
+   ld (hl),de
+   ex de,hl
+ELSE
    ld (_j),hl
+ENDIF
 
    ; for ..; j>0; ..
+IF __CPU_GBZ80__
+   jp c,i_6          ; ********  WARNING, THIS IS AN UNTESTED REPLACEMENT FOR THE MISSING 'S' FLAG ON GAMEBOY
+ELSE
    jp m,i_6
+ENDIF
    
+IF !__CPU_GBZ80__ && !__CPU_INTEL__
    push ix
+ENDIF
    
    ; 1st arg:  base+j
+IF __CPU_GBZ80__
+   ld a,(_base)
+   ld e,a
+   ld a,(_base+1)
+   ld d,a
+ELSE
    ld de,(_base)
+ENDIF
+
    ;ld   hl,(_j)
    push hl
    add   hl,de
    ex (sp),hl  ; j <--> base+j
    
    ; 2nd arg: jd+j
+IF __CPU_GBZ80__
+   ld a,(_jd)
+   ld e,a
+   ld a,(_jd+1)
+   ld d,a
+ELSE
    ld de,(_jd)
+ENDIF
    add   hl,de
    push  hl
 
-   ld bc,ret_addr
-   push bc
-   jp (ix)     ; compare function
-.ret_addr
+   call l_jpix     ; compare function
 
    pop bc      ; we're keeping the same args for the next call !
    pop de
    
+IF !__CPU_GBZ80__ && !__CPU_INTEL__
    pop ix
+ENDIF
 
    ; if ((*compar)(base+j, jd+j) <=0) break;
    dec hl
+IF __CPU_GBZ80__ || __CPU_INTEL__
+   ld a,10000000b
+   and h
+ELSE
    bit 7,h
+ENDIF
    jr nz,i_6   ; Negative sign ?   exit loop
 
    
@@ -163,9 +307,27 @@ qsort_sccz80_enter:
    call  memswap_callee
 
    ; for ... j -= gap)
+IF __CPU_GBZ80__
+   ld a,(_j)
+   ld e,a
+   ld a,(_j+1)
+   ld d,a
+ELSE
    ld de,(_j)
+ENDIF
+
+IF __CPU_GBZ80__
+   ld a,(_gap)
+   ld l,a
+   ld a,(_gap+1)
+   ld h,a
+ELSE
    ld hl,(_gap)
+ENDIF
+
    jr i_11
+
+
 
 SECTION bss_clib
 
