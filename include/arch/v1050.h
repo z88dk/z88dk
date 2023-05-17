439c304a8ed8287c4d70f79efd31da589ce629e4
Stefano <stefano_bodrato@hotmail.com>
2023-05-17 11:51:38 +0200

    Create v1050.h

---
+++ include/arch/v1050.h
@@ -0,0 +1,12 @@
+#ifndef __ARCH_V1050_H__
+#define __ARCH_V1050_H__
+
+//////////////////////////
+//   Video interface
+//////////////////////////
+
+// Send a byte to the video section
+extern void __LIB__ v1050_sendchar(int chr)  __z88dk_fastcall;
+
+  
+#endif
