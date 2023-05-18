14ee93ffb0997bfe7bc7e5c77c21f99f36389392
Stefano <stefano_bodrato@hotmail.com>
2023-05-18 16:53:08 +0200

    Undo V1050 optimization
    For some reason the balance breaks and some data is lost during the transmission !
    Let's keep the decimal mode for now.

--- libsrc/target/v1050/graphics/__v1050_coord.asm
+++ libsrc/target/v1050/graphics/__v1050_coord.asm
@@ -9,7 +9,7 @@
 
 	EXTERN    __gfx_coords
 	EXTERN    v1050_sendchar
-;	EXTERN    v1050_sendchar_fast
+	EXTERN    v1050_sendchar_fast
 
 ;
 ;       $Id: __v1050_coord.asm $
@@ -26,66 +26,66 @@
 
 __v1050_coord:
 
-	push hl
-	ld l,'='
-	call v1050_sendchar
-	pop hl
-	push hl
-	call v1050_sendchar
-	pop hl
-	ld l,h
-	call v1050_sendchar
-	ret
+;;; Short way, binary spec, LSB/MSB
+;	push hl
+;	ld l,'='
+;	call v1050_sendchar
+;	pop hl
+;	push hl
+;	call v1050_sendchar
+;	pop hl
+;	ld l,h
+;	call v1050_sendchar
+;	ret
 	
-;; Alternative way, ASCII-decimal values
-;
-;	push de
-;
-;
-;; Converts HL into a BCD number
-;
-;	ld b,16  ; handle 16 bits,one bit per iteration
-;	;ld c,0
-;	LD	DE,0
-;cvtLoop:
-;	ADD	HL,HL
-;	LD	A,E
+; Alternative way, ASCII-decimal values
+
+	push de
+
+
+; Converts HL into a BCD number
+
+	ld b,16  ; handle 16 bits,one bit per iteration
+	;ld c,0
+	LD	DE,0
+cvtLoop:
+	ADD	HL,HL
+	LD	A,E
+	ADC	A,A
+	DAA
+	LD	E,A
+	LD	A,D
+	ADC	A,A
+	DAA
+	LD	D,A
+;	LD	A,C
 ;	ADC	A,A
 ;	DAA
-;	LD	E,A
-;	LD	A,D
-;	ADC	A,A
-;	DAA
-;	LD	D,A
-;;	LD	A,C
-;;	ADC	A,A
-;;	DAA
-;;	LD	C,A
-;	DJNZ cvtLoop
-;;	EX	HL,DE
-;
-;	ld a,d
-;	and $0f
-;	add '0'
-;	ld l,a
-;	call v1050_sendchar_fast
-;	
-;	ld a,e
-;	srl a
-;	srl a
-;	srl a
-;	srl a
-;	and $0f
-;	add '0'
-;	ld l,a
-;	call v1050_sendchar
-;
-;	ld a,e
-;	and $0f
-;	add '0'
-;	ld l,a
-;	call v1050_sendchar
-;
-;	pop de
-;	RET
-;
+;	LD	C,A
+	DJNZ cvtLoop
+;	EX	HL,DE
+
+	ld a,d
+	and $0f
+	add '0'
+	ld l,a
+	call v1050_sendchar_fast
+	
+	ld a,e
+	srl a
+	srl a
+	srl a
+	srl a
+	and $0f
+	add '0'
+	ld l,a
+	call v1050_sendchar
+
+	ld a,e
+	and $0f
+	add '0'
+	ld l,a
+	call v1050_sendchar
+
+	pop de
+	RET
