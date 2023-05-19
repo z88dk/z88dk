da6146cc9f2aa593021cf020d99ec03d2a3d826c
Stefano <stefano_bodrato@hotmail.com>
2023-05-19 12:54:44 +0200

    Update __v1050_coord.asm

--- libsrc/target/v1050/graphics/__v1050_coord.asm
+++ libsrc/target/v1050/graphics/__v1050_coord.asm
@@ -26,66 +26,67 @@
 
 __v1050_coord:
 
-;;; Short way, binary spec, LSB/MSB
-;	push hl
-;	ld l,'='
-;	call v1050_sendchar
-;	pop hl
-;	push hl
-;	call v1050_sendchar
-;	pop hl
-;	ld l,h
-;	call v1050_sendchar
-;	ret
+;; Short way, binary spec, LSB/MSB
+	push hl
+	ld l,'='
+	call v1050_sendchar
+	pop hl
+	push hl
+	call v1050_sendchar_fast
+	pop hl
+	ld l,h
+	call v1050_sendchar
+	ret
 	
-; Alternative way, ASCII-decimal values
-
-	push de
-
-
-; Converts HL into a BCD number
-
-	ld b,16  ; handle 16 bits,one bit per iteration
-	;ld c,0
-	LD	DE,0
-cvtLoop:
-	ADD	HL,HL
-	LD	A,E
-	ADC	A,A
-	DAA
-	LD	E,A
-	LD	A,D
-	ADC	A,A
-	DAA
-	LD	D,A
-;	LD	A,C
+;; Alternative way, ASCII-decimal values
+;
+;	push de
+;
+;
+;; Converts HL into a BCD number
+;
+;	ld b,16  ; handle 16 bits,one bit per iteration
+;	;ld c,0
+;	LD	DE,0
+;cvtLoop:
+;	ADD	HL,HL
+;	LD	A,E
 ;	ADC	A,A
 ;	DAA
-;	LD	C,A
-	DJNZ cvtLoop
-;	EX	HL,DE
-
-	ld a,d
-	and $0f
-	add '0'
-	ld l,a
-	call v1050_sendchar_fast
-	
-	ld a,e
-	srl a
-	srl a
-	srl a
-	srl a
-	and $0f
-	add '0'
-	ld l,a
-	call v1050_sendchar
-
-	ld a,e
-	and $0f
-	add '0'
-	ld l,a
-	call v1050_sendchar
-
-	pop de
-	RET
+;	LD	E,A
+;	LD	A,D
+;	ADC	A,A
+;	DAA
+;	LD	D,A
+;;	LD	A,C
+;;	ADC	A,A
+;;	DAA
+;;	LD	C,A
+;	DJNZ cvtLoop
+;;	EX	HL,DE
+;
+;	ld a,d
+;	and $0f
+;	add '0'
+;	ld l,a
+;	call v1050_sendchar_fast
+;	
+;	ld a,e
+;	srl a
+;	srl a
+;	srl a
+;	srl a
+;	and $0f
+;	add '0'
+;	ld l,a
+;	call v1050_sendchar
+;
+;	ld a,e
+;	and $0f
+;	add '0'
+;	ld l,a
+;	call v1050_sendchar
+;
+;	pop de
+;	RET
+;
