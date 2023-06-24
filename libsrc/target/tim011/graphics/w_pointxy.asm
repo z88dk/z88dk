43ece373baccf21f8bfda1471bd312feee9e43fb
dom <dom@suborbital.org.uk>
2023-06-25 00:40:51 +0100

    (tim011) Hires graphics, fatpix broken at the moment

---
+++ libsrc/target/tim011/graphics/w_pointxy.asm
@@ -0,0 +1,10 @@
+; Plot pixel at (x,y)
+
+
+	SECTION	code_clib
+	PUBLIC	w_pointxy
+	defc	NEEDpoint = 1
+
+
+w_pointxy:
+	INCLUDE	"target/tim011/graphics/pixel.inc"
---
+++ libsrc/target/tim011/graphics/w_respixl.asm
@@ -0,0 +1,10 @@
+; Plot pixel at (x,y)
+
+
+	SECTION	code_clib
+	PUBLIC	w_respixel
+	defc	NEEDunplot = 1
+
+
+w_respixel:
+	INCLUDE	"target/tim011/graphics/pixel.inc"
