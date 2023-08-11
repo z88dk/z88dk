4789e644c4b0e3e296c4563175735007f687230f
Phillip Stevens <phillip.stevens@gmail.com>
2023-08-11 12:44:34 +1000

    test - add unsigned tests particuarly for rabbit hardware

--- test/suites/sccz80/mult.c
+++ test/suites/sccz80/mult.c
@@ -7,17 +7,56 @@
   #ifndef __GBZ80__
 void test_mult_longlong() 
 {
-     long long val = 3;
+     long long val1 = 3;
+     long long val2 = 5;
 
-     Assert( val * 655360 == 655360 * 3, "3 * 655360");
-     Assert( val * 256 == 768, "3 * 256");
-     Assert( val * 8  == 24, "3 * 8");
-     Assert( val * 4  == 12, "3 * 4");
-     Assert( val * 2  == 6, "3 * 2");
-     Assert( val * 3  == 9, "3 * 3");
-     Assert( val * 5  == 15, "3 * 5");
-     Assert( val * 6  == 18, "3 * 6");
+     Assert(  val1 * 655360 == 655360 * 3, "3 * 655360");
+     Assert(  val1 * 256 == 768, "3 * 256");
+     Assert(  val1 * 8  == 24, "3 * 8");
+     Assert(  val1 * 4  == 12, "3 * 4");
+     Assert(  val1 * 2  == 6, "3 * 2");
+     Assert(  val1 * 3  == 9, "3 * 3");
+     Assert(  val1 * 5  == 15, "3 * 5");
+     Assert(  val1 * 6  == 18, "3 * 6");
+
+     Assert(  val1 *  val2  ==  15, " 3 *  5");
+     Assert(  val1 * -val2  == -15, " 3 * -5");
+     Assert( -val1 *  val2  == -15, "-3 *  5");
+     Assert( -val1 * -val2  ==  15, "-3 * -5");
+     Assert(  val2 *  val1  ==  15, " 5 *  3");
+     Assert(  val2 * -val1  == -15, " 5 * -3");
+     Assert( -val2 *  val1  == -15, "-5 *  3");
+     Assert( -val2 * -val1  ==  15, "-5 * -3");
 }
+
+void test_mult_unsigned_longlong() 
+{
+     unsigned long long val1 = 3;
+     unsigned long long val2 = 5;
+     unsigned long long val3 = 0x2AAAAAAB;
+     unsigned long long val4 = 0x2AAAAAAAAAAAAAAB;
+
+     Assert( val1 * 655360 == 655360 * 3, "3 * 655360");
+     Assert( val1 * 256 == 768, "3 * 256");
+     Assert( val1 * 8  == 24, "3 * 8");
+     Assert( val1 * 4  == 12, "3 * 4");
+     Assert( val1 * 2  == 6, "3 * 2");
+     Assert( val1 * 3  == 9, "3 * 3");
+     Assert( val1 * 5  == 15, "3 * 5");
+     Assert( val1 * 6  == 18, "3 * 6");
+
+     Assert( val1 * val2  ==  15, "3 * 5");
+     Assert( val2 * val1  ==  15, "5 * 3");
+
+     Assert( val1 * val3  ==  0x80000001, "3 * 0x2AAAAAAB");
+     Assert( val3 * val1  ==  0x80000001, "0x2AAAAAAB * 3");
+     Assert( val1 * val4  ==  0x8000000000000001, "3 * 0x2AAAAAAAAAAAAAAB");
+     Assert( val4 * val1  ==  0x8000000000000001, "0x2AAAAAAAAAAAAAAB * 3");
+
+     Assert( val1 * val3 * 3 ==  0x180000003, "3 * 0x2AAAAAAB * 3");
+     Assert( val3 * val1 * 3 ==  0x180000003, "0x2AAAAAAB * 3 * 3");
+}
+
   #endif
 #endif
 
@@ -25,17 +64,29 @@ void test_quickmult_long()
 {
      long val = 3;
 
-     Assert( val * 256 == 768, "3 * 256");
-     Assert( val * 64 == 192, "3 * 64");
-     Assert( val * 2  == 6, "3 * 2");
-     Assert( val * 3  == 9, "3 * 3");
-     Assert( val * 4  == 12, "3 * 4");
-     Assert( val * 5  == 15, "3 * 5");
-     Assert( val * 6  == 18, "3 * 6");
-     Assert( val * 7  == 21, "3 * 7");
-     Assert( val * 8  == 24, "3 * 8");
-     Assert( val * 9  == 27, "3 * 9");
-     Assert( val * 40 == 120, "3 * 40");
+     Assert(  val * 256 == 768, "3 * 256");
+     Assert(  val * 64 == 192, "3 * 64");
+     Assert(  val * 2  == 6, "3 * 2");
+     Assert(  val * 3  == 9, "3 * 3");
+     Assert(  val * 4  == 12, "3 * 4");
+     Assert(  val * 5  == 15, "3 * 5");
+     Assert(  val * 6  == 18, "3 * 6");
+     Assert(  val * 7  == 21, "3 * 7");
+     Assert(  val * 8  == 24, "3 * 8");
+     Assert(  val * 9  == 27, "3 * 9");
+     Assert(  val * 40 == 120, "3 * 40");
+
+     Assert( -val * 256 == -768, "3 * 256");
+     Assert( -val * 64 == -192, "3 * 64");
+     Assert( -val * 2  == -6, "3 * 2");
+     Assert( -val * 3  == -9, "3 * 3");
+     Assert( -val * 4  == -12, "3 * 4");
+     Assert( -val * 5  == -15, "3 * 5");
+     Assert( -val * 6  == -18, "3 * 6");
+     Assert( -val * 7  == -21, "3 * 7");
+     Assert( -val * 8  == -24, "3 * 8");
+     Assert( -val * 9  == -27, "3 * 9");
+     Assert( -val * 40 == -120, "3 * 40");
 }
 
 void test_mult_long()
@@ -53,6 +104,25 @@ void test_mult_long()
      Assert( -val2 * -val1  ==  15, "-5 * -3");
 }
 
+void test_mult_unsigned_long()
+{
+     unsigned long val1 = 3;
+     unsigned long val2 = 5;
+     unsigned long val3 = 0x2AAB;
+     unsigned long val4 = 0x2AAAAAAB;
+
+     Assert( val1 * val2  ==  15, "3 * 5");
+     Assert( val2 * val1  ==  15, "5 * 3");
+
+     Assert( val1 * val3  ==  0x8001, "3 * 0x2AAB");
+     Assert( val3 * val1  ==  0x8001, "0x2AAB * 3");
+     Assert( val1 * val4  ==  0x80000001, "3 * 0x2AAAAAAB");
+     Assert( val4 * val1  ==  0x80000001, "0x2AAAAAAB * 3");
+
+     Assert( val1 * val3 * 3 ==  0x18003, "3 * 0x2AAB * 3");
+     Assert( val3 * val1 * 3 ==  0x18003, "0x2AAB * 3 * 3");
+}
+
 void test_mult_int()
 {
      int val1 = 3;
@@ -68,6 +138,26 @@ void test_mult_int()
      Assert( -val2 * -val1  ==  15, "-5 * -3");
 }
 
+void test_mult_unsigned_int()
+{
+     unsigned int val1 = 3;
+     unsigned int val2 = 5;
+
+     unsigned long val3 = 0x2B;
+     unsigned long val4 = 0x2AAB;
+
+     Assert( val1 * val2  ==  15, "3 * 5");
+     Assert( val2 * val1  ==  15, "5 * 3");
+
+     Assert( val1 * val3  ==  0x81, "3 * 0x2B");
+     Assert( val3 * val1  ==  0x81, "0x2B * 3");
+     Assert( val1 * val4  ==  0x8001, "3 * 0x2AAB");
+     Assert( val4 * val1  ==  0x8001, "0x2AAB * 3");
+
+     Assert( val1 * val3 * 3 ==  0x183, "3 * 0x2B * 3");
+     Assert( val3 * val1 * 3 ==  0x183, "0x2B * 3 * 3");
+}
+
 int suite_mult()
 {
     suite_setup("Multiplication Tests");
@@ -75,11 +165,14 @@ int suite_mult()
     suite_add_test(test_quickmult_long);
 
     suite_add_test(test_mult_int);
+    suite_add_test(test_mult_unsigned_int);
     suite_add_test(test_mult_long);
+    suite_add_test(test_mult_unsigned_long);
 
 #ifndef __8080__
   #ifndef __GBZ80__
     suite_add_test(test_mult_longlong);
+    suite_add_test(test_mult_unsigned_longlong);
   #endif
 #endif
 
