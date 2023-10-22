b85d68df62027ac46708a8f22384300edac3cfdc
Paulo Custodio <pauloscustodio@gmail.com>
2023-10-22 22:13:44 +0100

    need to link statically when cross-compiling

--- src/z80asm/build_ldflags.pl
+++ src/z80asm/build_ldflags.pl
@@ -8,36 +8,35 @@ my($gcc, $cross) = @ARGV;
 
 my $file = "build_ldflags$$";
 
-# check if -lstdc++fs exists
-open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
-print $fh "int main(){}";
-close($fh);
-
-if (0 == system "$gcc -o $file $file.cpp -lstdc++fs 2> $file.err") {
-	print "-lstdc++fs ";
+# when corss compiling, need to link statically
+if ($cross) {
+	print "-static-libstdc++ -static-libgcc ";
 }
 else {
-	# check if we need boost::filesystem
+	# check if -lstdc++fs exists
 	open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
-	print $fh <<END;
-#if __has_include(<filesystem>)	// std::filesystem from C++17
-#ifdef WIN32
-#if CROSS == 1
--static-libstdc++ -static-libgcc
-#endif
-#endif
-#else							// boost::filesystem from Boost
+	print $fh "int main(){}";
+	close($fh);
+
+	if (0 == system "$gcc -o $file $file.cpp -lstdc++fs 2> $file.err") {
+		print "-lstdc++fs ";
+	}
+	else {
+		# check if we need boost::filesystem
+		open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
+		print $fh <<END;
+#if !__has_include(<filesystem>)	// std::filesystem from C++17
 -lboost_filesystem-mt
 #endif
 END
-
-	open(my $pipe, "$gcc -DCROSS=$cross -E $file.cpp |") or die;
-	while (<$pipe>) {
-		chomp;
-		s/^#.*//;
-		print "$_ ";
+		open(my $pipe, "$gcc -E $file.cpp |") or die;
+		while (<$pipe>) {
+			chomp;
+			s/^#.*//;
+			print "$_ ";
+		}
+		close($pipe);
 	}
-	close($pipe);
+	print "\n";
 }
-print "\n";
 unlink "$file.cpp", "$file.err", $file;
