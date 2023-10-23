3787da33d5b188f232a80d24c7ce0e3e18cca0e1
Paulo Custodio <pauloscustodio@gmail.com>
2023-10-23 21:20:07 +0100

    This breaks the macOS cross build since -static-libgcc isn't an option there.

--- src/z80asm/build_ldflags.pl
+++ src/z80asm/build_ldflags.pl
@@ -8,35 +8,36 @@ my($gcc, $cross) = @ARGV;
 
 my $file = "build_ldflags$$";
 
-# when cross compiling, need to link statically
-if ($cross) {
-	print "-static-libstdc++ -static-libgcc ";
+# check if -lstdc++fs exists
+open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
+print $fh "int main(){}";
+close($fh);
+
+if (0 == system "$gcc -o $file $file.cpp -lstdc++fs 2> $file.err") {
+	print "-lstdc++fs ";
 }
 else {
-	# check if -lstdc++fs exists
+	# check if we need boost::filesystem
 	open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
-	print $fh "int main(){}";
-	close($fh);
-
-	if (0 == system "$gcc -o $file $file.cpp -lstdc++fs 2> $file.err") {
-		print "-lstdc++fs ";
-	}
-	else {
-		# check if we need boost::filesystem
-		open(my $fh, ">", "$file.cpp") or die "open $file.cpp:$!";
-		print $fh <<END;
-#if !__has_include(<filesystem>)	// std::filesystem from C++17
+	print $fh <<END;
+#if __has_include(<filesystem>)	// std::filesystem from C++17
+#ifdef WIN32
+#if CROSS == 1
+-static-libstdc++ -static-libgcc
+#endif
+#endif
+#else							// boost::filesystem from Boost
 -lboost_filesystem-mt
 #endif
 END
-		open(my $pipe, "$gcc -E $file.cpp |") or die;
-		while (<$pipe>) {
-			chomp;
-			s/^#.*//;
-			print "$_ ";
-		}
-		close($pipe);
+
+	open(my $pipe, "$gcc -DCROSS=$cross -E $file.cpp |") or die;
+	while (<$pipe>) {
+		chomp;
+		s/^#.*//;
+		print "$_ ";
 	}
-	print "\n";
+	close($pipe);
 }
+print "\n";
 unlink "$file.cpp", "$file.err", $file;
