d4ae716dbcdc3c2f2dde4d19621882d2cfbddf50
suborb <dom@suborbital.org.uk>
2023-10-31 20:06:14 +0000

    (fsync) Missing nc100 version + it's an int return

--- libsrc/target/osca/fcntl/flos/fsync.asm
+++ libsrc/target/osca/fcntl/flos/fsync.asm
@@ -1,5 +1,3 @@
-; $Id: fsync.asm,v 1.4 2016-03-06 21:39:54 dom Exp $
-
         SECTION code_clib
 
         PUBLIC  fsync
@@ -8,7 +6,5 @@
 fsync:
 _fsync:
         ld      hl, -1                  ;return -1
-        ld      d, h
-        ld      e, l
         ret
 
--- libsrc/target/zx/fcntl/zxbasdrv/fsync.asm
+++ libsrc/target/zx/fcntl/zxbasdrv/fsync.asm
@@ -1,4 +1,3 @@
-; $Id: fsync.asm,v 1.4 2016-03-06 21:39:54 dom Exp $
 
         SECTION code_clib
 
@@ -8,7 +7,5 @@
 fsync:
 _fsync:
         ld      hl, -1                  ;return -1
-        ld      d, h
-        ld      e, l
         ret
 
