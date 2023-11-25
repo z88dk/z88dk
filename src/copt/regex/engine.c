a902c07ff11065220f84214bf5d7ceed78b82171
Paulo Custodio <pauloscustodio@gmail.com>
2023-11-25 20:58:57 +0000

    (copt) cleanup warnings on MSVC x64

--- src/copt/regex/engine.c
+++ src/copt/regex/engine.c
@@ -201,8 +201,8 @@ int eflags)
 
 	/* fill in the details if requested */
 	if (nmatch > 0) {
-		pmatch[0].rm_so = m->coldp - m->offp;
-		pmatch[0].rm_eo = endp - m->offp;
+		pmatch[0].rm_so = (regoff_t)(m->coldp - m->offp);
+		pmatch[0].rm_eo = (regoff_t)(endp - m->offp);
 	}
 	if (nmatch > 1) {
 		assert(m->pmatch != NULL);
@@ -391,12 +391,12 @@ sopno stopst)
 		case OLPAREN:
 			i = OPND(m->g->strip[ss]);
 			assert(0 < i && i <= m->g->nsub);
-			m->pmatch[i].rm_so = sp - m->offp;
+			m->pmatch[i].rm_so = (regoff_t)(sp - m->offp);
 			break;
 		case ORPAREN:
 			i = OPND(m->g->strip[ss]);
 			assert(0 < i && i <= m->g->nsub);
-			m->pmatch[i].rm_eo = sp - m->offp;
+			m->pmatch[i].rm_eo = (regoff_t)(sp - m->offp);
 			break;
 		default:		/* uh oh */
 			assert(nope);
@@ -584,7 +584,7 @@ sopno lev)			/* PLUS nesting level */
 		i = OPND(s);
 		assert(0 < i && i <= m->g->nsub);
 		offsave = m->pmatch[i].rm_so;
-		m->pmatch[i].rm_so = sp - m->offp;
+		m->pmatch[i].rm_so = (regoff_t)(sp - m->offp);
 		dp = backref(m, sp, stop, ss+1, stopst, lev);
 		if (dp != NULL)
 			return(dp);
@@ -595,7 +595,7 @@ sopno lev)			/* PLUS nesting level */
 		i = OPND(s);
 		assert(0 < i && i <= m->g->nsub);
 		offsave = m->pmatch[i].rm_eo;
-		m->pmatch[i].rm_eo = sp - m->offp;
+		m->pmatch[i].rm_eo = (regoff_t)(sp - m->offp);
 		dp = backref(m, sp, stop, ss+1, stopst, lev);
 		if (dp != NULL)
 			return(dp);
