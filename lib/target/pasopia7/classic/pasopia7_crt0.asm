fbc19da9d0f311bf7c1f6bacdf28d30673a70648
dom <dom@suborbital.org.uk>
2024-03-07 22:49:50 +0000

    (pasopia) Update crt

--- lib/target/pasopia7/classic/pasopia7_crt0.asm
+++ lib/target/pasopia7/classic/pasopia7_crt0.asm
@@ -4,54 +4,47 @@
 
 	MODULE pasopia7_crt0
 
-
-;--------
-; Include zcc_opt.def to find out some info
-;--------
-
-        defc    crt0 = 1
-        INCLUDE "zcc_opt.def"
-
-;--------
-; Some scope definitions
-;--------
-
-        EXTERN    _main           ;main() is always external to crt0 code
-
-	EXTERN	asm_im1_handler
-	EXTERN	asm_nmi_handler
-
-        PUBLIC    cleanup         ;jp'd to by exit()
-        PUBLIC    l_dcal          ;jp(hl)
+    defc    crt0 = 1
+    INCLUDE "zcc_opt.def"
 
 
-        defc    TAR__clib_exit_stack_size = 32
-        defc    TAR__register_sp = 65280
-        defc    TAR__fputc_cons_generic = 1
-	defc	CRT_KEY_DEL = 12
-	defc	CRT_ORG_CODE = 0
-        defc    CONSOLE_COLUMNS = 40
-        defc    CONSOLE_ROWS = 25
-	defc	__CPU_CLOCK = 4000000
-	defc	CLIB_FGETC_CONS_DELAY = 150
+    EXTERN    _main           ;main() is always external to crt0 code
 
-	defc	GRAPHICS_CHAR_SET = 135
-	defc	GRAPHICS_CHAR_UNSET = 32
-	PUBLIC	GRAPHICS_CHAR_SET
-	PUBLIC	GRAPHICS_CHAR_UNSET
+    EXTERN	asm_im1_handler
+    EXTERN	asm_nmi_handler
+
+    PUBLIC    cleanup         ;jp'd to by exit()
+    PUBLIC    l_dcal          ;jp(hl)
 
 
-        ; We want to intercept rst38 to our interrupt routine
-        defc    TAR__crt_enable_rst = $8080
-        EXTERN  asm_im1_handler
-        defc    _z80_rst_38h = asm_im1_handler
-        IFNDEF CRT_ENABLE_NMI
-            defc        TAR__crt_enable_nmi = 1
-            EXTERN      asm_nmi_handler
-            defc        _z80_nmi = asm_nmi_handler
-        ENDIF
+    defc    TAR__clib_exit_stack_size = 32
+    defc    TAR__register_sp = 65280
+    defc    TAR__fputc_cons_generic = 1
+    defc    TAR__crt_on_exit = $10001     ;loop forever
+    defc	CRT_KEY_DEL = 12
+    defc	CRT_ORG_CODE = 0
+    defc    CONSOLE_COLUMNS = 40
+    defc    CONSOLE_ROWS = 25
+    defc	__CPU_CLOCK = 4000000
+    defc	CLIB_FGETC_CONS_DELAY = 150
 
-        INCLUDE "crt/classic/crt_rules.inc"
+    defc	GRAPHICS_CHAR_SET = 135
+    defc	GRAPHICS_CHAR_UNSET = 32
+    PUBLIC	GRAPHICS_CHAR_SET
+    PUBLIC	GRAPHICS_CHAR_UNSET
+
+
+    ; We want to intercept rst38 to our interrupt routine
+    defc    TAR__crt_enable_rst = $8080
+    EXTERN  asm_im1_handler
+    defc    _z80_rst_38h = asm_im1_handler
+    IFNDEF CRT_ENABLE_NMI
+        defc        TAR__crt_enable_nmi = 1
+        EXTERN      asm_nmi_handler
+        defc        _z80_nmi = asm_nmi_handler
+    ENDIF
+
+    INCLUDE "crt/classic/crt_rules.inc"
 
 	org	CRT_ORG_CODE	
 
@@ -81,20 +74,20 @@ program:
     call    _main
 cleanup:
     call    crt0_exit
-    jp      cleanup
+    INCLUDE "crt/classic/crt_terminate.inc"
 
 
 l_dcal: jp      (hl)            ;Used for function pointer calls
 
 
 
-	INCLUDE "crt/classic/crt_runtime_selection.inc" 
-	
-	INCLUDE	"crt/classic/crt_section.inc"
+    INCLUDE "crt/classic/crt_runtime_selection.inc" 
 
-	SECTION	bss_crt
-	PUBLIC	__pasopia_page
+    INCLUDE "crt/classic/crt_section.inc"
+
+    SECTION bss_crt
+    PUBLIC  __pasopia_page
 __pasopia_page:	defb	0
 
-	INCLUDE	"target/pasopia7/classic/bootstrap.asm"
+    INCLUDE "target/pasopia7/classic/bootstrap.asm"
 
