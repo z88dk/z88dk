f262826341b0f0203aeef83d47abc6942ff2fda0
dom <dom@suborbital.org.uk>
2024-03-08 22:16:57 +0000

    (fcntl/dummy) Reformat

--- libsrc/fcntl/dummy/rename.asm
+++ libsrc/fcntl/dummy/rename.asm
@@ -1,13 +1,13 @@
 ; Dummy file libs
 ;
 
-        SECTION code_clib
-        PUBLIC  rename
-        PUBLIC  _rename
-        PUBLIC  ___rename
+    SECTION code_clib
+    PUBLIC  rename
+    PUBLIC  _rename
+    PUBLIC  ___rename
 
 rename:
 _rename:
 ___rename:
-        ld      hl, -1                  ;error
-        ret
+    ld      hl, -1                      ;error
+    ret
