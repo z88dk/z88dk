1f6ef6ac42d173d77bbc3e153a326ae0275aa331
dom <dom@suborbital.org.uk>
2024-03-08 22:07:11 +0000

    (classic) Games reformat

--- libsrc/games/bit_beep.asm
+++ libsrc/games/bit_beep.asm
@@ -4,13 +4,13 @@
 ;
 ; void bit_beep(int duration, int period);
 ;
-        SECTION code_clib
-        PUBLIC  bit_beep
-        PUBLIC  _bit_beep
-        EXTERN  beeper
+    SECTION code_clib
+    PUBLIC  bit_beep
+    PUBLIC  _bit_beep
+    EXTERN  beeper
 
-        EXTERN  bit_open_di
-        EXTERN  bit_close_ei
+    EXTERN  bit_open_di
+    EXTERN  bit_close_ei
 
 ;
 ; Stub by Stefano Bodrato - 8/10/2001
@@ -19,13 +19,13 @@
 
 bit_beep:
 _bit_beep:
-        call    bit_open_di
-        pop     bc
-        pop     hl
-        pop     de
-        push    de
-        push    hl
-        push    bc
+    call    bit_open_di
+    pop     bc
+    pop     hl
+    pop     de
+    push    de
+    push    hl
+    push    bc
 
-        call    beeper
-        jp      bit_close_ei
+    call    beeper
+    jp      bit_close_ei
