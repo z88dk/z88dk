0a4368c420f2a91a10488566f1eaf6161032971f
Paulo Custodio <pauloscustodio@gmail.com>
2024-03-24 19:19:42 +0000

    Incomplete

---
+++ src/z80asm/z80asm2/t/test_files.cpp
@@ -0,0 +1,10 @@
+//-----------------------------------------------------------------------------
+// z80asm - reading of input files
+// Copyright (C) Paulo Custodio, 2011-2024
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#include "files.h"
+#include "t/test.h"
+using namespace std;
+
