d957d158e3f5fc1444140a605471599330df5cc1
Stefano <stefano_bodrato@hotmail.com>
2024-04-16 15:40:28 +0200

    Update ozrotatepointz.c

--- libsrc/lib3d/ozrotatepointz.c
+++ libsrc/lib3d/ozrotatepointz.c
@@ -3,13 +3,13 @@ lib3d.c
 
 Standard Wizard 3d and 4d math functions
 
-Copyright© 2002, Mark Hamilton
+CopyrightÂ© 2002, Mark Hamilton
 
 */
 
 #include <lib3d.h>
 
-void ozrotatepointz(Vector_t *v, int rot)
+void ozrotatepointz(vector_t *v, int rot)
 {
     static long x, y;
     x = v->x;
