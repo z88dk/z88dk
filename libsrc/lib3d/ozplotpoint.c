8b355501799e0bde4b993da5436bd6d567ac98eb
Stefano <stefano_bodrato@hotmail.com>
2024-04-16 15:41:46 +0200

    Update ozplotpoint.c

--- libsrc/lib3d/ozplotpoint.c
+++ libsrc/lib3d/ozplotpoint.c
@@ -3,17 +3,17 @@ lib3d.c
 
 Standard Wizard 3d and 4d math functions
 
-Copyright© 2002, Mark Hamilton
+CopyrightÂ© 2002, Mark Hamilton
 
 */
 
 #include <lib3d.h>
 
-void ozplotpoint(Vector_t *v, Point_t *p)
+void ozplotpoint(vector_t *v, Point_t *p)
 {
-    Vector_t temp;
+    vector_t temp;
 
-    /* flip x and y to rotate points 90° */
+    /* flip x and y to rotate points 90Â° */
     temp.x = v->y;
     temp.y = v->x;
     temp.z = v->z + 256; /* add a large number so it doesn't look too big */
