e466e406d5a5c5297d31232bb4da3561684329ce
suborb <dom@suborbital.org.uk>
2024-05-06 21:40:51 +0100

    (far) Some more fixes and tests
    Mostly around array access and picking up near via far if the pointer
    was picked up far

--- src/sccz80/expr.c
+++ src/sccz80/expr.c
@@ -472,20 +472,19 @@ SYMBOL *deref(LVALUE* lval, char isaddr)
 {
     Type *old_type = lval->ltype;
 
-
     lval->symbol = NULL;
     if ( ispointer(lval->ltype) && lval->ltype->ptr->kind == KIND_FUNC ) {
         return lval->symbol;
     }
 
     lval->ltype = lval->ltype->ptr;
-    if ( lval->ltype->kind != KIND_PTR && lval->ltype->kind != KIND_CPTR )
+    if ( !ispointer(lval->ltype) ) 
         lval->ptr_type = KIND_NONE;
     else
         lval->ptr_type = lval->ltype->ptr->kind;
     lval->val_type = lval->indirect_kind = lval->ltype->kind;
 
-    if ( old_type->kind == KIND_CPTR ) {
+    if ( old_type->kind == KIND_CPTR || (lval->flags & FARACC)) {
         lval->flags |= FARACC;
     } else {
         lval->flags &= ~FARACC;
@@ -627,8 +626,8 @@ int heirb(LVALUE* lval)
     if (ch() == '[' || ch() == '(' || ch() == '.' || (ch() == '-' && nch() == '>'))
         while (1) {
             if (cmatch('[')) {
+                int savesp;
                 Type *type;
-
                 if (k && ispointer(lval->ltype)) {
                     rvalue(lval);
                 } else if ( !ispointer(lval->ltype) && lval->ltype->kind != KIND_ARRAY) {
@@ -638,6 +637,7 @@ int heirb(LVALUE* lval)
                     return 0;
                 }
                 setstage(&before, &start);
+                savesp = Zsp;
                 if (lval->ltype->kind == KIND_CPTR)
                     lpush();
                 else
@@ -648,9 +648,7 @@ int heirb(LVALUE* lval)
                 val = dval;
                 needchar(']');
                 if (con) {
-                    Zsp += 2; /* undo push */
-                    if (lval->ltype->kind == KIND_CPTR)
-                        Zsp += 2;
+                    Zsp = savesp;
                     if ( val > lval->ltype->len && lval->ltype->len != -1 && lval->ltype->kind == KIND_ARRAY) {
                         warningfmt("unknown","Access of array at index %d is greater than size %d", val, lval->ltype->len);
                     }
@@ -659,7 +657,7 @@ int heirb(LVALUE* lval)
                     }
                     cscale(lval->ltype, &val);
                     val += lval->offset;
-                    if (ptr && ptr->storage == STKLOC && lval->ltype->kind == KIND_ARRAY && ptr->ctype->kind != KIND_PTR) {
+                    if (ptr && ptr->storage == STKLOC && lval->ltype->kind == KIND_ARRAY &&  (ptr->ctype->kind != KIND_PTR && ptr->ctype->kind != KIND_CPTR)) {
                         /* constant offset to array on stack */
                         /* do all offsets at compile time */
                         clearstage(before1, 0);
@@ -675,7 +673,10 @@ int heirb(LVALUE* lval)
                         clearstage(before, 0);
                         //        if (lval->symbol->more)
                         //                cscale(lval->val_type,tagtab+ptr->tag_idx,&val);
+
+                        lval->val_type = ((lval->flags & FARACC) || lval->val_type == KIND_CPTR) ? KIND_CPTR : KIND_PTR;
                         zadd_const(lval, val  - lval->offset);
+                     //   lval->val_type = stype;
                         lval->offset = 0;
                     }
                 } else {
@@ -700,7 +701,7 @@ int heirb(LVALUE* lval)
                     }
                     /* If near, then pop other side back, otherwise
                        load high reg with de and do an add  */
-                    if (lval->ltype->kind == KIND_CPTR) {
+                    if (lval->ltype->kind == KIND_CPTR || (lval->flags & FARACC) ) {
                         const2(0);
                     } else {
                         zpop();
@@ -753,7 +754,7 @@ int heirb(LVALUE* lval)
                 lval->val_type = lval->ltype->kind;
                 lval->symbol = NULL;
                 // Function returing pointer
-                if ( lval->ltype->kind == KIND_PTR || lval->ltype->kind == KIND_CPTR ) {
+                if ( ispointer(lval->ltype) ) {
                     lval->val_type = lval->ltype->kind;
                     lval->indirect_kind = lval->ltype->kind;
                 }
@@ -814,13 +815,19 @@ int heirb(LVALUE* lval)
                  * Here, we're trying to chase our member up, we have to be careful
                  * not to access via far methods near data..
                  */
-                if (k && direct == 0)
+                if (k && direct == 0) {
                     rvalue(lval);
+                }
 
                 debug(DBG_FAR1, "prev=%s name=%s flags %d oflags %d", lval->symbol->name, ptr->name, lval->flags, lval->oflags);
                 flags = member_type->flags;
 
-                if ( (lval->flags & FARACC) || lval->ltype->kind == KIND_CPTR ) {
+                // We need to acccess via FARACC if:
+                // 1. We're accessing via a cptr
+                // 2. Our parent was FARACC
+                if ( lval->ltype->kind == KIND_CPTR ) {
+                    flags |= FARACC;
+                } else if ( (lval->flags & FARACC) && (member_type->kind == KIND_CPTR || lval->ltype    ->kind == KIND_STRUCT) ) {
                     flags |= FARACC;
                 }
                 lval->flags = flags;
--- testsuite/misc_far.c
+++ testsuite/misc_far.c
@@ -4,16 +4,53 @@ typedef union {
   long b;
 } un_t;
 
+struct x {
+        long c;
+};
+
 typedef struct {
   char c;
   un_t pt;
+  char arr[10];
+  struct x *extra;
 } st_t;
 
 void callit();
 
 void func(st_t *__far far_p) {
   callit(far_p);
+}
+
+void func0(st_t *__far far_p) {
   callit(&(far_p->c));
+}
+void func1(st_t *__far far_p) {
   callit(&(far_p->pt));
+}
+void func2(st_t *__far far_p) {
   callit(&(far_p->pt.b));
+}
+void func2a(st_t *__far far_p) {
+  callit((far_p->pt.a));
+}
+void func2b(st_t *__far far_p) {
+  callit((far_p->pt.b));
+}
+
+void func3(st_t *__far far_p) {
+  far_p->arr[2] = 10;
+}
+void func4(st_t *__far far_p) {
+  callit(&far_p->arr[5]);
+}
+
+
+void func5(st_t *__far far_p) {
+  // This is incorrect, c is accessed via far
+  callit(&far_p->extra->c);
+}
+
+void func5a(st_t *__far far_p) {
+  // This is incorrect, c is accessed via far
+  callit(far_p->extra->c);
 }
