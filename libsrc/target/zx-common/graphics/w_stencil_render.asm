eb9937ba86432ab1a677b1feedf3ac7d0d6e281d
Stefano <stefano_bodrato@hotmail.com>
2024-06-03 16:52:03 +0200

    Update w_stencil_render.asm

--- libsrc/target/zx-common/graphics/w_stencil_render.asm
+++ libsrc/target/zx-common/graphics/w_stencil_render.asm
@@ -27,7 +27,10 @@
     ; but it is assembled as part of zx-common
     EXTERN  SCREEN_BASE
   ENDIF
-
+  IF    FORts2068|zxn
+    EXTERN  hl_inc_x_MODE6
+  ENDIF
+  
 ;
 ;    $Id: w_stencil_render.asm,v 1.6 2016-07-14 17:44:17 pauloscustodio Exp $
 ;
@@ -185,7 +188,7 @@ nextcol:
 	and     7
   IF    FORts2068|zxn
     cp      6
-    jp      z, nextcol_hires            ;No colour in hires
+    jp      z, hl_inc_x_MODE6            ;No colour in hires
   ENDIF
     cp      2                           ;High colour
     jr      nz, standard_modes
@@ -221,14 +224,3 @@ standard_modes:
     pop     hl
     inc     hl
     ret
-
-  IF    FORts2068|zxn
-nextcol_hires:
-    ld      a, h
-    xor     @00100000
-    cp      h
-    ld      h, a
-    ret     nc
-    inc     hl
-    ret
-  ENDIF
