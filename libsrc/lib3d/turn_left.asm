37c1c575812deca14755ff8ca2ad889b927bcc1d
Phillip Stevens <phillip.stevens@gmail.com>
2024-06-07 12:36:14 +1000

    lib3d - turtle asm machine types

--- libsrc/lib3d/turn_left.asm
+++ libsrc/lib3d/turn_left.asm
@@ -7,15 +7,30 @@
     PUBLIC  turn_left
     PUBLIC  _turn_left
 
+    EXTERN  l_sub
+
     EXTERN  __direction
 
 .turn_left
 ._turn_left
     ; __FASTCALL
-    ld      de,(__direction)
+
+  IF __CPU_8085__
+    ld      bc,hl
+    ld      hl,(__direction)
+    sub     hl,bc
+  ELIF __CPU_8080__ || __CPU_GBZ80__
     ex      de,hl
+    ld      hl,(__direction)
+    ex      de,hl
+    call    l_sub
+  ELSE
+    ex      de,hl
+    ld      hl,(__direction)
     and     a
     sbc     hl,de
+  ENDIF
+
     ld      (__direction),hl
     ret     nc
 
--- libsrc/lib3d/turn_right.asm
+++ libsrc/lib3d/turn_right.asm
@@ -7,18 +7,32 @@
     PUBLIC  turn_right
     PUBLIC  _turn_right
 
+    EXTERN  l_sub
+
     EXTERN  __direction
 
 .turn_right
 ._turn_right
     ; __FASTCALL
-    ld      de,(__direction)
+
     ex      de,hl
+    ld      hl,(__direction)
     add     hl,de
     ld      (__direction),hl
-    and     a
+
+  IF __CPU_8085__
+    ld      bc,360
+    sub     hl,bc
+  ELIF __CPU_8080__ || __CPU_GBZ80__
     ld      de,360
+    ex      de,hl
+    call    l_sub
+  ELSE
+    ld      de,360
+    and     a
     sbc     hl,de
+  ENDIF
+
     ret     c
 
     ld      (__direction),hl
