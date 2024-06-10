f1ba112ad5d84601ce6e0b68d6e1445171854b03
Stefano <stefano_bodrato@hotmail.com>
2024-06-10 10:53:32 +0200

    Update __generic_w_incx.asm

--- libsrc/gfx/wide/__generic_w_incx.asm
+++ libsrc/gfx/wide/__generic_w_incx.asm
@@ -16,11 +16,11 @@ __generic_w_incx:
 
 IF  !__CPU_INTEL__&!__CPU_GBZ80__
     ;push    de
-    ld      hl,(curx)
+    ld      hl,(__generic_w_curx)
     ld      de,8
     add     hl,de
-    ld      (curx),hl
-    ld      de,(cury)
+    ld      (__generic_w_curx),hl
+    ld      de,(__generic_w_cury)
     call	w_pixeladdress		; bitpos0 = pixeladdress(x,y)
     ld      h,d
     ld      l,e
