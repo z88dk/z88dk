10dfba13689872ee0587b5fb89b7f70951786c06
suborb <dom@suborbital.org.uk>
2024-06-27 22:47:00 +0100

    (gal) Default back to screen mode 0

--- lib/target/gal/classic/gal_crt0.asm
+++ lib/target/gal/classic/gal_crt0.asm
@@ -33,12 +33,17 @@ IF !DEFINED_CONSOLE_ROWS
     defc    CONSOLE_ROWS = 16
 ENDIF
 
+IF !DEFINED_CLIB_DEFAULT_SCREEN_MODE 
+    defc CLIB_DEFAULT_SCREEN_MODE = 0
+ENDIF
+
     defc    TAR__no_ansifont = 1
     defc    TAR__clib_exit_stack_size = 32
     defc    TAR__register_sp = -1
     defc    TAR__crt_interrupt_mode_exit = 1
     defc    __CPU_CLOCK = 3072000
     INCLUDE	"crt/classic/crt_rules.inc"
+
 
     org     CRT_ORG_CODE
 
