76cf93dbf73d82d06ec0bc516061de18f50752c0
suborb <dom@suborbital.org.uk>
2024-07-19 21:03:01 +0100

    Issue #2586: Better __z88dk_callee handling on 8080/8085/gbz80?

---
+++ testsuite/8080/Issue_2586_8080_callee.c
@@ -0,0 +1,36 @@
+int size10_i(int a, int b, int c, int d, int e) __z88dk_callee
+{
+  return 0;
+}
+
+long size10_l(int a, int b, int c, int d, int e) __z88dk_callee
+{
+  return 0;
+}
+
+double size10_d(int a, int b, int c, int d, int e) __z88dk_callee
+{
+  return 0;
+}
+
+int size12_i(int a, int b, int c, int d, long e) __z88dk_callee
+{
+  return 0;
+}
+
+long size12_l(int a, int b, int c, int d, long e) __z88dk_callee
+{
+  return 0;
+}
+
+double size12_d(int a, int b, int c, int d, long e) __z88dk_callee
+{
+  return 0;
+}
+
+long size26_l(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m) __z88dk_callee
+{
+  return 0;
+}
+
+
