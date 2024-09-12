4f6f0542c910312aedd56a086ffe7c09d74ac277
Stefano <stefano_bodrato@hotmail.com>
2024-09-12 16:40:15 +0200

    Update curses.h

--- include/curses.h
+++ include/curses.h
@@ -62,13 +62,14 @@ extern void    __LIB__     curs_hline_callee(int ch, int n) __smallc __z88dk_cal
 #define ACS_BLCORNER 'o'
 #define ACS_BRCORNER 'o'
 
-unsigned int curs_w, curs_h;
+// TODO: Those 2 variables should become "extern" and get somehow initialized.
+unsigned int COLS, LINES;
 
 #ifdef border
 #undef border
 #endif
 
-#define border(ls,rs,ts,bs,tl,tr,bl,br) screensize(&curs_w,&curs_h);gotoxy(0,0);fputc_cons(tl?tl:ACS_ULCORNER);hline(ts?ts:ACS_HLINE,curs_w-2);fputc_cons(tr?tr:ACS_URCORNER);vline(ls?ls:ACS_VLINE,curs_h-2);gotoxy(0,curs_h-1);fputc_cons(bl?bl:ACS_BLCORNER);hline(bs?bs:ACS_HLINE,curs_w-2);fputc_cons(bl?bl:ACS_BRCORNER);gotoxy(curs_w-1,1);vline(rs?rs:ACS_VLINE,curs_h-2);gotoxy(1,1)
+#define border(ls,rs,ts,bs,tl,tr,bl,br) screensize(&COLS,&LINES);gotoxy(0,0);fputc_cons(tl?tl:ACS_ULCORNER);hline(ts?ts:ACS_HLINE,COLS-2);fputc_cons(tr?tr:ACS_URCORNER);vline(ls?ls:ACS_VLINE,LINES-2);gotoxy(0,LINES-1);fputc_cons(bl?bl:ACS_BLCORNER);hline(bs?bs:ACS_HLINE,COLS-2);fputc_cons(bl?bl:ACS_BRCORNER);gotoxy(COLS-1,1);vline(rs?rs:ACS_VLINE,LINES-2);gotoxy(1,1)
 
 // --------------------------------------------------------------------------------------------------------
 
