8267462ab31c284aeca81b3f5ca49f7d5e769f8a
Craig Hackney <iratahack@digitalxfer.com>
2024-09-30 20:00:51 -0700

    Skip 1 out of 6 interrupts for Aquarius.

--- examples/sound/arkos/arkos_interrupt.c
+++ examples/sound/arkos/arkos_interrupt.c
@@ -6,9 +6,9 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <interrupt.h>
 #include <intrinsic.h>
 #include <stdint.h>
-#include <interrupt.h>
 
 #ifdef __SPECTRUM__
 #include <spectrum.h>
@@ -24,39 +24,45 @@
 
 extern uint8_t song[];
 
-void wrapper() __naked
-{
+void wrapper() __naked {
 __asm
-   INCLUDE "hocuspocus.asm"
+  INCLUDE "hocuspocus.asm"
+  SECTION code_compiler
 __endasm;
 }
 
-void service_interrupt( void )
-{
-    M_PRESERVE_ALL;
+void service_interrupt(void) {
+  M_PRESERVE_ALL;
+#ifdef __AQUARIUS__
+  static int NTSCCount = 6;
+  if (--NTSCCount)
     ply_akg_play();
-    M_RESTORE_ALL;
+  else
+    NTSCCount = 6;
+#else
+  ply_akg_play();
+#endif
+  M_RESTORE_ALL;
 }
 
-void init_interrupts( void ) {
-    intrinsic_di();
+void init_interrupts(void) {
+  intrinsic_di();
 #if __SPECTRUM__
-    zx_im2_init(0xd300, 0xd4);
-   add_raster_int(0x38);
+  zx_im2_init(0xd300, 0xd4);
+  add_raster_int(0x38);
 #endif
 #ifndef NO_INTERRUPT_INIT
-   im1_init();
+  im1_init();
 #endif
-    add_raster_int( service_interrupt );
-    intrinsic_ei();
+  add_raster_int(service_interrupt);
+  intrinsic_ei();
 }
 
-
-void main( void ) {
-    ply_akg_init( song, 0 );
-    init_interrupts();
-    while ( 1 ) {
-        // do whatever in your main loop
-        // music playback should happen in interrupt context
-    }
+void main(void) {
+  ply_akg_init(song, 0);
+  init_interrupts();
+  while (1) {
+    // do whatever in your main loop
+    // music playback should happen in interrupt context
+  }
 }
