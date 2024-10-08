2f8e8d542d866b083e80f623bc88bfaa4dfe6e87
Paulo Custodio <pauloscustodio@gmail.com>
2024-10-08 19:04:25 +0100

    Add 8080_strict and 8080 zilog mode

--- src/z80asm/dev/cpu2/make_opcodes.py
+++ src/z80asm/dev/cpu2/make_opcodes.py
@@ -1,144 +1,199 @@
 #!/usr/bin/env python3
 
-# build data structure of all opcodes the assembler will parse
+#------------------------------------------------------------------------------
+# z80asm Z80 macro assembler
+#
+# Copyright (C) Paulo Custodio, 2011-2024
+# License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+#------------------------------------------------------------------------------
+
+# Build data structure of all opcodes the assembler will parse
 
 import json
 import re
+import sys
 
-opcodes = {}
+table = {}
 
-def add(cpu, instr, ops):
-    global opcodes
-    if instr in opcodes and cpu in opcodes[instr]:
+def add1(cpu, instr, opcodes, synth):
+    global table
+    
+    if instr in table and cpu in table[instr]:
         raise ValueError(f"duplicate instruction {cpu}: {instr}")
-    if not instr in opcodes:
-        opcodes[instr] = {}
-    opcodes[instr][cpu] = ops
+    if not instr in table:
+        table[instr] = {}
+    table[instr][cpu] = {'synth':synth, 'opcodes':opcodes}
 
-def build_8080(cpu):
+def add(cpu, instr, opcode):
+    add1(cpu, instr, [opcode], False)
+
+def add_synth(cpu, instr, sub_instrs):
+    opcodes = []
+    for sub_instr in sub_instrs:
+        info = table[sub_instr][cpu]
+        for sub_opcodes in info['opcodes']:
+            opcodes.append(sub_opcodes)
+    add1(cpu, instr, opcodes, True)
+
+def build_8080_strict(cpu, **kwargs):
     reg8 = {'b':0, 'c':1, 'd':2, 'e':3, 'h':4, 'l':5, 'a':7}
     reg8m = {'b':0, 'c':1, 'd':2, 'e':3, 'h':4, 'l':5, 'm':6, 'a':7}
     reg16bdhsp = {'b':0, 'd':1, 'h':2, 'sp':3}
     reg16bd = {'b':0, 'd':1}
     reg16bdhpsw = {'b':0, 'd':1, 'h':2, 'psw':3}
-    flags={'nz':0, 'z':1, 'nc':2, 'c':3, 'po':4, 'pe':5, 'p':6, 'm':7}
+    if 'zilog' in kwargs:   # cp=compare, jp=jump
+        flags={'nz':0, 'z':1, 'nc':2, 'c':3, 'po':4, 'pe':5, 'm':7}
+    else:                   # cp=call p, jp=jmp p
+        flags={'nz':0, 'z':1, 'nc':2, 'c':3, 'po':4, 'pe':5, 'p':6, 'm':7}
     
     # move, load and store
     for dname, di in reg8.items():
         for sname, si in reg8.items():
-            add(cpu, f"mov {dname}, {sname}", [[0x40+di*8+si]])
+            add(cpu, f"mov {dname}, {sname}", [0x40+di*8+si])
 
     for rname, ri in reg8.items():
-        add(cpu, f"mov m, {rname}", [[0x70+ri]])
-        add(cpu, f"mov {rname}, m", [[0x46+8*ri]])
-        add(cpu, f"mvi {rname}", [[0x06+8*ri]])
-
-    add(cpu, f"mvi m", [[0x36]])
+        add(cpu, f"mov m, {rname}", [0x70+ri])
+        add(cpu, f"mov {rname}, m", [0x46+8*ri])
+        
+    for rname, ri in reg8m.items():
+        add(cpu, f"mvi {rname}, %n", [0x06+8*ri, '%n'])
 
     for rname, ri in reg16bdhsp.items():
-        add(cpu, f"lxi {rname}, %m", [[0x01+16*ri, '%m', '%m']])
+        add(cpu, f"lxi {rname}, %m", [0x01+16*ri, '%m', '%m'])
 
     for rname, ri in reg16bd.items():
-        add(cpu, f"stax {rname}", [[0x02+16*ri]])
-        add(cpu, f"ldax {rname}", [[0x0a+16*ri]])
+        add(cpu, f"stax {rname}", [0x02+16*ri])
+        add(cpu, f"ldax {rname}", [0x0a+16*ri])
 
-    add(cpu, f"sta %m", [[0x32, '%m', '%m']])
-    add(cpu, f"lda %m", [[0x3a, '%m', '%m']])
-    add(cpu, f"shld %m", [[0x22, '%m', '%m']])
-    add(cpu, f"lhld %m", [[0x2a, '%m', '%m']])
-    add(cpu, f"xchg", [[0xeb]])
+    add(cpu, f"sta %m", [0x32, '%m', '%m'])
+    add(cpu, f"lda %m", [0x3a, '%m', '%m'])
+    add(cpu, f"shld %m", [0x22, '%m', '%m'])
+    add(cpu, f"lhld %m", [0x2a, '%m', '%m'])
+    add(cpu, f"xchg", [0xeb])
 
     # stack ops
     for rname, ri in reg16bdhpsw.items():
-        add(cpu, f"push {rname}", [[0xe5+16*ri]])
-        add(cpu, f"pop {rname}", [[0xe1+16*ri]])
+        add(cpu, f"push {rname}", [0xc5+16*ri])
+        add(cpu, f"pop {rname}", [0xc1+16*ri])
         
-    add(cpu, f"xthl", [[0xe3]])
-    add(cpu, f"sphl", [[0xf9]])
+    add(cpu, f"xthl", [0xe3])
+    add(cpu, f"sphl", [0xf9])
     
     # jump
-    add(cpu, f"jmp %m", [[0x3c, '%m', '%m']])
+    add(cpu, f"jmp %m", [0x3c, '%m', '%m'])
     for fname, fi in flags.items():
-        add(cpu, f"j{fname} %m", [[0xc2+8*fi, '%m', '%m']])
-    add(cpu, f"pchl", [[0xe9]])
+        add(cpu, f"j{fname} %m", [0xc2+8*fi, '%m', '%m'])
+    add(cpu, f"pchl", [0xe9])
     
     # call
-    add(cpu, f"call %m", [[0xcd, '%m', '%m']])
+    add(cpu, f"call %m", [0xcd, '%m', '%m'])
     for fname, fi in flags.items():
-        add(cpu, f"c{fname} %m", [[0xc4+8*fi, '%m', '%m']])
+        add(cpu, f"c{fname} %m", [0xc4+8*fi, '%m', '%m'])
     
     # return
-    add(cpu, f"ret", [[0xc9]])
+    add(cpu, f"ret", [0xc9])
     for fname, fi in flags.items():
-        add(cpu, f"r{fname}", [[0xc0+8*fi]])
+        add(cpu, f"r{fname}", [0xc0+8*fi])
     
     # restart
-    add(cpu, f"rst %c", [[str(0xe7)+'+%c']])
+    for addr in range(0, 0x40, 8):
+        add(cpu, f"rst {addr:02x}h", [0xc7+addr])
     
     # increment and decrement
     for rname, ri in reg8m.items():
-        add(cpu, f"inr {rname}", [[0x04+8*ri]])
-        add(cpu, f"dcr {rname}", [[0x05+8*ri]])
+        add(cpu, f"inr {rname}", [0x04+8*ri])
+        add(cpu, f"dcr {rname}", [0x05+8*ri])
 
     for rname, ri in reg16bdhsp.items():
-        add(cpu, f"inx {rname}", [[0x03+16*ri]])
-        add(cpu, f"dcx {rname}", [[0x0b+16*ri]])
+        add(cpu, f"inx {rname}", [0x03+16*ri])
+        add(cpu, f"dcx {rname}", [0x0b+16*ri])
     
     # add
     for rname, ri in reg8m.items():
-        add(cpu, f"add {rname}", [[0x80+ri]])
-        add(cpu, f"adc {rname}", [[0x81+ri]])
+        add(cpu, f"add {rname}", [0x80+ri])
+        add(cpu, f"adc {rname}", [0x81+ri])
 
-    add(cpu, f"adi %n", [[0xc6, '%n']])
-    add(cpu, f"aci %n", [[0xce, '%n']])
+    add(cpu, f"adi %n", [0xc6, '%n'])
+    add(cpu, f"aci %n", [0xce, '%n'])
 
     for rname, ri in reg16bdhsp.items():
-        add(cpu, f"dad {rname}", [[0x09+16*ri]])
+        add(cpu, f"dad {rname}", [0x09+16*ri])
 
     # subtract
     for rname, ri in reg8m.items():
-        add(cpu, f"sub {rname}", [[0x90+ri]])
-        add(cpu, f"sbb {rname}", [[0x91+ri]])
+        add(cpu, f"sub {rname}", [0x90+ri])
+        add(cpu, f"sbb {rname}", [0x91+ri])
 
-    add(cpu, f"sui %n", [[0xd6, '%n']])
-    add(cpu, f"sbi %n", [[0xde, '%n']])
+    add(cpu, f"sui %n", [0xd6, '%n'])
+    add(cpu, f"sbi %n", [0xde, '%n'])
 
     # logical
     for rname, ri in reg8m.items():
-        add(cpu, f"ana {rname}", [[0xa0+ri]])
-        add(cpu, f"xra {rname}", [[0xa8+ri]])
-        add(cpu, f"ora {rname}", [[0xb0+ri]])
-        add(cpu, f"cmp {rname}", [[0xb8+ri]])
+        add(cpu, f"ana {rname}", [0xa0+ri])
+        add(cpu, f"xra {rname}", [0xa8+ri])
+        add(cpu, f"ora {rname}", [0xb0+ri])
+        add(cpu, f"cmp {rname}", [0xb8+ri])
     
-    add(cpu, f"ani %n", [[0xe6, '%n']])
-    add(cpu, f"xri %n", [[0xee, '%n']])
-    add(cpu, f"ori %n", [[0xf6, '%n']])
-    add(cpu, f"cpi %n", [[0xfe, '%n']])
+    add(cpu, f"ani %n", [0xe6, '%n'])
+    add(cpu, f"xri %n", [0xee, '%n'])
+    add(cpu, f"ori %n", [0xf6, '%n'])
+    add(cpu, f"cpi %n", [0xfe, '%n'])
 
     # rotate
-    add(cpu, f"rlc", [[0x07]])
-    add(cpu, f"rrc", [[0x0f]])
-    add(cpu, f"ral", [[0x17]])
-    add(cpu, f"rar", [[0x1f]])
+    add(cpu, f"rlc", [0x07])
+    add(cpu, f"rrc", [0x0f])
+    add(cpu, f"ral", [0x17])
+    add(cpu, f"rar", [0x1f])
     
     # specials
-    add(cpu, f"daa", [[0x27]])
-    add(cpu, f"cma", [[0x2f]])
-    add(cpu, f"stc", [[0x37]])
-    add(cpu, f"cmc", [[0x3f]])
+    add(cpu, f"daa", [0x27])
+    add(cpu, f"cma", [0x2f])
+    add(cpu, f"stc", [0x37])
+    add(cpu, f"cmc", [0x3f])
     
     # input/output
-    add(cpu, f"in %n", [[0xdb, '%n']])
-    add(cpu, f"out %n", [[0xd3, '%n']])
+    add(cpu, f"in %n", [0xdb, '%n'])
+    add(cpu, f"out %n", [0xd3, '%n'])
     
     # control
-    add(cpu, f"ei", [[0xfb]])
-    add(cpu, f"di", [[0xf3]])
-    add(cpu, f"nop", [[0x00]])
-    add(cpu, f"hlt", [[0x76]])
+    add(cpu, f"ei", [0xfb])
+    add(cpu, f"di", [0xf3])
+    add(cpu, f"nop", [0x00])
+    add(cpu, f"hlt", [0x76])
 
+def build_8080_zilog(cpu):
+    build_8080_strict(cpu, zilog=True)
+    
+    # synthetic opcodes
+    
+    # restart
+    for i in range(1, 8):
+        addr = 8*i
+        add_synth(cpu, f"rst {i}", [f"rst {addr:02x}h"])
+    
+    # register pair names
+    for rp in ['bc', 'de', 'hl']:
+        for op in ['inx', 'dcx', 'dad', 'push', 'pop']:
+            add_synth(cpu, f"{op} {rp}", [op+" "+rp[0]]);
+        add_synth(cpu, f"lxi {rp}, %m", ["lxi "+rp[0]+", %m"])
+    for rp in ['bc', 'de']:
+        for op in ['stax', 'ldax']:
+            add_synth(cpu, f"{op} {rp}", [op+" "+rp[0]]);
 
-build_8080("8080")
+    # 16-bit mov
+    for src in ['bc', 'de', 'hl']:
+        for dst in ['bc', 'de', 'hl']:
+            if src != dst:
+                add_synth(cpu, f"mov {dst}, {src}", 
+                               ["mov "+dst[0]+", "+src[0],
+                                "mov "+dst[1]+", "+src[1]]);
 
-f = open("opcodes.json", "w")
-f.write(json.dumps(opcodes, indent=4, sort_keys=True))
+build_8080_strict("8080_strict")
+build_8080_zilog("8080")
+
+if len(sys.argv) != 2:
+    raise ValueError(f"Usage: make_opcodes.py output.json")
+output = sys.argv[1]
+
+f = open(output, "w")
+f.write(json.dumps(table, indent=4, sort_keys=True))
---
+++ src/z80asm/dev/cpu2/make_table.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+#------------------------------------------------------------------------------
+# z80asm Z80 macro assembler
+#
+# Copyright (C) Paulo Custodio, 2011-2024
+# License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+#------------------------------------------------------------------------------
+
+# Build table of all opcodes
+
+import json
+import re
+import sys
+
+table = {}
+
+ASSEMBLY_SIZE = 28-1
+CPU_SIZE = 12-1
+OPCODES_SIZE = 60-1
+
+def align(size, text):
+    if len(text) > size:
+        raise ValueError(f"{text} length > {size}")
+    return text + " "*(size - len(text))
+
+def write_row(f, assembly, cpu, opcodes):
+    f.write("|" + align(ASSEMBLY_SIZE, assembly)
+          + "|" + align(CPU_SIZE, cpu)
+          + "|" + align(OPCODES_SIZE, opcodes) 
+          + "|\n")
+
+def format_opcodes(opcodes):
+    output = "* " if opcodes['synth'] else "  "
+    code = []
+    for bytes in opcodes['opcodes']:
+        hex_dump = []
+        for byte in bytes:
+            if re.match(r'^\d+$', str(byte)):
+                hex_dump.append(f"{byte:02X}")
+            else:
+                if len(byte) != 2:
+                    raise ValueError(f"unexpected byte {byte}")
+                hex_dump.append(byte)
+        code.append(" ".join(hex_dump))
+    output += ",".join(code)
+    return output
+
+def write_table(f):
+    global table
+    
+    write_row(f, "="*ASSEMBLY_SIZE, "="*CPU_SIZE, "="*OPCODES_SIZE)
+    write_row(f, "Assembly", "CPU", "S Opcodes")
+    write_row(f, "="*ASSEMBLY_SIZE, "="*CPU_SIZE, "="*OPCODES_SIZE)
+    
+    for opcode in sorted(table):
+        for cpu in sorted(table[opcode]):
+            write_row(f, opcode, cpu, format_opcodes(table[opcode][cpu]))
+
+    write_row(f, "="*ASSEMBLY_SIZE, "="*CPU_SIZE, "="*OPCODES_SIZE)
+
+if len(sys.argv) != 3:
+    raise ValueError(f"Usage: make_table.py input.json output.txt")
+input = sys.argv[1]
+output = sys.argv[2]
+
+with open(input, 'r') as f:
+    table = json.load(f)
+with open(output, 'w') as f:
+    write_table(f)
