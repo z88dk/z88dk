85b904634a449ac0973d03c840755e219e08d361
suborb <dom@suborbital.org.uk>
2024-12-12 19:17:15 +0000

    (eg2000) Allow palette mode switching

---
+++ include/arch/trs80/eg2000.h
@@ -0,0 +1,10 @@
+
+#ifndef ARCH_TRS80_EG2000_H
+#define ARCH_TRS80_EG2000_H
+
+#define EG2000_PALETTE_MODE_NORTHERN 0
+#define EG2000_PALETTE_MODE_SOUTHERN 1
+
+extern void __LIB__ eg2000_switch_palette_mode(int mode);
+
+#endif
