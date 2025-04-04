3bdb983565204083fbc475b3f1271a42fb8c5a88
suborb <dom@suborbital.org.uk>
2025-04-04 21:12:45 +0100

    Fix #2692: Make gets strip the line feed

--- libsrc/stdio/gets.c
+++ libsrc/stdio/gets.c
@@ -10,11 +10,21 @@
 #define ANSI_STDIO
 
 #include <stdio.h>
+#include <string.h>
 
 char *gets(char *s) 
 {
+    char *ret;
     if (fchkstd(stdin))
-        return(fgets_cons(s,255));
+        ret = fgets_cons(s,255);
     else
-        return(fgets(s,255,stdin));
+        ret = fgets(s,255,stdin);
+    if ( ret ) {
+        char *end = strchr(ret, '\n');
+  
+        if (end != NULL ) {
+	    *end = 0;
+        }
+    }
+    return ret;
 }
