8b9363533a4da112d3183adb33be063ee2e8d23d
Paulo Custodio <pauloscustodio@gmail.com>
2025-06-10 19:59:23 +0100

    z80asm2: do expression operator evaluation in the Operator module

--- src/z80asm/z80asm2/error.h
+++ src/z80asm/z80asm2/error.h
@@ -17,14 +17,13 @@ public:
     void error_division_by_zero() { error("division by zero"); }
     void error_duplicate_definition(const string& name) { error("duplicate definition", name); }
     void error_expected_eol() { error("expected end of line"); }
-    void error_extra_operands() { error("extra operands"); }
-    void error_insuficient_operands() { error("insufficient operands"); }
+    void error_extra_operands(const string& op) { error("extra operands", op); }
+    void error_insufficient_operands(const string& op) { error("insufficient operands", op); }
     void error_invalid_char(char c) { error("invalid character", std::to_string(static_cast<int>(c))); }
     void error_invalid_escape_char(char c) { error("invalid escape character", string(1, c)); }
     void error_invalid_quoted_char() { error("invalid quoted character"); }
     void error_mismatched_ternary() { error("mismatched ternary operator"); }
     void error_open_file(const string& filename) { error("file open", filename); }
-    void error_operand_expected() { error("operand expected"); }
     void error_recursive_evaluation(const string& name) { error("recursive evaluation", name); }
     void error_recursive_include(const string& filename) { error("recursive include", filename); }
     void error_syntax() { error("syntax error"); }
