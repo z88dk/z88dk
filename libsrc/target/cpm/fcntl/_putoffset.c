c3486b0d902a08dfdf9e051b75a126a44c0256fa
Phillip Stevens <phillip.stevens@gmail.com>
2025-06-12 21:03:36 +1000

    cpm fcntl - 8080 gbz80 (avoid slow ldi)

--- libsrc/target/cpm/fcntl/_putoffset.c
+++ libsrc/target/cpm/fcntl/_putoffset.c
@@ -13,14 +13,16 @@
 
 void _putoffset(unsigned char *where,long offset)
 {
-    where[0] = offset & 0xFF;
-    where[1] = ((unsigned long)offset >> 8 ) & 0xFF;
-    where[2] = ((unsigned long)offset >> 16) & 0xFF;
+    unsigned char *offset_bytes = (unsigned char *)&offset;
+
+    where[0] = offset_bytes[0];
+    where[1] = offset_bytes[1];
+    where[2] = offset_bytes[2];
 }
 
 #elif defined(__8085)
 
-// Function _putoffset flags 0x00000280 __smallc 
+// Function _putoffset __smallc
 // void _putoffset(unsigned char * where, long offset)
 // parameter 'long offset' at sp+2 size(4)
 // parameter 'unsigned char * where' at sp+6 size(2)
@@ -40,9 +42,35 @@ void _putoffset(unsigned char *where, long offset)
     #endasm
 }
 
+#elif defined(__8080) || defined(__GBZ80)
+
+// Function _putoffset __smallc
+// void _putoffset(unsigned char * where, long offset)
+// parameter 'long offset' at sp+2 size(4)
+// parameter 'unsigned char * where' at sp+6 size(2)
+
+void _putoffset(unsigned char *where, long offset)
+{
+    #asm
+    ld hl,6
+    add hl,sp
+    ld e,(hl)
+    inc hl
+    ld d,(hl)
+    ld hl,2
+    add hl,sp
+    ld a,(hl+)
+    ld (de+),a
+    ld a,(hl+)
+    ld (de+),a
+    ld a,(hl)
+    ld (de),a
+    #endasm
+}
+
 #else
 
-// Function _putoffset flags 0x00000280 __smallc 
+// Function _putoffset __smallc
 // void _putoffset(unsigned char * where, long offset)
 // parameter 'long offset' at sp+2 size(4)
 // parameter 'unsigned char * where' at sp+6 size(2)
