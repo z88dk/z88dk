77e00c6800628b21108b10365e49109973ba119d
suborb <dom@suborbital.org.uk>
2025-07-25 09:47:45 +0100

    (phc25) This needs to be checked in

---
+++ libsrc/video/mc6847/stdio/printc_MODE0_phc25.asm
@@ -0,0 +1,37 @@
+IF FORphc25
+
+    SECTION code_driver
+    PUBLIC  printc_MODE0
+    PUBLIC  generic_console_set_ink
+    PUBLIC  generic_console_set_paper
+    PUBLIC  generic_console_set_attribute
+
+    EXTERN  __mc6847_MODE2_attr
+    EXTERN  mc6847_map_colour
+    EXTERN  generic_console_text_xypos
+
+
+generic_console_set_ink:
+    call    mc6847_map_colour
+    ld      a, b
+    rrca
+    rrca
+    and     @11000000
+    ld      (__mc6847_MODE2_attr), a
+    ret
+generic_console_set_paper:
+    call    mc6847_map_colour
+    ld      a, b
+    rrca
+    rrca
+    and     @11000000
+    ld      (__mc6847_MODE2_attr+1), a
+generic_console_set_attribute:
+    ret
+
+printc_MODE0:
+    call    generic_console_text_xypos
+    ld      (hl), d
+    ret
+
+ENDIF
