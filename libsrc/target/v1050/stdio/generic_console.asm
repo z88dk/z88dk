a811da2bef7c28a2dc2c78b3fd4c98f58c31256a
suborb <dom@suborbital.org.uk>
2025-08-15 21:01:29 +0100

    (v1050) Implement vpeek via a back screen

--- libsrc/target/v1050/stdio/generic_console.asm
+++ libsrc/target/v1050/stdio/generic_console.asm
@@ -9,12 +9,16 @@
     PUBLIC  generic_console_cls
     PUBLIC  generic_console_scrollup
     PUBLIC  generic_console_printc
+    PUBLIC  generic_console_vpeek
     PUBLIC  generic_console_set_ink
     PUBLIC  generic_console_set_paper
     PUBLIC  generic_console_set_attribute
     PUBLIC  generic_console_ioctl
 
     EXTERN  asm_load_charset
+
+    EXTERN  CONSOLE_COLUMNS
+    EXTERN  CONSOLE_ROWS
 
     EXTERN  __console_x
     EXTERN  __console_y
@@ -26,6 +30,11 @@
     defc    CLIB_GENCON_CAPS=CAP_GENCON_UDGS|CAP_GENCON_CUSTOM_FONT|CAP_GENCON_INVERSE|CAP_GENCON_BOLD|CAP_GENCON_UNDERLINE
 
 generic_console_cls:
+    ld      hl,screen_copy
+    ld      de,screen_copy+1
+    ld      bc,+(CONSOLE_ROWS * CONSOLE_COLUMNS) - 1
+    ld      (hl),32
+    ldir
     ld      l, 0x0C
     jp      v1050_sendchar
 
@@ -58,8 +67,11 @@ noinv:
 ; a = character to print
 ; e = raw
 generic_console_printc:
-
     push    af
+    push    bc
+    call    xypos
+    ld      (hl),a
+    pop     bc
 
 		; force cursor position
     ld      l, 27
@@ -105,6 +117,29 @@ noudg:
 
     ld      l, a
     jp      v1050_sendchar
+
+;Entry: c = x,
+;       b = y
+;       e = rawmode
+;Exit:  nc = success
+;        a = character,
+;        c = failure
+generic_console_vpeek:
+    call    xypos
+    ld      a,(hl)
+    and     a
+    ret
+
+xypos:
+    ld      hl,screen_copy - CONSOLE_COLUMNS
+    ld      de,CONSOLE_COLUMNS
+    inc     b
+generic_console_printc_1:
+    add     hl,de
+    djnz    generic_console_printc_1
+generic_console_printc_3:
+    add     hl,bc                   ;hl now points to address in display
+    ret
 
 
 ; a = ioctl
@@ -156,8 +191,19 @@ failure:
 
 
 generic_console_scrollup:
-
-		; place cursor at the bottom row..
+    push    de
+    push    bc
+    ld      hl,screen_copy + CONSOLE_COLUMNS
+    ld      de,screen_copy
+    ld      bc,+((CONSOLE_ROWS-1) * CONSOLE_COLUMNS)
+    ldir
+    ex      de,hl
+    ld      b,CONSOLE_COLUMNS
+cls_1:
+    ld      (hl),32
+    inc     hl
+    djnz    cls_1
+    ; place cursor at the bottom row..
     ld      l, 27
     call    v1050_sendchar
     ld      l, '='
@@ -168,7 +214,10 @@ generic_console_scrollup:
     call    v1050_sendchar
 		; and send LF
     ld      l, 10
-    jp      v1050_sendchar
+    call    v1050_sendchar
+    pop     bc
+    pop     de
+    ret
 
 
 generic_console_set_ink:
@@ -176,6 +225,10 @@ generic_console_set_paper:
     ret
 
 
+    SECTION	bss_clib
+
+screen_copy:	
+    defs	80 * 25		;Hopefully big enough?
 
     SECTION code_crt_init
 
