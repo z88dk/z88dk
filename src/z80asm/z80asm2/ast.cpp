b268f0208441a42cd10bdc60385f348c1f73fa37
Paulo Custodio <pauloscustodio@gmail.com>
2025-10-11 11:59:50 +0100

    Add skeleton Assembler in C++, implement ErrorReporting and unit tests

---
+++ src/z80asm/z80asm2/ast.cpp
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------------
+// Z80 assembler
+// Copyright (C) Paulo Custodio, 2011-2025
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#include "ast.h"
+#include "ast_node.h"
+
+AST::AST() {
+}
+
+ASTNode* AST::root() const {
+    return root_;
+}
---
+++ src/z80asm/z80asm2/ast.h
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+// Z80 assembler
+// Copyright (C) Paulo Custodio, 2011-2025
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <vector>
+#include <memory>
+
+class ASTNode;
+
+class AST {
+public:
+    AST();
+    ASTNode* root() const;
+private:
+    ASTNode* root_{ nullptr };
+};
