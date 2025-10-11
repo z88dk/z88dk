c7eccf6af133392a2563be07633a43abf2f9fafc
Paulo Custodio <pauloscustodio@gmail.com>
2025-10-11 11:59:50 +0100

    Add skeleton Assembler in C++, implement ErrorReporting and unit tests

---
+++ src/z80asm/z80asm2/error_reporter.h
@@ -0,0 +1,50 @@
+//-----------------------------------------------------------------------------
+// Z80 assembler
+// Copyright (C) Paulo Custodio, 2011-2025
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define X(code, msg) code,
+enum class ErrorCode {
+#include "error_codes.def"
+    Count
+};
+#undef X
+
+class Location {
+public:
+    Location(const std::string& filename_ = "", int line_num_ = 0);
+    std::string filename;
+    int line_num{ 0 };
+    std::string source_line;
+    std::string expanded_line;
+    bool is_c_source{ false };
+    // ...constructors and methods as before
+};
+
+class ErrorReporter {
+public:
+    ErrorReporter();
+
+    void error(ErrorCode code, const std::string& arg = "");
+    void warning(ErrorCode code, const std::string& arg = "");
+    void error(const Location& loc, ErrorCode code, const std::string& arg = "");
+    void warning(const Location& loc, ErrorCode code, const std::string& arg = "");
+
+    int error_count() const;
+
+private:
+    int error_count_ = 0;
+    static const char* error_messages[static_cast<int>(ErrorCode::Count)];
+    void print_message(const std::string& prefix, const std::string& message);
+    void print_message(const Location& loc, const std::string& prefix,
+                       const std::string& message);
+    std::string format_message(ErrorCode code, const std::string& arg) const;
+
+};
