330f00ace173e35ca6432f61cb58ee1871dc1c1f
Paulo Custodio <pauloscustodio@gmail.com>
2025-10-18 17:02:59 +0100

    Preprocessor: add MACRO, ENDM, LOCAL and EXITM

--- src/z80asm/z80asm2/keywords.h
+++ src/z80asm/z80asm2/keywords.h
@@ -26,9 +26,11 @@ Keyword to_keyword(const std::string& s);
 bool keyword_is_directive(Keyword kw);
 bool keyword_is_name_directive(Keyword kw);
 
-// Convert string to upper case
+// Convert string to upper/lower case
 std::string to_upper(const std::string& s);
+std::string to_lower(const std::string& s);
 
 // trim whitspace at the beginnint and at the end
 std::string ltrim(const std::string& s);
 std::string rtrim(const std::string& s);
+std::string trim(const std::string& s);
