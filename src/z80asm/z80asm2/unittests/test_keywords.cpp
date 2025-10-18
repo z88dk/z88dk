b407fe0413663b7d1ff879487d0eb59efa588580
Paulo Custodio <pauloscustodio@gmail.com>
2025-10-18 17:02:59 +0100

    Preprocessor: add MACRO, ENDM, LOCAL and EXITM

---
+++ src/z80asm/z80asm2/unittests/test_keywords.cpp
@@ -0,0 +1,78 @@
+//-----------------------------------------------------------------------------
+// z80asm preprocessor unit tests
+// Copyright (C) Paulo Custodio, 2011-2025
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#define CATCH_CONFIG_MAIN
+#include "catch_amalgamated.hpp"
+#include "../keywords.h"
+
+TEST_CASE("to_keyword converts strings to Keyword enum", "[to_keyword]") {
+    REQUIRE(to_keyword("LD") == Keyword::LD);
+    REQUIRE(to_keyword("ld") == Keyword::LD);
+    REQUIRE(to_keyword("Ld") == Keyword::LD);
+    REQUIRE(to_keyword("AF") == Keyword::AF);
+    REQUIRE(to_keyword("af") == Keyword::AF);
+    REQUIRE(to_keyword("Af") == Keyword::AF);
+    REQUIRE(to_keyword("AF'") == Keyword::AF_);
+    REQUIRE(to_keyword("af'") == Keyword::AF_);
+    REQUIRE(to_keyword("Af'") == Keyword::AF_);
+    REQUIRE(to_keyword("NONEXISTENT") == Keyword::None);
+    REQUIRE(to_keyword("") == Keyword::None);
+}
+
+TEST_CASE("to_upper converts strings to uppercase", "[to_upper]") {
+    REQUIRE(to_upper("abc") == "ABC");
+    REQUIRE(to_upper("AbC123") == "ABC123");
+    REQUIRE(to_upper("already UPPER") == "ALREADY UPPER");
+    REQUIRE(to_upper("") == "");
+    REQUIRE(to_upper("!@#") == "!@#");
+}
+
+TEST_CASE("ltrim removes leading whitespace", "[ltrim]") {
+    REQUIRE(ltrim("   abc") == "abc");
+    REQUIRE(ltrim("\t\txyz") == "xyz");
+    REQUIRE(ltrim("  \t  test  ") == "test  ");
+    REQUIRE(ltrim("") == "");
+    REQUIRE(ltrim("nochange") == "nochange");
+}
+
+TEST_CASE("rtrim removes trailing whitespace", "[rtrim]") {
+    REQUIRE(rtrim("abc   ") == "abc");
+    REQUIRE(rtrim("xyz\t\t") == "xyz");
+    REQUIRE(rtrim("  test  \t  ") == "  test");
+    REQUIRE(rtrim("") == "");
+    REQUIRE(rtrim("nochange") == "nochange");
+}
+
+TEST_CASE("trim removes leading and trailing whitespace", "[trim]") {
+    REQUIRE(trim("   abc   ") == "abc");
+    REQUIRE(trim("\t\txyz\t") == "xyz");
+    REQUIRE(trim("  \t  test  \t ") == "test");
+    REQUIRE(trim("") == "");
+    REQUIRE(trim("nochange") == "nochange");
+}
+
+TEST_CASE("keyword_is_directive recognizes directive keywords",
+          "[keyword_is_directive]") {
+    // Known directive keywords that should return true
+    const std::vector<std::string> directives = {
+        "MACRO", "ENDM", "DEFINE", "DEFL", "INCLUDE", "UNDEF",
+        "LINE", "EXITM"
+    };
+
+    for (const auto& d : directives) {
+        INFO("directive: " << d);
+        REQUIRE(keyword_is_directive(to_keyword(d)));
+        // also ensure lowercase works (to_keyword is case-insensitive)
+        INFO("directive(lower): " << d);
+        REQUIRE(keyword_is_directive(to_keyword(to_lower(d))));
+    }
+
+    // Some non-directives that must return false
+    REQUIRE_FALSE(keyword_is_directive(to_keyword("LD")));
+    REQUIRE_FALSE(keyword_is_directive(to_keyword("AF")));
+    REQUIRE_FALSE(keyword_is_directive(to_keyword("NONEXISTENT")));
+}
+
