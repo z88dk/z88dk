24333b194dd69e63abd52e5143224122edf31229
Paulo Custodio <pauloscustodio@gmail.com>
2025-10-26 21:54:00 +0000

    Preprocessor: add LINE directive
    Remove dead code from Location

--- src/z80asm/z80asm2/location.cpp
+++ src/z80asm/z80asm2/location.cpp
@@ -7,44 +7,30 @@
 #include "location.h"
 
 Location::Location(const std::string& filename, int line_num)
-    : filename_(filename), line_num_(line_num), inc_line_nums_(true) {
+    : filename_(filename), line_num_(line_num) {
 }
 
 void Location::clear() {
     filename_.clear();
     line_num_ = 0;
-    inc_line_nums_ = true;
 }
 
 bool Location::empty() const {
     return filename_.empty() && line_num_ == 0;
 }
 
-// Computes and sets the logical line number based on #line directive
-void Location::set_logical_line_num(int line_directive_value,
-                                    int line_directive_physical_line,
-                                    int physical_line_num) {
-    if (inc_line_nums_) {
-        // #line N means the *next* physical line is N
-        int logical = line_directive_value +
-                      (physical_line_num - line_directive_physical_line) - 1;
-        line_num_ = logical;
-    }
+const std::string& Location::filename() const {
+    return filename_;
 }
 
-// Sets the line number to the physical line number
-void Location::set_physical_line_num(int physical_line_num) {
-    line_num_ = physical_line_num;
+int Location::line_num() const {
+    return line_num_;
 }
 
-// Increments the logical line number (only when enabled)
-void Location::inc_line_num() {
-    if (inc_line_nums_) {
-        ++line_num_;
-    }
+void Location::set_filename(const std::string& filename) {
+    filename_ = filename;
 }
 
-void Location::set_inc_line_nums(bool enable) {
-    inc_line_nums_ = enable;
+void Location::set_line_num(int line_num) {
+    line_num_ = line_num;
 }
-
