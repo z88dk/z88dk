485efd41f2255c5c817be4b87dd1c206d685a9e8
Paulo Custodio <pauloscustodio@gmail.com>
2025-12-22 22:44:48 +0000

    Update workflows - add Perl modules to workflows
    Remove /WX from test modules
    Fixed to use MinGW Perl instead of msys Perl

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init --recursive
 sudo apt-get install -y build-essential bison flex libxml2-dev subversion zlib1g-dev m4 ragel re2c dos2unix texinfo texi2html gdb curl perl cpanminus ccache libboost-all-dev libmodern-perl-perl libyaml-perl liblocal-lib-perl libcapture-tiny-perl libpath-tiny-perl libtext-table-perl libdata-hexdump-perl libregexp-common-perl libclone-perl libfile-slurp-perl
-cpanm --local-lib=~/perl5 App::Prove Array::Compare CPU::Z80::Assembler Data::Dump Data::HexDump File::Path List::Uniq Modern::Perl Object::Tiny::RW Regexp::Common Test::Harness Text::Diff Text::Table YAML::Tiny
+cpanm --local-lib=~/perl5 App::Prove Array::Compare Clone CPU::Z80::Assembler Crypt::Mode::CBC Crypt::Mode::ECB Data::Dump Data::HexDump File::Path List::Uniq Modern::Perl Object::Tiny Object::Tiny::RW Regexp::Common Spreadsheet::ParseXLSX Test::Harness Text::Diff Text::Table YAML::Tiny
 eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 #export BUILD_SDCC=1
 #export BUILD_SDCC_HTTP=1
