64f32af8b010b5cffe97fd48d955b4b27e73c36c
Paulo Custodio <pauloscustodio@gmail.com>
2026-03-12 23:14:03 +0000

    Add source file reader, keywords and tokens

---
+++ src/z80asm/z80asm2/keywords.cpp
@@ -0,0 +1,102 @@
+//-----------------------------------------------------------------------------
+// Z80 assembler
+// Copyright (C) Paulo Custodio, 2011-2026
+// License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+//-----------------------------------------------------------------------------
+
+#include "keywords.h"
+#include "utils.h"
+#include <unordered_map>
+
+Keyword keyword_lookup(const std::string& s) {
+    static const std::unordered_map<std::string, Keyword> keyword_map = {
+#define X(id, text, flags) { text, Keyword::id },
+#include "keywords.def"
+    };
+    auto it = keyword_map.find(to_upper(s));
+    return it != keyword_map.end() ? it->second : Keyword::None;
+}
+
+std::string keyword_to_string(Keyword kw) {
+    static const char* keyword_strings[] = {
+        "None",
+#define X(id, text, flags) text,
+#include "keywords.def"
+    };
+    return keyword_strings[static_cast<int>(kw)];
+}
+
+static const int keyword_flags[] = {
+    0,	// None
+#define X(id, text, flags) flags,
+#include "keywords.def"
+};
+
+bool keyword_is_preproc_directive(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_PREPROC_DIRECTIVE) != 0;
+}
+
+bool keyword_directive_has_file_arg(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & DIRECTIVE_HAS_FILE_ARG) != 0;
+}
+
+bool keyword_is_preproc_name_directive(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_PREPROC_NAME_DIRECTIVE) != 0;
+}
+
+bool keyword_is_asm_directive(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_ASM_DIRECTIVE) != 0;
+}
+
+bool keyword_is_conditional_directive(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_CONDITIONAL_DIRECTIVE) != 0;
+}
+
+bool keyword_is_hla_directive(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_HLA_DIRECTIVE) != 0;
+}
+
+bool keyword_is_register(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_REGISTER) != 0;
+}
+
+bool keyword_is_segment_register(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_SEGMENT_REGISTER) != 0;
+}
+
+bool keyword_is_8bit_register(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_8BIT_REGISTER) != 0;
+}
+
+bool keyword_is_x_register(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_X_REGISTER) != 0;
+}
+
+bool keyword_is_flag(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_FLAG) != 0;
+}
+
+bool keyword_is_opcode(Keyword kw) {
+    int flags = keyword_flags[static_cast<int>(kw)];
+    return (flags & IS_OPCODE) != 0;
+}
+
+bool keyword_is_instruction(Keyword kw) {
+    return keyword_is_preproc_directive(kw) ||
+           keyword_is_preproc_name_directive(kw) ||
+           keyword_is_conditional_directive(kw) ||
+           keyword_is_asm_directive(kw) ||
+           keyword_is_opcode(kw);
+}
+
