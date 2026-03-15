3e0d823b7f3767cee711c04c56827fe01ded0a35
Paulo Custodio <pauloscustodio@gmail.com>
2026-03-15 00:51:14 +0000

    Add command line processing, source file searching, wildcard matching, errors and options

--- src/z80asm/z80asm2/lexer.cpp
+++ src/z80asm/z80asm2/lexer.cpp
@@ -4,8 +4,11 @@
 // License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
 //-----------------------------------------------------------------------------
 
-#include "lexer.h"
 #include "keywords.h"
+#include "lexer.h"
+#include "source.h"
+#include "utils.h"
+#include <cstring>
 
 Token Token::token(TokenType type_, const std::string& text_,
                    const SourceLoc& loc) {
@@ -68,4 +71,72 @@ Token Token::end_of_file(const SourceLoc& loc) {
     t.type = TokenType::EndOfFile;
     t.loc = loc;
     return t;
+}
+
+static void tokenize_line(SourceLine& line, bool& in_multiline_comment) {
+    const char* text = line.text;
+    size_t pos = 0;
+    size_t length = std::strlen(text);
+    while (pos < length) {
+        char c = text[pos];
+
+        // end of multi-line comment
+        if (in_multiline_comment) {
+            const char* end = std::strstr(text + pos, "*/");
+            if (end != nullptr) {
+                // leave pos after '/'
+                pos = static_cast<size_t>(end - text) + 2;
+                in_multiline_comment = false;
+            }
+            else {
+                // not found in this line
+                pos = length;
+            }
+            continue;
+        }
+
+        // skip whitespace
+        if (is_space(c)) {
+            pos++;
+            continue;
+        }
+
+        // skip assembly comments
+        if (c == ';') {
+            break;
+        }
+
+        // skip multi-line comments and single-line C++ comments
+        if (c == '/' && pos + 1 < length) {
+            char next = text[pos + 1];
+            if (next == '/') {
+                // single-line comment, skip rest of line
+                break;
+            }
+            else if (next == '*') {
+                // start of multi-line comment
+                in_multiline_comment = true;
+                pos += 2;
+                continue;
+            }
+        }
+
+        // define loc for the following token
+        /*
+        SourceLoc loc = line.loc;
+        loc.column = static_cast<int>(pos) + 1;
+        */
+
+
+    }
+}
+
+void tokenize(SourceFile& source) {
+    bool in_multiline_comment = false;
+    for (SourceLine& line : source.lines) {
+        tokenize_line(line, in_multiline_comment);
+        SourceLoc end_loc = line.loc;
+        end_loc.column = static_cast<int>(std::strlen(line.text)) + 1;
+        line.tokens.push_back(Token::end_of_line(end_loc));
+    }
 }
