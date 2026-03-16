14d8fed4a1177d71c30bcb0973f6e3a1488c78fe
Stefano <stefano_bodrato@hotmail.com>
2026-03-16 17:28:47 +0100

    Create getfcb.c

---
+++ libsrc/target/hdos/fcntl/getfcb.c
@@ -0,0 +1,29 @@
+/*
+ *  Get a free File Control Block
+ *
+ *  Stefano, 2026
+ *
+ *  $Id: getfcb.c $
+ */
+
+
+#include <arch/hdos.h>
+#include <stdio.h>
+
+
+
+struct fcb *getfcb(void)
+{
+    struct fcb  *fcb;
+
+    for ( fcb = _fcb ; fcb < &_fcb[MAXFILE]; fcb++ ) {
+        if ( fcb->use == 0 ) {
+            fcb->use   = U_READ;
+            fcb->rwptr = 0;
+            fcb->cached_record = 0xffffffff;
+            fcb->rnr_dirty = 0;
+            return fcb;
+        }
+    }
+    return NULL;
+}
