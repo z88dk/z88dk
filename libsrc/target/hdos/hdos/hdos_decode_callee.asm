934f6f5e3498645138c59556726540fb8163ee83
Stefano <stefano_bodrato@hotmail.com>
2026-03-16 15:08:36 +0100

    Create hdos_decode_callee.asm

---
+++ libsrc/target/hdos/hdos/hdos_decode_callee.asm
@@ -0,0 +1,37 @@
+;
+;   Heath/Zenith HDOS
+;
+;   Stefano Bodrato - 2026
+;
+;
+;   $Id: hdos_decode_callee.asm $
+;
+
+; int hdos_decode(char *name, int channel);
+
+    SECTION code_clib
+    PUBLIC  hdos_decode_callee
+    PUBLIC  _hdos_decode_callee
+
+    EXTERN  hdos_default
+
+    PUBLIC    asm_hdos_decode
+
+    INCLUDE "target/hdos/def/hdos.def"
+
+hdos_decode_callee:
+_hdos_decode_callee:
+        pop     af
+        pop     bc
+        pop     hl
+        push    af
+
+asm_hdos_decode:
+    ld     a,c
+    ld     de,hdos_default
+    rst    38h
+    defb   DECODE
+    ld     hl,1
+    ret    c          ; error
+    dec    hl
+    ret
