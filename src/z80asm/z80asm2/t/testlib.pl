81cae4a2e41edeb7b7ee1a99a592d90d9c541811
Paulo Custodio <pauloscustodio@gmail.com>
2026-03-22 12:44:45 +0000

    Rewrite the lexer with focus on: - memory foot-print of the Token object (using uint16_t, interned strings, uint8_t TokenType) - interned strings stored in a vector instead of a set, more memory efficient - do not store raw source text in memory, store only offsets and lengths to retrieve text on demand (error messages, list files) - caching of open file handles to retrieve lines quickly if needed - separation of concerns - move upper case symbols, IX-IY swap, AF' detection, .ASSUME detection to semantic step - replace a giant C++ lexer of 1200 lines by a re2c of 500 lines (less is more!) - POD - structs with free-standing functions instead of classes - remove cludges from the lexer such as PlusA, this belongs in the parser

---
+++ src/z80asm/z80asm2/t/testlib.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/env perl
+
+#------------------------------------------------------------------------------
+# Z80 assembler
+# Copyright (C) Paulo Custodio, 2011-2026
+# License: The Artistic License 2.0, http://www.perlfoundation.org/artistic_license_2_0
+#------------------------------------------------------------------------------
+
+use Modern::Perl;
+use Test::More;
+use Test::HexDifferences;
+use Config;
+use Capture::Tiny 'capture_merged';
+use Path::Tiny;
+use Text::Diff;
+
+$ENV{PATH} = join($Config{path_sep}, 
+			".",
+			"../../../bin",
+			$ENV{PATH});
+
+use vars '$test', '$null';
+$test = "test_".($0 =~ s/\W/_/gr);
+$null = ($^O eq 'MSWin32') ? 'nul' : '/dev/null';
+
+unlink_testfiles();
+
+#------------------------------------------------------------------------------
+sub check_bin_file {
+    my($got_file, $exp_bin) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    
+	my $got_bin = path($got_file)->slurp;
+	eq_or_dump_diff($got_bin, $exp_bin, "bin file $got_file ok");
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub check_text_file {
+    my($got_file, $exp_text) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    
+	(my $got_text = path($got_file)->slurp) =~ s/\r\n/\n/g;
+	$exp_text =~ s/\r\n/\n/g;
+	
+	my $diff = diff(\$exp_text, \$got_text, {STYLE => 'Table'});
+	is $diff, "", "text file $got_file ok";
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub z80asm_ok {
+    my($options, $files, $exp_warn, @pairs_asm_bin) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    
+    # build $asm and $bin
+    my($asm, $bin) = ("","");
+    while (my($a, $b) = splice(@pairs_asm_bin, 0, 2)) {
+        $asm .= "$a\n";
+        $bin .= $b;
+    }
+    
+    # save asm file
+    my $asm_file = "${test}.asm";
+    my $bin_file = "${test}.bin";
+    path($asm_file)->spew($asm);
+    unlink($bin_file);
+    
+    # assemble
+    $options ||= "-b";
+    $files ||= $asm_file;
+
+    run_ok("z88dk-z80asm $options $files 2> ${test}.stderr");
+    check_bin_file($bin_file, $bin);
+    check_text_file("${test}.stderr", $exp_warn) if $exp_warn;
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub z80asm_nok {
+    my($options, $files, $asm, $exp_err) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    
+    # save asm file
+    my $asm_file = "${test}.asm";
+	path($asm_file)->spew($asm);
+    
+    # assemble
+    $options ||= "-b";
+    $files ||= $asm_file;
+
+    capture_nok("z88dk-z80asm $options $files", $exp_err);
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub capture_ok {
+    my($cmd, $exp_out) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+    
+    run_ok($cmd." > ${test}.stdout");
+    check_text_file("${test}.stdout", $exp_out);
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub capture_nok {
+    my($cmd, $exp_err) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    run_nok($cmd." 2> ${test}.stderr");
+    check_text_file("${test}.stderr", $exp_err);
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub run_ok {
+    my($cmd) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+	
+	ok 1, "Running: $cmd";
+    ok 0==system($cmd), $cmd;
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub run_nok {
+    my($cmd) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+	
+	ok 1, "Running: $cmd";
+    ok 0!=system($cmd), $cmd;
+	
+	(Test::More->builder->is_passing) or die;
+}
+
+#------------------------------------------------------------------------------
+sub unlink_testfiles {
+	my(@additional) = @_;
+    unlink(<${test}*>, @additional) 
+        if Test::More->builder->is_passing;
+}
+
+1;
