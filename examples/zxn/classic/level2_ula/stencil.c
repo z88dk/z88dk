cb23e03391792298891260a17fd56f480fe3e968
suborb <dom@suborbital.org.uk>
2026-03-29 20:36:24 +0100

    (zxn) Comment the example a little

--- examples/zxn/classic/level2_ula/stencil.c
+++ examples/zxn/classic/level2_ula/stencil.c
@@ -1,10 +1,6 @@
 /*=========================================================================
 
-Demonstration on how to use the 'stencil' object.
-
-zcc +zx -vn -lndos -create-app  stencil.c -o stencil
-	
-$Id: stencil.c,v 1.3 2016-10-27 15:05:45 stefano Exp $
+Small demo showing use of ULA and layer2 at the same time
 
 =========================================================================*/
 
@@ -16,38 +12,32 @@ $Id: stencil.c,v 1.3 2016-10-27 15:05:45 stefano Exp $
 #include <arch/zxn.h>
 #include <arch/zxn/color.h>
 
-/* When the graphics library does not use the "wide" mode functions
-	   the stencil object could be 50% smaller */
 
 unsigned char stencil[256 * 4];
 int x, y, r, p;
 
 void main()
 {
+    // Switch to 14Mhz mode
+    ZXN_NEXTREG(REG_TURBO_MODE, RTM_14MHZ);
 
-
-   ZXN_NEXTREG(REG_TURBO_MODE, RTM_14MHZ);
-
+    // We'll print to the ULA layer, and have graphics on layer2
     conio_set_screen_mode(0);
     zxn_set_graphics_mode(128);
     clg();
 
-
-   ZXN_NEXTREGA(REG_SPRITE_LAYER_SYSTEM, RSLS_LAYER_PRIORITY_SUL);
-
-
+    // Setup the layer order
+    ZXN_NEXTREGA(REG_SPRITE_LAYER_SYSTEM, RSLS_LAYER_PRIORITY_SUL);
 
     // Setup transparency
     ZXN_NEXTREG(REG_GLOBAL_TRANSPARENCY_COLOR, ZXN_RGB332_NEXTOS_BRIGHT_MAGENTA);
 
-   // Setup the ULA layer. Mark most of the ULA layer as transparent
-   zx_border(INK_BLACK);
-   zx_cls_attr(0x5b);   // bright magenta ink + paper
-
+    // Setup the ULA layer. Mark most of the ULA layer as transparent
+    zx_border(INK_BLACK);
+    zx_cls_attr(0x5b); // bright magenta ink + paper (this affects ULA only)
 
     // paint polygon or circle
     for (;;) {
-
         // get a random position and size for the object
         x = rand() % getmaxx();
         y = rand() % getmaxy();
@@ -64,18 +54,22 @@ void main()
             // fill object with a texture of a random brightness
             zx_setink(rand());
             stencil_render(stencil, rand() % 12);
+
+            // And just print something to the ULA screen
             zx_setink(INK_WHITE);
-	    printf("%d %d %d\n",x,y,r);
+            printf("%d %d %d\n", x, y, r);
         }
+
+        // Allow changing of the grpahics mode
         int k = getk();
-        if ( k == '2' ) {
-           clg();
-           zxn_set_graphics_mode(128);
-           clg();
-        } else if ( k == 'u' ) {
-           clg();
-           zxn_set_graphics_mode(0);
-           clg();
+        if (k == '2') {
+            clg();
+            zxn_set_graphics_mode(128);
+            clg();
+        } else if (k == 'u') {
+            clg();
+            zxn_set_graphics_mode(0);
+            clg();
         }
     }
 }
