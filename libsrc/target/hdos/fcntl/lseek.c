f6e571d03bd0329ac9faf0ed0769e73804e67067
Stefano <stefano_bodrato@hotmail.com>
2026-03-30 13:29:01 +0200

    Update lseek.c

--- libsrc/target/hdos/fcntl/lseek.c
+++ libsrc/target/hdos/fcntl/lseek.c
@@ -17,24 +17,39 @@
 long lseek(int fd,long posn, int whence)
 {
     struct fcb *fc = (struct fcb *)fd;
-    long    pos;
+    unsigned long    pos;
     int     cnt;
     char    buffer[SECSIZE];
 
     switch(whence) {
-    default:
-        pos = posn;
-        break;
     case 1:
         pos = fc->rwptr + posn;
         break;
     case 2:
+		fputc_cons('/');
         if ( (fc->use == U_WRITE || fc->use == U_RDWR) && fsync(fd) != 0 ) return -1L;
+		
+		pos=(65535-hdos_posit(fc->ch,65535))*256L;
 
-        // Quick and dirty simplification (bad approximation)
-        while (readbyte(fd) != EOF) {};
-        pos = fc->rwptr + posn;
-        break; 
+		fputc_cons('.');
+        if ((fc->mode & _IOTEXT) && (pos >= 256L)) { 
+		fputc_cons('!');
+            do { 
+                fc->rwptr = pos-256L;
+                cnt=0; 
+                if (read(fd, buffer, 256)!=-1) { 
+                    while (cnt < 256 && buffer[255-cnt]==__STDIO_EOFMARKER) 
+                        cnt++; 
+                    pos -= (unsigned long)cnt; 
+                } 
+            } while (cnt == 256 && pos >= 256L); 
+        }
+
+        pos = pos + posn; 
+        break;
+    default:
+        pos = posn;
+        break;
     }
 
     if (pos < 0L)
