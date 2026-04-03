f2a85ee3cff20109321a2968f1cb656447f38b2f
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-03 15:37:40 +0100

    Add INCLUDE directive

---
+++ src/z80asm/z80asm2/t/30-directive/input/include_05.asm
@@ -0,0 +1,3 @@
+ld a,1
+include "include_05_i1.inc" ; comment
+ld a,2
---
+++ src/z80asm/z80asm2/t/30-directive/input/include_16.asm
@@ -0,0 +1,5 @@
+ld a,1
+#include "include_06.inc" ; includes local file
+ld a,2
+#include <include_06.inc> ; includes library file
+ld a,3
