7ae389849989c637e2ef9aafd434e7152aeb5a8f
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-03 20:35:54 +0100

    Add INCBIN directive

---
+++ src/z80asm/z80asm2/t/30-directive/input/incbin_05.asm
@@ -0,0 +1,3 @@
+ld a,1
+incbin "incbin_05.bin" ; comment
+ld a,2
---
+++ src/z80asm/z80asm2/t/copy_tests.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Path::Tiny;
+
+@ARGV==2 or die "usage: $0 from_text to_text\n";
+my($from_text, $to_text) = @ARGV;
+
+my($dir) = path("t");
+my $iter = $dir->iterator({recurse => 1});
+while (my $next = $iter->()) {
+	next if $next =~ /\.bak$/;
+	if ($next =~ /$from_text/) {
+		my $new = path($next =~ s/$from_text/$to_text/gr);
+		say $new;
+		$new->parent->mkdir;
+		my $content = $next->slurp_raw;
+		$content =~ s/$from_text/$to_text/g;
+		$new->spew_raw($content);
+	}
+}
