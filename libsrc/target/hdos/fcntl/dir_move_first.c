b427f89c9a4786f4867579422f5469e0e2b0b0ef
Stefano <stefano_bodrato@hotmail.com>
2026-04-09 21:42:21 +0200

    Update dir_move_first.c

--- libsrc/target/hdos/fcntl/dir_move_first.c
+++ libsrc/target/hdos/fcntl/dir_move_first.c
@@ -25,21 +25,22 @@ int dir_move_first(void)
         hdos_dir_fp = NULL;
     }
 
-    hdos_dir_fp = (void *)fopen("DIRECT.SYS", "rb");
+    hdos_dir_fp = (FILE *)fopen("DIRECT.SYS", "rb");
     if (!hdos_dir_fp)
         return 0x24;     /* simulate FLOS error code $24 (= Reached end of directory) */
 
-    while (1) {
+    //while (1) {
         size_t r = fread(hdos_dir_buf, 1, HDOS_DIRENT_LEN, (FILE *)hdos_dir_fp);
-        if (r < HDOS_DIRENT_LEN) {
-            hdos_dir_eof = 1;
-            return 0x24;
-        }
+		rewind(hdos_dir_fp);  // odd workaround obtained by trial and error, it just works
+        //if (r < HDOS_DIRENT_LEN) {
+        //    hdos_dir_eof = 1;
+        //    return 0x24;
+        //}
 
         //hdos_dir_index++;
 
-        if (!is_empty_dirent(hdos_dir_buf))
+        //if (!is_empty_dirent(hdos_dir_buf))
             //return 0;    /* Directory cursor ready */
 			return dir_move_next();
-    }
+    //}
 }
