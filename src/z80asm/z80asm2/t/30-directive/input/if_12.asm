f2d3d809b03469828eea5f1fcda0e6469cba6077
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-11 22:50:21 +0100

    Add IF, ELIF, ELSEIF, ELSE, ENDIF directives

---
+++ src/z80asm/z80asm2/t/30-directive/input/if_12.asm
@@ -0,0 +1,43 @@
+#define expr 1
+
+#if expr
+	ld a, 1
+#else
+	not reached
+#endif
+
+#if !expr
+	not reached
+#else 
+	ld a, 2
+#endif
+
+#if not_defined
+	not reached
+#elseif !not_defined
+	ld a, 3
+#else
+	not reached
+#endif
+
+#if not_defined
+	not reached
+#elif !not_defined
+	ld a, 4
+#else
+	not reached
+#endif
+
+#if 1
+	#if 1
+		#if 1
+			ld a, 5
+		#else
+			not reached
+		#endif
+	#else
+		not reached
+	#endif
+#else
+	not reached
+#endif
