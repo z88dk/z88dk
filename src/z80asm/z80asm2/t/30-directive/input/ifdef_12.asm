029bbee730a682ccd075570f9816d272c6a14760
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-12 18:06:31 +0100

    Add IFDEF, IFNDEF, ELSEIFDEF, ELSEIFNDEF, ELIFDEF, ELIFNDEF directives

---
+++ src/z80asm/z80asm2/t/30-directive/input/ifdef_12.asm
@@ -0,0 +1,45 @@
+#define X 1
+Y EQU 2
+Z = 3
+
+#ifdef X
+	ld a, 1
+#else
+	not reached
+#endif
+
+#ifdef Y
+	ld a, 2
+#else 
+	not reached
+#endif
+
+#ifdef not_defined_1
+	not reached
+#elseifdef not_defined_2
+	not reached
+#else
+	ld a, 3
+#endif
+
+#ifdef not_defined_1
+	not reached
+#elifdef not_defined_2
+	not reached
+#else
+	ld a, 4
+#endif
+
+#ifdef X
+	#ifdef Y
+		#ifdef Z
+			ld a, 5
+		#else
+			not reached
+		#endif
+	#else
+		not reached
+	#endif
+#else
+	not reached
+#endif
