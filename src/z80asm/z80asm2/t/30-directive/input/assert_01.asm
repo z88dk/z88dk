59eeec69162632495232395c54c5532fc9425ea8
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-12 19:15:15 +0100

    Add ASSERT directive

---
+++ src/z80asm/z80asm2/t/30-directive/input/assert_01.asm
@@ -0,0 +1,20 @@
+define X
+ld a, 1
+assert ; no arguments
+ld a, 2
+assert asmpc ; not constant
+ld a, 3
+assert X extra
+ld a, 4
+assert X , extra
+ld a, 5
+assert X , "hello" extra
+ld a, 6
+assert X ; no error
+ld a, 7
+assert !X ; error
+ld a, 8
+assert X, "user error" ; no error
+ld a, 9
+assert !X, "user error" ; error
+ld a, 10
