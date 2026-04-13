3d9d5df0f8226ee6174807ccc7acf2dda8e5b448
Paulo Custodio <pauloscustodio@gmail.com>
2026-04-13 08:17:57 +0100

    Add IFNDEF tests

---
+++ src/z80asm/z80asm2/t/30-directive/input/ifndef_11.asm
@@ -0,0 +1,22 @@
+#ifndef ; no symbol
+#endif
+#ifndef 23 ; no symbol
+#endif
+#ifndef X extra ; extra input
+#endif
+#else ; else without if
+#elseifndef ; elseifndef without if
+#elifndef ; elifndef without if
+#endif ; endif without if
+#ifndef X
+#else
+#else ; two elses
+#endif
+#ifndef X
+#else
+#elseifndef ; elseifndef after else
+#endif
+#ifndef X
+#else
+#elifndef ; elifndef after else
+#endif
