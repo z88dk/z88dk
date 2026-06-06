ffae3673ea4a3a758eafd1246db59e87a27ac0bc
Paulo Custodio <pauloscustodio@gmail.com>
2026-06-06 19:04:10 +0100

    Add centralized range checks for copper unit. Fix CU.MOVE value range to -128..255. Add check for CALL_PKG not available on Rabbits (no RST $08) Add checks for all CU.* only available on z80n

---
+++ src/z80asm/z80asm2/t/20-preproc/input/cu_wait_02.asm
@@ -0,0 +1,14 @@
+#define X 311
+#define Y 55
+cu.wait 
+cu.wait 0
+cu.wait 0,
+cu.wait 0,0,
+cu.wait -1, 0
+cu.wait 0, 0
+cu.wait X, 0
+cu.wait X+1, 0
+cu.wait 0, -1
+cu.wait 0, 0
+cu.wait 0, Y
+cu.wait 0, Y+1
---
+++ src/z80asm/z80asm2/t/35-preproc-only/input/cu_wait_01.asm
@@ -0,0 +1,6 @@
+cu.wait 0,1
+cu.wait 0,2
+cu.wait 0,55
+cu.wait 1,0
+cu.wait 2,0
+cu.wait 311,0
