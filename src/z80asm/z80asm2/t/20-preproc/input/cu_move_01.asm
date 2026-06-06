7e8057bdde443ee281a41a9214d85ea25ed24977
Paulo Custodio <pauloscustodio@gmail.com>
2026-06-06 12:44:18 +0100

    Compute CU_WAIT and CU_MOVE expressions at preprocessing time

--- src/z80asm/z80asm2/t/20-preproc/input/cu_move_01.asm
+++ src/z80asm/z80asm2/t/20-preproc/input/cu_move_01.asm
@@ -1,6 +1,15 @@
+#define X 127
+#define Y 255
 cu.move 
 cu.move 0
 cu.move 0,
 cu.move 0,0,
+cu.move -1,0
 cu.move 0,0
-cu.move 127,255
+cu.move X,0
+cu.move X+1,0
+cu.move 0,-1
+cu.move 0,0
+cu.move 0,Y
+cu.move 0,Y+1
+
