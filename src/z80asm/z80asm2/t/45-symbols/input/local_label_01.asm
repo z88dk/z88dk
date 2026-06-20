6be6decbe7a5362f84b05700eee41e46291f6675
Paulo Custodio <pauloscustodio@gmail.com>
2026-06-20 11:59:57 +0100

    Resolve local labels

---
+++ src/z80asm/z80asm2/t/45-symbols/input/local_label_01.asm
@@ -0,0 +1,2 @@
+@l1:            ; error, no parent label
+    jp @l2      ; error, undefined symbol
---
+++ src/z80asm/z80asm2/t/45-symbols/input/local_label_10.asm
@@ -0,0 +1,16 @@
+g1:
+    jp g1
+@l1:
+    jp @l1
+    jp @l2
+    jp g1@l1    
+    jp g1@l2    
+@l2:
+g2:
+    jp g2
+@l1:
+    jp @l1
+    jp @l2
+    jp g2@l1    
+    jp g2@l2    
+@l2:
