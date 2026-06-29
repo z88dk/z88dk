b4b0cd338ef77a115ede573507e0bee6ed9f063e
suborb <dom@suborbital.org.uk>
2026-06-29 21:50:20 +0000

    80cc: fnptr dispatch thunks (l_jpix/l_jpiy) + gbz80 long-shift helper list

--- libsrc/l/sccz80/9-common/l_jpix.asm
+++ libsrc/l/sccz80/9-common/l_jpix.asm
@@ -2,8 +2,12 @@
 SECTION code_clib
 SECTION code_l_sccz80
 
+IF !__CPU_INTEL__ && !__CPU_GBZ80__
+
 PUBLIC l_jpix
 
 l_jpix:
 
    jp (ix)
+
+ENDIF
--- libsrc/l/sccz80/9-common/l_jpiy.asm
+++ libsrc/l/sccz80/9-common/l_jpiy.asm
@@ -2,8 +2,13 @@
 SECTION code_clib
 SECTION code_l_sccz80
 
+IF !__CPU_INTEL__ && !__CPU_GBZ80__
+
+
 PUBLIC l_jpiy
 
 l_jpiy:
 
    jp (iy)
+
+ENDIF
