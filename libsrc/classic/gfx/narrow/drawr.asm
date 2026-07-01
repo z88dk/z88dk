048218c27018ac16c4f94d964b65b32a14778d57
Stefano <stefano_bodrato@hotmail.com>
2026-07-01 16:04:03 +0200

    Update drawr.asm

--- libsrc/classic/gfx/narrow/drawr.asm
+++ libsrc/classic/gfx/narrow/drawr.asm
@@ -1,24 +1,42 @@
-; ----- void  drawr(int x, int y)
+; ----- void  draw(int x, int y, int x2, int y2)
 
 
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
     SECTION code_graphics
 
-    PUBLIC  drawr
-    PUBLIC  _drawr
-    PUBLIC  ___drawr
+    PUBLIC  draw
+    PUBLIC  _draw
+    PUBLIC  ___draw
 
-    EXTERN  asm_drawr
+    EXTERN  asm_draw
+    INCLUDE "classic/gfx/grafix.inc"
 
 
-drawr:
-_drawr:
-___drawr:
-    pop     bc                          ; ret addr
-    pop     de                          ; y
-    pop     hl                          ; x
-    push    hl
+draw:
+_draw:
+___draw:
+
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
+    push    ix
+    ld      ix, 2
+    add     ix, sp
+    ld      e, (ix+2)                   ;y1
+    ld      d, (ix+4)                   ;x1
+    ld      l, (ix+6)                   ;y0
+    ld      h, (ix+8)                   ;x0
+    pop     ix
+ELSE
+    pop     af
+    pop     de                          ; y1
+    pop     bc
+    ld      d, c                        ; x1
+
+    pop     hl                          ; y0
+    pop     de                          ; x0
     push    de
-    push    bc                          ; ret addr
-    jp      asm_drawr
+    push    hl
+    ld      h, e                        ; x0
+
+    push    bc
+    push    de                          ; foo value, the original value is gone
+    push    af
 ENDIF
