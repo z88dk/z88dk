3487df5740ee962f0555b46bd2aee85d48d43ddd
Stefano <stefano_bodrato@hotmail.com>
2026-07-01 18:45:43 +0200

    Update xordraw.asm

--- libsrc/classic/gfx/narrow/xordraw.asm
+++ libsrc/classic/gfx/narrow/xordraw.asm
@@ -1,7 +1,6 @@
 ; ----- void  xordraw(int x, int y, int x2, int y2)
 
 
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
     SECTION code_graphics
 
     PUBLIC  xordraw
@@ -9,17 +8,38 @@ IF  !__CPU_INTEL__&!__CPU_GBZ80__
     PUBLIC  ___xordraw
 
     EXTERN  asm_xordraw
+    INCLUDE "classic/gfx/grafix.inc"
+
+
 xordraw:
 _xordraw:
 ___xordraw:
+
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
     push    ix
     ld      ix, 2
     add     ix, sp
-    ld      l, (ix+6)                   ;y0
-    ld      h, (ix+8)                   ;x0
     ld      e, (ix+2)                   ;y1
     ld      d, (ix+4)                   ;x1
+    ld      l, (ix+6)                   ;y0
+    ld      h, (ix+8)                   ;x0
     pop     ix
+ELSE
+    pop     af
+    pop     de                          ; y1
+    pop     bc
+    ld      d, c                        ; x1
+
+    pop     hl                          ; y0
+    pop     de                          ; x0
+    push    de
+    push    hl
+    ld      h, e                        ; x0
+
+    push    bc
+    push    de                          ; foo value, the original value is gone
+    push    af
+ENDIF
+
     jp      asm_xordraw
 
-ENDIF
