8136bfcfc4a0726a1cd2585002f34f7401863b6b
Stefano <stefano_bodrato@hotmail.com>
2026-07-03 15:16:20 +0200

    Update dcircle.asm

--- libsrc/classic/gfx/narrow/dcircle.asm
+++ libsrc/classic/gfx/narrow/dcircle.asm
@@ -1,6 +1,10 @@
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
+
     SECTION code_graphics
     PUBLIC  draw_circle
+    INCLUDE "classic/gfx/grafix.inc"
+
+
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
 
 
     DEFVARS 0
@@ -126,7 +130,6 @@ l9920:
     sub     (ix+radius)
     ld      h, a
     call    doplot
-
 ;Line 9930
     ld      a, (ix+cx)
     add     a, (ix+scale)
@@ -148,5 +151,283 @@ doplot:
     ret
 jpix:
     jp      (ix)
+
+ELSE
+
+
+
+
+; Entry:
+;     B=x0
+;     C=y0
+;     D=radius
+;     E=scale factor
+;     HL=plot routine
+
+draw_circle:
+
+    ld      a,b
+    ld      (x0),a
+
+    ld      a,c
+    ld      (y0),a
+
+    ld      a,d
+    ld      (radius),a
+
+    ld      a,e
+    ld      (scale),a
+	
+    ld      (func), hl
+
+
+    call    l9900
+    ret
+
+
+l9900:
+
+    xor     a
+    ld      (cx),a
+
+    ld      a,d
+    and     $fe
+    rra
+    ld      (da),a
+
+
+l9905:
+
+    ld      a,(cx)
+    ld      b,a
+
+    ld      a,(radius)
+    cp      b
+    ret     z
+    ret     c
+
+
+    ld      a,(da)
+    or      a
+    jp      p,l9915
+
+    ld      b,a
+    ld      a,(radius)
+    add     a,b
+    ld      (da),a
+
+    ld      a,(radius)
+    push    hl
+    ld      hl,scale
+    sub     (hl)
+    pop     hl
+    ld      (radius),a
+
+
+l9915:
+
+    ld      a,(da)
+    dec     a
+    push    hl
+    ld      hl,cx
+    sub     (hl)
+    pop     hl
+
+    ld      (da),a
+
+
+l9920:
+
+    ;
+    ; y0 + radius
+    ;
+
+    ld      a,(y0)
+    push    hl
+    ld      hl,radius
+    add     a,(hl)
+    pop     hl
+
+    ld      l,a
+    ld      (asave),a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,cx
+    add     a,(hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+    ld      a,(asave)
+    ld      l,a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,cx
+    sub     (hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+
+    ;
+    ; y0 - radius
+    ;
+
+    ld      a,(y0)
+    push    hl
+    ld      hl,radius
+    sub     (hl)
+    pop     hl
+
+    ld      l,a
+    ld      (asave),a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,cx
+    add     a,(hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+    ld      a,(asave)
+    ld      l,a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,cx
+    sub     (hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+
+    ;
+    ; y0 + cx
+    ;
+
+    ld      a,(y0)
+    push    hl
+    ld      hl,cx
+    add     a,(hl)
+    pop     hl
+
+    ld      l,a
+    ld      (asave),a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,radius
+    add     a,(hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+    ld      a,(asave)
+    ld      l,a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,radius
+    sub     (hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+
+    ;
+    ; y0 - cx
+    ;
+
+    ld      a,(y0)
+    push    hl
+    ld      hl,cx
+    sub     (hl)
+    pop     hl
+
+    ld      l,a
+    ld      (asave),a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,radius
+    add     a,(hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+    ld      a,(asave)
+    ld      l,a
+
+    ld      a,(x0)
+    push    hl
+    ld      hl,radius
+    sub     (hl)
+    pop     hl
+    ld      h,a
+
+    call    doplot
+
+
+l9930:
+
+    ld      a,(cx)
+    push    hl
+    ld      hl,scale
+    add     a,(hl)
+    pop     hl
+
+    ld      (cx),a
+
+    jp      l9905
+
+
+; Entry:
+;     H=x
+;     L=y
+
+doplot:
+
+    ret     c
+
+    ld      hl,(func)
+    push    hl
+    ret
+
+
+SECTION bss_graphics
+
+x0:
+    defb    0
+
+y0:
+    defb    0
+
+radius:
+    defb    0
+
+cx:
+    defb    0
+
+da:
+    defb    0
+
+scale:
+    defb    0
+
+asave:
+    defb    0
+
+func:
+    defw    0
 
 ENDIF
