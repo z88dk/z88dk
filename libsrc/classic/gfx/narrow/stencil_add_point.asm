aa5a7c0487accb47b8e92a48e0605d54f4f5cc1b
Stefano <stefano_bodrato@hotmail.com>
2026-07-07 10:14:21 +0200

    Update stencil_add_point.asm

--- libsrc/classic/gfx/narrow/stencil_add_point.asm
+++ libsrc/classic/gfx/narrow/stencil_add_point.asm
@@ -8,27 +8,30 @@
 ;     Stefano Bodrato - 08/10/2009
 ;
 ;
-;    $Id: stencil_add_point.asm,v 1.5 2016-04-22 20:29:51 dom Exp $
+;    $Id: stencil_add_point.asm $
 ;
 
 ;; void stencil_add_point(int x, int y, unsigned char *stencil)
 
 
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
     SECTION code_graphics
     PUBLIC  stencil_add_point
     PUBLIC  _stencil_add_point
 
-    EXTERN  line
     EXTERN  stencil_add_pixel
 
+    ; No paging required on the 'stencil' structures !
         ;EXTERN    __gfx_vram_page_in
         ;EXTERN    __gfx_vram_page_out
 
     EXTERN  stencil_ptr
+    INCLUDE "classic/gfx/grafix.inc"
 
 stencil_add_point:
 _stencil_add_point:
+
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
+
     push    ix
     ld      ix, 2
     add     ix, sp
@@ -45,7 +48,23 @@ _stencil_add_point:
     ld      h, (ix+6)                   ;x0
 
     pop     ix
-    jp      stencil_add_pixel
 
+ELSE
+
+    pop     af
+    pop     hl
+    ld      (stencil_ptr), hl
+
+    pop     de                          ; y
+    pop     hl                          ; x
+    ld      d,l
+    
+    push    hl
+    push    de
+
+    push    hl
+    push    af
 
 ENDIF
+
+    jp      stencil_add_pixel
