49c7a95f4efb410f3234a7bd2033aaa0fa28764a
Stefano <stefano_bodrato@hotmail.com>
2026-07-08 17:19:16 +0200

    Update clga.asm

--- libsrc/classic/gfx/narrow/clga.asm
+++ libsrc/classic/gfx/narrow/clga.asm
@@ -1,7 +1,5 @@
 ;Usage: clga(int tlx, int tly, int tlx2, int tly2)
 
-
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
     SECTION code_graphics
 
     PUBLIC  clga
@@ -13,6 +11,8 @@ IF  !__CPU_INTEL__&!__CPU_GBZ80__
 clga:
 _clga:
 ___clga:
+
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
     push    ix
     ld      ix, 2
     add     ix, sp
@@ -21,5 +21,19 @@ ___clga:
     ld      l, (ix+6)
     ld      h, (ix+8)
     pop     ix
-    jp      asm_clga
+ELSE
+    pop     af
+    pop     bc                          ; height
+    pop     de
+    ld      b, e                        ; width
+    pop     hl                          ; x
+    pop     de
+    push    de
+    push    hl
+    ld      h, e                        ; y
+    push    de                          ; this value is lost
+    push    bc
+    push    af
 ENDIF
+
+    jp      asm_clga
