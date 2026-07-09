eec7f9635868d05137ad135865fd7975bd29cd87
Stefano <stefano_bodrato@hotmail.com>
2026-07-09 13:18:49 +0200

    Update move.asm

--- libsrc/classic/gfx/narrow/move.asm
+++ libsrc/classic/gfx/narrow/move.asm
@@ -5,7 +5,6 @@
 ;     $Id: move.asm $
 ;
 
-IF  !__CPU_INTEL__&!__CPU_GBZ80__
     SECTION code_graphics
     PUBLIC  move
     PUBLIC  _move
@@ -23,6 +22,7 @@ IF  !__CPU_INTEL__&!__CPU_GBZ80__
 move:
 _move:
 ___move:
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
     push    ix
     ld      ix, 2
     add     ix, sp
@@ -30,19 +30,43 @@ ___move:
     ld      d, (ix+3)
     ld      l, (ix+4)                   ;px
     ld      h, (ix+5)
+ELSE
+    pop     af
+    pop     de
+    pop     hl
+    push    hl
+    push    de
+ENDIF
 
   IFDEF _GFX_PAGE_VRAM
     call    __gfx_vram_page_in
   ENDIF
 
     ld      a, (__pen)
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
     ld      ix, setxy
+ELSE
+    EXTERN  __plot_ADDR
+    push    hl
+    ld      hl,setxy
+    ld      (__plot_ADDR),hl
+    pop     hl
+ENDIF
     rla
     jr      nc, pen_up
+IF  !__CPU_INTEL__&!__CPU_GBZ80__
     ld      ix, plotpixel
+ELSE
+    EXTERN  __plot_ADDR
+    push    hl
+    ld      hl,plotpixel
+    ld      (__plot_ADDR),hl
+    pop     hl
+ENDIF
 pen_up:
 
     call    Line_r
+
   IF    _GFX_PAGE_VRAM
     jp      __graphics_end
   ELSE
@@ -51,4 +75,3 @@ pen_up:
     ENDIF
     ret
   ENDIF
-ENDIF
