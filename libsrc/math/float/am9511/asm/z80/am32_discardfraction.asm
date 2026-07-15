63379fee4db17af306a578ad4f72378d895fc3bc
feilipu <phillip.stevens@gmail.com>
2026-07-15 21:12:47 +0800

    am9511 - fix discard fraction

--- libsrc/math/float/am9511/asm/z80/am32_discardfraction.asm
+++ libsrc/math/float/am9511/asm/z80/am32_discardfraction.asm
@@ -16,26 +16,32 @@ PUBLIC asm_am9511_discardfraction
 
 ; Entry: dehl = 32 bit float
 ; Exit:  dehl = 32 bit float without fractional part
+;        NC = already integer (result equals input)
+;        C  = fractional part was discarded
 
 .asm_am9511_discardfraction
     sla e                       ; get the exponent
     rl d
-    jr Z,zero_legal             ; return IEEE signed zero
+    jr Z,zero_legal             ; exp 0 -> signed zero, NC
 
     ld a,d                      ; exponent
     rr d                        ; keep sign and exponent safe
     rr e
     sub $7f                     ; exponent value of 127 is 1.xx
-    jr C,return_zero
+    jr C,return_zero            ; |x| < 1 -> signed zero, C
 
     inc a
     cp 24
-    ret NC                      ; no shift needed, all integer
+    jr C,mask_frac
+    or a                        ; |x| >= 2^23: already integer, NC
+    ret
 
-                                ; build mask of integer bits
-                                ; a = number of bits to keep
-    exx
-    ld hl,0
+.mask_frac
+    push de                     ; save original
+    push hl
+
+    exx                         ; build mask of integer bits
+    ld hl,0                     ; a = number of bits to keep
     ld e,l
 
 .shift_right                    ; shift mantissa mask right
@@ -59,18 +65,45 @@ PUBLIC asm_am9511_discardfraction
     exx
     and l
     exx
-    ld l,a
+    ld l,a                      ; dehl = truncated
+
+    pop bc                      ; orig L
+    ld a,l
+    cp c
+    jr NZ,changed_pop
+    ld a,h
+    cp b
+    jr NZ,changed_pop
+    pop bc                      ; orig H
+    ld a,e
+    cp c
+    jr NZ,changed
+    ld a,d
+    cp b
+    jr NZ,changed
+    or a                        ; identical -> NC
     ret
 
+.changed_pop
+    pop bc                      ; drop orig H
+.changed
+    scf
+    ret
 
 .return_zero
     rl d                        ; get the sign bit
     ld d,0
+    ld e,d                      ; use 0
+    ld h,d
+    ld l,d
+    rr d                        ; restore the sign
+    scf                         ; nonzero |x|<1 -> C
+    ret                         ; return IEEE signed ZERO in DEHL
 
 .zero_legal
     ld e,d                      ; use 0
     ld h,d
     ld l,d
     rr d                        ; restore the sign
+    or a                        ; NC
     ret                         ; return IEEE signed ZERO in DEHL
-
