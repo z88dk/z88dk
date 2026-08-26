9966459c1b540549c3d7bbabd656bf3705ec9e91
Paulo Custodio <pauloscustodio@gmail.com>
2026-08-26 22:46:41 +0100

    Add BASIC parser

--- src/zx81bas/preproc.h
+++ src/zx81bas/preproc.h
@@ -7,20 +7,18 @@
 #pragma once
 
 #include "dump_context.h"
+#include "errors.h"
 #include <string>
 #include <utility>
 #include <vector>
 
 struct SrcLine : TreeNode {
     std::string text;		// line text
-    std::string filename;	// source location
-    int line_num = 0;
+    SourceLoc loc;          // source location
 
     explicit SrcLine() = default;
-    explicit SrcLine(std::string text_, std::string filename_, int line_num_)
-        : text(std::move(text_)),
-          filename(std::move(filename_)),
-          line_num(line_num_) {}
+    explicit SrcLine(const std::string& text_, std::string filename_, int line_num_)
+        : text(text_), loc(filename_, line_num_) {}
 
 #ifdef _DEBUG
     void dump(DumpContext ctx) const override;
@@ -30,7 +28,5 @@ struct SrcLine : TreeNode {
 bool preproc(std::string input_file, std::vector<SrcLine>& out_lines);
 
 // used during lexing, where pragmas are !ASM and !ENDASM
-bool match_ASM(const std::string& text, const std::string& filename,
-               int line_num);
-bool match_BASIC(const std::string& text, const std::string& filename,
-                 int line_num);
+bool match_ASM(const std::string& text, const SourceLoc& loc);
+bool match_BASIC(const std::string& text, const SourceLoc& loc);
