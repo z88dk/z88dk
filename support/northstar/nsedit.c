913ec76f0902cbc8f5d8f17a62cd2451d29c829b
Stefano <stefano_bodrato@hotmail.com>
2026-09-02 16:35:47 +0200

    Update nsedit.c

--- support/northstar/nsedit.c
+++ support/northstar/nsedit.c
@@ -5,6 +5,7 @@
 
        gcc -o nsedit nsedit.c
 
+
     Supports:
         89600-byte images  (350 blocks)
         179200-byte images (700 blocks)
@@ -12,15 +13,16 @@
 
 
     Commands:
-
         nsedit dir image.nsi
-
         nsedit extract image.nsi DOSNAME outfile
-
         nsedit insert image.nsi infile DOSNAME <GO address>
-
         nsedit delete image.nsi DOSNAME
 
+
+    Common file types:
+        1 executable program files
+        2 BASIC programs
+        3 BASIC DATA
 
 */
 
@@ -29,29 +31,28 @@
 #include <stdint.h>
 #include <string.h>
 
-#define BLOCK_SIZE 256
 #define DIR_BLOCKS 4
 #define DIR_ENTRIES 64
+
 
 #pragma pack(push,1)
 
 typedef struct
 {
     char     name[8];
-
     uint16_t start_block;
-
     uint16_t length;
-
     uint8_t type;
-
     uint16_t go_address;
-
     uint8_t reserved;
-
 } NSDirEntry;
 
 #pragma pack(pop)
+
+
+// 256 will be later updated, we use it for the initial image checks
+int BLOCK_SIZE = 256;
+
 
 static long image_blocks(FILE *fp)
 {
@@ -64,20 +65,31 @@ static long image_blocks(FILE *fp)
     return size / BLOCK_SIZE;
 }
 
-static uint32_t used_blocks(
-    NSDirEntry dir[DIR_ENTRIES])
+
+static void heading_summary(FILE *img)
 {
-    uint32_t used = 0;
-
-    for(int i=0;i<DIR_ENTRIES;i++)
-    {
-        if(dir[i].name[0]==' ')
-            continue;
-
-        used += dir[i].length;
-    }
-
-    return used;
+	int img_blk = image_blocks(img);
+	
+	switch (img_blk) {
+	case 350:
+		printf ("SSSD - 35 tracks, 256 bytes/sec\n");
+		BLOCK_SIZE = 256;
+		break;
+	case 700:
+		printf ("SSDD - 35 tracks, 512 bytes/sec\n");
+		BLOCK_SIZE = 512;
+		break;
+	case 1400:
+		printf ("SSDD - 70 tracks, 512 bytes/sec\n");
+		BLOCK_SIZE = 512;
+		break;
+	default:
+		printf ("Unknown disk type !\n");
+		BLOCK_SIZE = 512;
+		break;
+	}
+	
+	printf ("%uK disk image\n", img_blk*BLOCK_SIZE/1024);
 }
 
 static int read_directory(FILE *fp, NSDirEntry dir[DIR_ENTRIES])
@@ -116,16 +128,6 @@ static void entry_name(char out[9], const NSDirEntry *e)
     }
 }
 
-static void make_dos_name(char out[8], const char *name)
-{
-    memset(out,' ',8);
-
-    while(*name && *name!='.' && (out-(char *)0) < 0) {}
-
-    for(int i=0;i<8 && name[i];i++)
-        out[i]=name[i];
-}
-
 static int find_entry(
     NSDirEntry dir[DIR_ENTRIES],
     const char *name)
@@ -137,7 +139,7 @@ static int find_entry(
     for(int i=0;i<8 && name[i];i++)
         dosname[i]=name[i];
 
-    for(int i=0;i<DIR_ENTRIES;i++)
+    for(int i=1;i<DIR_ENTRIES;i++)
     {
         if(memcmp(dir[i].name,dosname,8)==0)
             return i;
@@ -146,18 +148,18 @@ static int find_entry(
     return -1;
 }
 
+
 static uint16_t first_free_block(NSDirEntry dir[DIR_ENTRIES])
 {
     uint16_t max = 4;
 
-    for(int i=0;i<DIR_ENTRIES;i++)
+    for(int i=1;i<DIR_ENTRIES;i++)
     {
         if(dir[i].name[0]==' ')
             continue;
 
         uint16_t end =
-            dir[i].start_block +
-            dir[i].length;
+            dir[i].start_block + dir[i].length;
 
         if(end > max)
             max = end;
@@ -176,15 +178,36 @@ static void list_directory(FILE *fp)
         return;
     }
 
-    long used = used_blocks(dir);
+    //long used = used_blocks(dir);
 
-    printf("\n");
-    printf("Used blocks : %ld - %ld bytes\n", used, used * BLOCK_SIZE);
+    uint32_t used = 0;
+    uint32_t used_dd = 0;
+    uint32_t bytes_len = 0;
+
+    for(int i=1;i<DIR_ENTRIES;i++)
+    {
+        if (dir[i].name[0] == ' ')
+            continue;
+
+		// Getting rid of BLOCK_SIZE, we try to keep this stuff dynamic
+		if (dir[i].type & 0x80) {
+			bytes_len += dir[i].length*512;
+			used_dd += dir[i].length;
+		} else {
+			used += dir[i].length;
+			bytes_len += dir[i].length*256;
+		}
+    }
+
+	long total_bytes = image_blocks(fp) * BLOCK_SIZE;
+	long free_bytes = total_bytes - DIR_BLOCKS * BLOCK_SIZE - bytes_len;
+
+    printf("\n('*' = Double Density)\n");
 
     printf(
         "\n"
         "NAME       START  LEN  TYPE GO\n"
-        "----------------------------------\n");
+        "------------------------------\n");
 
     for(int i=0;i<DIR_ENTRIES;i++)
     {
@@ -195,23 +218,66 @@ static void list_directory(FILE *fp)
 
         entry_name(name,&dir[i]);
 
-        if (dir[i].type)
-            printf(
-                "%-8s %5u  %4u   %c%2u  %04X\n",
-                name,
-                dir[i].start_block,
-                dir[i].length,
-                (dir[i].type/128 ? '+' : '-'),
-                (dir[i].type)&0x7f,
-                dir[i].go_address);
+		uint8_t meta0 = dir[i].go_address & 0xff;
+		uint8_t meta1 = dir[i].go_address >> 8;
+		uint8_t meta2 = dir[i].reserved;
+
+        if ((dir[i].type & 0x7f) == 0)
+        {
+			if ((i==0) && (dir[i].start_block==0))
+				printf(
+					"%-8s     0         [DIR]  %02Xh,%02Xh,%02Xh\n---------------------------\n",
+					name,
+					meta0,
+					meta1,
+					meta2);
+			else
+			if (dir[i].start_block==4) {
+				printf(
+					"%-8s     4   %4u  [BOOT] %02Xh,%02Xh,%02Xh\n---------------------------\n",
+					name,
+					dir[i].length,
+					meta0,
+					meta1,
+					meta2);
+			} else {
+				printf(
+					"%-8s %5u  %4u   %c%2u    %02Xh,%02Xh,%02Xh\n",
+					name,
+					dir[i].start_block,
+					dir[i].length,
+					(dir[i].type & 0x80) ? '*' : ' ',
+					dir[i].type & 0x7f,
+					meta0,
+					meta1,
+					meta2);
+			}
+        }
         else
-            printf(
-                "%-8s %5u  %4u   -%2u  ----\n",
-                name,
-                dir[i].start_block,
-                dir[i].length,
-                dir[i].type);
+        {
+			if (dir[i].start_block==4) {
+				printf(
+					"%-8s     4   %4u  [CPM]  %02Xh,%02Xh,%02Xh\n---------------------------\n",
+					name,
+					dir[i].length,
+					meta0,
+					meta1,
+					meta2);
+			} else {
+				printf(
+					"%-8s %5u  %4u   %c%2u  %04Xh\n",
+					name,
+					dir[i].start_block,
+					dir[i].length,
+					(dir[i].type & 0x80) ? '*' : ' ',
+					dir[i].type & 0x7f,
+					dir[i].go_address);
+			}
+        }
     }
+    printf("\n\nUsed blocks : %u SD + %u DD, %u bytes", used, used_dd, bytes_len);
+    printf("\n%u bytes free.\n", free_bytes);
+
 }
 
 static int extract_file(
@@ -265,6 +331,9 @@ static int delete_file(
     return write_directory(img,dir);
 }
 
+
+// The inserted file is always a binary file
+
 static int insert_file(
     FILE *img,
     const char *hostfile,
@@ -291,7 +360,7 @@ static int insert_file(
         return 0;
     }
 
-    for(int i=0;i<DIR_ENTRIES;i++)
+    for(int i=1;i<DIR_ENTRIES;i++)
     {
         if(dir[i].name[0]==' ')
         {
@@ -317,18 +386,16 @@ static int insert_file(
 
     rewind(in);
 
-    blocks=(uint16_t)((filesize+255)/256);
+    blocks=(uint16_t)((filesize+(BLOCK_SIZE-1))/BLOCK_SIZE);
 
     start=first_free_block(dir);
 
     max_blocks=(long)image_blocks(img);
 
-    if((long)start + blocks > max_blocks)
+    if((long)start + blocks >= max_blocks)
     {
         fclose(in);
-
         fprintf(stderr,"Disk full\n");
-
         return 0;
     }
 
@@ -364,7 +431,10 @@ static int insert_file(
 
     dir[slot].start_block=start;
     dir[slot].length=blocks;
-    dir[slot].type=129;
+	if (BLOCK_SIZE == 512)
+		dir[slot].type=129;
+	else
+		dir[slot].type=1;
     dir[slot].go_address=goaddr;
 
     if(!write_directory(img,dir))
@@ -372,6 +442,7 @@ static int insert_file(
 
     return 1;
 }
+
 
 static int move_blocks(
     FILE *img,
@@ -446,9 +517,12 @@ int compress_image(FILE *img)
            image,
            DIR_BLOCKS * BLOCK_SIZE);
 
-    for (int i = 0; i < DIR_ENTRIES; i++)
+    for (int i = 1; i < DIR_ENTRIES; i++)
     {
         if (dir[i].name[0] == ' ')
+            continue;
+
+        if (dir[i].start_block <= 4)
             continue;
 
         uint16_t old_start =
@@ -513,17 +587,13 @@ static void usage(void)
         "North Star DOS utility\n\n"
 
         "Usage:\n"
-
         "  nsedit dir image.nsi\n"
-
         "  nsedit extract image.nsi DOSNAME outfile\n"
-
-        "  nsedit insert image.nsi infile DOSNAME  <GO address>\n"
-
-        "  nsedit compress image.nsi\n"
-
+        "  nsedit insert image.nsi infile DOSNAME GOADDRESS\n"
+//        "  nsedit compress image.nsi\n"
         "  nsedit delete image.nsi DOSNAME\n");
 }
+
 
 int main(int argc,char *argv[])
 {
@@ -546,13 +616,9 @@ int main(int argc,char *argv[])
             perror(argv[2]);
             return 1;
         }
-
-        printf(
-            "Image blocks : %ld\n\n",
-            image_blocks(img));
-
+		
+		heading_summary(img);
         list_directory(img);
-
         fclose(img);
 
         return 0;
@@ -574,6 +640,7 @@ int main(int argc,char *argv[])
             return 1;
         }
 
+		heading_summary(img);
         read_directory(img,dir);
 
         int idx=find_entry(dir,argv[3]);
@@ -591,7 +658,7 @@ int main(int argc,char *argv[])
                 argv[4]))
         {
             fprintf(stderr,"Extract failed\n");
-        }
+        } else  printf ("Extract OK\n");
 
         fclose(img);
 
@@ -623,7 +690,10 @@ int main(int argc,char *argv[])
                         "Invalid GO address\n");
                 return 1;
             }
-        } else goaddr=0x2D00;
+        } else {
+                fprintf(stderr,
+                        "WARNING - Setting GO address to 0\n");
+		}
 
         img=fopen(argv[2],"r+b");
 
@@ -633,6 +703,7 @@ int main(int argc,char *argv[])
             return 1;
         }
 
+		heading_summary(img);
         if(!insert_file(
                 img,
                 argv[3],
@@ -640,39 +711,40 @@ int main(int argc,char *argv[])
                 goaddr))
         {
             fprintf(stderr,"Insert failed\n");
-        }
+        } else  printf ("Insert OK\n");
 
         fclose(img);
 
         return 0;
     }
 
-if(strcmp(argv[1],"compress")==0)
-{
-    if(argc!=3)
-    {
-        usage();
-        return 1;
-    }
-
-    img=fopen(argv[2],"r+b");
-
-    if(!img)
-    {
-        perror(argv[2]);
-        return 1;
-    }
-
-    if(!compress_image(img))
-    {
-        fprintf(stderr,
-                "Compression failed\n");
-    }
-
-    fclose(img);
-
-    return 0;
-}
+//	if(strcmp(argv[1],"compress")==0)
+//	{
+//		if(argc!=3)
+//		{
+//			usage();
+//			return 1;
+//		}
+//
+//		img=fopen(argv[2],"r+b");
+//
+//		if(!img)
+//		{
+//			perror(argv[2]);
+//			return 1;
+//		}
+//
+//		heading_summary(img);
+//		if(!compress_image(img))
+//		{
+//			fprintf(stderr,
+//					"Compression failed\n");
+//		} else  printf ("Compression OK\n");
+//
+//		fclose(img);
+//
+//		return 0;
+//	}
 
 
     if(strcmp(argv[1],"delete")==0)
@@ -691,6 +763,7 @@ if(strcmp(argv[1],"compress")==0)
             return 1;
         }
 
+		heading_summary(img);
         read_directory(img,dir);
 
         int idx=find_entry(dir,argv[3]);
@@ -705,7 +778,7 @@ if(strcmp(argv[1],"compress")==0)
         if(!delete_file(img,dir,idx))
         {
             fprintf(stderr,"Delete failed\n");
-        }
+        } else  printf ("Delete OK\n");
 
         fclose(img);
 
