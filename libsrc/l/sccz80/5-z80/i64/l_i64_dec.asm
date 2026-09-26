4364ad274f197b92e97d65902155f4aafe630828
suborb <dom@suborbital.org.uk>
2026-09-26 09:39:17 +0100

    Rework #3121 and #3122 - 64 bit operation fixes + opts
    Thanks to both @sahlberg and @ped7h

--- libsrc/l/sccz80/5-z80/i64/l_i64_dec.asm
+++ libsrc/l/sccz80/5-z80/i64/l_i64_dec.asm
@@ -4,13 +4,15 @@
     EXTERN  __i64_acc
 
 ; Entry: acc = LHS
-; Exit:  acc = acc + 1
+; Exit:  acc = acc - 1
 l_i64_dec:
 	ld	hl,__i64_acc
 	ld	b,8
+	xor	a
 loop:
+	cp	(hl)    ; CF = 0 < byte (HL)
 	dec	(hl)
-	ret	nz
-	dec	hl
+	ret	c		; borrow only if the byte wrapped to 0xff
+	inc	hl
 	djnz	loop
-        ret
+	ret
